67458791f48673e384d4b56d4cb532b329d51a11
Vyacheslav Akhmecht <coffeemug@guibas.(none)>
2009-10-03 17:30:50 -0700

    Block size benchmark is now working

--- bench/block_size.py
+++ bench/block_size.py
@@ -1,26 +1,43 @@
 #!/usr/bin/python
 
+import sys
 import subprocess
 from StringIO import StringIO
+from datetime import datetime
 
-# Parameters to the benchmark
-duration = 5
-password = 'legion25'
-devices = ['/dev/sda', '/dev/sdb']
-distros = ['uniform', 'normal', 'pow']
-blocks = [x * 512 for x in range(1, 21)]
+# Load the parameters
+try:
+    from block_size_config import *
+except:
+    print "Please copy block_size_config.py.gen to block_size_config.py and set appropriate variables"
+    sys.exit(-1)
 
-# Run the benchmark
+
+# Open the results file
+now = datetime.now()
+stats_file = open('%s@%s' % (logname, now.strftime("%Y-%m-%d.%H-%M-%S")), 'w')
+
+# Print the headers
+stats_file.write('# block-size\n')
 for device in devices:
     for distro in distros:
-        for block in blocks:
-            p = subprocess.Popen(['sudo', '-S',
-                                  'rebench', '-d', str(duration), '-u', distro, '-b', str(block), device],
+        stats_file.write('# %s (%s)\n' % (device, distro))
+
+# Run the benchmark
+for block in blocks:
+    # Output the block size
+    stats_file.write('%d\t' % block)
+    for device in devices:
+        for distro in distros:
+            # Run the benchmark
+            p = subprocess.Popen(['sudo', '-S', 'rebench', '-n', '-d', str(duration),
+                                  '-b', str(block), '-u', distro, device],
                                  stdin=subprocess.PIPE, stdout=subprocess.PIPE)
             p.stdin.write(password)
             p.stdin.close()
             p.wait()
-            for line in p.stdout:
-                print line
+            # Output the result for the run
+            stats_file.write('%s\t' % p.stdout.readline().split()[0])
+    stats_file.write('\n')
 
-
+stats_file.close()
