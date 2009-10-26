652770a12ec7dbfe5876147fb27629f8ae319c3e
Vyacheslav Akhmecht <coffeemug@guibas.(none)>
2009-10-26 01:00:43 -0700

    Limiting output text

--- src/main.cc
+++ src/main.cc
@@ -57,6 +57,7 @@ void event_handler(event_queue_t *event_queue, event_t *event) {
             printf("File notify (fd %d, res: %d) %s\n",
                    event->source, event->result, strerror(-event->result));
         } else {
+            ((char*)event->buf)[11] = 0;
             printf("File notify (fd %d, res: %d) %s\n",
                    event->source, event->result, (char*)event->buf);
         }
