990ef65fa2557ff0e084ab54669ab6a8e841498d
Slava Akhmechet <coffeemug@newton.Belkin>
2009-11-29 05:10:40 -0800

    Making check function a macro (inlining will do us good)

--- src/utils.cc
+++ src/utils.cc
@@ -1,20 +1,9 @@
 
-#include <errno.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include <unistd.h>
 #include <new>
 #include <exception>
 #include "utils.hpp"
-
-void check(const char *str, int error) {
-    if (error) {
-        if(errno == 0)
-            errno = EINVAL;
-        perror(str);
-        exit(-1);
-    }
-}
 
 int get_cpu_count() {
     return sysconf(_SC_NPROCESSORS_ONLN);
--- src/utils.hpp
+++ src/utils.hpp
@@ -2,10 +2,18 @@
 #ifndef __UTILS_HPP__
 #define __UTILS_HPP__
 
+#include <errno.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-// TODO: make checking inline
-void check(const char *str, int error);
+#define check(msg, err)       \
+    if (err) {                \
+        if(errno == 0)        \
+            errno = EINVAL;   \
+        perror(msg);          \
+        exit(-1);             \
+    }
+
 
 int get_cpu_count();
 void *malloc_aligned(size_t size, size_t alignment = 64);
