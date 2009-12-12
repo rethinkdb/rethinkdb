820a2101db554c847beed33c002e9783e5559fa6
Slava Akhmechet <coffeemug@newton.Belkin>
2009-12-11 16:46:13 -0800

    Adding support for file name and line number in failed tests

--- scripts/retest.hpp
+++ scripts/retest.hpp
@@ -8,16 +8,20 @@
 
 // Generic assertion
 template<typename T>
-void assert(bool cond, const char* op, T x, T y) {
+void assert_impl(bool cond, const char* op, T x, T y, const char *file, unsigned int line) {
     using namespace std;
     if(!cond) {
-        cout << endl << "FAILED: " << x << " " << op << " " << y << endl;
+        const char *real_file = file + 1;
+        cout << endl << "FAILED: " << x << " " << op << " " << y
+             << " (" << real_file << ":" << line << ")"
+             << endl;
         exit(-1);
     }
 }
 
 // User assertion
-void assert(bool cond) {
+#define assert(C)  assert_impl(C, __FILE__, __LINE__)
+void assert_impl(bool cond, const char *file, unsigned int line) {
     using namespace std;
     if(!cond) {
         cout << endl;
@@ -27,63 +31,75 @@ void assert(bool cond) {
 
 
 // Equality
+#define assert_eq(X, Y)  assert_eq_impl(X, Y, __FILE__, __LINE__)
+
 template<typename T>
-void assert_eq(T x, T y) {
-    assert(x == y, "==", x, y);
+void assert_eq_impl(T x, T y, const char *file, unsigned int line) {
+    assert_impl(x == y, "==", x, y, file, line);
 }
 template<>
-void assert_eq(const char* x, const char* y) {
-    assert(strcmp(x, y) == 0, "==", x, y);
+void assert_eq_impl(const char* x, const char* y, const char *file, unsigned int line) {
+    assert_impl(strcmp(x, y) == 0, "==", x, y, file, line);
 }
 
 // Inequality
+#define assert_ne(X, Y)  assert_ne_impl(X, Y, __FILE__, __LINE__)
+
 template<typename T>
-void assert_ne(T x, T y) {
-    assert(x != y, "!=", x, y);
+void assert_ne_impl(T x, T y, const char *file, unsigned int line) {
+    assert_impl(x != y, "!=", x, y, file, line);
 }
 template<>
-void assert_ne(const char* x, const char* y) {
-    assert(strcmp(x, y) != 0, "!=", x, y);
+void assert_ne_impl(const char* x, const char* y, const char *file, unsigned int line) {
+    assert_impl(strcmp(x, y) != 0, "!=", x, y, file, line);
 }
 
 // Less than
+#define assert_lt(X, Y)  assert_lt_impl(X, Y, __FILE__, __LINE__)
+
 template<typename T>
-void assert_lt(T x, T y) {
-    assert(x < y, "<", x, y);
+void assert_lt_impl(T x, T y, const char *file, unsigned int line) {
+    assert_impl(x < y, "<", x, y, file, line);
 }
 template<>
-void assert_lt(const char* x, const char* y) {
-    assert(strcmp(x, y) < 0, "<", x, y);
+void assert_lt_impl(const char* x, const char* y, const char *file, unsigned int line) {
+    assert_impl(strcmp(x, y) < 0, "<", x, y, file, line);
 }
 
 // Less than or equal
+#define assert_lte(X, Y)  assert_lte_impl(X, Y, __FILE__, __LINE__)
+
 template<typename T>
-void assert_lte(T x, T y) {
-    assert(x <= y, "<=", x, y);
+void assert_lte_impl(T x, T y, const char *file, unsigned int line) {
+    assert_impl(x <= y, "<=", x, y, file, line);
 }
 template<>
-void assert_lte(const char* x, const char* y) {
-    assert(strcmp(x, y) <= 0, "<=", x, y);
+void assert_lte_impl(const char* x, const char* y, const char *file, unsigned int line) {
+    assert_impl(strcmp(x, y) <= 0, "<=", x, y, file, line);
 }
 
 // Greater than
+#define assert_gt(X, Y)  assert_gt_impl(X, Y, __FILE__, __LINE__)
+
 template<typename T>
-void assert_gt(T x, T y) {
-    assert(x > y, ">", x, y);
+void assert_gt_impl(T x, T y, const char *file, unsigned int line) {
+    assert_impl(x > y, ">", x, y, file, line);
 }
 template<>
-void assert_gt(const char* x, const char* y) {
-    assert(strcmp(x, y) > 0, ">", x, y);
+void assert_gt_impl(const char* x, const char* y, const char *file, unsigned int line) {
+    assert_impl(strcmp(x, y) > 0, ">", x, y, file, line);
 }
 
 // Greater than or equal
+#define assert_gte(X, Y)  assert_eq_impl(X, Y, __FILE__, __LINE__)
+
 template<typename T>
-void assert_gte(T x, T y) {
-    assert(x >= y, ">=", x, y);
+void assert_gte_impl(T x, T y, const char *file, unsigned int line) {
+    assert_impl(x >= y, ">=", x, y, file, line);
 }
 template<>
-void assert_gte(const char* x, const char* y) {
-    assert(strcmp(x, y) >= 0, ">=", x, y);
+void assert_gte_impl(const char* x, const char* y, const char *file, unsigned int line) {
+    assert_impl(strcmp(x, y) >= 0, ">=", x, y, file, line);
 }
 
 #endif // __RETEST_HPP__
