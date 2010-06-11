5bc87beb7ecf8c92d62fadc3879646e1b76fcc45
Slava Akhmechet <coffeemug@newton.Belkin>
2010-06-11 13:48:53 -0700

    Adding a todo to ensure we always get called from the right event queue

--- src/include/buffer_cache/mirrored.hpp
+++ src/include/buffer_cache/mirrored.hpp
@@ -58,6 +58,11 @@ public:
 
     // Transaction API
     transaction_t* begin_transaction(event_queue_t *event_queue) {
+        // TODO: event queue should be accessed as a globally
+        // available thread local variable. We should store it at the
+        // beginning of the transaction, and then check for every
+        // cache operation that the global thread local variable is
+        // the same as the one stored at the very beginning.
         return event_queue;
     }
     void end_transaction(transaction_t* transaction) {
