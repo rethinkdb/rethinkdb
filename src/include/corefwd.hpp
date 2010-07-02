3923313b923c5d54ce6625a3b51cc21dacbead4e
Joe Doliner <jdoliner@newton.Belkin>
2010-07-02 11:50:40 -0700

    Reenables the delete_fsm.

--- src/include/corefwd.hpp
+++ src/include/corefwd.hpp
@@ -53,8 +53,8 @@ class btree_get_fsm;
 template <typename config_t>
 class btree_set_fsm;
 
-//template <typename config_t>
-//class btree_delete_fsm;
+template <typename config_t>
+class btree_delete_fsm;
 
 template <class config_t>
 class btree_admin;
