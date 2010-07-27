c4b65501c8a2c1da382e018405d901a326f2f7cd
jdoliner <jdoliner@rethinkdb.com>
2010-07-27 12:30:28 -0700

    Updates binary test to test for binary pipelining, (by way of multi_{get, set, delete}). And everything fails. The test does however pass on memcached.

--- test/integration/binary.py
+++ test/integration/binary.py
@@ -5,7 +5,7 @@ from random import shuffle, randint
 from time import sleep
 import os
 
-NUM_INTS = 8000
+NUM_INTS = 80
 NUM_THREADS = 1
 bin = True
 
@@ -15,6 +15,14 @@ def rethinkdb_insert(mc, ints, clone):
         if (0 == mc.set(str(i), str(i))):
             raise ValueError("Insert of %d failed" % i)
         clone[str(i)] = str(i)
+
+def rethinkdb_multi_insert(mc, ints, clone):
+    insert_dict = {}
+    for i in ints:
+        insert_dict[str(i)] = str(i)
+        clone[str(i)] = str(i)
+    if (0 == mc.set_multi(insert_dict)):
+        raise ValueError("Multi insert failed")
 
 def rethinkdb_delete(mc, ints, clone):
     for i in ints:
@@ -26,6 +34,21 @@ def rethinkdb_delete(mc, ints, clone):
             print "Verifying"
             rethinkdb_verify(mc, ints, clone)
 
+def rethinkdb_multi_delete(mc, ints, clone):
+    deletes = []
+    for i in ints:
+        deletes.append(str(i))
+
+    deletes_lists = split_list(deletes, randint(NUM_INTS // 40, NUM_INTS // 5))
+
+    for list in deletes_lists:
+        mc.delete_multi(list)
+        for i in list:
+            del clone[str(i)]
+
+    print "Verifying"
+    rethinkdb_multi_verify(mc, ints, clone)
+
 def rethinkdb_verify(mc, ints, clone):
     for i in ints:
         key = str(i)
@@ -34,6 +57,12 @@ def rethinkdb_verify(mc, ints, clone):
         if clone_value != stored_value:
             raise ValueError("Error, incorrent value in the database! (%s=>%s, should be %s)" % \
                 (key, stored_value, clone_value))
+
+def rethinkdb_multi_verify(mc, ints, clone):
+    get_response = mc.get_multi(map(str, ints))
+    for key, stored_value in zip(ints, get_response):
+        if stored_value != clone.get(key):
+            raise ValueError("Error, incorrent value in the database! (%s=>%s, should be %s)" % (key, stored_value, clone_value))
 
 def split_list(alist, parts):
     length = len(alist)
@@ -45,7 +74,8 @@ def test_against_server_at(port):
     mc = memcache.Client(["localhost:%d" % port], binary = bin)
     clone = {}
 
-    print "Shuffling numbers"
+    print "====Testing singular operations===="
+
     ints = range(0, NUM_INTS)
     shuffle(ints)
 
@@ -57,6 +87,20 @@ def test_against_server_at(port):
 
     print "Deleting numbers"
     rethinkdb_delete(mc, ints, clone)
+
+    print "====Testing multi operations===="
+
+    ints = range(0, NUM_INTS)
+    shuffle(ints)
+
+    print "Inserting numbers (multi)"
+    rethinkdb_multi_insert(mc, ints, clone)
+
+    print "Verifying (multi)"
+    rethinkdb_multi_verify(mc, ints, clone)
+
+    print "Deleting numbers (multi)"
+    rethinkdb_multi_delete(mc, ints, clone)
     
     print "Done"
 
