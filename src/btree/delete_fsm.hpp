f57fd602d6b86abcf3233afc499ad6e8feb301ba
Aditya <aditya@rethinkdb.com>
2010-08-06 15:26:37 -0700

    making delete_fsm a subclass of modify_fsm.

--- src/btree/delete_fsm.hpp
+++ src/btree/delete_fsm.hpp
@@ -7,75 +7,85 @@
 #include "btree/fsm.hpp"
 
 template <class config_t>
-class btree_delete_fsm : public btree_fsm<config_t>,
+class btree_delete_fsm : public btree_modify_fsm<config_t>,
                          public alloc_mixin_t<tls_small_obj_alloc_accessor<alloc_t>, btree_delete_fsm<config_t> >
 {
 public:
-    typedef typename config_t::btree_fsm_t btree_fsm_t;
-    typedef typename config_t::node_t node_t;
-    typedef typename config_t::cache_t cache_t;
-    typedef typename btree_fsm_t::transition_result_t transition_result_t;
-    typedef typename cache_t::buf_t buf_t;
-    
-    using btree_fsm_t::key;
-
-public:
-    enum state_t {
-        start_transaction,
-        acquire_superblock,
-        acquire_root,
-        acquire_node,
-        delete_complete,
-        acquire_sibling,
-        insert_root_on_collapse,
-        insert_root_on_split,
-        committing,
-    };
-
     enum op_result_t {
         btree_incomplete,
         btree_found,
         btree_not_found
     };
 
+    typedef typename btree_modify_fsm<config_t>::cache_t cache_t;
+    typedef typename btree_modify_fsm<config_t>::transition_result_t transition_result_t;
+    typedef typename btree_modify_fsm<config_t>::buf_t buf_t;
+    typedef typename btree_modify_fsm<config_t>::btree_fsm_t btree_fsm_t;
+    typedef typename btree_modify_fsm<config_t>::node_t node_t;
+
+    using btree_modify_fsm<config_t>::state;
+    using btree_modify_fsm<config_t>::sb_buf;
+    using btree_modify_fsm<config_t>::buf;
+    using btree_modify_fsm<config_t>::last_buf;
+    using btree_modify_fsm<config_t>::node_id;
+    using btree_modify_fsm<config_t>::last_node_id;
+    using btree_modify_fsm<config_t>::have_computed_new_value;
+    using btree_modify_fsm<config_t>::new_value;
+    using btree_modify_fsm<config_t>::set_was_successful;
+    using btree_modify_fsm<config_t>::sib_buf;
+    using btree_modify_fsm<config_t>::sib_node_id;
+
+    // the enum
+    using btree_modify_fsm<config_t>::insert_root;
+    using btree_modify_fsm<config_t>::start_transaction;
+    using btree_modify_fsm<config_t>::acquire_superblock;
+    using btree_modify_fsm<config_t>::acquire_root;
+    using btree_modify_fsm<config_t>::acquire_node;
+    using btree_modify_fsm<config_t>::insert_root;
+    using btree_modify_fsm<config_t>::delete_complete;
+    using btree_modify_fsm<config_t>::acquire_sibling;
+    using btree_modify_fsm<config_t>::insert_root_on_collapse;
+    using btree_modify_fsm<config_t>::insert_root_on_split;
+    using btree_modify_fsm<config_t>::update_complete;
+    using btree_modify_fsm<config_t>::committing;
+
+    // from btree_fsm
+    using btree_modify_fsm<config_t>::transaction;
+    using btree_modify_fsm<config_t>::cache;
+    using btree_modify_fsm<config_t>::key;
+
+    // methods
+    using btree_modify_fsm<config_t>::do_start_transaction;
+    using btree_modify_fsm<config_t>::do_acquire_superblock;
+    using btree_modify_fsm<config_t>::do_insert_root;
+    using btree_modify_fsm<config_t>::do_acquire_root;
+    using btree_modify_fsm<config_t>::do_acquire_node;
+    using btree_modify_fsm<config_t>::is_finished;
+    using btree_modify_fsm<config_t>::do_insert_root_on_collapse;
+    using btree_modify_fsm<config_t>::do_acquire_sibling;
 public:
     explicit btree_delete_fsm(btree_key *_key)
-        : btree_fsm_t(_key),
-          op_result(btree_incomplete), state(start_transaction),
-          buf(NULL), last_buf(NULL), sb_buf(NULL),  sib_buf(NULL), node_id(cache_t::null_block_id)
+        : btree_modify_fsm<config_t>(_key),
+          op_result(btree_incomplete)
         {}
-
     virtual transition_result_t do_transition(event_t *event);
-
-    virtual bool is_finished() {
-        return state == committing && transaction == NULL;
+    bool operate(btree_value *old_value, btree_value **new_value) {
+        // If the key didn't exist before, we fail
+        if (!old_value) return false;
+        return true;
     }
-
+    
 public:
     op_result_t op_result;
 
-private:
-    using btree_fsm<config_t>::cache;
-    using btree_fsm<config_t>::transaction;
-
+public:
+    // methods that delete_fsm has but modify_fsm doesn't.
+    // These should be empty virtual functions in modify_fsm.
     void split_internal_node(buf_t *buf, buf_t **rbuf, block_id_t *rnode_id, btree_key *median);
     transition_result_t do_insert_root_on_split(event_t *event);
-
-    transition_result_t do_start_transaction(event_t *event);
-    transition_result_t do_acquire_superblock(event_t *event);
-    transition_result_t do_acquire_root(event_t *event);
-    transition_result_t do_acquire_node(event_t *event);
-    transition_result_t do_acquire_sibling(event_t *event);
-    transition_result_t do_insert_root_on_collapse(event_t *event);
-
-    int set_root_id(block_id_t root_id, event_t *event);
-
-private:
-    // Some relevant state information
-    state_t state;
     
-    buf_t *buf, *last_buf, *sb_buf, *sib_buf;
-    block_id_t node_id, last_node_id, sib_node_id;
+    // methods in modify_fsm that delete_fsm overrides
+    transition_result_t do_insert_root(event_t *event);
 
 };
 
