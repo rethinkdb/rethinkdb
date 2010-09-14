615154b602f4b71ff2e09729853f094d000ae123
Slava Akhmechet <slava@rethinkdb.com>
2010-09-13 19:38:45 -0700

    Forgotten file

---
+++ lib/retester/gitroot.py
@@ -0,0 +1,21 @@
+
+import subprocess, os
+
+def run_command(cmd, expected_rcs = [0]):
+    """Run the command and return the result as a string. Substitute for subprocess.check_output()
+    for those of us on Python 2.6."""
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    output = process.communicate()[0]
+    if process.returncode not in expected_rcs:
+        raise IOError("%s exited with return code %d and message %r" % \
+            (repr(" ".join(cmd)), process.returncode, output))
+    return output
+
+def get_repo_root():
+    """Returns the root of the git repository we're currently in."""
+    return run_command(["git", "rev-parse", "--show-cdup"]).strip()
+
+def set_repo_cwd(_dir):
+    """Sets the current directory relative to the repo"""
+    os.chdir(os.path.join(os.path.abspath(get_repo_root()), _dir))
+
