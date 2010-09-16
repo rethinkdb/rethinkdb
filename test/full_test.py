e063c878312569a26636a56627046141bb8b272a
Slava Akhmechet <slava@rethinkdb.com>
2010-09-16 11:30:43 -0700

    Changing the tests to have longer duration in questionable cases

--- test/full_test.py
+++ test/full_test.py
@@ -16,103 +16,122 @@ for mode in ["debug", "release"]:
 # Go through all our environments
 for mode in ["debug", "release"]:
     for checker in ["valgrind", None]:
-        # Run tests
         for protocol in ["text"]: # ["text", "binary"]:
-            do_test("integration/append_prepend.py",
-                    { "auto"        : True,
-                      "mode"        : mode,
-                      "no-valgrind" : not checker,
-                      "protocol"    : protocol },
-                    repeat=3)
+            for (cores, slices) in [(1, 1), (2, 2)]:
+                # Run tests
+                do_test("integration/append_prepend.py",
+                        { "auto"        : True,
+                          "mode"        : mode,
+                          "no-valgrind" : not checker,
+                          "protocol"    : protocol,
+                          "cores"       : cores,
+                          "slices"      : slices },
+                        repeat=3)
             
-            do_test("integration/cas.py",
-                    { "auto"        : True,
-                      "mode"        : mode,
-                      "no-valgrind" : not checker,
-                      "protocol"    : protocol },
-                    repeat=3)
+                do_test("integration/cas.py",
+                        { "auto"        : True,
+                          "mode"        : mode,
+                          "no-valgrind" : not checker,
+                          "protocol"    : protocol,
+                          "cores"       : cores,
+                          "slices"      : slices },
+                        repeat=3)
             
-            do_test("integration/flags.py",
-                    { "auto"        : True,
-                      "mode"        : mode,
-                      "no-valgrind" : not checker,
-                      "protocol"    : protocol },
-                    repeat=3)
+                do_test("integration/flags.py",
+                        { "auto"        : True,
+                          "mode"        : mode,
+                          "no-valgrind" : not checker,
+                          "protocol"    : protocol,
+                          "cores"       : cores,
+                          "slices"      : slices },
+                        repeat=3)
             
-            do_test("integration/incr_decr.py",
-                    { "auto"        : True,
-                      "mode"        : mode,
-                      "no-valgrind" : not checker,
-                      "protocol"    : protocol },
-                    repeat=3)
+                do_test("integration/incr_decr.py",
+                        { "auto"        : True,
+                          "mode"        : mode,
+                          "no-valgrind" : not checker,
+                          "protocol"    : protocol,
+                          "cores"       : cores,
+                          "slices"      : slices },
+                        repeat=3)
             
-            do_test("integration/serial_mix.py",
-                    { "auto"        : True,
-                      "mode"        : mode,
-                      "no-valgrind" : not checker,
-                      "protocol"    : protocol },
-                    repeat=20)
+                do_test("integration/serial_mix.py",
+                        { "auto"        : True,
+                          "mode"        : mode,
+                          "no-valgrind" : not checker,
+                          "protocol"    : protocol,
+                          "cores"       : cores,
+                          "slices"      : slices,
+                          "duration"    : 240 },
+                        repeat=5, timeout=250)
             
-            do_test("integration/multi_serial_mix.py",
-                    { "auto"        : True,
-                      "mode"        : mode,
-                      "no-valgrind" : not checker,
-                      "protocol"    : protocol,
-                      "cores"       : 1,
-                      "slices"      : 1,
-                      "num-testers" : 16 },
-                    repeat=3)
+                do_test("integration/serial_mix.py",
+                        { "auto"        : True,
+                          "mode"        : mode,
+                          "no-valgrind" : not checker,
+                          "protocol"    : protocol,
+                          "cores"       : cores,
+                          "slices"      : slices,
+                          "duration"    : 240
+                          "restart-server-prob" : "0.0005"},
+                        repeat=5, timeout=250)
             
-            do_test("integration/expiration.py",
-                    { "auto"        : True,
-                      "mode"        : mode,
-                      "no-valgrind" : not checker,
-                      "protocol"    : protocol },
-                    repeat=5)
+                do_test("integration/multi_serial_mix.py",
+                        { "auto"        : True,
+                          "mode"        : mode,
+                          "no-valgrind" : not checker,
+                          "protocol"    : protocol,
+                          "cores"       : cores,
+                          "slices"      : slices,
+                          "num-testers" : 16,
+                          "duration"    : 240},
+                        repeat=5, timeout=250)
             
-            do_test("integration/multi_serial_mix.py",
-                    { "auto"        : True,
-                      "mode"        : mode,
-                      "no-valgrind" : not checker,
-                      "protocol"    : protocol },
-                    repeat=20)
+                do_test("integration/expiration.py",
+                        { "auto"        : True,
+                          "mode"        : mode,
+                          "no-valgrind" : not checker,
+                          "protocol"    : protocol,
+                          "cores"       : cores,
+                          "slices"      : slices },
+                        repeat=5)
             
-            do_test("integration/pipeline.py",
-                    { "auto"        : True,
-                      "mode"        : mode,
-                      "no-valgrind" : not checker,
-                      "protocol"    : protocol },
-                    repeat=3)
+                do_test("integration/pipeline.py",
+                        { "auto"        : True,
+                          "mode"        : mode,
+                          "no-valgrind" : not checker,
+                          "protocol"    : protocol,
+                          "cores"       : cores,
+                          "slices"      : slices },
+                        repeat=3)
             
-            do_test("integration/bin_pipeline.py",
-                    { "auto"        : True,
-                      "mode"        : mode,
-                      "no-valgrind" : not checker,
-                      "protocol"    : protocol },
-                    repeat=3)
+                do_test("integration/bin_pipeline.py",
+                        { "auto"        : True,
+                          "mode"        : mode,
+                          "no-valgrind" : not checker,
+                          "protocol"    : protocol,
+                          "cores"       : cores,
+                          "slices"      : slices },
+                        repeat=3)
             
-            do_test("integration/many_keys.py",
-                    { "auto"        : True,
-                      "mode"        : mode,
-                      "no-valgrind" : not checker,
-                      "protocol"    : protocol,
-                      "num-keys"    : 50000},
-                    repeat=3, timeout=300)
+                do_test("integration/many_keys.py",
+                        { "auto"        : True,
+                          "mode"        : mode,
+                          "no-valgrind" : not checker,
+                          "protocol"    : protocol,
+                          "cores"       : cores,
+                          "slices"      : slices,
+                          "num-keys"    : 50000},
+                        repeat=3, timeout=300)
             
-            do_test("integration/serial_mix.py",
-                    { "auto"        : True,
-                      "mode"        : mode,
-                      "no-valgrind" : not checker,
-                      "protocol"    : protocol,
-                      "restart-server-prob" : "0.0005"},
-                    repeat=20)
-            
-            do_test("integration/corruption.py",
-                    { "auto"        : True,
-                      "mode"        : mode,
-                      "no-valgrind" : not checker,
-                      "protocol"    : protocol },
-                    repeat=12)
+                do_test("integration/corruption.py",
+                        { "auto"        : True,
+                          "mode"        : mode,
+                          "no-valgrind" : not checker,
+                          "protocol"    : protocol,
+                          "cores"       : cores,
+                          "slices"      : slices },
+                        repeat=12)
             
 # Report the results
 report()
--- test/smoke_test.py
+++ test/smoke_test.py
@@ -2,7 +2,7 @@
 from retester import do_test, report
 
 # Clean the repo
-do_test("pwd; cd ../src/; make clean")
+do_test("cd ../src/; make clean")
 
 # Build everything
 for mode in ["debug", "release"]:
@@ -16,63 +16,86 @@ for mode in ["debug", "release"]:
 # Do quick smoke tests in some environments
 for (mode, checker, protocol) in [("debug", "valgrind", "text"),
                                   ("release", None, "text")]:
-    do_test("integration/append_prepend.py",
-            { "auto"        : True,
-              "mode"        : mode,
-              "no-valgrind" : not checker,
-              "protocol"    : protocol })
+    for (cores, slices) in [(1, 1), (2, 2)]:
+        do_test("integration/append_prepend.py",
+                { "auto"        : True,
+                  "mode"        : mode,
+                  "no-valgrind" : not checker,
+                  "protocol"    : protocol,
+                  "cores"       : cores,
+                  "slices"      : slices })
     
-    do_test("integration/cas.py",
-            { "auto"        : True,
-              "mode"        : mode,
-              "no-valgrind" : not checker,
-              "protocol"    : protocol })
+        do_test("integration/cas.py",
+                { "auto"        : True,
+                  "mode"        : mode,
+                  "no-valgrind" : not checker,
+                  "protocol"    : protocol,
+                  "cores"       : cores,
+                  "slices"      : slices })
     
-    do_test("integration/flags.py",
-            { "auto"        : True,
-              "mode"        : mode,
-              "no-valgrind" : not checker,
-              "protocol"    : protocol })
+        do_test("integration/flags.py",
+                { "auto"        : True,
+                  "mode"        : mode,
+                  "no-valgrind" : not checker,
+                  "protocol"    : protocol,
+                  "cores"       : cores,
+                  "slices"      : slices })
     
-    do_test("integration/incr_decr.py",
-            { "auto"        : True,
-              "mode"        : mode,
-              "no-valgrind" : not checker,
-              "protocol"    : protocol })
+        do_test("integration/incr_decr.py",
+                { "auto"        : True,
+                  "mode"        : mode,
+                  "no-valgrind" : not checker,
+                  "protocol"    : protocol,
+                  "cores"       : cores,
+                  "slices"      : slices })
     
-    do_test("integration/expiration.py",
-            { "auto"        : True,
-              "mode"        : mode,
-              "no-valgrind" : not checker,
-              "protocol"    : protocol })
+        do_test("integration/expiration.py",
+                { "auto"        : True,
+                  "mode"        : mode,
+                  "no-valgrind" : not checker,
+                  "protocol"    : protocol,
+                  "cores"       : cores,
+                  "slices"      : slices })
     
-    do_test("integration/many_keys.py",
-            { "auto"        : True,
-              "mode"        : mode,
-              "no-valgrind" : not checker,
-              "protocol"    : protocol })
+        do_test("integration/many_keys.py",
+                { "auto"        : True,
+                  "mode"        : mode,
+                  "no-valgrind" : not checker,
+                  "protocol"    : protocol,
+                  "cores"       : cores,
+                  "slices"      : slices })
 
-    do_test("integration/serial_mix.py",
-            { "auto"        : True,
-              "mode"        : mode,
-              "no-valgrind" : not checker,
-              "protocol"    : protocol },
-            repeat=10)
+        do_test("integration/serial_mix.py",
+                { "auto"        : True,
+                  "mode"        : mode,
+                  "no-valgrind" : not checker,
+                  "protocol"    : protocol,
+                  "cores"       : cores,
+                  "slices"      : slices,
+                  "duration"    : 30 },
+                repeat=5,
+                timeout=35)
     
-    do_test("integration/serial_mix.py",
-            { "auto"        : True,
-              "mode"        : mode,
-              "no-valgrind" : not checker,
-              "protocol"    : protocol,
-              "restart-server-prob" : "0.0005"},
-            repeat=10)
+        do_test("integration/serial_mix.py",
+                { "auto"        : True,
+                  "mode"        : mode,
+                  "no-valgrind" : not checker,
+                  "protocol"    : protocol,
+                  "cores"       : cores,
+                  "slices"      : slices,
+                  "duration"    : 30
+                  "restart-server-prob" : "0.0005" },
+                repeat=5,
+                timeout=35)
     
-    do_test("integration/multi_serial_mix.py",
-            { "auto"        : True,
-              "mode"        : mode,
-              "no-valgrind" : not checker,
-              "protocol"    : protocol },
-            repeat=10)
+        do_test("integration/multi_serial_mix.py",
+                { "auto"        : True,
+                  "mode"        : mode,
+                  "no-valgrind" : not checker,
+                  "protocol"    : protocol,
+                  "cores"       : cores,
+                  "slices"      : slices },
+                repeat=5)
     
 # Report the results
 report()
