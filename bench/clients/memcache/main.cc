da587be91ab84623ff5f19ea5a7cfb5ad0364196
Slava Akhmechet <slava@rethinkdb.com>
2010-10-10 22:09:23 -0700

    Phew, final support for QPS and latency files

--- bench/clients/memcache/main.cc
+++ bench/clients/memcache/main.cc
@@ -1,18 +1,51 @@
 
+#include <map>
 #include <unistd.h>
 #include <getopt.h>
 #include <stdio.h>
 #include <vector>
 #include <time.h>
+#include <stdio.h>
 #include <libmemcached/memcached.h>
 
 using namespace std;
 
 #define MAX_HOST    255
+#define MAX_FILE    255
 
 /* Returns random number between [min, max] */
 size_t random(size_t min, size_t max) {
     return rand() % (max - min + 1) + min;
+}
+
+/* Timing related functions */
+typedef unsigned long long ticks_t;
+ticks_t secs_to_ticks(float secs) {
+    return (unsigned long long)secs * 1000000000L;
+}
+
+ticks_t get_ticks() {
+    timespec tv;
+    clock_gettime(CLOCK_MONOTONIC, &tv);
+    return secs_to_ticks(tv.tv_sec) + tv.tv_nsec;
+}
+
+long get_ticks_res() {
+    timespec tv;
+    clock_getres(CLOCK_MONOTONIC, &tv);
+    return secs_to_ticks(tv.tv_sec) + tv.tv_nsec;
+}
+
+float ticks_to_secs(ticks_t ticks) {
+    return ticks / 1000000000.0f;
+}
+
+float ticks_to_ms(ticks_t ticks) {
+    return ticks / 1000000.0f;
+}
+
+float ticks_to_us(ticks_t ticks) {
+    return ticks / 1000.0f;
 }
 
 /* Helpful typedefs */
@@ -170,6 +203,8 @@ public:
           duration(10000000L)
         {
             strcpy(host, "localhost");
+            latency_file[0] = 0;
+            qps_file[0] = 0;
         }
 
     void print() {
@@ -180,7 +215,17 @@ public:
         keys.print();
         printf(", values: ");
         values.print();
-        printf(" , duration: %ld]\n", duration);
+        printf(" , duration: %ld", duration);
+
+        if(latency_file[0] != 0) {
+            printf(", latency file: %s", latency_file);
+        }
+        
+        if(qps_file[0] != 0) {
+            printf(", QPS file: %s", qps_file);
+        }
+        
+        printf("]\n", duration);
     }
     
 public:
@@ -191,6 +236,123 @@ public:
     distr_t keys;
     distr_t values;
     long duration;
+    char latency_file[MAX_FILE];
+    char qps_file[MAX_FILE];
+};
+
+/* Information shared between clients */
+struct shared_t {
+public:
+    shared_t(config_t *_config)
+        : config(_config),
+          qps_offset(0), latencies_offset(0),
+          qps_fd(NULL), latencies_fd(NULL)
+        {
+            pthread_mutex_init(&mutex, NULL);
+            
+            if(config->qps_file[0] != 0) {
+                qps_fd = fopen(config->qps_file, "wa");
+            }
+            if(config->latency_file[0] != 0) {
+                latencies_fd = fopen(config->latency_file, "wa");
+            }
+        }
+
+    ~shared_t() {
+        if(qps_fd) {
+            fwrite(qps, 1, qps_offset, qps_fd);
+            fclose(qps_fd);
+        }
+        if(latencies_fd) {
+            fwrite(latencies, 1, latencies_offset, latencies_fd);
+            fclose(latencies_fd);
+        }
+
+        pthread_mutex_destroy(&mutex);
+    }
+    
+    void push_qps(int _qps, int tick) {
+        if(!qps_fd)
+            return;
+        
+        lock();
+
+        // Collect qps info from all clients before attempting to print
+        int qps_count = 0, agg_qps = 0;
+        map<int, pair<int, int> >::iterator op = qps_map.find(tick);
+        if(op != qps_map.end()) {
+            qps_count = op->second.first;
+            agg_qps = op->second.second;
+        }
+
+        qps_count += 1;
+        agg_qps += _qps;
+        
+        if(qps_count == config->clients) {
+            _qps = agg_qps;
+            qps_map.erase(op);
+        } else {
+            qps_map[tick] = pair<int, int>(qps_count, agg_qps);
+            unlock();
+            return;
+        }
+            
+        int _off = snprintf(qps + qps_offset, sizeof(qps) - qps_offset, "%d\n", _qps);
+        if(_off >= sizeof(qps) - qps_offset) {
+            // Couldn't write everything, flush
+            fwrite(qps, 1, qps_offset, qps_fd);
+            
+            // Write again
+            qps_offset = 0;
+            _off = snprintf(qps + qps_offset, sizeof(qps) - qps_offset, "%d\n", _qps);
+        }
+        qps_offset += _off;
+        
+        unlock();
+    }
+
+    void push_latency(float latency) {
+        if(!latencies_fd)
+            return;
+
+        lock();
+        
+        int _off = snprintf(latencies + latencies_offset, sizeof(latencies) - latencies_offset, "%.2f\n", latency);
+        if(_off >= sizeof(latencies) - latencies_offset) {
+            // Couldn't write everything, flush
+            fwrite(latencies, 1, latencies_offset, latencies_fd);
+            
+            // Write again
+            latencies_offset = 0;
+            _off = snprintf(latencies + latencies_offset, sizeof(latencies) - latencies_offset, "%.2f\n", latency);
+        }
+        latencies_offset += _off;
+
+        unlock();
+    }
+
+private:
+    config_t *config;
+    map<int, pair<int, int> > qps_map;
+    char qps[40960], latencies[40960];
+    int qps_offset, latencies_offset;
+    FILE *qps_fd, *latencies_fd;
+    pthread_mutex_t mutex;
+
+private:
+    void lock() {
+        pthread_mutex_lock(&mutex);
+    }
+    void unlock() {
+        pthread_mutex_unlock(&mutex);
+    }
+
+};
+
+/* Communication structure for main thread and clients */
+struct client_data_t {
+    config_t *config;
+    shared_t *shared;
 };
 
 /* Usage */
@@ -206,7 +368,7 @@ void usage(const char *name) {
     printf("\t-n, --host\n\t\tServer host to connect to. Defaults to [%s].\n", _d.host);
     printf("\t-p, --port\n\t\tServer port to connect to. Defaults to [%d].\n", _d.port);
     printf("\t-c, --clients\n\t\tNumber of concurrent clients. Defaults to [%d].\n", _d.clients);
-    printf("\t-l, --load\n\t\tTarget load to generate. Expects a value in format D/U/I/R, where\n" \
+    printf("\t-w, --workload\n\t\tTarget load to generate. Expects a value in format D/U/I/R, where\n" \
            "\t\t\tD - number of deletes\n" \
            "\t\t\tU - number of updates\n" \
            "\t\t\tI - number of inserts\n" \
@@ -222,6 +384,10 @@ void usage(const char *name) {
     printf("].\n");
     printf("\t-d, --duration\n\t\tDuration of the run specified in number of operations.\n" \
            "\t\tDefaults to [%ld].\n", _d.duration);
+    printf("\t-l, --latency-file\n\t\tFile name to output individual latency information (in us).\n" \
+           "\t\tThe information is not outputted if this argument is skipped.\n");
+    printf("\t-q, --qps-file\n\t\tFile name to output QPS information.\n" \
+           "\t\tThe information is not outputted if this argument is skipped.\n");
 
     printf("\nAdditional information:\n");
     printf("\t\tDISTR format describes a range and can be specified in as MIN-MAX\n");
@@ -237,19 +403,21 @@ void parse(config_t *config, int argc, char *argv[]) {
         int do_help = 0;
         struct option long_options[] =
             {
-                {"host",       required_argument, 0, 'n'},
-                {"port",       required_argument, 0, 'p'},
-                {"clients",    required_argument, 0, 'c'},
-                {"load",       required_argument, 0, 'l'},
-                {"keys",       required_argument, 0, 'k'},
-                {"values",     required_argument, 0, 'v'},
-                {"duration",   required_argument, 0, 'd'},
-                {"help",       no_argument, &do_help, 1},
+                {"host",           required_argument, 0, 'n'},
+                {"port",           required_argument, 0, 'p'},
+                {"clients",        required_argument, 0, 'c'},
+                {"workload",       required_argument, 0, 'w'},
+                {"keys",           required_argument, 0, 'k'},
+                {"values",         required_argument, 0, 'v'},
+                {"duration",       required_argument, 0, 'd'},
+                {"latency-file",   required_argument, 0, 'l'},
+                {"qps-file",       required_argument, 0, 'q'},
+                {"help",           no_argument, &do_help, 1},
                 {0, 0, 0, 0}
             };
 
         int option_index = 0;
-        int c = getopt_long(argc, argv, "n:p:c:l:k:v:d:h", long_options, &option_index);
+        int c = getopt_long(argc, argv, "n:p:c:w:k:v:d:l:q:h", long_options, &option_index);
 
         if(do_help)
             c = 'h';
@@ -271,7 +439,7 @@ void parse(config_t *config, int argc, char *argv[]) {
         case 'c':
             config->clients = atoi(optarg);
             break;
-        case 'l':
+        case 'w':
             config->load.parse(optarg);
             break;
         case 'k':
@@ -282,6 +450,12 @@ void parse(config_t *config, int argc, char *argv[]) {
             break;
         case 'd':
             config->duration = atol(optarg);
+            break;
+        case 'l':
+            strncpy(config->latency_file, optarg, MAX_FILE);
+            break;
+        case 'q':
+            strncpy(config->qps_file, optarg, MAX_FILE);
             break;
         case 'h':
             usage(argv[0]);
@@ -297,7 +471,9 @@ void parse(config_t *config, int argc, char *argv[]) {
 /* The function that does the work */
 void* run_client(void* data) {
     // Grab the config
-    config_t *config = (config_t*)data;
+    client_data_t *client_data = (client_data_t*)data;
+    config_t *config = client_data->config;
+    shared_t *shared = client_data->shared;
     
     // Connect to the server
     memcached_st memcached;
@@ -308,6 +484,8 @@ void* run_client(void* data) {
     vector<payload_t> keys;
 
     // Perform the ops
+    ticks_t last_time = get_ticks(), last_qps_time = last_time, now_time;
+    int qps = 0, tick = 0;
     for(int i = 0; i < config->duration / config->clients; i++) {
         // Generate the command
         load_t::load_op_t cmd = config->load.toss();
@@ -386,6 +564,22 @@ void* run_client(void* data) {
             free(_value);
             break;
         };
+        now_time = get_ticks();
+        qps++;
+
+        // Deal with individual op latency
+        ticks_t latency = now_time - last_time;
+        shared->push_latency(ticks_to_us(latency));
+        last_time = now_time;
+
+        // Deal with QPS
+        if(ticks_to_secs(now_time - last_qps_time) >= 1.0f) {
+            shared->push_qps(qps, tick);
+
+            last_qps_time = now_time;
+            qps = 0;
+            tick++;
+        }
     }
     
     // Disconnect
@@ -408,13 +602,19 @@ int main(int argc, char *argv[])
     parse(&config, argc, argv);
     config.print();
 
+    // Create the shared structure
+    shared_t shared(&config);
+    client_data_t client_data;
+    client_data.config = &config;
+    client_data.shared = &shared;
+
     // Let's rock 'n roll
     int res;
     vector<pthread_t> threads(config.clients);
     
     // Create the threads
     for(int i = 0; i < config.clients; i++) {
-        int res = pthread_create(&threads[i], NULL, run_client, &config);
+        int res = pthread_create(&threads[i], NULL, run_client, &client_data);
         if(res != 0) {
             fprintf(stderr, "Can't create thread");
             exit(-1);
