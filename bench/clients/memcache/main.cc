3305775cab265a3ef30aedf540bd5d439430a64b
Slava Akhmechet <slava@rethinkdb.com>
2010-10-10 17:49:06 -0700

    Implementing core functionality in the client

--- bench/clients/memcache/main.cc
+++ bench/clients/memcache/main.cc
@@ -1,11 +1,23 @@
 
 #include <stdio.h>
 #include <vector>
-#include <sstream>
+#include <time.h>
 #include <libmemcached/memcached.h>
+
+using namespace std;
 
 #define MAX_HOST    255
 
+/* Returns random number between [min, max] */
+size_t random(size_t min, size_t max) {
+    return rand() % (max - min + 1) + min;
+}
+
+/* Helpful typedefs */
+typedef pair<char*, size_t> payload_t;
+
+/* Defines a load in terms of ratio of deletes, updates, inserts, and
+ * reads. */
 struct load_t {
 public:
     load_t()
@@ -18,6 +30,30 @@ public:
           inserts(i), reads(r)
         {}
 
+    enum load_op_t {
+        delete_op, update_op, insert_op, read_op
+    };
+    
+    // Generates an operation to perform using the ratios as
+    // probabilities.
+    load_op_t toss() {
+        int total = deletes + updates + inserts + reads;
+        int rand_op = random(0, total - 1);
+        int acc = 0;
+
+        if(rand_op >= acc && rand_op < (acc += deletes))
+            return delete_op;
+        if(rand_op >= acc && rand_op < (acc += updates))
+            return update_op;
+        if(rand_op >= acc && rand_op < (acc += inserts))
+            return insert_op;
+        if(rand_op >= acc && rand_op < (acc += reads))
+            return read_op;
+        
+        fprintf(stderr, "Something horrible has happened with the toss\n");
+        exit(-1);
+    }
+
     void print() {
         printf("%d/%d/%d/%d", deletes, updates, inserts, reads);
     }
@@ -29,6 +65,7 @@ public:
     int reads;
 };
 
+/* Defines a distribution of values, from min to max. */
 struct distr_t {
 public:
     distr_t(int _min, int _max)
@@ -38,6 +75,23 @@ public:
     distr_t()
         : min(8), max(16)
         {}
+
+    // Generates a random payload within given bounds. It is the
+    // user's responsibility to clean up the payload.
+    void toss(payload_t *payload) {
+        // generate the size
+        int s = random(min, max);
+        
+        // malloc and fill memory
+        char *l = (char*)malloc(s);
+        for(int i = 0; i < s; i++) {
+            l[i] = random(65, 90);
+        }
+        
+        // fill the payload
+        payload->first = l;
+        payload->second = s;
+    }
     
     void print() {
         printf("%d-%d", min, max);
@@ -47,11 +101,13 @@ public:
     int min, max; 
 };
 
+/* Defines a client configuration, including sensible default
+ * values.*/
 struct config_t {
 public:
     config_t()
         : port(11211),
-          clients(512), load(load_t()),
+          clients(64), load(load_t()),
           keys(distr_t(8, 16)), values(distr_t(8, 128)),
           duration(10000000)
         {
@@ -79,6 +135,7 @@ public:
     long duration;
 };
 
+/* Usage */
 void usage(const char *name) {
     // Create a default config
     config_t _d;
@@ -111,41 +168,124 @@ void usage(const char *name) {
     printf("\nAdditional information:\n");
     printf("\t\tDISTR format describes a range and can be specified in as MIN-MAX\n");
     
-    exit(0);
+    exit(-1);
 }
 
 void* run_client(void* data) {
+    // Grab the config
+    config_t *config = (config_t*)data;
+    
     // Connect to the server
     memcached_st memcached;
     memcached_create(&memcached);
-    memcached_server_add(&memcached, "localhost", 11213);    
+    memcached_server_add(&memcached, config->host, config->port);
+
+    // Store the keys so we can run updates and deletes.
+    vector<payload_t> keys;
 
     // Perform the ops
-    for(int i = 0; i < 100; i++) {
-        // Convert i to string
-        std::string s;
-        std::stringstream out;
-        out << i;
-        s = out.str();
-        const char *v = s.c_str();
-        int len = strlen(v);
-        memcached_set(&memcached, v, len, v, len, 0, 0);
+    for(int i = 0; i < config->duration; i++) {
+        // Generate the command
+        load_t::load_op_t cmd = config->load.toss();
+
+        int _val;
+        payload_t key, value;
+        
+        char *_value;
+        size_t _value_length;
+        uint32_t _flags;
+        memcached_return_t _error;
+        
+        switch(cmd) {
+        case load_t::delete_op:
+            // Find the key
+            if(keys.empty())
+                break;
+            _val = random(0, keys.size() - 1);
+            key = keys[_val];
+            // Delete it from the server
+            _error = memcached_delete(&memcached, key.first, key.second, 0);
+            if(_error != MEMCACHED_SUCCESS) {
+                fprintf(stderr, "Error performing delete operation (%d)\n", _error);
+                exit(-1);
+            }
+            // Our own bookkeeping
+            free(key.first);
+            keys[_val] = keys[keys.size() - 1];
+            keys.erase(keys.begin() + _val);
+            break;
+            
+        case load_t::update_op:
+            // Find the key and generate the payload
+            if(keys.empty())
+                break;
+            key = keys[random(0, keys.size() - 1)];
+            config->values.toss(&value);
+            // Send it to server
+            _error = memcached_set(&memcached, key.first, key.second, value.first, value.second, 0, 0);
+            if(_error != MEMCACHED_SUCCESS) {
+                fprintf(stderr, "Error performing update operation (%d)\n", _error);
+                exit(-1);
+            }
+            // Free the value
+            free(value.first);
+            break;
+            
+        case load_t::insert_op:
+            // Generate the payload
+            config->keys.toss(&key);
+            config->values.toss(&value);
+            // Send it to server
+            _error = memcached_set(&memcached, key.first, key.second, value.first, value.second, 0, 0);
+            if(_error != MEMCACHED_SUCCESS) {
+                fprintf(stderr, "Error performing insert operation (%d)\n", _error);
+                exit(-1);
+            }
+            // Free the value and save the key
+            free(value.first);
+            keys.push_back(key);
+            break;
+            
+        case load_t::read_op:
+            // Find the key
+            if(keys.empty())
+                break;
+            key = keys[random(0, keys.size() - 1)];
+            // Read it from the server
+            _value = memcached_get(&memcached, key.first, key.second,
+                                   &_value_length, &_flags, &_error);
+            if(_error != MEMCACHED_SUCCESS) {
+                fprintf(stderr, "Error performing read operation (%d)\n", _error);
+                exit(-1);
+            }
+            // Our own bookkeeping
+            free(_value);
+            break;
+        };
     }
     
     // Disconnect
     memcached_free(&memcached);
+
+    // Free all the keys
+    for(vector<payload_t>::iterator i = keys.begin(); i != keys.end(); i++) {
+        free(i->first);
+    }
 }
 
 int main(int argv, char *argc[])
 {
-    usage(argc[0]);
+    // Initialize randomness
+    srand(time(NULL));
+    
+    // Parse the arguments
+    config_t config;
     int res;
-    int nthreads = 25;
-    std::vector<pthread_t> threads(nthreads);
+    vector<pthread_t> threads(config.clients);
     
     // Create the threads
-    for(int i = 0; i < nthreads; i++) {
-        int res = pthread_create(&threads[i], NULL, run_client, NULL);
+    for(int i = 0; i < config.clients; i++) {
+        int res = pthread_create(&threads[i], NULL, run_client, &config);
         if(res != 0) {
             fprintf(stderr, "Can't create thread");
             exit(-1);
@@ -153,7 +293,7 @@ int main(int argv, char *argc[])
     }
 
     // Wait for the threads to finish
-    for(int i = 0; i < nthreads; i++) {
+    for(int i = 0; i < config.clients; i++) {
         res = pthread_join(threads[i], NULL);
         if(res != 0) {
             fprintf(stderr, "Can't join on the thread");
