6a2b852aa443b2c644fea3a1abffd6493bcb2add
Sam Hughes <sam@rethinkdb.com>
2010-10-14 16:41:22 -0700

    Randomized flush times.

---
+++ src/buffer_cache/mirrored/writeback/flush_time_randomizer.cc
@@ -0,0 +1,28 @@
+#include "flush_time_randomizer.hpp"
+
+#include <stdlib.h>
+#include <algorithm>
+
+#include "errors.hpp"
+
+flush_time_randomizer_t::flush_time_randomizer_t(int flush_timer_ms_)
+    : flush_timer_ms(flush_timer_ms_), first_time_interval(rand() % std::max(1, flush_timer_ms)), done_first_time_interval(false) {
+    assert(flush_timer_ms >= 0 || flush_timer_ms == NEVER_FLUSH);
+ }
+
+int flush_time_randomizer_t::next_time_interval() {
+    assert(flush_timer_ms != 0);
+
+    if (!done_first_time_interval) {
+        done_first_time_interval = true;
+        return first_time_interval;
+    }
+
+    int r = rand();
+    // We have about a 15/16 chance of returning flush_timer_ms.
+    if ((r & 0xF) == 0) {
+        // Otherwise, we return a value in (flush_timer_ms / 2, flush_timer_ms].
+        return flush_timer_ms - ((r >> 4) % (flush_timer_ms >> 1));
+    }
+    return flush_timer_ms;
+}
---
+++ src/buffer_cache/mirrored/writeback/flush_time_randomizer.hpp
@@ -0,0 +1,35 @@
+#ifndef __BUFFER_CACHE_MIRRORED_WRITEBACK_FLUSHTIMERANDOMIZER_HPP__
+#define __BUFFER_CACHE_MIRRORED_WRITEBACK_FLUSHTIMERANDOMIZER_HPP__
+
+#include "config/cmd_args.hpp"
+
+// Provides flush intervals for us to use.
+class flush_time_randomizer_t {
+public:
+    // TODO: explicit
+    explicit flush_time_randomizer_t(int flush_timer_ms_);
+
+    // Creates a probability distribution to twiggle with flush timing
+    // intervals, to combat any long-term tendency of flushes to get
+    // grouped together.  Returns a value in (0, flush_timer_ms], but
+    // usually returns flush_timer_ms.
+    int next_time_interval();
+
+    // returns flush_timer_ms == NEVER_FLUSH, meaning we never flush.
+    inline bool is_never_flush() const { return flush_timer_ms == NEVER_FLUSH; }
+
+    // returns flush_timer_ms == 0, meaning we flush immediately.
+    inline bool is_zero() const { return flush_timer_ms == 0; }
+private:
+    const int flush_timer_ms;
+    const int first_time_interval;
+    bool done_first_time_interval;
+
+};
+
+
+
+
+
+
+#endif  // __BUFFER_CACHE_MIRRORED_WRITEBACK_FLUSHTIMERANDOMIZER_HPP__
