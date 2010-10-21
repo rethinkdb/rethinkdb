85b904951afa1033b2f6b88d49b9720af6d72d14
jdoliner <jdoliner@rethinkdb.com>
2010-10-21 15:36:47 -0700

    Further extending graphing ability.

--- bench/stress_client.py
+++ bench/stress_client.py
@@ -2,7 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
 import re
-from mpl_toolkits.axes_grid1 import make_axes_locatable
+from mpl_toolkits.axes_grid import make_axes_locatable
+
+def normalize(array):
+    denom = max(map(lambda x: abs(x), array))
+    if denom == 0:
+        return array
+    else:
+        return map(lambda x: float(x) / denom, array)
 
 class default_empty_dict(dict):
     def __getitem__(self, key):
@@ -22,6 +29,10 @@ class line():
     def __init__(self, _regex, _fields):
         self.regex = _regex
         self.fields = _fields
+
+    def __repr__(self):
+        return self.regex
+
     def parse_line(self, line):
         matches = re.match(self.regex, line)
         if matches:
@@ -52,8 +63,7 @@ def until(line, data):
     while len(data) > 0:
         matches = line.parse_line(data[len(data) - 1])
         data.pop()
-        if matches:
-            return matches
+        return matches
     return False
 
 #iterate through lines while they match (and there's data)
@@ -71,22 +81,35 @@ def read_while(lines, data):
             break
     return res
 
-class IOStat():
-    file_hdr_line   = line("^Linux\s+[\d.]+\s+\(\w+\)\s+[\d\\]+\s+[\d_x]+\s+\(\d+\s+CPU\)$", [])
+class TimeSeries():
+    def __init__(self, file_name):
+        pass
+        
+    def parse_file(self, file_name):
+        pass
+
+    def histogram(self, out_fname):
+        pass
+
+    def plot(self, out_fname):
+        pass
+
+class IOStat(TimeSeries):
+    file_hdr_line   = line("Linux.*", [])
     avg_cpu_hdr_line= line("^avg-cpu:  %user   %nice %system %iowait  %steal   %idle$", [])
     avg_cpu_line    = line("^" + "\s+([\d\.]+" * 6 + "$", [('user', 'f'), ('nice', 'f'), ('system', 'f'), ('iowait', 'f'),  ('steal', 'f'),   ('idle', 'f')])
     dev_hdr_line    = line("^Device:            tps   Blk_read/s   Blk_wrtn/s   Blk_read   Blk_wrtn$", [])
     dev_line        = line("^(\w+)\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)\s+(\d+)\s+(\d+)$", [('device', 's'), ('tps', 'f'), (' Blk_read', 'f'), (' Blk_wrtn', 'f'), (' Blk_read', 'd'), (' Blk_wrtn', 'd')])
 
     def __init__(self, file_name):
-        res = default_empty_dict()
         self.data = self.parse_file(file_name)
 
     def parse_file(self, file_name):
+        res = default_empty_dict()
         data = open(file_name).readlines()
         data.reverse()
         m = until(self.file_hdr_line, data)
-        assert m
+        assert m != False
         while True:
             m = until(self.avg_cpu_hdr_line, data)
             if not m:
@@ -97,17 +120,51 @@ class IOStat():
             for val in m.iteritems():
                 res['cpu_' + val[0]].append(val[1])
 
-            m = until([self.dev_hdr_line], data)
+            m = read_while([self.dev_hdr_line], data)
             assert m
             for device in m:
                 dev_name = device.pop('device')
                 for val in device.iteritems():
                     res['dev:' + dev_name + '_' + val[0]].append(val[1])
 
+        return res
+
+    def histogram(self, out_fname):
+        pass
+
+    def plot(self, out_fname):
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        for series in self.data.iteritems():
+            ax.plot(range(len(series)), normalize(series), 'g.-')
+        ax.set_xlabel('Time (seconds)')
+        ax.set_xlim(0, len(self.data))
+        ax.set_ylim(0, 1.0)
+        ax.grid(True)
+        plt.savefig(out_fname)
+
+
 class VMStat():
     file_hdr_line   = line("^procs -----------memory---------- ---swap-- -----io---- -system-- ----cpu----$", [])
-    stats_hdr       = line("^r  b   swpd   free   buff  cache   si   so    bi    bo   in   cs us sy id wa$", [])
+    stats_hdr_line  = line("^r  b   swpd   free   buff  cache   si   so    bi    bo   in   cs us sy id wa$", [])
+    stats_line      = line("^" + "\s+(\d+)" * 14 + "$", [('r', 'd'),  ('b', 'd'),   ('swpd', 'd'),   ('free', 'd'),   ('buff', 'd'),  ('cache', 'd'),   ('si', 'd'),   ('so', 'd'),    ('bi', 'd'),    ('bo', 'd'),   ('in', 'd'),   ('cs', 'd'), ('us', 'd'), ('sy', 'd'), ('id', 'd'), ('wa', 'd')])
 
+    def __init__(self, file_name):
+        self.data = self.parse_file(file_name)
+
+    def parse_file(self, file_name):
+        res = default_empty_dict()
+        data = open(file_name).readlines()
+        data.reverse()
+        m = until(self.file_hdr_line, data)
+        assert m
+        m = take(self.stats_hdr_line, data)
+        assert m
+        m = read_while([self.stats_line], data)
+        for stat_line in m:
+            for val in stat_line.iteritems():
+                res[val[0]].append(val[1])
+        return res
 
 class Latency():
     line = line("(\d+)\s+([\d.]+)\n", [('tick', 'd'), ('latency', 'f')])
