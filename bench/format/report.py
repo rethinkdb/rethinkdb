4addeafa6756d98210463a3740d1e8486a3574c9
jdoliner <jdoliner@rethinkdb.com>
2010-11-02 17:25:55 -0700

    Parse email flag as second argument.

--- bench/format/report.py
+++ bench/format/report.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python
 from format import *
 import sys
-email_flag = '--email'
 def usage(prog_name):
     print "Usage: %s dbench_output_dir %s email_to_report_to" % (prog_name, email_flag)
-if len(sys.argv) != 4 or sys.argv[2] != email_flag:
+if len(sys.argv) != 3:
     usage(sys.argv[0])
     sys.exit(2)
-db = dbench(sys.argv[1] + '/', sys.argv[3])
+db = dbench(sys.argv[1] + '/', sys.argv[2])
 db.report()
