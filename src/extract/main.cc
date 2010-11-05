65b294d651a139b3acb5368032ae7e692e01bd88
Sam Hughes <sam@rethinkdb.com>
2010-11-05 10:07:20 -0700

    Added pool->shutdown() to rethinkdb-extract so that the program terminates.

--- src/extract/main.cc
+++ src/extract/main.cc
@@ -161,6 +161,7 @@ int main(int argc, char **argv) {
         thread_pool_t *pool;
         void on_cpu_switch() {
             dumpfile(*config);
+            pool->shutdown();
         }
     } starter;
 
