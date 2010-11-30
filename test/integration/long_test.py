8d15bd414075639a4f8e48729fa477aa693aff60
Joe Doliner <jdoliner@gamil.com>
2010-11-30 10:38:49 -0800

    Send emails when long test completes. No more valgrind.

--- test/integration/long_test.py
+++ test/integration/long_test.py
@@ -3,8 +3,8 @@ import os
 from test_common import *
 def test_function(opts, port):
     stress_client = opts['stress']
-    os.system(stress_client + ' -s localhost:%d -d infinity' % port)
+    os.system(stress_client + ' -s localhost:%d -d infinity -c 16' % port)
 
 if __name__ == "__main__":
     op = make_option_parser()
-    auto_server_test_main(test_function, op.parse(sys.argv), timeout = 120)
+    auto_server_test_main(test_function, op.parse(sys.argv), timeout = None)
