e57c9fee10795b0f56d6260030e9358431cac98b
Tim Maxwell <tim@rethinkdb.com>
2010-12-06 14:41:10 -0800

    Documented message hub better.

--- src/arch/linux/arch.hpp
+++ src/arch/linux/arch.hpp
@@ -29,7 +29,6 @@ struct linux_io_config_t {
     }
     
     static bool continue_on_cpu(int cpu, linux_cpu_message_t *msg) {
-        msg->return_cpu = get_cpu_id();
         if (cpu == get_cpu_id()) {
             // The CPU to continue on is the CPU we are already on
             return true;
