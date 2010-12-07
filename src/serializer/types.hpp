d90048cff9955c8411e8c6d04482e85dce12a6b1
Sam Hughes <sam@rethinkdb.com>
2010-12-07 11:16:50 -0800

    Moved block_size_t to serializer/types.hpp

--- src/serializer/types.hpp
+++ src/serializer/types.hpp
@@ -57,5 +57,27 @@ struct buf_data_t {
 } __attribute__((__packed__));
 
 
+class block_size_t {
+public:
+    // This is a bit ugly in that things could use the wrong method:
+    // things could call value() instead of ser_value() or vice versa.
+
+    // The "block size" used by things above the serializer.
+    uint64_t value() const { return ser_bs_ - sizeof(buf_data_t); }
+
+    // The "block size" used by things in the serializer.
+    uint64_t ser_value() const { return ser_bs_; }
+
+    // Avoid using this function.  We want there to be a small
+    // number of uses so that we can be sure it's impossible to pass
+    // the wrong value as a block_size_t.
+    static block_size_t unsafe_make(uint64_t ser_bs) {
+        return block_size_t(ser_bs);
+    }
+private:
+    explicit block_size_t(uint64_t ser_bs) : ser_bs_(ser_bs) { }
+    uint64_t ser_bs_;
+};
+
 
 #endif  // __SERIALIZER_TYPES_HPP__
