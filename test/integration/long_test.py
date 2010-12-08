22e3645477ee86f3672005dbcb203630a0c0894d
Tea Pot <teapot>
2010-12-08 10:21:59 -0800

    Bigger cache in long test.

--- test/integration/long_test.py
+++ test/integration/long_test.py
@@ -3,7 +3,7 @@ import os
 from test_common import *
 def test_function(opts, port):
     stress_client = opts['stress']
-    os.system(stress_client + ' -s localhost:%d -d infinity -c 16' % port)
+    os.system(stress_client + ' -s localhost:%d -d infinity -c 32 -w 0/0/1/0/1/1/0' % port)
 
 if __name__ == "__main__":
     op = make_option_parser()
