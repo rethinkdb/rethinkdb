0ccb8f196ebe477a93b187eb740ef84dc710543c
Daniel Ehrenberg <dan@rethinkdb.com>
2010-12-16 11:56:22 -0800

    Adding file with co_{read,write} to repository

---
+++ src/arch/arch.cc
@@ -0,0 +1,23 @@
+#include "coroutine/coroutines.hpp"
+#include "arch/arch.hpp"
+
+struct io_coroutine_adapter_t : public iocallback_t {
+    coro_t *cont;
+    io_coroutine_adapter_t() : cont(coro_t::self()) { }
+    void on_io_complete(event_t *e) {
+        cont->notify();
+    }
+};
+
+void co_read(direct_file_t *file, size_t offset, size_t length, void *buf) {
+    io_coroutine_adapter_t adapter;
+    file->read_async(offset, length, buf, &adapter);
+    coro_t::wait();
+}
+
+void co_write(direct_file_t *file, size_t offset, size_t length, void *buf) {
+    io_coroutine_adapter_t adapter;
+    file->write_async(offset, length, buf, &adapter);
+    coro_t::wait();
+}
+
