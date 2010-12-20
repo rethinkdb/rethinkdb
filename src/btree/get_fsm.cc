442a75de0ab883b2dc86114fc980d6693ea5bc96
Dan Ehrenberg <dan@rethinkdb.com>
2010-12-20 11:40:49 -0800

    Btree coroutine supports perfmon again

--- src/btree/get_fsm.cc
+++ src/btree/get_fsm.cc
@@ -54,17 +54,19 @@ buf_t *co_acquire_transaction(transaction_t *transaction, block_id_t block_id, a
 }
 
 void co_btree_get(btree_key *_key, btree_key_value_store_t *store, store_t::get_callback_t *cb) {
+    ticks_t start_time;
+    pm_cmd_get.begin(&start_time);
     union {
         char value_memory[MAX_TOTAL_NODE_CONTENTS_SIZE+sizeof(btree_value)];
         btree_value value;
     };
-    value_memory[0] = 0;
+    value_memory[0] = 0; //Make GCC happy--I don't know how to mark value_memory as unused but OK
 
     union {
         char key_memory[MAX_KEY_SIZE+sizeof(btree_key)];
         btree_key key;
     };
-    key_memory[0] = 0;
+    key_memory[0] = 0; //Make GCC happy--I don't know how to mark value_memory as unused but OK
 
     keycpy(&key, _key);
     transaction_t *transaction = NULL;
@@ -72,113 +74,112 @@ void co_btree_get(btree_key *_key, btree_key_value_store_t *store, store_t::get_
     cache_t *cache = &slice->cache;
     int home_thread = get_thread_id();
 
-    {
-        coro_t::move_to_thread(slice->home_thread);
-        //Acquire the superblock
-        buf_t *buf, *last_buf;
-        transaction = cache->begin_transaction(rwi_read, NULL);
-        assert(transaction); // Read-only transaction always begins immediately.
-        last_buf = co_acquire_transaction(transaction, SUPERBLOCK_ID, rwi_read);
-        assert(last_buf);
-        block_id_t node_id = ptr_cast<btree_superblock_t>(last_buf->get_data_read())->root_block;
-        assert(node_id != SUPERBLOCK_ID);
+    coro_t::move_to_thread(slice->home_thread);
+    //Acquire the superblock
+    buf_t *buf, *last_buf;
+    transaction = cache->begin_transaction(rwi_read, NULL);
+    assert(transaction); // Read-only transaction always begins immediately.
+    last_buf = co_acquire_transaction(transaction, SUPERBLOCK_ID, rwi_read);
+    assert(last_buf);
+    block_id_t node_id = ptr_cast<btree_superblock_t>(last_buf->get_data_read())->root_block;
+    assert(node_id != SUPERBLOCK_ID);
 
-        //Acquire the root
-        if (node_id == NULL_BLOCK_ID) {
-            //No root exists
-            last_buf->release();
-            last_buf = NULL;
-            // Commit transaction now because we won't be returning to this core
-            bool committed __attribute__((unused)) = transaction->commit(NULL);
-            assert(committed);   // Read-only transactions complete immediately
-            coro_t::move_to_thread(home_thread);
-            cb->not_found();
-            return;
-        }
-
-        //Acquire the leaf node
-        const node_t *node;
-        while (true) {
-            buf = co_acquire_transaction(transaction, node_id, rwi_read);
-            assert(buf);
-            node_handler::validate(cache->get_block_size(), ptr_cast<node_t>(buf->get_data_read()));
-            
-            // Release the previous buffer
-            last_buf->release();
-            last_buf = NULL;
-
-            node = ptr_cast<node_t>(buf->get_data_read());
-            if(!node_handler::is_internal(node)) break;
-
-            block_id_t next_node_id = internal_node_handler::lookup(ptr_cast<internal_node_t>(node), &key);
-            assert(next_node_id != NULL_BLOCK_ID);
-            assert(next_node_id != SUPERBLOCK_ID);
-            last_buf = buf;
-            node_id = next_node_id;
-        }
-
-        //Got down to the leaf, now examine it
-        bool found = leaf_node_handler::lookup(ptr_cast<leaf_node_t>(node), &key, &value);
-        buf->release();
-        buf = NULL;
-        
-        if (found && value.expired()) {
-            delete_expired(&key, store);
-            found = false;
-        }
-        
-        /* The get_fsm has two paths it takes: one for large values and one for small ones. For large
-        values, it holds onto the large value buffer while it goes back to the request handler's core
-        and delivers the large value. Then it returns again to the cache's core and frees the value,
-        and finally goes to the request handler's core again to free itself. For small values, it
-        duplicates the value into an internal buffer and then goes to the request handler's core,
-        delivers the value, and frees itself all in one trip. If the value is not found, we take
-        a third path that is basically the same as the one for small values. */
-        if (!found) {
-            // Commit transaction now because we won't be returning to this core
-            bool committed __attribute__((unused)) = transaction->commit(NULL);
-            assert(committed);   // Read-only transactions complete immediately
-            coro_t::move_to_thread(home_thread);
-            cb->not_found();
-            return;
-        } else if (value.is_large()) {
-            // Don't commit transaction yet because we need to keep holding onto
-            // the large buf until it's been read.
-            assert(value.is_large());
-
-            large_buf_t *large_value = new large_buf_t(transaction);
-
-            co_acquire_large_value(large_value, value.lb_ref(), rwi_read);
-            assert(large_value->state == large_buf_t::loaded);
-            assert(large_value->get_root_ref().block_id == value.lb_ref().block_id);
-
-            const_buffer_group_t value_buffers;
-            for (int i = 0; i < large_value->get_num_segments(); i++) {
-                uint16_t size;
-                const void *data = large_value->get_segment(i, &size);
-                value_buffers.add_buffer(size, data);
-            }
-            coro_t::move_to_thread(home_thread);
-            co_value(cb, &value_buffers, value.mcflags(), 0);
-            {
-                coro_t::on_thread_t mover(slice->home_thread);
-                large_value->release();
-                delete large_value;
-                bool committed __attribute__((unused)) = transaction->commit(NULL);
-                assert(committed);   // Read-only transactions complete immediately
-            }
-        } else {
-            // Commit transaction now because we won't be returning to this core
-            bool committed __attribute__((unused)) = transaction->commit(NULL);
-            assert(committed);   // Read-only transactions complete immediately
-            
-            const_buffer_group_t value_buffers;
-            value_buffers.add_buffer(value.value_size(), value.value());
-            coro_t::move_to_thread(home_thread);
-            co_value(cb, &value_buffers, value.mcflags(), 0);
-        }
-
+    //Acquire the root
+    if (node_id == NULL_BLOCK_ID) {
+	//No root exists
+	last_buf->release();
+	last_buf = NULL;
+	// Commit transaction now because we won't be returning to this core
+	bool committed __attribute__((unused)) = transaction->commit(NULL);
+	assert(committed);   // Read-only transactions complete immediately
+	coro_t::move_to_thread(home_thread);
+	cb->not_found();
+	pm_cmd_get.end(&start_time);
+	return;
     }
+
+    //Acquire the leaf node
+    const node_t *node;
+    while (true) {
+	buf = co_acquire_transaction(transaction, node_id, rwi_read);
+	assert(buf);
+	node_handler::validate(cache->get_block_size(), ptr_cast<node_t>(buf->get_data_read()));
+	
+	// Release the previous buffer
+	last_buf->release();
+	last_buf = NULL;
+
+	node = ptr_cast<node_t>(buf->get_data_read());
+	if(!node_handler::is_internal(node)) break;
+
+	block_id_t next_node_id = internal_node_handler::lookup(ptr_cast<internal_node_t>(node), &key);
+	assert(next_node_id != NULL_BLOCK_ID);
+	assert(next_node_id != SUPERBLOCK_ID);
+	last_buf = buf;
+	node_id = next_node_id;
+    }
+
+    //Got down to the leaf, now examine it
+    bool found = leaf_node_handler::lookup(ptr_cast<leaf_node_t>(node), &key, &value);
+    buf->release();
+    buf = NULL;
+    
+    if (found && value.expired()) {
+	delete_expired(&key, store);
+	found = false;
+    }
+    
+    /* The get_fsm has two paths it takes: one for large values and one for small ones. For large
+    values, it holds onto the large value buffer while it goes back to the request handler's core
+    and delivers the large value. Then it returns again to the cache's core and frees the value,
+    and finally goes to the request handler's core again to free itself. For small values, it
+    duplicates the value into an internal buffer and then goes to the request handler's core,
+    delivers the value, and frees itself all in one trip. If the value is not found, we take
+    a third path that is basically the same as the one for small values. */
+    if (!found) {
+	// Commit transaction now because we won't be returning to this core
+	bool committed __attribute__((unused)) = transaction->commit(NULL);
+	assert(committed);   // Read-only transactions complete immediately
+	coro_t::move_to_thread(home_thread);
+	cb->not_found();
+	return;
+    } else if (value.is_large()) {
+	// Don't commit transaction yet because we need to keep holding onto
+	// the large buf until it's been read.
+	assert(value.is_large());
+
+	large_buf_t *large_value = new large_buf_t(transaction);
+
+	co_acquire_large_value(large_value, value.lb_ref(), rwi_read);
+	assert(large_value->state == large_buf_t::loaded);
+	assert(large_value->get_root_ref().block_id == value.lb_ref().block_id);
+
+	const_buffer_group_t value_buffers;
+	for (int i = 0; i < large_value->get_num_segments(); i++) {
+	    uint16_t size;
+	    const void *data = large_value->get_segment(i, &size);
+	    value_buffers.add_buffer(size, data);
+	}
+	coro_t::move_to_thread(home_thread);
+	co_value(cb, &value_buffers, value.mcflags(), 0);
+	{
+	    coro_t::on_thread_t mover(slice->home_thread);
+	    large_value->release();
+	    delete large_value;
+	    bool committed __attribute__((unused)) = transaction->commit(NULL);
+	    assert(committed);   // Read-only transactions complete immediately
+	}
+    } else {
+	// Commit transaction now because we won't be returning to this core
+	bool committed __attribute__((unused)) = transaction->commit(NULL);
+	assert(committed);   // Read-only transactions complete immediately
+	
+	const_buffer_group_t value_buffers;
+	value_buffers.add_buffer(value.value_size(), value.value());
+	coro_t::move_to_thread(home_thread);
+	co_value(cb, &value_buffers, value.mcflags(), 0);
+    }
+    pm_cmd_get.end(&start_time);
 }
 
 void btree_get(btree_key *key, btree_key_value_store_t *store, store_t::get_callback_t *cb) {
