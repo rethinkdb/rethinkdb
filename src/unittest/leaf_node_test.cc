8d6cfb7652f546b0704db2de24f699e7cb2ef218
Sam Hughes <sam@rethinkdb.com>
2010-12-23 11:31:11 -0800

    LeafNodeTest InsertionLookup

--- src/unittest/leaf_node_test.cc
+++ src/unittest/leaf_node_test.cc
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <vector>
 
 #include "unittest/gtest.hpp"
@@ -7,12 +8,48 @@
 
 namespace unittest {
 
-// TODO: this is rather duplicative of fsck::check_subtree_leaf_node.
-void verify_leaf_node_contiguity(block_size_t block_size, leaf_node_t *buf) {
+// TODO: This is rather duplicative of fsck::check_value.
+void expect_valid_value_shallowly(const btree_value *value) {
+    EXPECT_TRUE(!(value->metadata_flags & ~(MEMCACHED_FLAGS | MEMCACHED_CAS | MEMCACHED_EXPTIME | LARGE_VALUE)));
 
+    size_t size = value->value_size();
 
+    if (value->is_large()) {
+        EXPECT_GT(size, MAX_IN_NODE_VALUE_SIZE);
 
+        // This isn't fsck, we can't follow large buf pointers.
+    } else {
+        EXPECT_LE(size, MAX_IN_NODE_VALUE_SIZE);
+    }
+}
 
+// TODO: This is rather duplicative of fsck::check_subtree_leaf_node.
+void verify(block_size_t block_size, const leaf_node_t *buf) {
+
+    ASSERT_LE(offsetof(leaf_node_t, pair_offsets) + buf->npairs * 2, buf->frontmost_offset);
+    ASSERT_LE(buf->frontmost_offset, block_size.value());
+
+    std::vector<uint16_t> offsets(buf->pair_offsets, buf->pair_offsets + buf->npairs);
+    std::sort(offsets.begin(), offsets.end());
+
+    uint16_t expected = buf->frontmost_offset;
+    for (std::vector<uint16_t>::const_iterator p = offsets.begin(), e = offsets.end(); p < e; ++p) {
+        ASSERT_LE(expected, block_size.value());
+        ASSERT_EQ(expected, *p);
+        expected += leaf_node_handler::pair_size(leaf_node_handler::get_pair(buf, *p));
+    }
+    ASSERT_EQ(block_size.value(), expected);
+
+    const btree_key *last_key = NULL;
+    for (const uint16_t *p = buf->pair_offsets, *e = p + buf->npairs; p < e; ++p) {
+        const btree_leaf_pair *pair = leaf_node_handler::get_pair(buf, *p);
+        if (last_key != NULL) {
+            const btree_key *next_key = &pair->key;
+            EXPECT_LT(leaf_key_comp::compare(last_key, next_key), 0);
+            last_key = next_key;
+        }
+        expect_valid_value_shallowly(pair->value());
+    }
 }
 
 TEST(LeafNodeTest, Offsets) {
@@ -43,6 +80,82 @@ TEST(LeafNodeTest, Offsets) {
     EXPECT_EQ(174, reinterpret_cast<byte *>(p.value()) - reinterpret_cast<byte *>(&p));
 }
 
+block_size_t make_bs() {
+    // TODO: Test weird block sizes.
+    return block_size_t::unsafe_make(4096);
+}
+
+leaf_node_t *malloc_leaf_node(block_size_t bs) {
+    return reinterpret_cast<leaf_node_t *>(malloc(bs.value()));
+}
+
+btree_key *malloc_key(const char *s) {
+    size_t origlen = strlen(s);
+    EXPECT_LE(origlen, MAX_KEY_SIZE);
+
+    size_t len = std::min<size_t>(origlen, MAX_KEY_SIZE);
+    btree_key *k = reinterpret_cast<btree_key *>(malloc(sizeof(btree_key) + len));
+    k->size = len;
+    memcpy(k->contents, s, len);
+    return k;
+}
+
+btree_value *malloc_value(const char *s) {
+    size_t origlen = strlen(s);
+    EXPECT_LE(origlen, MAX_IN_NODE_VALUE_SIZE);
+
+    size_t len = std::min<size_t>(origlen, MAX_IN_NODE_VALUE_SIZE);
+
+    btree_value *v = reinterpret_cast<btree_value *>(malloc(sizeof(btree_value) + len));
+    v->size = len;
+    v->metadata_flags = 0;
+    memcpy(v->value(), s, len);
+    return v;
+}
+
+TEST(LeafNodeTest, Initialization) {
+    block_size_t bs = make_bs();
+    leaf_node_t *node = malloc_leaf_node(bs);
+
+    leaf_node_handler::init(bs, node, repli_timestamp::invalid);
+    verify(bs, node);
+    free(node);
+}
+
+// Inserts one value and looks it up.
+TEST(LeafNodeTest, InsertionLookup) {
+    block_size_t bs = make_bs();
+    leaf_node_t *node = malloc_leaf_node(bs);
+
+    leaf_node_handler::init(bs, node, repli_timestamp::invalid);
+    verify(bs, node);
+
+    const char *key = "key";
+    const char *value = "value";
+
+    btree_key *k = malloc_key(key);
+    btree_value *v = malloc_value(value);
+
+    ASSERT_TRUE(leaf_node_handler::insert(bs, node, k, v, repli_timestamp::invalid));
+    verify(bs, node);
+
+    union {
+        byte readval_padding[MAX_TOTAL_NODE_CONTENTS_SIZE + sizeof(btree_value)];
+        btree_value readval;
+    };
+
+    (void)readval_padding;
+
+    ASSERT_TRUE(leaf_node_handler::lookup(node, k, &readval));
+
+    ASSERT_EQ(0, memcmp(v, &readval, v->full_size()));
+
+    verify(bs, node);
+
+    free(k);
+    free(v);
+    free(node);
+}
 
 }  // namespace unittest
 
