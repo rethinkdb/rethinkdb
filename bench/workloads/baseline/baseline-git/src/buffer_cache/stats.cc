63cdbcedf9b53a4e5b8e26f4e48b750e7f4183a5
Daniel Mewes <daniel@rethinkdb.com>
2010-12-23 18:51:30 -0800

    Use a bench_baseline tag for getting the baseline version of RethinkDB and stress client.

--- bench/workloads/baseline/baseline-git/src/buffer_cache/stats.cc
+++
@@ -1,7 +0,0 @@
-#include "buffer_cache/stats.hpp"
-
-perfmon_counter_t
-    pm_n_blocks_in_memory("blocks_in_memory[blocks]"),
-    pm_n_blocks_dirty("blocks_dirty[blocks]"),
-    pm_n_blocks_total("blocks_total[blocks]");
-
