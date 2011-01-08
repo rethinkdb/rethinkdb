a35475701cdfa55aab2f2a486cd2505fe4aae4fb
Ivan Tarasov <ivan@rethinkdb.com>
2011-01-08 02:42:47 -0800

    Add configurable plotting

--- test/long_test.py
+++ test/long_test.py
@@ -4,22 +4,18 @@ from datetime import datetime
 from subprocess import Popen, PIPE
 from optparse import OptionParser
 from retester import send_email, do_test
-import numpy as np
-#import matplotlib.pyplot as plt
-#import matplotlib.mlab as mlab
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'common')))
 from test_common import *
 from git_util import *
 
 long_test_branch = "ivan_long_test"
-long_test_port = 10297
 no_checkout_arg = "--no-checkout"
 long_test_logs_dir = "~/long_test_logs"
 
 rdb_num_threads = 12
 rdb_db_files = ["/dev/sdb", "/dev/sdc", "/dev/sdd", "/dev/sde"] 
-rdb_cache_size = 38000
+rdb_cache_size = 25000
 
 rdbstat_path = "bench/dbench/rdbstat"
 
@@ -72,13 +68,13 @@ def parse_arguments(args):
     op["nreads"]    = IntFlag("--nreads",    64)
     op["nappends"]  = IntFlag("--nappends",  0)
     op["nprepends"] = IntFlag("--nprepends", 0)
-    op['duration']  = StringFlag("--duration", "30s") # RSI
+    op['duration']  = StringFlag("--duration", "infinity")
     op['checkout']  = BoolFlag(no_checkout_arg, invert = True)  # Tim says that means that checkout is True by default
     op['make']      = BoolFlag("--no-make", invert = True)
     op['clients']   = IntFlag("--clients", 512)
-    op['reporting_interval']   = IntFlag("--rinterval", 7200) # RSI
+    op['reporting_interval']   = IntFlag("--rinterval", 28800)
     op['emailfrom'] = StringFlag("--emailfrom", 'buildbot@rethinkdb.com:allspark')
-    op['recipient'] = StringFlag("--email", 'ivan@rethinkdb.com') # RSI
+    op['recipient'] = StringFlag("--email", 'ivan@rethinkdb.com, daniel@rethinkdb.com') # RSI
 
     opts = op.parse(args)
     opts["netrecord"] = False   # We don't want to slow down the network
@@ -136,8 +132,8 @@ def shutdown():
         rdbstat.stop()
         rdbstat = None
     if stats_sender:
-        # stats_sender.post_results()
         stats_sender.stop()
+        stats_sender.post_results()
         stats_sender = None
 
 def set_signal_handler():
@@ -148,27 +144,68 @@ def set_signal_handler():
     signal.signal(signal.SIGTERM, signal_handler)
     signal.signal(signal.SIGQUIT, signal_handler)
 
+def simple_plotter(name, multiplier = 1):
+    return lambda (old, new): new[name]*multiplier if new[name] else None
+
+def differential_plotter(name):
+    return lambda (old, new): (new[name]-old[name])/(new['uptime']-old['uptime']) if new[name] and old[name] else None
+
+def two_stats_diff_plotter(stat1, stat2):
+    return lambda (old, new): new[stat1]-new[stat2] if new[stat1] and new[stat2] else None
+
+def two_stats_ratio_plotter(dividend, divisor):
+    return lambda (old, new): new[dividend]/new[divisor] if new[dividend] and new[divisor] and new[divisor] != 0 else None
+
 class StatsSender(object):
     monitoring = [
+        'blocks_dirty[blocks]',
+        'blocks_in_memory[blocks]',
+        'cmd_get_avg',
+        'cmd_set_avg',
+        'cmd_get_total',
+        'cmd_set_total',
         'io_writes_started[iowrites]',
         'io_reads_started[ioreads]',
         'io_writes_completed[iowrites]',
         'io_reads_completed[ioreads]',
-
-        'blocks_dirty[blocks]',
-        'blocks_in_memory[blocks]',
-        'cpu_user_avg',
-        'cpu_system_avg',
         'cpu_combined_avg',
-        'memory_faults_avg',
+        'cpu_system_avg',
+        'cpu_user_avg',
+        'memory_faults_max',
         'memory_real[bytes]',
         'memory_virtual[bytes]',
         'serializer_bytes_in_use',
         'serializer_old_garbage_blocks',
         'serializer_old_total_blocks',
-        'serializer_reads_total',
-        'serializer_writes_total',
-        'uptime'
+#        'serializer_reads_total',
+#        'serializer_writes_total',
+        'uptime',
+    ]
+    bucket_size = 100
+    single_plot_height = 128
+    plot_style_quantile = 'quantile %d 0.05,0.5,0.95' % bucket_size
+    plot_style_quantile_log = 'quantilel %d 0.05,0.5,0.95' % bucket_size
+    plot_style_line = 'lines'
+
+    plotters = [
+        ('blocks_dirty', plot_style_quantile, simple_plotter('blocks_dirty[blocks]')),
+        ('blocks_in_memory', plot_style_quantile, simple_plotter('blocks_in_memory[blocks]')),
+        ('cmd_get_avg', plot_style_quantile_log, simple_plotter('cmd_get_avg')),
+        ('cmd_get/s', plot_style_quantile_log, differential_plotter('cmd_get_total')),
+        ('cmd_set_avg', plot_style_quantile_log, simple_plotter('cmd_set_avg')),
+        ('cmd_set/s', plot_style_quantile_log, differential_plotter('cmd_set_total')),
+        ('io_writes/s', plot_style_quantile, differential_plotter('io_writes_started[iowrites]')),
+        ('io_reads/s', plot_style_quantile, differential_plotter('io_reads_started[ioreads]')),
+        ('outstanding_io_writes', plot_style_quantile, two_stats_diff_plotter('io_writes_started[iowrites]', 'io_writes_completed[iowrites]')),
+        ('outstanding_io_reads', plot_style_quantile, two_stats_diff_plotter('io_reads_started[ioreads]', 'io_reads_completed[ioreads]')),
+        ('cpu_combined_avg', plot_style_quantile, simple_plotter('cpu_combined_avg')),
+        ('cpu_system_avg', plot_style_quantile, simple_plotter('cpu_system_avg')),
+        ('cpu_user_avg', plot_style_quantile, simple_plotter('cpu_user_avg')),
+        ('memory_faults_max', plot_style_quantile_log, simple_plotter('memory_faults_max')),
+        ('memory_real', plot_style_line, simple_plotter('memory_real[bytes]', 1.0/(1024*1024))),
+        ('memory_virtual', plot_style_line, simple_plotter('memory_virtual[bytes]', 1.0/(1024*1024))),
+        ('gc_ratio', plot_style_line, two_stats_ratio_plotter('serializer_old_garbage_blocks', 'serializer_old_total_blocks')),
+        ('serializer_MB_in_use', plot_style_quantile, simple_plotter('serializer_bytes_in_use', 1.0/(1024*1024))),
     ]
     def __init__(self, opts, server):
         def stats_aggregator(stats):
@@ -179,8 +216,7 @@ class StatsSender(object):
             self.stats.append(stats_snapshot)
 
             if self.need_to_post():
-                all_stats = self.reset_stats()
-                self.post_results(all_stats) # new thread
+                self.post_results() # new thread ?
 
         self.opts = opts
         self.stats = []
@@ -196,37 +232,47 @@ class StatsSender(object):
         self.stats = []
         return old_stats
 
-    def post_results(self, all_stats):
-        from email.mime.image import MIMEImage
-        from email.mime.multipart import MIMEMultipart
+    def post_results(self):
+        all_stats = self.reset_stats()
+        if len(all_stats) > 0:
+            from email.mime.image import MIMEImage
+            from email.mime.multipart import MIMEMultipart
 
-        msg = MIMEMultipart()
-        msg['Subject'] = 'Long test [%fs]' % all_stats[-1]['uptime']
-        msg['From'] = 'buildbot@rethinkdb.com'
-        msg['To'] = self.opts['recipient']
+            msg = MIMEMultipart()
+            msg['Subject'] = 'Long test [%fs]' % all_stats[-1]['uptime']
+            msg['From'] = 'buildbot@rethinkdb.com'
+            msg['To'] = self.opts['recipient']
 
-        msg.preamble = 'Preamble is here'
+            msg.preamble = 'Preamble is here'
 
-        for img in self.generate_plots(all_stats):
-            msg.attach(MIMEImage(img))
+            for img in self.generate_plots(all_stats):
+                msg.attach(MIMEImage(img))
 
-        os.environ['RETESTER_EMAIL_SENDER'] = self.opts['emailfrom']
-        send_email(None, msg, self.opts['recipient'])
-        print "Sent an email"
+            os.environ['RETESTER_EMAIL_SENDER'] = self.opts['emailfrom']
+            send_email(None, msg, self.opts['recipient'])
+            print "Sent an email"
 
     def generate_plots(self, all_stats):
-        # tplot -o result.png -of png -if - -tf num -k 'cmd_get' 'quantilel 100 0.05,0.5,0.95' -k 'cmd_set' 'quantilel 100 0.05,0.5,0.95' -k transactions_starting_avg 'quantilel 100 0.05,0.5,0.95' -k cmd_set/s 'quantilel 100 0.05,0.5,0.95' -k cmd_get/s 'quantilel 100 0.05,0.5,0.95'  -k io_writes/s 'quantile 100 0.05,0.5,0.95' -k outstanding_writes 'quantile 100 0.05,0.5,0.95' -or 1024x2048
-        plot_instructions = [ '-k', 'io_writes/s', 'quantile 36 0.05,0.5,0.95' ]
-        tplot = Popen(['tplot', '-if', '-', '-o', '/dev/stdout', '-of', 'png', '-tf', 'num', '-or', '1024x768'] + plot_instructions, stdin=PIPE, stdout=PIPE)
-        ts = None
-        io_writes_total = None
-        for stats in all_stats:
-            old_ts = ts
-            ts = stats['uptime']
-            old_io_writes_total = io_writes_total
-            io_writes_total = stats['io_writes_started[iowrites]']
-            if old_ts and ts - old_ts > 0:
-                print >>tplot.stdin, "%f =io_writes/s %f" % (ts, (io_writes_total - old_io_writes_total) / (ts - old_ts))
+        zipped_stats = zip(all_stats, all_stats[1:])
+        plot_instructions = []
+        for (name, plot_style, plotter) in StatsSender.plotters:
+            plot_instructions.extend(['-k', name, plot_style])
+
+        tplot = Popen(['tplot',
+            '-if', '-',
+            '-o', '/dev/stdout',
+            '-of', 'png',
+            '-tf', 'num',
+            '-or', '1024x%d' % (len(StatsSender.plotters)*StatsSender.single_plot_height),
+            ] + plot_instructions, stdin=PIPE, stdout=PIPE)
+
+        for stats_pair in zipped_stats:
+            for (name, plot_style, plotter) in StatsSender.plotters:
+                val = plotter(stats_pair)
+                if val:
+                    ts = stats_pair[1]['uptime']
+                    print >>tplot.stdin, "%f =%s %f" % (ts, name, val)
+
         tplot.stdin.close()
         return [tplot.stdout.read()]
 
