5e3bfcd929ce809cf36d9c541b1126a62b65abef
Sam Hughes <sam@rethinkdb.com>
2011-01-17 21:39:54 -0800

    Fixed linking errors, made message_callback_t abstract, but that doesnot mean replication works or anything.

--- src/replication/slave.hpp
+++ src/replication/slave.hpp
@@ -12,17 +12,17 @@ namespace replication {
 class message_callback_t {
 public:
     // These call .swap on their parameter, taking ownership of the pointee.
-    void hello(boost::scoped_ptr<hello_message_t>& message);
-    void send(boost::scoped_ptr<backfill_message_t>& message);
-    void send(boost::scoped_ptr<announce_message_t>& message);
-    void send(boost::scoped_ptr<set_message_t>& message);
-    void send(boost::scoped_ptr<append_message_t>& message);
-    void send(boost::scoped_ptr<prepend_message_t>& message);
-    void send(boost::scoped_ptr<nop_message_t>& message);
-    void send(boost::scoped_ptr<ack_message_t>& message);
-    void send(boost::scoped_ptr<shutting_down_message_t>& message);
-    void send(boost::scoped_ptr<goodbye_message_t>& message);
-    void conn_closed();
+    virtual void hello(boost::scoped_ptr<hello_message_t>& message) = 0;
+    virtual void send(boost::scoped_ptr<backfill_message_t>& message) = 0;
+    virtual void send(boost::scoped_ptr<announce_message_t>& message) = 0;
+    virtual void send(boost::scoped_ptr<set_message_t>& message) = 0;
+    virtual void send(boost::scoped_ptr<append_message_t>& message) = 0;
+    virtual void send(boost::scoped_ptr<prepend_message_t>& message) = 0;
+    virtual void send(boost::scoped_ptr<nop_message_t>& message) = 0;
+    virtual void send(boost::scoped_ptr<ack_message_t>& message) = 0;
+    virtual void send(boost::scoped_ptr<shutting_down_message_t>& message) = 0;
+    virtual void send(boost::scoped_ptr<goodbye_message_t>& message) = 0;
+    virtual void conn_closed() = 0;
 };
 
 void parse_messages(tcp_conn_t *conn, message_callback_t *receiver);
