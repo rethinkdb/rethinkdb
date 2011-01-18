42c9a5c15dcf4ed20fd83c560b5954e5e7d724c8
jdoliner <jdoliner@gmail.com>
2011-01-17 18:49:25 -0800

    Adds failover structs.
    Failover structs allow you to describe the servers behaviour upon failure.

---
+++ src/replication/failover.hpp
@@ -0,0 +1,24 @@
+#ifndef __FAILOVER_HPP__
+#define __FAILOVER_HPP__
+
+#include "containers/intrusive_list.hpp"
+
+class failover_callback_t : 
+    public intrusive_list_node_t<failover_callback_t>
+{
+    virtual ~failover_callback_t() {}
+    virtual void on_failure() = 0;
+};
+
+struct Failover_t {
+public:
+    Failover_t() {}
+    ~Failover_t() {}
+
+private:
+    intrusive_list_t<failover_callback_t> callbacks;
+    void add_callback(failover_callback_t *cb);
+    void on_failure();
+}
+
+#endif  // __FAILOVER_HPP__
