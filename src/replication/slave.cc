22ab10251f6f19aa20340d959131df402476c89b
jdoliner <jdoliner@gmail.com>
2011-01-20 15:45:16 -0800

    Hooks up slave as a store.
    Slave now sends operations down based on its failover state.

--- src/replication/slave.cc
+++ src/replication/slave.cc
@@ -197,31 +197,96 @@ slave_t::~slave_t() {}
 /* store interface */
 
 void slave_t::get(store_key_t *key, get_callback_t *cb)
-{}
+{
+    internal_store->get(key,cb);
+}
+
 void slave_t::get_cas(store_key_t *key, get_callback_t *cb)
-{}
+{
+    internal_store->get_cas(key,cb);
+}
+
 void slave_t::set(store_key_t *key, data_provider_t *data, mcflags_t flags, exptime_t exptime, set_callback_t *cb)
-{}
+{
+    if (respond_to_queries)
+        internal_store->set(key, data, flags, exptime, cb);
+    else
+        cb->not_allowed();
+}
+
 void slave_t::add(store_key_t *key, data_provider_t *data, mcflags_t flags, exptime_t exptime, set_callback_t *cb)
-{}
+{
+    if (respond_to_queries)
+        internal_store->add(key, data, flags, exptime, cb);
+    else
+        cb->not_allowed();
+}
+
 void slave_t::replace(store_key_t *key, data_provider_t *data, mcflags_t flags, exptime_t exptime, set_callback_t *cb)
-{}
+{
+    if (respond_to_queries)
+        internal_store->replace(key, data, flags, exptime, cb);
+    else
+        cb->not_allowed();
+}
+
 void slave_t::cas(store_key_t *key, data_provider_t *data, mcflags_t flags, exptime_t exptime, cas_t unique, set_callback_t *cb)
-{}
+{
+    if (respond_to_queries)
+        internal_store->cas(key, data, flags, exptime, unique, cb);
+    else
+        cb->not_allowed();
+}
+
 void slave_t::incr(store_key_t *key, unsigned long long amount, incr_decr_callback_t *cb)
-{}
+{
+    if (respond_to_queries)
+        internal_store->incr(key, amount, cb);
+    else
+        cb->not_allowed();
+}
+
 void slave_t::decr(store_key_t *key, unsigned long long amount, incr_decr_callback_t *cb)
-{}
+{
+    if (respond_to_queries)
+        internal_store->decr(key, amount, cb);
+    else
+        cb->not_allowed();
+}
+
 void slave_t::append(store_key_t *key, data_provider_t *data, append_prepend_callback_t *cb)
-{}
+{
+    if (respond_to_queries)
+        internal_store->append(key, data, cb);
+    else
+        cb->not_allowed();
+}
+
 void slave_t::prepend(store_key_t *key, data_provider_t *data, append_prepend_callback_t *cb)
-{}
+{
+    if (respond_to_queries)
+        internal_store->prepend(key, data, cb);
+    else
+        cb->not_allowed();
+}
+
 void slave_t::delete_key(store_key_t *key, delete_callback_t *cb)
-{}
+{
+    if (respond_to_queries)
+        internal_store->delete_key(key, cb);
+    else
+        cb->not_allowed();
+}
+
 void slave_t::replicate(replicant_t *cb, repli_timestamp cutoff)
-{}
+{
+    internal_store->replicate(cb, cutoff);
+}
+
 void slave_t::stop_replicating(replicant_t *cb)
-{}
+{
+    internal_store->stop_replicating(cb);
+}
 
  /* message_callback_t interface */
 void slave_t::hello(boost::scoped_ptr<hello_message_t>& message)
