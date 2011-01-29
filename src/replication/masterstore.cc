141b7ad0398120771a018c3585ef22911e09995d
Sam Hughes <sam@rethinkdb.com>
2011-01-28 17:52:46 -0800

    Removed everything in replication/messages.hpp, because that was horrible.

--- src/replication/masterstore.cc
+++ src/replication/masterstore.cc
@@ -1,19 +1,36 @@
 #include "masterstore.hpp"
 
+#include <boost/scoped_ptr.hpp>
+
+#include "replication/net_structs.hpp"
+#include "replication/protocol.hpp"
+
 namespace replication {
 
 void masterstore_t::add_slave(tcp_conn_t *conn) {
     if (slave_ != NULL) {
         throw masterstore_exc_t("We already have a slave.");
     }
+    slave_ = conn;
 }
 
 void masterstore_t::get_cas(store_key_t *key, castime_t castime) {
-    
+    size_t n = sizeof(headed<net_get_cas_t>) + key->size;
+    scoped_malloc<headed<net_get_cas_t> > message(n);
+    message->hdr.message_multipart_aspect = SMALL;
+    message->hdr.msgcode = GET_CAS;
+    message->data.proposed_cas = castime.proposed_cas;
+    message->data.timestamp = castime.timestamp;
+    memcpy(message->data.key, key->contents, key->size);
+
+    slave_->write_buffered(message.get(), n);
+}
+
+void masterstore_t::set(store_key_t *key, data_provider_t *data, mcflags_t flags, exptime_t exptime, castime_t castime) {
+
 
 
 }
-
 
 
 
--- src/replication/net_structs.hpp
+++ src/replication/net_structs.hpp
@@ -4,8 +4,7 @@
 #include <stdint.h>
 
 #include "btree/serializer_config_block.hpp"
-#include "btree/node.hpp"
-#include "btree/leaf_node.hpp"
+#include "btree/value.hpp"
 
 namespace replication {
 
@@ -14,7 +13,11 @@ enum multipart_aspect { SMALL = 0x81, FIRST = 0x82, MIDDLE = 0x83, LAST = 0x84 }
 enum message_code { BACKFILL = 0x01, ANNOUNCE = 0x02, NOP = 0x03, ACK = 0x04, SHUTTING_DOWN = 0x05,
                     GOODBYE = 0x06,
 
-                    SET = 0x21, APPEND = 0x22, PREPEND = 0x23, DELETE = 0x24 };
+                    GET_CAS = 0x21, SET = 0x22, APPEND = 0x23, PREPEND = 0x24, DELETE = 0x25 };
+
+enum role_enum_t { role_master = 0, role_new_slave = 1, role_slave = 2 };
+
+
 
 struct net_castime_t {
     cas_t proposed_cas;
@@ -32,114 +35,85 @@ struct net_hello_t {
 struct net_header_t {
     uint8_t message_multipart_aspect;
     uint8_t msgcode;
+    uint16_t msgsize;
+} __attribute__((__packed__));
+
+struct net_multipart_header_t {
+    net_header_t header;
+    uint32_t ident;
 } __attribute__((__packed__));
 
 struct net_backfill_t {
-    net_header_t header;
     repli_timestamp timestamp;
 } __attribute__((__packed__));
 
 struct net_announce_t {
-    net_header_t header;
     repli_timestamp from;
     repli_timestamp to;
 } __attribute__((__packed__));
 
 struct net_nop_t {
-    net_header_t header;
     repli_timestamp timestamp;
 } __attribute__((__packed__));
 
 struct net_ack_t {
-    net_header_t header;
     repli_timestamp timestamp;
 } __attribute__((__packed__));
 
 struct net_shutting_down_t {
-    net_header_t header;
     repli_timestamp timestamp;
 } __attribute__((__packed__));
 
 struct net_goodbye_t {
-    net_header_t header;
     repli_timestamp timestamp;
 } __attribute__((__packed__));
 
 
 struct net_get_cas_t {
-    net_header_t header;
-    net_castime_t castime;
-    char keydata[];
-
-    const btree_key *key() const { return reinterpret_cast<const btree_key *>(keydata); }
-} __attribute__((__packed__));
-
-struct net_small_set_t {
-    net_header_t header;
+    cas_t proposed_cas;
     repli_timestamp timestamp;
-    char btree_pair[];
-
-    const btree_leaf_pair *leaf_pair() const { return reinterpret_cast<const btree_leaf_pair *>(btree_pair); }
-    const btree_key *key() const { return &leaf_pair()->key; }
-    const btree_value *value() const { return leaf_pair()->value(); }
+    uint16_t key_size;
+    char key[];
 } __attribute__((__packed__));
 
-struct net_small_append_prepend_t {
-    net_header_t header;
-    uint16_t size;
+struct net_set_t {
     repli_timestamp timestamp;
-    char data[];
-
-    const btree_key *key() const { return reinterpret_cast<const btree_key *>(data); }
-    const byte *value_beg() const { return reinterpret_cast<const byte *>(data + key()->full_size()); }
-    const byte *value_end() const { return data + size; }
+    cas_t proposed_cas;
+    mcflags_t flags;
+    exptime_t exptime;
+    uint16_t key_size;
+    uint32_t value_size;
+    char keyvalue[];
 } __attribute__((__packed__));
 
-struct net_size_header_t {
-    net_header_t header;
-    uint16_t size;
-} __attribute__((__packed__));
-
-struct net_large_operation_first_t {
-    net_size_header_t header;
+struct net_append_t {
     repli_timestamp timestamp;
-    uint32_t identifier;
+    cas_t proposed_cas;
+    uint16_t key_size;
+    uint32_t value_size;
+
+    // The first key_size bytes are for the key, the next value_size
+    // bytes (possibly spanning multiple messages) are for the value.
+    char keyvaluedata[];
 } __attribute__((__packed__));
 
-struct net_large_operation_middle_t {
-    net_size_header_t header;
-    uint32_t identifier;
+struct net_prepend_t {
+    repli_timestamp timestamp;
+    cas_t proposed_cas;
+    uint16_t key_size;
+    uint32_t value_size;
+
+    // The first key_size bytes are for the key, the next value_size
+    // bytes (possibly spanning multiple messages) are for the value.
+    char keyvaluedata[];
 } __attribute__((__packed__));
 
-struct net_large_operation_last_t {
-    net_size_header_t header;
-    uint32_t identifier;
+
+template <class T>
+struct headed {
+    net_header_t hdr;
+    T data;
 } __attribute__((__packed__));
-
-struct net_large_set_t {
-    net_large_operation_first_t op_header;
-    metadata_flags_t metadata_flags;
-    char key_value[];
-
-    const btree_key *key() const { return reinterpret_cast<const btree_key *>(key_value); }
-    const char *metadata() const { return key_value + 1 + key()->size; }
-
-    // The size needed before we can call fitsize().
-    static size_t prefitsize() { return sizeof(net_large_set_t) + 1; }
-
-    // fitsize is the offset at which the value begins.
-    size_t fitsize() const { return sizeof(net_large_set_t) + 1 + key()->size + metadata_size(metadata_flags); }
-} __attribute__((__packed__));
-
-struct net_large_append_prepend_t {
-    net_large_operation_first_t op_header;
-    char data[];
-    const btree_key *key() const { return reinterpret_cast<const btree_key *>(data); }
-
-    static size_t prefitsize() { return sizeof(net_large_append_prepend_t) + 1; }
-    size_t fitsize() const { return sizeof(net_large_set_t) + 1 + key()->size; }
-} __attribute__((__packed__));
-
 
 }  // namespace replication
 
