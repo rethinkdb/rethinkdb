7ca338bf06df00d4066a9437c4020d33178e56bc
Sam Hughes <sam@rethinkdb.com>
2011-02-01 15:41:26 -0800

    Harmless off-by-one error in masterstore for message size cutoff.  (fixed).

--- src/replication/masterstore.cc
+++ src/replication/masterstore.cc
@@ -24,13 +24,16 @@ void masterstore_t::get_cas(store_key_t *key, castime_t castime) {
     message->data.timestamp = castime.timestamp;
     memcpy(message->data.key, key->contents, key->size);
 
-    slave_->write_buffered(message.get(), n);
+    {
+        mutex_acquisition_t lock(&message_contiguity_);
+        slave_->write_buffered(message.get(), n);
+    }
 }
 
 void masterstore_t::set(store_key_t *key, data_provider_t *data, mcflags_t flags, exptime_t exptime, castime_t castime) {
     size_t n = sizeof(headed<net_set_t>) + key->size + data->get_size();
 
-    if (n < 0xFFFF) {
+    if (n <= 0xFFFF) {
         scoped_malloc<headed<net_set_t> > message(n);
         message->hdr.message_multipart_aspect = SMALL;
         message->hdr.msgcode = SET;
