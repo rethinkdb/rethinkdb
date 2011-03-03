rethinkdb/rethinkdb

commit 295e2437b082df2cb20916b10ef7b3a499f153dc

Applied 4c1b60201319d06d3e30358a78e3867943de9399, messily, and in particular fixed semantic checking cache crap, and in particular did not apply broken large buf acquisition for delete code.

src/serializer/log/log_serializer.cc
@@ -316,6 +316,20 @@ struct ls_block_writer_t :
                 /* We tell the data_block_manager to write a zero block to
                    make recovery from a corrupted file more likely. */
 
+                // We write a zero buffer with the given block_id at the front, if necessary.
+                if (write.write_empty_deleted_block) {
+                    zerobuf = ser->malloc();
+                    bzero(zerobuf, ser->get_block_size().value());
+                    memcpy(zerobuf, &log_serializer_t::zerobuf_magic, sizeof(block_magic_t));
+
+                    off64_t new_offset;
+                    done = ser->data_block_manager->write(zerobuf, write.block_id, ser->current_transaction_id, &new_offset, this);
+                    ser->lba_index->set_block_offset(write.block_id, recency, flagged_off64_t::deleteblock(new_offset));
+                } else {
+                    done = true;
+                    // TODO this should not be flagged_off64_t::padding(), use a different constant.
+                    ser->lba_index->set_block_offset(write.block_id, recency, flagged_off64_t::padding());
+                }
                 // We write a zero buffer with the given block_id at the front.
                 zerobuf = ser->malloc();
                 bzero(zerobuf, ser->get_block_size().value());

src/btree/set.cc
@@ -103,6 +103,10 @@ struct btree_set_oper_t : public btree_modify_oper_t {
         }
     }
 
+    virtual void actually_acquire_large_value(large_buf_t *lb, const large_buf_ref *lbref) {
+        co_acquire_large_value_for_delete(lb, lbref, rwi_write);
+    }
+
     ticks_t start_time;
 
     data_provider_t *data;

src/buffer_cache/co_functions.cc
@@ -60,6 +60,11 @@ void co_acquire_large_value_rhs(large_buf_t *large_value, const large_buf_ref *r
     coro_t::wait();
 }
 
+void co_acquire_large_value_for_delete(large_buf_t *large_value, const large_buf_ref *root_ref_, access_t access_) {
+    large_value_acquired_t acquired;
+    large_value->acquire_for_delete(root_ref_, access_, &acquired);
+    coro_t::wait();
+}
 
 // Well this is repetitive.
 struct transaction_begun_callback_t : public transaction_begin_callback_t {

src/buffer_cache/co_functions.hpp
@@ -12,6 +12,7 @@ buf_t *co_acquire_block(transaction_t *transaction, block_id_t block_id, access_
 void co_acquire_large_value(large_buf_t *large_value, const large_buf_ref *root_ref_, access_t access_);
 void co_acquire_large_value_lhs(large_buf_t *large_value, const large_buf_ref *root_ref_, access_t access_);
 void co_acquire_large_value_rhs(large_buf_t *large_value, const large_buf_ref *root_ref_, access_t access_);
+void co_acquire_large_value_for_delete(large_buf_t *large_value, const large_buf_ref *root_ref_, access_t access_);
 
 // Avoid using this, use transactor_t instead.
 transaction_t *co_begin_transaction(cache_t *cache, access_t access);

src/buffer_cache/large_buf.cc
@@ -194,19 +194,21 @@ struct acquire_buftree_fsm_t : public block_available_callback_t, public tree_av
     buftree_t *tr;
     int index;
     large_buf_t *lb;
+    bool should_load_leaves;
 
     // When this becomes 0, we return and destroy.
     int life_counter;
 
-    acquire_buftree_fsm_t(large_buf_t *lb_, block_id_t block_id_, int64_t offset_, int64_t size_, int levels_, tree_available_callback_t *cb_, int index_)
-        : block_id(block_id_), offset(offset_), size(size_), levels(levels_), cb(cb_), tr(new buftree_t()), index(index_), lb(lb_) {
+    acquire_buftree_fsm_t(large_buf_t *lb_, block_id_t block_id_, int64_t offset_, int64_t size_, int levels_, tree_available_callback_t *cb_, int index_, bool should_load_leaves_ = true)
+        : block_id(block_id_), offset(offset_), size(size_), levels(levels_), cb(cb_), tr(new buftree_t()), index(index_), lb(lb_), should_load_leaves(should_load_leaves_) {
 #ifndef NDEBUG
         tr->level = levels_;
 #endif
 }
 
     void go() {
-        buf_t *buf = lb->transaction->acquire(block_id, lb->access, this);
+        bool should_load = should_load_leaves || levels != 1;
+        buf_t *buf = lb->transaction->acquire(block_id, lb->access, this, should_load);
         if (buf) {
             on_block_available(buf);
         }
@@ -343,6 +345,12 @@ void large_buf_t::acquire_lhs(const large_buf_ref *root_ref_, access_t access_, 
     int64_t beg = 0;
     int64_t end = std::min(int64_t(1), root_ref_->size);
     acquire_slice(root_ref_, access_, beg, end - beg, callback_);
+}
+
+void large_buf_t::acquire_for_delete(const large_buf_ref *root_ref_, access_t access_, large_buf_available_callback_t *callback_) {
+    // TODO: Insert the proper implementation for this.
+
+
 }
 
 void large_buf_t::buftree_acquired(buftree_t *tr, int index) {
@@ -594,7 +602,7 @@ std::vector<buftree_t *> large_buf_t::removes_level(const std::vector<buftree_t 
         ids[i] = node->kids[i];
     }
 
-    tr->buf->mark_deleted();
+    tr->buf->mark_deleted(false);
     tr->buf->release();
 #ifndef NDEBUG
     num_bufs--;
@@ -605,7 +613,7 @@ std::vector<buftree_t *> large_buf_t::removes_level(const std::vector<buftree_t 
 }
 
 buftree_t *large_buf_t::remove_level(buftree_t *tr, block_id_t id, block_id_t *idout) {
-    tr->buf->mark_deleted();
+    tr->buf->mark_deleted(false);
     tr->buf->release();
 #ifndef NDEBUG
     num_bufs--;
@@ -686,7 +694,7 @@ buftree_t *large_buf_t::walk_tree_structure(buftree_t *tr, int64_t offset, int64
 }
 
 void mark_deleted_and_release(large_buf_t *lb, buf_t *b) {
-    b->mark_deleted();
+    b->mark_deleted(false);
     b->release();
 #ifndef NDEBUG
     lb->num_bufs--;
@@ -694,7 +702,7 @@ void mark_deleted_and_release(large_buf_t *lb, buf_t *b) {
 }
 
 void mark_deleted_only(large_buf_t *lb, buf_t *b) {
-    b->mark_deleted();
+    b->mark_deleted(false);
 }
 void release_only(large_buf_t *lb, buf_t *b) {
     b->release();

src/buffer_cache/large_buf.hpp
@@ -82,6 +82,7 @@ public:
     void acquire(const large_buf_ref *root_ref_, access_t access_, large_buf_available_callback_t *callback_);
     void acquire_rhs(const large_buf_ref *root_ref_, access_t access_, large_buf_available_callback_t *callback_);
     void acquire_lhs(const large_buf_ref *root_ref_, access_t access_, large_buf_available_callback_t *callback_);
+    void acquire_for_delete(const large_buf_ref *root_ref_, access_t access_, large_buf_available_callback_t *callback_);
 
     // refsize_adjustment_out parameter forces callers to recognize
     // that the size may change, so hopefully they'll update their

src/buffer_cache/mirrored/mirrored.cc
@@ -67,19 +67,22 @@ struct load_buf_fsm_t :
 
 // This form of the buf constructor is used when the block exists on disk and needs to be loaded
 
-mc_inner_buf_t::mc_inner_buf_t(cache_t *cache, block_id_t block_id)
+mc_inner_buf_t::mc_inner_buf_t(cache_t *cache, block_id_t block_id, bool should_load)
     : cache(cache),
       block_id(block_id),
       data(cache->serializer->malloc()),
       next_patch_counter(1),
       refcount(0),
       do_delete(false),
+      write_empty_deleted_block(false),
       cow_will_be_needed(false),
       writeback_buf(this),
       page_repl_buf(this),
       page_map_buf(this),
       transaction_id(NULL_SER_TRANSACTION_ID) {
-    new load_buf_fsm_t(this);
+    if (should_load) {
+        new load_buf_fsm_t(this);
+    }
 
     pm_n_blocks_in_memory++;
     refcount++; // Make the refcount nonzero so this block won't be considered safe to unload.
@@ -96,6 +99,7 @@ mc_inner_buf_t::mc_inner_buf_t(cache_t *cache)
       next_patch_counter(1),
       refcount(0),
       do_delete(false),
+      write_empty_deleted_block(false),
       cow_will_be_needed(false),
       writeback_buf(this),
       page_repl_buf(this),
@@ -467,14 +471,14 @@ mc_buf_t *mc_transaction_t::allocate() {
 }
 
 mc_buf_t *mc_transaction_t::acquire(block_id_t block_id, access_t mode,
-                                    block_available_callback_t *callback) {
+                                    block_available_callback_t *callback, bool should_load) {
     rassert(mode == rwi_read || mode == rwi_read_outdated_ok || access != rwi_read);
     assert_thread();
        
     inner_buf_t *inner_buf = cache->page_map.find(block_id);
     if (!inner_buf) {
         /* The buf isn't in the cache and must be loaded from disk */
-        inner_buf = new inner_buf_t(cache, block_id);
+        inner_buf = new inner_buf_t(cache, block_id, should_load);
     }
 
     buf_t *buf = new buf_t(inner_buf, mode);
@@ -623,7 +627,7 @@ void mc_cache_t::init_patch_storage() {
     inner_buf_t *c_buf = page_map.find(MC_CONFIGBLOCK_ID);
     if (!c_buf) {
         /* The buf isn't in the cache and must be loaded from disk */
-        c_buf = new mc_inner_buf_t(this, MC_CONFIGBLOCK_ID);
+        c_buf = new mc_inner_buf_t(this, MC_CONFIGBLOCK_ID, true);
     }
     c_buf->lock.co_lock(rwi_read);
     mc_config_block_t *c = reinterpret_cast<mc_config_block_t *>(c_buf->data);

src/buffer_cache/mirrored/mirrored.hpp
@@ -72,6 +72,7 @@ class mc_inner_buf_t : public home_thread_mixin_t {
     
     /* true if we are being deleted */
     bool do_delete;
+    bool write_empty_deleted_block;
     
     /* true if there is a mc_buf_t that holds a pointer to the data in read-only outdated-OK
     mode. */
@@ -85,7 +86,7 @@ class mc_inner_buf_t : public home_thread_mixin_t {
     bool safe_to_unload();
     
     // Load an existing buf from disk
-    mc_inner_buf_t(cache_t *cache, block_id_t block_id);
+    mc_inner_buf_t(cache_t *cache, block_id_t block_id, bool should_load);
     
     // Create an entirely new buf
     explicit mc_inner_buf_t(cache_t *cache);
@@ -149,10 +150,11 @@ public:
         return inner_buf->block_id;
     }
     
-    void mark_deleted() {
+    void mark_deleted(bool write_null = true) {
         rassert(mode == rwi_write);
         rassert(!inner_buf->safe_to_unload());
         inner_buf->do_delete = true;
+        inner_buf->write_empty_deleted_block = write_null;
         inner_buf->writeback_buf.set_dirty();
         ensure_flush(); // Disable patch log system for the buffer
     }
@@ -193,7 +195,7 @@ public:
     bool commit(transaction_commit_callback_t *callback);
 
     buf_t *acquire(block_id_t block_id, access_t mode,
-                   block_available_callback_t *callback);
+                   block_available_callback_t *callback, bool should_load = true);
     buf_t *allocate();
     repli_timestamp get_subtree_recency(block_id_t block_id);
 

commit 18f92b974e9c77d72c4fa5060f7a2cac5e0667af

Fixes incomplete merge from 295e2437b082df2cb20916b10ef7b3a499f153dc, which caused segmentation faults when updating large buffers.

src/serializer/log/log_serializer.cc
@@ -330,14 +330,6 @@ struct ls_block_writer_t :
                     // TODO this should not be equal to flagged_off64_t::padding(), use a different constant.
                     ser->lba_index->set_block_offset(write.block_id, recency, flagged_off64_t::delete_id());
                 }
-                // We write a zero buffer with the given block_id at the front.
-                zerobuf = ser->malloc();
-                bzero(zerobuf, ser->get_block_size().value());
-                memcpy(zerobuf, &log_serializer_t::zerobuf_magic, sizeof(block_magic_t));
-
-                off64_t new_offset;
-                done = ser->data_block_manager->write(zerobuf, write.block_id, ser->current_transaction_id, &new_offset, this);
-                ser->lba_index->set_block_offset(write.block_id, recency, flagged_off64_t::deleteblock(new_offset));
             }
             if (done) {
                 return do_finish();