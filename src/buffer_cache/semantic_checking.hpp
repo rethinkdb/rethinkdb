956133fc15e209d0569d781b27bcf6e5cc27cf76
Daniel Mewes <daniel@rethinkdb.com>
2011-03-14 14:23:57 -0700

    Work around semantic checking problems when snapshots are used

--- src/buffer_cache/semantic_checking.hpp
+++ src/buffer_cache/semantic_checking.hpp
@@ -61,10 +61,11 @@ public:
 
 private:
     friend class scc_transaction_t<inner_cache_t>;
+    bool snapshotted;
     typename inner_cache_t::buf_t *inner_buf;
     void on_block_available(typename inner_cache_t::buf_t *buf);
     block_available_callback_t *available_cb;
-    explicit scc_buf_t(scc_cache_t<inner_cache_t> *);
+    explicit scc_buf_t(scc_cache_t<inner_cache_t> *, bool snapshotted);
     scc_cache_t<inner_cache_t> *cache;
 private:
     crc_t compute_crc() {
@@ -88,7 +89,11 @@ class scc_transaction_t :
     typedef scc_block_available_callback_t<inner_cache_t> block_available_callback_t;
 
 public:
-    void snapshot() { inner_transaction->snapshot(); }
+    // TODO: Implement semantic checking for snapshots!
+    void snapshot() {
+        snapshotted = true;
+        inner_transaction->snapshot();
+    }
     bool commit(transaction_commit_callback_t *callback);
 
     buf_t *acquire(block_id_t block_id, access_t mode,
@@ -99,6 +104,8 @@ public:
     scc_cache_t<inner_cache_t> *cache;
 
 private:
+    bool snapshotted; // Disables CRC checks
+
     friend class scc_cache_t<inner_cache_t>;
     scc_transaction_t(access_t, scc_cache_t<inner_cache_t> *);
     access_t access;
