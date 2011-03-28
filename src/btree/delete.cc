2b148f5e54f44b4fce9256c25eb63c8efa8e455a
Sam Hughes <sam@rethinkdb.com>
2011-03-28 08:31:20 -0700

    Added and used do_superblock_sidequest, we now apparently are putting stuff in the delete queue.

--- src/btree/delete.cc
+++ src/btree/delete.cc
@@ -1,6 +1,7 @@
 #include "btree/delete.hpp"
-
 #include "btree/modify_oper.hpp"
+
+#include "replication/delete_queue.hpp"
 
 struct btree_delete_oper_t : public btree_modify_oper_t {
 
@@ -19,6 +20,19 @@ struct btree_delete_oper_t : public btree_modify_oper_t {
 
     int compute_expected_change_count(UNUSED const size_t block_size) {
         return 1;
+    }
+
+    // You must have the btree superblock acquired when calling this
+    // function.
+    void do_superblock_sidequest(boost::shared_ptr<transactor_t>& txor,
+                                 UNUSED buf_lock_t& superblock,
+                                 repli_timestamp recency,
+                                 const store_key_t *key) {
+        slice->assert_thread();
+        // TODO: Consider just making the superblock store the delete
+        // queue block -- the treatment of transactional behavior is
+        // less error-prone that way.
+        replication::add_key_to_delete_queue(txor, DELETE_QUEUE_ID, recency, key);
     }
 };
 
--- src/btree/modify_oper.cc
+++ src/btree/modify_oper.cc
@@ -188,6 +188,8 @@ void run_btree_modify_oper(btree_modify_oper_t *oper, btree_slice_t *slice, cons
         boost::shared_ptr<transactor_t> txor(new transactor_t(saver, slice->cache(), rwi_write, oper->compute_expected_change_count(slice->cache()->get_block_size().value()),  castime.timestamp));
 
         buf_lock_t sb_buf(saver, *txor, SUPERBLOCK_ID, rwi_write);
+        oper->do_superblock_sidequest(txor, sb_buf, castime.timestamp, &store_key);
+
         buf_lock_t last_buf;
         buf_lock_t buf;
         get_root(saver, *txor, sb_buf, block_size, &buf, castime.timestamp);
