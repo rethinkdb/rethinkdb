9e361da44c8959791acee4f4fc55a79dca200de8
Ivan Tarasov <ivan@rethinkdb.com>
2011-03-28 17:10:43 -0700

    Add meaningful names to snapshots unit-tests

--- src/unittest/snapshots_test.cc
+++ src/unittest/snapshots_test.cc
@@ -112,7 +112,7 @@ private:
         transactor_t t1(saver, cache, rwi_read, 0, repli_timestamp::invalid);
     }
 
-    static void test1(thread_saver_t &saver, cache_t *cache) {
+    static void test_snapshot_acq_blocks_on_unfinished_create(thread_saver_t &saver, cache_t *cache) {
         // t0:create(A), t1:snap(), t1:acq(A) blocks, t0:release(A), t1 unblocks, t1 sees the block.
         transactor_t t0(saver, cache, rwi_write, 0, current_time());
         transactor_t t1(saver, cache, rwi_read, 0, repli_timestamp::invalid);
@@ -127,8 +127,8 @@ private:
             buf1->release();
     }
 
-    static void test2(thread_saver_t &saver, cache_t *cache) {
-        // t0:create(A), t0:release(A), t1:snap(), t2:acqw(A), t2:change(A), t2:release(A), t1:acq(A), t1 sees the change
+    static void test_snapshot_sees_changes_started_before_its_first_block_acq(thread_saver_t &saver, cache_t *cache) {
+        // t0:create+release(A), t1:snap(), t2:acqw(A), t2:change+release(A), t1:acq(A), t1 sees the change
         transactor_t t0(saver, cache, rwi_write, 0, current_time());
 
         block_id_t block_A, block_B;
@@ -148,8 +148,8 @@ private:
         buf1->release();
     }
 
-    static void test3(thread_saver_t &saver, cache_t *cache) {
-        // t0:create(A), t0:release(A), t1:snap(), t1:acq(A), t2:acqw(A) doesn't block, t2:change(A), t2:release(A), t3:snap(), t3:acq(A), t1 doesn't see the change, t3 does see the change
+    static void test_snapshot_doesnt_see_later_changes_and_doesnt_block_them(thread_saver_t &saver, cache_t *cache) {
+        // t0:create+release(A), t1:snap(), t1:acq(A), t2:acqw(A) doesn't block, t2:change+release(A), t3:snap(), t3:acq(A), t1 doesn't see the change, t3 does see the change
         transactor_t t0(saver, cache, rwi_write, 0, current_time());
 
         block_id_t block_A, block_B;
@@ -178,8 +178,8 @@ private:
         buf3->release();
     }
 
-    static void test4(thread_saver_t &saver, cache_t *cache) {
-        // t0:create(A,B), t0:release(A,B), t1:snap(), t1:acq(A), t2:acqw(A) doesn't block, t2:acqw(B), t1:acq(B) doesn't block
+    static void test_snapshot_doesnt_block_or_get_blocked_on_txns_that_acq_first_block_later(thread_saver_t &saver, cache_t *cache) {
+        // t0:create+release(A,B), t1:snap(), t1:acq(A), t2:acqw(A) doesn't block, t2:acqw(B), t1:acq(B) doesn't block
         transactor_t t0(saver, cache, rwi_write, 0, current_time());
 
         block_id_t block_A, UNUSED block_B;
@@ -206,8 +206,8 @@ private:
         buf2_B->release();
     }
 
-    static void test5(thread_saver_t &saver, cache_t *cache) {
-        // t0:create(A,B), t0:release(A,B), t1:acqw(A), t1:acqw(B), t1:release(A), t2:snap(), t2:acq(A), t2:release(A), t2:acq(B) blocks, t1:release(B), t2 unblocks
+    static void test_snapshot_blocks_on_txns_that_acq_first_block_earlier(thread_saver_t &saver, cache_t *cache) {
+        // t0:create+release(A,B), t1:acqw(A), t1:acqw(B), t1:release(A), t2:snap(), t2:acq+release(A), t2:acq(B) blocks, t1:release(B), t2 unblocks
         transactor_t t0(saver, cache, rwi_write, 0, current_time());
 
         block_id_t block_A, block_B;
@@ -236,7 +236,7 @@ private:
 
     static void test_issue_194(thread_saver_t &saver, cache_t *cache) {
         // issue 194 unit-test
-        // t0:create(A,B), t0:release(A,B), t1:acqw(A), t1:release(A), t2:acqw(A), t3:snap(), t3:acq(A) blocks, t2:release(A), t1:acqw(B), t1:release(B), t2:acqw(B) (fails with assertion if issue 194 is not fixed)
+        // t0:create+release(A,B), t1:acqw+release(A), t2:acqw(A), t3:snap(), t3:acq(A) blocks, t2:release(A), t1:acqw+release(B), t2:acqw(B) (fails with assertion if issue 194 is not fixed)
         transactor_t t0(saver, cache, rwi_write, 0, current_time());
 
         block_id_t block_A, block_B;
@@ -299,12 +299,12 @@ private:
             thread_saver_t saver;
 
             // It's nice to see the progress of these tests, so we use log_call
+            log_call(test_snapshot_acq_blocks_on_unfinished_create, saver, cache);
             log_call(test_snapshot_acq_blocks_on_older_write_txns, saver, cache);
-            log_call(test1, saver, cache);
-            log_call(test2, saver, cache);
-            log_call(test3, saver, cache);
-            log_call(test4, saver, cache);
-            log_call(test5, saver, cache);
+            log_call(test_snapshot_sees_changes_started_before_its_first_block_acq, saver, cache);
+            log_call(test_snapshot_doesnt_see_later_changes_and_doesnt_block_them, saver, cache);
+            log_call(test_snapshot_doesnt_block_or_get_blocked_on_txns_that_acq_first_block_later, saver, cache);
+            log_call(test_snapshot_blocks_on_txns_that_acq_first_block_earlier, saver, cache);
             //log_call(test_issue_194, saver, cache);
         }
         log_call(thread_pool.shutdown);
