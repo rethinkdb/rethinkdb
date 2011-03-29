252226f9b6d3e754ad7b157a99cb34ef16c8baf6
Tim Maxwell <tim@rethinkdb.com>
2011-03-29 13:03:27 -0700

    Closes #197. (Removes acquisition_cond_t from value_data_provider_t, and instead makes it only return once it has acquired the first round.)

--- test/regression/issue_197.py
+++ test/regression/issue_197.py
@@ -12,8 +12,11 @@ except:
     raise
 
 def test_function(opts, port, test_dir):
+
     model = stress.FuzzyModel(1)   # Every single operation will hit the same key
     key_generator = stress.SeedKeyGenerator()
+
+    print "Setting up connections..."
 
     read_client = stress.Client()
     read_connection = stress.Connection("localhost:%d" % port)
@@ -24,11 +27,16 @@ def test_function(opts, port, test_dir):
     write_op = stress.UpdateOp(key_generator, model.random_chooser(), None, write_connection, size=1000)
     write_client.add_op(1, write_op)
 
+    duration = 10
+    print "Running for %d seconds..." % duration
+
     read_client.start()
     write_client.start()
-    time.sleep(10)
+    time.sleep(duration)
     read_client.stop()
     write_client.stop()
+
+    print "Did %d inserts and %d reads." % (write_op.poll()["queries"], read_op.poll()["queries"])
 
 if __name__ == "__main__":
     op = make_option_parser()
