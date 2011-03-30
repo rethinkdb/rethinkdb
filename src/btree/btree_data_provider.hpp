404e7fdd99031dcbf54c51839a811592f877bd94
Tim Maxwell <tim@rethinkdb.com>
2011-03-30 13:03:32 -0700

    Made large_value_data_provider_t's destructor wait until acquire_in_background() finishes. Fixes a glitch in 252226f9b6d3e754ad7b157a99cb34ef16c8baf6.

--- src/btree/btree_data_provider.hpp
+++ src/btree/btree_data_provider.hpp
@@ -42,6 +42,7 @@ private:
 public:
     size_t get_size() const;
     const const_buffer_group_t *get_data_as_buffers() throw (data_provider_failed_exc_t);
+    ~large_value_data_provider_t();
 
 private:
     /* We hold a shared pointer to the transactor so the transaction does not end while
@@ -63,6 +64,7 @@ private:
     large_buf_ref_buffer_t lb_ref;
     large_buf_t large_value;
     cond_t large_value_cond;
+    bool have_value;
     void acquire_in_background(threadsafe_cond_t *acquisition_cond);
 
     friend class value_data_provider_t;
