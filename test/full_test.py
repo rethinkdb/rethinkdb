084e896c1e434d74ef5655247d1caa33ae51891c
Tim Maxwell <tim@rethinkdb.com>
2011-03-31 13:07:32 -0700

    Added issue 197 regression test to full test.

--- test/full_test.py
+++ test/full_test.py
@@ -383,6 +383,15 @@ def run_all_tests(mode, checker, protocol, cores, slices):
                     "slices"      : slices},
                   repeat=10, timeout=30 * ec2)
 
+    do_test_cloud("regression/issue_197.py",
+                  { "auto"        : True,
+                    "mode"        : mode,
+                    "no-valgrind" : not checker,
+                    "protocol"    : protocol,
+                    "cores"       : cores,
+                    "slices"      : slices},
+                  repeat=1, timeout=30 * ec2)
+
     do_test_cloud("integration/replication.py",
                   { "auto"        : True,
                     "mode"        : mode,
