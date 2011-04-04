5bc37db75befe280e5208fbbdd8eb1b91bb57217
RethinkDB <rethinkdb@Lex-Luthor.local>
2011-04-04 14:13:52 -0700

    Adds installation for build files.

--- scripts/VirtuaBuild/builder.py
+++ scripts/VirtuaBuild/builder.py
@@ -14,11 +14,11 @@ def help():
     print "                 Which branch in git to build off of."
 
 
-suse = vm_build.target('12c1cf78-5dc5-4baa-8e93-ac6fdd1ebf1f', '192.168.0.173', 'rethinkdb', 'make LEGACY_LINUX=1 NO_EVENTFD=1 DEBUG=0 rpm-suse10', 'rpm', (lambda x: "rpm -i %s" % x), "rpm -e rethinkdb")
-redhat5_1 = vm_build.target('5eaf8089-9ae4-4493-81fc-a885dc8e08ff', '192.168.0.159', 'rethinkdb', 'make rpm DEBUG=0 LEGACY_LINUX=1 NO_EVENTFD=1', 'rpm', (lambda x: "rpm -i %s" % x), "rpm -e rethinkdb")
-ubuntu = vm_build.target('b555d9f6-441f-4b00-986f-b94286d122e9', '192.168.0.172', 'rethinkdb', 'make deb DEBUG=0', 'deb', (lambda x: "rpm -i %s" % x), "rpm -e rethinkdb")
-debian = vm_build.target('3ba1350e-eda8-4166-90c1-714be0960724', '192.168.0.176', 'root', 'make deb DEBUG=0 NO_EVENTFD=1 LEGACY_LINUX=1', 'deb', None, None)
-centos5_5 = vm_build.target('46c6b842-b4ac-4cd6-9eae-fe98a7246ca9', '192.168.0.177', 'root', 'make rpm DEBUG=0 LEGACY_LINUX=1', 'rpm', None, None)
+suse = vm_build.target('12c1cf78-5dc5-4baa-8e93-ac6fdd1ebf1f', '192.168.0.173', 'rethinkdb', 'make LEGACY_LINUX=1 NO_EVENTFD=1 DEBUG=0 rpm-suse10', 'rpm', vm_build.rpm_install, vm_build.rpm_uninstall)
+redhat5_1 = vm_build.target('5eaf8089-9ae4-4493-81fc-a885dc8e08ff', '192.168.0.159', 'rethinkdb', 'make rpm DEBUG=0 LEGACY_LINUX=1 NO_EVENTFD=1', 'rpm', vm_build.rpm_install, vm_build.rpm_uninstall)
+ubuntu = vm_build.target('b555d9f6-441f-4b00-986f-b94286d122e9', '192.168.0.172', 'rethinkdb', 'make deb DEBUG=0', 'deb', vm_build.deb_install, vm_build.deb_uninstall)
+debian = vm_build.target('3ba1350e-eda8-4166-90c1-714be0960724', '192.168.0.176', 'root', 'make deb DEBUG=0 NO_EVENTFD=1 LEGACY_LINUX=1', 'deb', vm_build.deb_install, vm_build.deb_uninstall)
+centos5_5 = vm_build.target('46c6b842-b4ac-4cd6-9eae-fe98a7246ca9', '192.168.0.177', 'root', 'make rpm DEBUG=0 LEGACY_LINUX=1', 'rpm', vm_build.rpm_install, vm_build.rpm_uninstall)
 
 targets = {"suse" : suse, "redhat5_1" : redhat5_1, "ubuntu" : ubuntu, "debian" : debian, "centos5_5" : centos5_5}
 
@@ -38,8 +38,23 @@ if opts["help"]:
     help()
     sys.exit(0)
 
+
 if (opts["target"]):
-    targets[opts["target"]].run(opts["branch"], opts["target"])
-else:
-    for name,target in targets.iteritems():
+    targets = {opts["target"] : targets[opts["target"]]}
+
+success = {}
+exception = {}
+for name,target in targets.iteritems():
+    try:
         target.run(opts["branch"], name)
+        success[name] = True
+    except target.RunError as err:
+        success[name] = False
+        exception[name] = err
+
+print "Build summary:"
+from termcolor import colored
+for name,val in success.iteritems():
+    print name, "." * (20 - len(name)), colored("[Pass]", "green") if val else colored("[Fail]", "red")
+    if (not val):
+        print "Failed on: ", exception[name]
--- scripts/VirtuaBuild/vm_build.py
+++ scripts/VirtuaBuild/vm_build.py
@@ -7,6 +7,18 @@ def remove_local(string):
     else:
         return string
 
+def rpm_install(path):
+    return "rpm -i %s" % path
+
+def rpm_uninstall(pkg_name): 
+    return "rpm -e %s" % pkg_name
+
+def deb_install(path):
+    return "dpkg -i %s" % path
+
+def deb_uninstall(pkg_name): 
+    return "dpkg -r %s" % pkg_name
+
 class VM():
     def __init__(self, uuid, hostname, username = 'rethinkdb', rootname = 'root'):
         self.uuid = uuid
@@ -25,41 +37,55 @@ class VM():
     def command(self, cmd_str, root = False):
         #print "Got command:\n", cmd_str
         str = "ssh -o ConnectTimeout=1000 %s@%s \"%s\"" % ((self.rootname if root else self.username), self.hostname, cmd_str)
-        print str
+        #print str
         return os.system(str)
 
     def popen(self, cmd_str, mode):
-        print cmd_str
+        #print cmd_str
         return os.popen("ssh %s@%s \"%s\"" % (self.username, self.hostname, cmd_str), mode)
 
 class target():
-    def __init__(self, build_uuid, build_hostname, username, build_cl, res_ext, install_cl_f, uninstall_cl):
+    def __init__(self, build_uuid, build_hostname, username, build_cl, res_ext, install_cl_f, uninstall_cl_f):
         self.build_uuid = build_uuid 
         self.build_hostname = build_hostname 
         self.username = username
         self.build_cl = build_cl
         self.res_ext = res_ext 
         self.install_cl_f = install_cl_f # path -> install cmd
-        self.uninstall_cl = uninstall_cl
+        self.uninstall_cl_f = uninstall_cl_f
+
+    class RunError(Exception):
+        def __init__(self, cmd, ret_val):
+            self.cmd = cmd
+            self.ret_val = ret_val
+        def __str__(self):
+            return repr(self.cmd) + " return with value: " + repr(self.ret_val)
 
     def run(self, branch, short_name):
         if (not os.path.exists("Built_Packages")): 
             os.mkdir("Built_Packages")
         build_vm = VM(self.build_uuid, self.build_hostname, self.username)
 
-        build_vm.command("cd rethinkdb && git pull && git checkout %s" % branch)
-        build_vm.command("cd rethinkdb/src &&"+self.build_cl)
+        def run_checked(cmd, root = False):
+            res = build_vm.command(cmd, root)
+            if res != 0:
+                raise self.RunError(cmd, res)
 
+        run_checked("cd rethinkdb && git checkout %s && git pull" % branch)
+        run_checked("cd rethinkdb/src &&"+self.build_cl)
+
+        dir = build_vm.popen("pwd", 'r').readline().strip('\n')
         p = build_vm.popen("find rethinkdb/build/packages -regex .*\\\\\\\\.%s" % self.res_ext, 'r')
-        res_paths = map((lambda x: x.strip('\n')), p.readlines())
+        raw = p.readlines()
+        res_paths = map((lambda x: os.path.join(dir, x.strip('\n'))), raw)
         print res_paths
         dest = os.path.abspath("Built_Packages")
 
         for path in res_paths:
             if (not os.path.exists(os.path.join(dest, short_name))): 
                 os.mkdir(os.path.join(dest, short_name))
-            #build_vm.command(self.install_cl_f("~rethinkdb/"+path), True)
-            #build_vm.command(self.uninstall_cl, True)
+            run_checked(self.install_cl_f(path), True)
+            run_checked(self.uninstall_cl_f("rethinkdb"), True)
             scp_string = "scp %s@%s:~/%s %s" % (self.username, self.build_hostname, path, os.path.join(dest, short_name))
             print scp_string
             os.system(scp_string)
