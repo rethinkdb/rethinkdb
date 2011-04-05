36b66fb9deb917c9771c874100b65dc82edeee53
Sam Hughes <sam@rethinkdb.com>
2011-04-05 10:52:03 -0700

    Made big_values.py expose the append bug.

--- test/integration/big_values.py
+++ test/integration/big_values.py
@@ -22,71 +22,56 @@ def expect(s, string):
     if msg != string:
         raise ValueError("Didn't get what we expected: expected %s, got %s" % (abbreviate(string), abbreviate(msg)));
 
-def test_value_of_size(size, port):
+def expect_get_response(s, value):
+    expect(s, "VALUE x 0 %d\r\n" % len(value))
+    print " value decl...",
+    expect(s, value + "\r\n")
+    print " content...",
+    expect(s, "END\r\n")
+    print " ok."
 
-    print "Sending a %d-byte value..." % size,
-    sys.stdout.flush()
-
-    s = socket.socket()
-    s.connect(("localhost", port))
-
-    s.send("set x 0 0 %d\r\n" % size + "a" * size + "\r\n")
-    msg = readline(s)
-
-    if msg == "STORED\r\n":
-        print " getting...",
-        s.send("get x\r\n");
-        print " sent get...",
-        expect(s, "VALUE x 0 %d\r\n" % size)
-        print " value decl...",
-        expect(s, "a" * size + "\r\n")
-        print " content...",
-        expect(s, "END\r\n")
-        too_big = False
-        print "ok."
-
-    elif msg == "SERVER_ERROR object too large for cache\r\n":
-        print "too big."
-        too_big = True
-
-    else:
-        print
-        raise ValueError("Something went wrong; sent a value of %d bytes and got %r" % (size, msg))
-
-    s.send("quit")
-    s.close()
-
-    return too_big
-
-def test_values_near_size(size, port):
+def test_sizes(x, y, s):
+    print "Sending a %d-byte value..." % x,
+    s.send(("set x 0 0 %d\r\n" % x) + ("a" * x) + "\r\n")
 
     max_legal_value_size = 1024 * 1024
 
-    for s in xrange(size - 5, size + 5):
-        too_big = test_value_of_size(s, port)
-        assert too_big == (s > max_legal_value_size)
+    if x <= max_legal_value_size:
+        expect(s, "STORED\r\n")
+        print " getting...",
+        s.send("get x\r\n")
+        print " sent get...",
+        expect_get_response(s, "a" * x)
+
+        print "Appending upto length %d..." % y,
+        s.send(("append x 0 0 %d\r\n" % (y - x)) + ("b" * (y - x)) + "\r\n")
+
+        if y <= max_legal_value_size:
+            expect(s, "STORED\r\n")
+            print " getting...",
+            s.send("get x\r\n")
+            print " sent get...",
+            expect_get_response(s, "a" * x + "b" * (y - x))
+        else:
+            expect(s, "SERVER_ERROR object too large for cache\r\n")
+            print " too big... ok."
+    else:
+        expect(s, "SERVER_ERROR object too large for cache\r\n")
+        print " too big... ok."
 
 def test(opts, port, test_dir):
 
-    # threshold for storing in node vs. storing in large bug
-    test_values_near_size(250, port)
+    sizes = range(249, 252) + range(4083, 4086) + range(8167, 8170) + range(65534, 65538) + range(73709, 73712) + range((234 / 4) * 4084 - 2, (234 / 4) * 4084 + 2) + range(1048575, 1048578)
 
-    # threshold for buffering vs. streaming
-    test_values_near_size(10000, port)
+    s = socket.socket()
+    s.connect(("localhost", port))
+    for x in sizes:
+        for y in sizes:
+            if x < y:
+                test_sizes(x, y, s)
 
-    # uint16_t overflow.
-    test_values_near_size(2 ** 16, port)
-
-    # A common failure threshold when netrecord is turned on.
-    test_values_near_size(73710, port)
-
-    test_values_near_size(500000, port)
-
-    # test values that are close to the limit
-    test_values_near_size(1024 * 1024, port)
-
-    # test values that are way too big
-    test_values_near_size(1024 * 1024 + 100, port)
+    s.send("quit\r\n")
+    s.close()
 
 if __name__ == "__main__":
     op = make_option_parser()
