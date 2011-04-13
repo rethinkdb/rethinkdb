ffc5877ba459027fc39c615771e08e99b7a4e4e8
Daniel Mewes <daniel@rethinkdb.com>
2011-04-13 11:52:20 -0700

    According to Slava, this should not have found it's way into the repository in the first place.
    Revert "Slava's changes to smoketest."
    
    This reverts commit f8b8d58369b264f06ea31391e826ea4337eb38c7.

--- test/smoke_test.py
+++ test/smoke_test.py
@@ -2,7 +2,28 @@
 from cloud_retester import do_test, do_test_cloud, report_cloud, setup_testing_nodes, terminate_testing_nodes
 
 # Clean the repo
-do_test("cd ../src/")
+do_test("cd ../src/; make clean")
+
+# Make every target first, as some tests depend on multiple targets
+for mode in ["debug", "release"]:
+    # Build our targets
+    do_test("cd ../src/; nice make -j",
+            { "DEBUG"            : 1 if mode    == "debug"    else 0 },
+            cmd_format="make")
+
+for special in ["NO_EPOLL", "MOCK_IO_LAYER", "MOCK_CACHE_CHECK", "VALGRIND"]:
+    do_test("cd ../src/; nice make -j",
+            { "DEBUG" : 1,
+              special : 1},
+            cmd_format="make")
+
+# Make sure auxillary tools compile
+do_test("cd ../bench/stress-client/; make clean; make",
+        {},
+        cmd_format="make")
+do_test("cd ../bench/serializer-bench/; make clean; make",
+        {},
+        cmd_format="make")
 
 # For safety: ensure that nodes are terminated in case of exceptions
 try:
@@ -10,7 +31,7 @@ try:
     setup_testing_nodes()
 
     # Do quick smoke tests in some environments
-    for (mode, checker, protocol) in [("release", False, "text")]:#, ("debug", "valgrind", "text")]:#, ("release", False, "text")]:
+    for (mode, checker, protocol) in [("debug", "valgrind", "text")]:
         # VERY basic tests
         do_test_cloud("integration/append_prepend.py",
                 { "auto"        : True,
