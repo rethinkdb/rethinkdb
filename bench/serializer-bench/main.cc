ae49f3866c99c8ac29f45a8a71bb97653ec6b424
Sam Hughes <sam@samuelhughes.com>
2011-04-14 12:54:56 -0700

    Made serializer-bench compile, replacing its use of current_time().

--- bench/serializer-bench/main.cc
+++ bench/serializer-bench/main.cc
@@ -50,7 +50,8 @@ struct transaction_t :
         
         ser_block_id_t begin = ser_block_id_t::make(stress::random(0, ser->max_block_id().value - updates));
 
-        repli_timestamp tstamp = current_time();
+        // We just need some value for this.
+        repli_timestamp tstamp = repli_time(time(NULL));
         for (unsigned i = 0; i < updates; i++) {
             writes.push_back(serializer_t::write_t::make(ser_block_id_t::make(begin.value + i), tstamp, dummy_buf, true, NULL));
         }
