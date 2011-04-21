fa75513c5e9eb5d84fed4594395c3bf9f1a34a66
Sam Hughes <sam@rethinkdb.com>
2011-04-21 15:11:21 -0700

    Refactored backfilling in terms of btree_parallel_traversal.

--- src/btree/backfill.cc
+++ src/btree/backfill.cc
@@ -83,15 +83,20 @@ struct interesting_children_callback_t : public acquisition_start_callback_t {
 };
 
 struct btree_traversal_helper_t {
+    virtual void preprocess_btree_superblock(backfill_state_t *state, const btree_superblock_t *superblock) = 0;
+
     // This is free to call mark_deleted.
     virtual void process_a_leaf(backfill_state_t *state, buf_t *leaf_node_buf) = 0;
 
     virtual void postprocess_internal_node(buf_t *internal_node_buf) = 0;
+
     virtual void postprocess_btree_superblock(buf_t *superblock_buf) = 0;
 
     virtual void filter_interesting_children(backfill_state_t *state, const block_id_t *block_ids, int num_block_ids, interesting_children_callback_t *cb) = 0;
 
     virtual access_t transaction_mode() = 0;
+    virtual access_t btree_superblock_mode() = 0;
+    virtual access_t btree_node_mode() = 0;
 
     virtual ~btree_traversal_helper_t() { }
 };
@@ -106,15 +111,13 @@ protected:
 // TODO: Actually use shutdown_mode.
 class backfill_state_t {
 public:
-    backfill_state_t(const thread_saver_t& saver, btree_slice_t *_slice, repli_timestamp _since_when, btree_traversal_helper_t *_helper)
-        : slice(_slice), since_when(_since_when),
-          transactor_ptr(boost::make_shared<transactor_t>(saver, _slice->cache(), rwi_read, _since_when)),
+    backfill_state_t(const thread_saver_t& saver, btree_slice_t *_slice, repli_timestamp _transaction_tstamp, btree_traversal_helper_t *_helper)
+        : slice(_slice),
+          transactor_ptr(boost::make_shared<transactor_t>(saver, _slice->cache(), helper->transaction_mode(), _transaction_tstamp)),
           helper(_helper), shutdown_mode(false) { }
 
     // The slice we're backfilling from.
     btree_slice_t *const slice;
-    // The time from which we're backfilling.
-    repli_timestamp const since_when;
     boost::shared_ptr<transactor_t> transactor_ptr;
     // The callback which receives key/value pairs.
     boost::scoped_ptr<btree_traversal_helper_t> helper;
@@ -213,6 +216,13 @@ private:
 };
 
 struct backfill_traversal_helper_t : public btree_traversal_helper_t {
+
+    // The deletes have to go first (since they get overridden by
+    // newer sets)
+    void preprocess_btree_superblock(backfill_state_t *state, const btree_superblock_t *superblock) {
+        dump_keys_from_delete_queue(state->transactor_ptr, superblock->delete_queue_block, since_when_, callback_);
+    }
+
     void process_a_leaf(backfill_state_t *state, buf_t *leaf_node_buf) {
         const leaf_node_t *data = reinterpret_cast<const leaf_node_t *>(leaf_node_buf->get_data_read());
 
@@ -225,7 +235,7 @@ struct backfill_traversal_helper_t : public btree_traversal_helper_t {
             repli_timestamp recency = leaf::get_timestamp_value(data, offset);
             const btree_leaf_pair *pair = leaf::get_pair(data, offset);
 
-            if (recency.time >= state->since_when.time) {
+            if (recency.time >= since_when_.time) {
                 const btree_value *value = pair->value();
                 unique_ptr_t<value_data_provider_t> data_provider(value_data_provider_t::create(value, state->transactor_ptr));
                 backfill_atom_t atom;
@@ -248,6 +258,8 @@ struct backfill_traversal_helper_t : public btree_traversal_helper_t {
     }
 
     access_t transaction_mode() { return rwi_read; }
+    access_t btree_superblock_mode() { return rwi_read; }
+    access_t btree_node_mode() { return rwi_read; }
 
     struct annoying_t : public get_subtree_recencies_callback_t {
         interesting_children_callback_t *cb;
@@ -315,7 +327,7 @@ struct acquire_a_node_fsm_t : public acquisition_waiter_callback_t, public block
     void you_may_acquire() {
         state->level_count(level) += 1;
 
-        buf_t *buf = state->transactor_ptr->get()->acquire(block_id, rwi_read, this);
+        buf_t *buf = state->transactor_ptr->get()->acquire(block_id, state->helper->btree_node_mode(), this);
         // TODO: This is worthless crap.  We haven't started the
         // acquisition, we've FINISHED acquiring the buf because the
         // interface of acquire is a complete lie.  Thus we have worse
@@ -357,17 +369,6 @@ protected:
     ~parent_releaser_t() { }
 };
 
-struct superblock_releaser_t : public parent_releaser_t {
-    buf_t *buf_;
-    backfill_state_t *state_;
-    virtual void release() {
-        state_->helper->postprocess_btree_superblock(buf_);
-        buf_->release();
-        delete this;
-    }
-    superblock_releaser_t(buf_t *buf, backfill_state_t *state) : buf_(buf), state_(state) { }
-};
-
 struct internal_node_releaser_t : public parent_releaser_t {
     buf_t *buf_;
     backfill_state_t *state_;
@@ -379,84 +380,52 @@ struct internal_node_releaser_t : public parent_releaser_t {
     internal_node_releaser_t(buf_t *buf, backfill_state_t *state) : buf_(buf), state_(state) { }
 };
 
-void btree_backfill(btree_slice_t *slice, repli_timestamp since_when, backfill_callback_t *callback) {
+void btree_parallel_traversal(btree_slice_t *slice, repli_timestamp transaction_tstamp, backfill_traversal_helper_t *helper) {
     thread_saver_t saver;
-    backfill_traversal_helper_t helper(callback, since_when);
-    backfill_state_t state(saver, slice, since_when, &helper);
-    buf_lock_t superblock_buf(saver, *state.transactor_ptr, SUPERBLOCK_ID, rwi_read);
+    backfill_state_t state(saver, slice, transaction_tstamp, helper);
+    buf_lock_t superblock_buf(saver, *state.transactor_ptr, SUPERBLOCK_ID, helper->btree_superblock_mode());
 
     const btree_superblock_t *superblock = reinterpret_cast<const btree_superblock_t *>(superblock_buf->get_data_read());
 
-    // TODO: The slave could sort the deletes and newer sets out, but
-    // there are fairly few deletes.  We could (and should) release
-    // the superblock lock much sooner than we do right now.
-
-    // The deletes have to go first (since they get overridden by
-    // newer sets)
-    dump_keys_from_delete_queue(state.transactor_ptr, superblock->delete_queue_block, since_when, callback);
+    helper->preprocess_btree_superblock(&state, superblock);
 
     block_id_t root_id = superblock->root_block;
     rassert(root_id != SUPERBLOCK_ID);
 
+
+    struct : public parent_releaser_t {
+        buf_t *buf;
+        backfill_state_t *state;
+        void release() {
+            state->helper->postprocess_btree_superblock(buf);
+            buf->release();
+        }
+    } superblock_releaser;
+    superblock_releaser.buf = superblock_buf.give_up_ownership();
+    superblock_releaser.state = &state;
+
     if (root_id == NULL_BLOCK_ID) {
+        superblock_releaser.release();
         // No root, so no keys in this entire shard.
-        callback->done();
     } else {
         boost::scoped_array<block_id_t> roots(new block_id_t[1]);
         roots[0] = root_id;
         state.level_count(0) += 1;
         state.acquisition_waiter_stacks.resize(1);
-        subtrees_backfill(&state, new superblock_releaser_t(superblock_buf.give_up_ownership(), &state), 1, roots, 1);
+        subtrees_backfill(&state, &superblock_releaser, 1, roots, 1);
         state.wait();
-        callback->done();
     }
 }
 
-struct subtrees_backfill_fsm_t : public get_subtree_recencies_callback_t, public acquisition_start_callback_t {
-    backfill_state_t *state;
-    parent_releaser_t *releaser;
-    int level;
-    boost::scoped_array<block_id_t> block_ids;
-    int num_block_ids;
-    boost::scoped_array<repli_timestamp> recencies;
-    int acquisition_countdown;
+void btree_backfill(btree_slice_t *slice, repli_timestamp since_when, backfill_callback_t *callback) {
+    rassert(coro_t::self());
 
-    void got_subtree_recencies() {
-        coro_t::spawn_now(boost::bind(&subtrees_backfill_fsm_t::got_subtree_recencies_in_coro, this));
-    }
+    backfill_traversal_helper_t helper(callback, since_when);
 
-    void got_subtree_recencies_in_coro() {
-        rassert(coro_t::self());
-        acquisition_countdown = num_block_ids + 1;
+    btree_parallel_traversal(slice, repli_timestamp::invalid, &helper);
 
-        for (int i = 0; i < num_block_ids; ++i) {
-            if (recencies[i].time >= state->since_when.time) {
-                do_a_subtree_backfill(state, level, block_ids[i], this);
-            } else {
-                decr_acquisition_countdown();
-            }
-        }
-
-        decr_acquisition_countdown();
-    }
-
-    void on_started_acquisition() {
-        rassert(coro_t::self());
-        decr_acquisition_countdown();
-    }
-
-    void decr_acquisition_countdown() {
-        rassert(coro_t::self());
-        rassert(acquisition_countdown > 0);
-        -- acquisition_countdown;
-        if (acquisition_countdown == 0) {
-            releaser->release();
-            state->level_count(level - 1) -= 1;
-            state->consider_pulsing();
-            delete this;
-        }
-    }
-};
+    callback->done();
+}
 
 void interesting_children_callback_t::receive_interesting_children(boost::scoped_array<block_id_t>& block_ids, int num_block_ids) {
     acquisition_countdown = num_block_ids + 1;
