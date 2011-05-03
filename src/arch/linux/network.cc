rethinkdb/rethinkdb

commit 9899954ce8c5e83ff33daccacf7bbb533773e6db

Fixed a glitch where if the connection was closed while a write was blocking, the write would never end.

src/arch/linux/network.cc
@@ -351,6 +351,7 @@ void linux_tcp_conn_t::shutdown_write() {
 }
 
 void linux_tcp_conn_t::on_shutdown_write() {
+
     rassert(!write_was_shut_down);
     write_was_shut_down = true;
 
@@ -373,21 +374,26 @@ void linux_tcp_conn_t::on_event(int events) {
     /* This is called by linux_event_watcher_t when error events occur. Ordinary
     poll_event_in/poll_event_out events are not sent through this function. */
 
-    if ((events & poll_event_err) && (events & poll_event_hup)) {
+    if (events == (poll_event_err & poll_event_hup) && write_in_progress) {
         /* We get this when the socket is closed but there is still data we are trying to send.
         For example, it can sometimes be reproduced by sending "nonsense\r\n" and then sending
         "set [key] 0 0 [length] noreply\r\n[value]\r\n" a hundred times then immediately closing the
         socket.
-        
+
         I speculate that the "error" part comes from the fact that there is undelivered data
         in the socket send buffer, and the "hup" part comes from the fact that the remote end
         has hung up.
-        
-        Ignore it; the other logic will handle it properly. */
+
+        Is it possible for an equivalent problem to occur for reads? How would we detect it? */
+
+        on_shutdown_write();
 
     } else if (events & poll_event_err) {
         /* We don't know why we got this, so shut the hell down. */
-        logERR("Unexpected poll_event_err. Events: %d\n", events);
+        logERR("Unexpected poll_event_err. events=%s, read=%s, write=%s\n",
+            format_poll_event(events).c_str(),
+            read_in_progress ? "yes" : "no",
+            write_in_progress ? "yes" : "no");
         if (!read_was_shut_down) shutdown_read();
         if (!write_was_shut_down) shutdown_write();
     }

src/arch/linux/event_queue.cc
@@ -5,6 +5,31 @@
 
 perfmon_duration_sampler_t pm_eventloop("eventloop", secs_to_ticks(1.0));
 
+std::string format_poll_event(int event) {
+    std::string s;
+    if (event & poll_event_in) {
+        s = "in";
+    }
+    if (event & poll_event_out) {
+        if (s != "") s += " ";
+        s += "out";
+    }
+    if (event & poll_event_err) {
+        if (s != "") s += " ";
+        s += "err";
+    }
+    if (event & poll_event_hup) {
+        if (s != "") s += " ";
+        s += "hup";
+    }
+    if (event & poll_event_rdhup) {
+        if (s != "") s += " ";
+        s += "rdhup";
+    }
+    if (s == "") s = "(none)";
+    return s;
+}
+
 // TODO: I guess signum is unused because we aren't interested, and
 // uctx is unused because we don't have any context data.  Is this the
 // true?
@@ -109,7 +134,9 @@ struct linux_event_watcher_guts_t : public linux_event_callback_t {
 
         int error_mask = poll_event_err | poll_event_hup | poll_event_rdhup;
         guarantee((event & (error_mask | old_mask)) == event, "Unexpected event received (from operating system?).");
+
         if (event & error_mask) {
+
             error_handler->on_event(event & error_mask);
 
             /* The error handler might have cancelled some waits, which would

src/arch/linux/event_queue.hpp
@@ -34,6 +34,8 @@ const int poll_event_err = 4;
 const int poll_event_hup = 8;
 const int poll_event_rdhup = 16;
 
+std::string format_poll_event(int event);
+
 // Queue stats (declared here so whichever queue is chosen can access it)
 extern perfmon_duration_sampler_t pm_eventloop;
 

src/replication/master.hpp
@@ -104,7 +104,6 @@ public:
         delete stream_copy;
 
         stream_exists_cond_.pulse();    // If anything was waiting for stream to close, signal it
-        debugf("Pulsing interrupt_streaming_cond_\n");
         interrupt_streaming_cond_.pulse_if_non_null();   // Will interrupt any running backfill/stream operation
     }
 

commit 76cda5a1e6f342cf0c132d268b1c53d1d25cee7b

Fixes a small glitch in the network hangup handling introduced by 9899954ce8c5e83ff33d

src/arch/linux/network.cc
@@ -374,7 +374,7 @@ void linux_tcp_conn_t::on_event(int events) {
     /* This is called by linux_event_watcher_t when error events occur. Ordinary
     poll_event_in/poll_event_out events are not sent through this function. */
 
-    if (events == (poll_event_err & poll_event_hup) && write_in_progress) {
+    if (events == (poll_event_err | poll_event_hup) && write_in_progress) {
         /* We get this when the socket is closed but there is still data we are trying to send.
         For example, it can sometimes be reproduced by sending "nonsense\r\n" and then sending
         "set [key] 0 0 [length] noreply\r\n[value]\r\n" a hundred times then immediately closing the