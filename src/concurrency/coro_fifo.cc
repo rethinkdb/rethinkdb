17fac06dcb6008f082604f0c77b3d40bc91f21a3
Sam Hughes <sam@rethinkdb.com>
2011-05-03 19:37:03 -0700

    Renamed some stuff in coro_fifo.

--- src/concurrency/coro_fifo.cc
+++ src/concurrency/coro_fifo.cc
@@ -1,13 +1,13 @@
 #include "concurrency/coro_fifo.hpp"
 
-void coro_fifo_t::inform_ready_to_exit(coro_fifo_acq_t *acq) {
+void coro_fifo_t::inform_ready_to_leave(coro_fifo_acq_t *acq) {
     assert_thread();
 
-    acq->is_waiting_to_exit_ = true;
+    acq->asked_to_leave_ = true;
     coro_fifo_acq_t *head = acquisitor_queue_.head();
     rassert(head, "there must be a head node because somebody in the queue called this function");
-    while (head && head->is_waiting_to_exit_) {
-        head->ready_to_exit_.pulse();
+    while (head && head->asked_to_leave_) {
+        head->ready_to_leave_.pulse();
         acquisitor_queue_.remove(head);
     }
 }
