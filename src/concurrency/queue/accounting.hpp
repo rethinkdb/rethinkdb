5473c1523abce5b025156eeacbc0920a1ed67d80
Tim Maxwell <tim@rethinkdb.com>
2011-05-04 15:54:21 -0700

    Added watchable_t and passive_producer_t. Introduced some queues that use passive_producer_t, including accounting_queue_t. Made disk IO use the new paradigm.

---
+++ src/concurrency/queue/accounting.hpp
@@ -0,0 +1,104 @@
+#ifndef __CONCURRENCY_QUEUE_ACCOUNTING_HPP__
+#define __CONCURRENCY_QUEUE_ACCOUNTING_HPP__
+
+#include "concurrency/queue/passive_producer.hpp"
+#include "containers/intrusive_list.hpp"
+
+/* `accounting_queue_t` is useful when you have some number of actors competing
+for a shared resource, and you want them to be granted access to the resource in
+a fixed ratio. The `accounting_queue_t` is a `passive_producer_t` that draws
+from a number of other `passive_producer_t`s. To associate the
+`accounting_queue_t` with a `passive_producer_t`, construct an
+`accounting_queue_t::account_t` object. The `account_t` constructor takes a
+`share` parameter; the relative ratio of the `share` parameters of the different
+`account_t`s determines which `passive_producer_t`s the `accounting_queue_t`
+will `pop()` from when its own `pop()` method is called. When one of the sub-
+`passive_producer_t`s is not available, then it is ignored until it becomes
+available. */
+
+template<class value_t>
+struct accounting_queue_t : public passive_producer_t<value_t> {
+
+    accounting_queue_t() :
+        passive_producer_t<value_t>(&available_var),
+        total_shares(0),
+        selector(0) { }
+
+    ~accounting_queue_t() {
+        rassert(active_accounts.empty());
+        rassert(inactive_accounts.empty());
+    }
+
+    struct account_t :
+        public intrusive_list_node_t<account_t>,
+        private watchable_t<bool>::watcher_t
+    {
+        account_t(accounting_queue_t *p, passive_producer_t<value_t> *s, int shares) :
+            parent(p), source(s), shares(shares), active(false)
+        {
+            rassert(shares > 0);
+            source->available->add_watcher(this);
+            if (source->available->get()) activate();
+            else parent->inactive_accounts.push_back(this);
+        }
+        ~account_t() {
+            if (active) deactivate();
+            else parent->inactive_accounts.remove(this);
+            source->available->remove_watcher(this);
+        }
+
+    private:
+        friend class accounting_queue_t;
+
+        void on_watchable_changed() {
+            if (source->available->get() && !active) {
+                parent->inactive_accounts.remove(this);
+                activate();
+            } else if (!source->available->get() && active) {
+                deactivate();
+                parent->inactive_accounts.push_back(this);
+            }
+        }
+
+        void activate() {
+            active = true;
+            parent->active_accounts.push_back(this);
+            parent->total_shares += shares;
+            parent->available_var.set(true);
+        }
+        void deactivate() {
+            active = false;
+            parent->active_accounts.remove(this);
+            parent->total_shares -= shares;
+            if (parent->active_accounts.empty()) parent->available_var.set(false);
+        }
+
+        accounting_queue_t *parent;
+        passive_producer_t<value_t> *source;
+        int shares;
+        bool active;
+    };
+
+private:
+    friend class account_t;
+
+    intrusive_list_t<account_t> active_accounts, inactive_accounts;
+
+    int total_shares, selector;
+
+    watchable_var_t<bool> available_var;
+    value_t produce_next_value() {
+        selector %= total_shares;
+        typename intrusive_list_t<account_t>::iterator it = active_accounts.begin();
+        int count = 0;
+        while (true) {
+            count += (*it).shares;
+            if (count > selector) break;
+            it++;
+        }
+        selector++;
+        return (*it).source->pop();
+    }
+};
+
+#endif /* __CONCURRENCY_QUEUE_ACCOUNTING_HPP__ */
---
+++ src/concurrency/queue/passive_producer.hpp
@@ -0,0 +1,44 @@
+#ifndef __CONCURRENCY_QUEUE_PASSIVE_PRODUCER_HPP__
+#define __CONCURRENCY_QUEUE_PASSIVE_PRODUCER_HPP__
+
+#include "concurrency/watchable.hpp"
+
+/* A passive producer is an object (often a queue) that a consumer can read from
+by calling a method. It's called "passive" because the consumer calls a method
+on the producer to ask for data, rather than the producer calling a method on the
+consumer to give it data.
+
+In general you must create the passive producer before you create the consumer,
+and destroy the consumer before you destroy the passive producer.
+
+`passive_producer_t` is not thread-safe. */
+
+template<class value_t>
+struct passive_producer_t {
+
+    /* true if any data is available. `available` is const so that nobody
+    screws around with it. */
+    watchable_t<bool> * const available;
+
+    /* `pop()` removes a value from the producer and returns it. It is
+    an error to call `pop()` when `available->get()` is `false`. */
+    value_t pop() {
+        rassert(available->get());
+        return produce_next_value();
+    }
+
+protected:
+    /* To implement a passive producer: Pass a `watchable_t<bool>*` to the
+    constructor; the current value of this `watchable_t<bool>` will be taken
+    to indicate whether data is available. Override `produce_next_value()`;
+    `produce_next_value()` will only be called when the `watchable_t<bool>` is
+    true, but it must never fail or block. */
+    passive_producer_t(watchable_t<bool> *w) : available(w) { }
+    virtual value_t produce_next_value() = 0;
+    virtual ~passive_producer_t() { };
+
+private:
+    DISABLE_COPYING(passive_producer_t);
+};
+
+#endif /* __CONCURRENCY_QUEUE_PASSIVE_PRODUCER_HPP__ */
