08e21f498cf27c94240addd9c029394661482458
Tim Maxwell <tim@rethinkdb.com>
2011-05-05 17:00:50 -0700

    Made backfill_in use a coro pool and queues so that it can pipeline operations.

--- src/concurrency/queue/limited_fifo.hpp
+++ src/concurrency/queue/limited_fifo.hpp
@@ -7,19 +7,20 @@
 
 /* `limited_fifo_queue_t` is a first-in, first-out queue that has a limited depth. If the
 consumer is not reading of the queue as fast as the producer is pushing things onto the
-queue, then `push()` will start to block. */
+queue, then `push()` will start to block.
+
+The `capacity` and `trickle_fraction` arguments, and the `set_capacity()` method, work as
+in `adjustable_semaphore_t`. */
 
 template<class value_t, class queue_t = std::list<value_t> >
 struct limited_fifo_queue_t :
     public home_thread_mixin_t,
     public passive_producer_t<value_t>
 {
-    limited_fifo_queue_t(int depth) :
+    limited_fifo_queue_t(int capacity, float trickle_fraction = 0.0) :
         passive_producer_t<value_t>(&available_var),
-        semaphore(depth)
-    {
-        rassert(depth > 0);
-    }
+        semaphore(capacity, trickle_fraction)
+        { }
 
     void push(const value_t &value) {
         on_thread_t thread_switcher(home_thread);
@@ -28,8 +29,12 @@ struct limited_fifo_queue_t :
         available_var.set(!queue.empty());
     }
 
+    void set_capacity(int capacity) {
+        semaphore.set_capacity(capacity);
+    }
+
 private:
-    semaphore_t semaphore;
+    adjustable_semaphore_t semaphore;
     watchable_var_t<bool> available_var;
     value_t produce_next_value() {
         assert_thread();
