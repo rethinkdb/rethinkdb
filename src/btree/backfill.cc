4f0d7416fdd227dc055c7d3059b4b2f772266d93
Sam Hughes <sam@rethinkdb.com>
2011-05-09 17:26:01 -0700

    Made backfilling snapshot.

--- src/btree/backfill.cc
+++ src/btree/backfill.cc
@@ -127,6 +127,8 @@ void btree_backfill(btree_slice_t *slice, repli_timestamp since_when, backfill_c
         thread_saver_t saver;
         boost::shared_ptr<transactor_t> txor = boost::make_shared<transactor_t>(saver, slice->cache(), helper.transaction_mode(), 0, repli_timestamp::invalid);
 
+        txor->get()->snapshot();
+
         btree_parallel_traversal(txor, slice, &helper);
     }
 
