13448774e8c18f591708bfa1788ffa09de1eb7ff
Sam Hughes <sam@rethinkdb.com>
2011-05-11 17:08:07 -0700

    Added order_source_pigeoncoop_t, but we are not using it anywhere.

--- src/concurrency/fifo_checker.hpp
+++ src/concurrency/fifo_checker.hpp
@@ -3,6 +3,9 @@
 
 #include <vector>
 
+#include <boost/function.hpp>
+
+#include "arch/arch.hpp"
 #include "utils2.hpp"
 
 // The memcached order_source_t and the backfill_receiver_t will want
@@ -30,15 +33,61 @@ private:
     friend class contiguous_order_sink_t;
 };
 
+class order_source_pigeoncoop_t {
+public:
+    order_source_pigeoncoop_t(int starting_bucket = 0)
+        : least_unregistered_bucket_(starting_bucket) { }
+
+    friend class order_source_t;
+private:
+    static void nop() { }
+
+    void unregister_bucket(int bucket) {
+        ASSERT_NO_CORO_WAITING;
+        rassert(bucket < least_unregistered_bucket_);
+        free_buckets_.push_back(bucket);
+    }
+
+    int register_for_bucket() {
+        ASSERT_NO_CORO_WAITING;
+        if (free_buckets_.empty()) {
+            int ret = least_unregistered_bucket_;
+            ++ least_unregistered_bucket_;
+            return ret;
+        } else {
+            int ret = free_buckets_.back();
+            rassert(ret < least_unregistered_bucket_);
+            free_buckets_.pop_back();
+            return ret;
+        }
+    }
+
+    // The bucket we should use next, if free_buckets_ is empty.
+    int least_unregistered_bucket_;
+
+    // The buckets less than least_unregistered_bucket_.
+    std::vector<int> free_buckets_;
+
+    DISABLE_COPYING(order_source_pigeoncoop_t);
+};
+
 class order_source_t {
 public:
-    order_source_t(int bucket = 0) : bucket_(bucket), counter_(0) { }
+    order_source_t(int bucket = 0, boost::function<void()> unregisterator = order_source_pigeoncoop_t::nop)
+        : bucket_(bucket), counter_(0), unregister_(unregisterator) { }
+
+    order_source_t(order_source_pigeoncoop_t *coop)
+        : bucket_(coop->register_for_bucket()), counter_(0),
+          unregister_(boost::bind(&order_source_pigeoncoop_t::unregister_bucket, coop, bucket_)) { }
+
+    ~order_source_t() { unregister_(); }
 
     order_token_t check_in() { return order_token_t(bucket_, ++counter_); }
 
 private:
     int bucket_;
     int64_t counter_;
+    boost::function<void ()> unregister_;
 
     DISABLE_COPYING(order_source_t);
 };
@@ -56,7 +105,8 @@ public:
 
             // We tolerate equality in this comparison because it can
             // be used to ensure that multiple actions don't get
-            // interrupted.
+            // interrupted.  And resending the same action isn't
+            // normally a problem.
             rassert(token.value_ >= last_seens_[token.bucket_], "token.value_ = %ld, last_seens_[token.bucket_] = %ld", token.value_, last_seens_[token.bucket_]);
             last_seens_[token.bucket_] = token.value_;
         }
