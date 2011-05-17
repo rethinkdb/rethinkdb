e82be26560593822e7744c94aed0ff458655370b
Sam Hughes <sam@rethinkdb.com>
2011-05-16 17:58:45 -0700

    Moved implementations of order_foo_t's from fifo_checker.hpp to fifo_checker.cc.

--- src/concurrency/fifo_checker.hpp
+++ src/concurrency/fifo_checker.hpp
@@ -23,19 +23,16 @@ public:
 
     // By default we construct a totally invalid order token, not
     // equal to ignore, that must be initialized.
-    order_token_t() : bucket_(-2), value_(-2) { }
+    order_token_t();
 
-    order_token_t with_read_mode() {
-        return order_token_t(bucket_, value_, true);
-    }
+    order_token_t with_read_mode() const;
 
-    int bucket() const { return bucket_; }
-    bool read_mode() const { return read_mode_; }
-    int64_t value() const { return value_; }
+    int bucket() const;
+    bool read_mode() const;
+    int64_t value() const;
 
 private:
-    explicit order_token_t(int bucket, int64_t x, bool read_mode)
-        : bucket_(bucket), read_mode_(read_mode), value_(x) { }
+    order_token_t(int bucket, int64_t x, bool read_mode);
     int bucket_;
     bool read_mode_;
     int64_t value_;
@@ -48,33 +45,15 @@ private:
 
 class order_source_pigeoncoop_t {
 public:
-    order_source_pigeoncoop_t(int starting_bucket = 0)
-        : least_unregistered_bucket_(starting_bucket) { }
+    order_source_pigeoncoop_t(int starting_bucket = 0);
 
     friend class order_source_t;
 private:
-    static void nop() { }
+    static void nop();
 
-    void unregister_bucket(int bucket, int64_t counter) {
-        ASSERT_NO_CORO_WAITING;
-        rassert(bucket < least_unregistered_bucket_);
-        free_buckets_.push_back(std::make_pair(bucket, counter));
-    }
+    void unregister_bucket(int bucket, int64_t counter);
 
-    std::pair<int, int64_t> register_for_bucket() {
-        ASSERT_NO_CORO_WAITING;
-        if (free_buckets_.empty()) {
-            int ret = least_unregistered_bucket_;
-            ++ least_unregistered_bucket_;
-            return std::pair<int, int64_t>(ret, 0);
-        } else {
-            std::pair<int, int64_t> ret = free_buckets_.back();
-            rassert(ret.first < least_unregistered_bucket_);
-            rassert(ret.second >= 0);
-            free_buckets_.pop_back();
-            return ret;
-        }
-    }
+    std::pair<int, int64_t> register_for_bucket();
 
     // The bucket we should use next, if free_buckets_ is empty.
     int least_unregistered_bucket_;
@@ -87,21 +66,11 @@ private:
 
 class order_source_t {
 public:
-    order_source_t(int bucket = 0, boost::function<void()> unregisterator = order_source_pigeoncoop_t::nop)
-        : bucket_(bucket), counter_(0), unregister_(unregisterator) { }
+    order_source_t(int bucket = 0, boost::function<void()> unregisterator = order_source_pigeoncoop_t::nop);
+    order_source_t(order_source_pigeoncoop_t *coop);
+    ~order_source_t();
 
-    order_source_t(order_source_pigeoncoop_t *coop) : bucket_(-2), counter_(-2) {
-        std::pair<int, int64_t> p = coop->register_for_bucket();
-        bucket_ = p.first;
-        counter_ = p.second;
-        unregister_ = boost::bind(&order_source_pigeoncoop_t::unregister_bucket, coop, bucket_, boost::ref(counter_));
-    }
-
-    ~order_source_t() { unregister_(); }
-
-    order_token_t check_in() { return order_token_t(bucket_, ++counter_, false); }
-
-    order_token_t check_in_read_mode() { return order_token_t(bucket_, ++counter_, true); }
+    order_token_t check_in();
 
 private:
     int bucket_;
@@ -113,35 +82,14 @@ private:
 
 class order_sink_t {
 public:
-    order_sink_t() { }
+    order_sink_t();
 
-    void check_out(order_token_t token) {
-        if (token.bucket_ != order_token_t::ignore.bucket_) {
-            rassert(token.bucket_ >= 0);
-            if (token.bucket_ >= int(last_seens_.size())) {
-                last_seens_.resize(token.bucket_ + 1, std::pair<int64_t, int64_t>(0, 0));
-            }
-
-            verify_token_value_and_update(token, &last_seens_[token.bucket_]);
-        }
-    }
+    void check_out(order_token_t token);
 
 private:
 
     friend class plain_sink_t;
-    static void verify_token_value_and_update(order_token_t token, std::pair<int64_t, int64_t> *ls_pair) {
-        // We tolerate equality in this comparison because it can
-        // be used to ensure that multiple actions don't get
-        // interrupted.  And resending the same action isn't
-        // normally a problem.
-        if (token.read_mode_) {
-            rassert(token.value_ >= ls_pair->first, "token.value_ = %ld, last_seens_[token.bucket_].first = %ld, token.bucket_ = %d", token.value_, ls_pair->first, token.bucket_);
-            ls_pair->second = std::max(ls_pair->second, token.value_);
-        } else {
-            rassert(token.value_ >= ls_pair->second, "token.value_ = %ld, last_seens_[token.bucket_].second = %ld, token.bucket_ = %d", token.value_, ls_pair->second, token.bucket_);
-            ls_pair->first = ls_pair->second = token.value_;
-        }
-    }
+    static void verify_token_value_and_update(order_token_t token, std::pair<int64_t, int64_t> *ls_pair);
 
     // .first = last seen write, .second = max(last seen read, last seen write)
     std::vector<std::pair<int64_t, int64_t> > last_seens_;
@@ -153,17 +101,9 @@ private:
 // only one source (like the top of a btree slice) and many sinks.
 class plain_sink_t {
 public:
-    plain_sink_t() : ls_pair_(0, 0) { }
+    plain_sink_t();
 
-    void check_out(order_token_t token) {
-        if (token.bucket_ != order_token_t::ignore.bucket_) {
-            // TODO: do read/write mode.
-            rassert(token.bucket_ >= 0);
-            rassert(token.bucket_ == 0, "Only bucket 0 allowed, you made a programmer error");
-
-            order_sink_t::verify_token_value_and_update(token, &ls_pair_);
-        }
-    }
+    void check_out(order_token_t token);
 
 private:
     std::pair<int64_t, int64_t> ls_pair_;
