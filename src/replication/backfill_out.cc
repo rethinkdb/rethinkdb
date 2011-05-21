5e48f7702dcec8f67f9239f561ecb7470eaf7869
timmaxw <tim@rethinkdb.com>
2011-05-20 20:07:57 -0700

    Moved the call to set_replication_clock() to before we call register_on_slice().

--- src/replication/backfill_out.cc
+++ src/replication/backfill_out.cc
@@ -358,12 +358,17 @@ struct backfill_and_streaming_manager_t :
         replication_clock_ = initial_replication_clock_.next();
         outstanding_backfills = internal_store_->btree_static_config.n_slices;
 
+        /* Store the current state of the replication clock. We must write it back to
+        the file before we run `register_on_slice()`, because after `register_on_slice()`
+        has been run operations can be spawned with the new timestmap. If an operation
+        happened with the new timestamp and that operation was written to disk but we
+        crashed before the new value of the replication clock could be written to disk,
+        then the database could behave incorrectly when it started back up. */
+        internal_store_->set_replication_clock(replication_clock_);
+
         pmap(internal_store_->btree_static_config.n_slices,
              boost::bind(&backfill_and_streaming_manager_t::register_on_slice, this,
                          _1, backfill_from, replication_clock_));
-
-        /* Store the current state of the replication clock */
-        internal_store_->set_replication_clock(replication_clock_);
 
         /* Start the timer that will repeatedly increment the replication clock */
         replication_clock_timer_.reset(new repeating_timer_t(1000,
