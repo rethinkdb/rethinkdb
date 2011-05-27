7b1b1d9843449924238758b9b935092e9fef1eab
Sam Hughes <sam@rethinkdb.com>
2011-05-27 15:45:14 -0700

    Made fifo checker code minimal in release mode given that it must follow the same interface.

--- src/concurrency/fifo_checker.hpp
+++ src/concurrency/fifo_checker.hpp
@@ -33,6 +33,7 @@ class order_token_t {
 public:
     static const order_token_t ignore;
 
+#ifndef NDEBUG
     // By default we construct a totally invalid order token, not
     // equal to ignore, that must be initialized.
     order_token_t();
@@ -42,12 +43,23 @@ public:
     int bucket() const;
     bool read_mode() const;
     int64_t value() const;
+#else
+    order_token_t() { }
+    order_token_t with_read_mode() const { return order_token_t(); }
+
+    int bucket() const { return 0; }
+    bool read_mode() const { return true; }
+    int64_t value() const { return 0; }
+#endif  // ifndef NDEBUG
 
 private:
+
+#ifndef NDEBUG
     order_token_t(int bucket, int64_t x, bool read_mode);
     int bucket_;
     bool read_mode_;
     int64_t value_;
+#endif  // ifndef NDEBUG
 
     friend class order_source_t;
     friend class order_sink_t;
@@ -63,12 +75,18 @@ private:
    mean order source, that next gets assigned a given bucket. */
 class order_source_pigeoncoop_t {
 public:
+#ifndef NDEBUG
     order_source_pigeoncoop_t(int starting_bucket = 0);
+#else
+    order_source_pigeoncoop_t(UNUSED int starting_bucket = 0) { }
+#endif
 
     friend class order_source_t;
 private:
-    static void nop();
 
+    static void nop() { }
+
+#ifndef NDEBUG
     void unregister_bucket(int bucket, int64_t counter);
 
     std::pair<int, int64_t> register_for_bucket();
@@ -78,6 +96,7 @@ private:
 
     // The buckets less than least_unregistered_bucket_ that are free.
     std::vector<std::pair<int, int64_t> > free_buckets_;
+#endif
 
     DISABLE_COPYING(order_source_pigeoncoop_t);
 };
@@ -88,16 +107,26 @@ private:
    available for reuse. */
 class order_source_t {
 public:
+#ifndef NDEBUG
     order_source_t(int bucket = 0, boost::function<void()> unregisterator = order_source_pigeoncoop_t::nop);
     order_source_t(order_source_pigeoncoop_t *coop);
     ~order_source_t();
 
     order_token_t check_in();
+#else
+    order_source_t(UNUSED int bucket = 0, UNUSED boost::function<void()> unregisterator = order_source_pigeoncoop_t::nop) { }
+    order_source_t(UNUSED order_source_pigeoncoop_t *coop) { }
+    ~order_source_t() { }
+
+    order_token_t check_in() { return order_token_t(); }
+#endif  // ndef NDEBUG
 
 private:
+#ifndef NDEBUG
     int bucket_;
     int64_t counter_;
     boost::function<void ()> unregister_;
+#endif  // ifndef NDEBUG
 
     DISABLE_COPYING(order_source_t);
 };
@@ -112,6 +141,7 @@ private:
  */
 class backfill_receiver_order_source_t {
 public:
+#ifndef NDEBUG
     backfill_receiver_order_source_t(int bucket = BACKFILL_RECEIVER_ORDER_SOURCE_BUCKET);
 
     void backfill_begun();
@@ -119,11 +149,24 @@ public:
 
     order_token_t check_in_backfill_operation();
     order_token_t check_in_realtime_operation();
+#else
+    backfill_receiver_order_source_t(UNUSED int bucket = BACKFILL_RECEIVER_ORDER_SOURCE_BUCKET) { }
+
+    void backfill_begun() { }
+    void backfill_done() { }
+
+    order_token_t check_in_backfill_operation() { return order_token_t(); }
+    order_token_t check_in_realtime_operation() { return order_token_t(); }
+
+#endif  // ifndef NDEBUG
 
 private:
+
+#ifndef NDEBUG
     int bucket_;
     int64_t counter_;
     bool backfill_active_;
+#endif  // ifndef NDEBUG
 
     DISABLE_COPYING(backfill_receiver_order_source_t);
 };
@@ -132,18 +175,27 @@ private:
    bucket had better arrive in the right order. */
 class order_sink_t {
 public:
+#ifndef NDEBUG
     order_sink_t();
 
     void check_out(order_token_t token);
+#else
+    order_sink_t() { }
+
+    void check_out(UNUSED order_token_t token) { }
+
+#endif  // ifndef NDEBUG
 
 private:
 
+#ifndef NDEBUG
     friend class plain_sink_t;
     static void verify_token_value_and_update(order_token_t token, std::pair<int64_t, int64_t> *ls_pair);
 
     // We keep two last seen values because reads can be reordered.
     // .first = last seen write, .second = max(last seen read, last seen write)
     std::vector<std::pair<int64_t, int64_t> > last_seens_;
+#endif  // ifndef NDEBUG
 
     DISABLE_COPYING(order_sink_t);
 };
@@ -153,13 +205,21 @@ private:
 // there's one bucket there's no point in instantiating a std::vector.
 class plain_sink_t {
 public:
+#ifndef NDEBUG
     plain_sink_t();
 
     void check_out(order_token_t token);
+#else
+    plain_sink_t() { }
+
+    void check_out(UNUSED order_token_t token) { }
+#endif  // ifndef NDEBUG
 
 private:
+#ifndef NDEBUG
     // The pair of last seen values.
     std::pair<int64_t, int64_t> ls_pair_;
+#endif
 
     DISABLE_COPYING(plain_sink_t);
 };
