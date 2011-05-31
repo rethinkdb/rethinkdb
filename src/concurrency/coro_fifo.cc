9ee1acc6e916f20cbeb3dcf3eca4bd20e4bcabb5
Sam Hughes <sam@rethinkdb.com>
2011-05-31 16:29:41 -0700

    Fixed bug in coro_fifo_t::inform_ready_to_leave where we did not update the variable named head.

--- src/concurrency/coro_fifo.cc
+++ src/concurrency/coro_fifo.cc
@@ -9,6 +9,7 @@ void coro_fifo_t::inform_ready_to_leave(coro_fifo_acq_t *acq) {
     while (head && head->asked_to_leave_) {
         head->ready_to_leave_.pulse();
         acquisitor_queue_.remove(head);
+        head = acquisitor_queue_.head();
     }
 }
 
