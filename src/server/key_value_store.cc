a73295f77d93860fd8339b36cb5d19a8a98ffd1a
Sam Hughes <sam@rethinkdb.com>
2011-06-06 15:43:40 -0700

    Changed a bunch of order_token_t::ignore, we might get false errors in this one.

--- src/server/key_value_store.cc
+++ src/server/key_value_store.cc
@@ -61,7 +61,7 @@ void shard_store_t::delete_all_keys_for_backfill(order_token_t token) {
 void shard_store_t::set_replication_clock(repli_timestamp_t t, order_token_t token) {
     on_thread_t th(home_thread());
     sink.check_out(token);
-    btree.set_replication_clock(substore_order_source.check_in());
+    btree.set_replication_clock(t, substore_order_source.check_in());
 }
 
 /* btree_key_value_store_t */
