fdf0418df65c95c9645f9a815755891e921c077d
Sam Hughes <sam@rethinkdb.com>
2011-06-09 16:44:07 -0700

    Simplified selective_passive_producer_t.

--- src/replication/backfill_in.hpp
+++ src/replication/backfill_in.hpp
@@ -12,70 +12,53 @@ namespace replication {
 first one is empty, it tries the next one. */
 
 template <class value_t>
-class selective_passive_producer_t : public passive_producer_t<value_t> {
+class selective_passive_producer_t : public passive_producer_t<value_t>, public watchable_t<bool>::watcher_t {
 public:
     selective_passive_producer_t(passive_producer_t<value_t> *selectee) :
-        passive_producer_t<value_t>(&available_var) {
+        passive_producer_t<value_t>(&available_var),
+	the_producer(NULL) {
         set_source(selectee);
     }
     ~selective_passive_producer_t() {
         set_source(NULL);
     }
     void set_source(passive_producer_t<value_t> *selectee) {
-        while (watcher_t *w = watchers.head()) {
-            watchers.remove(w);
-            delete w;
-        }
+	if (the_producer) {
+	    the_producer->available->remove_watcher(this);
+	    the_producer = NULL;
+	}
 	if (selectee != NULL) {
-	    watchers.push_back(new watcher_t(this, selectee));
+	    the_producer = selectee;
+	    the_producer->available->add_watcher(this);
         }
         recompute();
     }
 
+    void on_watchable_changed() {
+	recompute();
+    }
+
 private:
-    struct watcher_t :
-        public watchable_t<bool>::watcher_t,
-        public intrusive_list_node_t<watcher_t>
-    {
-        watcher_t(selective_passive_producer_t *par, passive_producer_t<value_t> *pro) :
-            parent(par), producer(pro)
-        {
-            producer->available->add_watcher(this);
-        }
-        ~watcher_t() {
-            producer->available->remove_watcher(this);
-        }
-        void on_watchable_changed() {
-            parent->recompute();
-        }
-	selective_passive_producer_t *parent;
-        passive_producer_t<value_t> *producer;
-    };
-    intrusive_list_t<watcher_t> watchers;
 
     void recompute() {
-        /* We are available if any of our sources is available. */
-        bool a = false;
-        for (typename intrusive_list_t<watcher_t>::iterator it = watchers.begin();
-                it != watchers.end(); it++) {
-            a = a || (*it).producer->available->get();
-        }
-        available_var.set(a);
+        /* We are available if our source is available. */
+        available_var.set(the_producer && the_producer->available->get());
     }
-    watchable_var_t<bool> available_var;
     value_t produce_next_value() {
         /* Find the first available source. */
-        for (typename intrusive_list_t<watcher_t>::iterator it = watchers.begin();
-                it != watchers.end(); it++) {
-            if ((*it).producer->available->get()) {
-                return (*it).producer->pop();
-            }
-        }
+	if (the_producer && the_producer->available->get()) {
+	    return the_producer->pop();
+	}
         /* If none of the producers were `available`, then our `available_var`
         should have been `false`, so `produce_next_value()` should never have
         been called. */
         unreachable();
     }
+
+    passive_producer_t<value_t> *the_producer;
+    watchable_var_t<bool> available_var;
+
+    DISABLE_COPYING(selective_passive_producer_t);
 };
 
 /* backfill_storer_t is the dual of the backfill_and_realtime_stream() function. It takes
