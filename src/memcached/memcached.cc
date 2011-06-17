edfb5eb0f4df42b019092a680adc383330918b9a
Bill <bill@rethinkdb.com>
2011-06-17 15:36:26 -0700

    fixed a bug in the memcached protocol implementation.
    Removed a space from the beginning of a line in debug.hpp.
    Fixed an error in the memcached protocol implementation regarding the
    vesion command. We inexplicably required that the version command supply
    an extra argument even though the protocol requires that the version
    command take no arguments. Though this extra argument was ignored, not
    supplying it was treated as an error. This was changed so that the
    argument number check for the version command now expects only one
    argument (the version command itself) rather than 2.
    
    Conflicts:
    
    	src/memcached/memcached.cc

--- src/memcached/memcached.cc
+++ src/memcached/memcached.cc
@@ -1061,8 +1061,8 @@ void handle_memcache(txt_memcached_handler_if *rh, order_source_t *order_source)
         } else if(!strcmp(args[0], "rethinkdb") || !strcmp(args[0], "rdb")) {
             rh->write(control_t::exec(args.size() - 1, args.data() + 1));
         } else if (!strcmp(args[0], "version")) {
-            if (args.size() == 2) {
-                rh->writef("VERSION rethinkdb-%s\r\n", RETHINKDB_VERSION);
+            if (args.size() == 1) {
+                rh.writef("VERSION rethinkdb-%s\r\n", RETHINKDB_VERSION);
             } else {
                 rh->error();
             }
