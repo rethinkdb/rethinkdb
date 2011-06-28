39c93d2072e813cd759fd24d51bd65ad97cc3df8
jdoliner <jdoliner@gmail.com>
2011-06-28 10:45:12 -0700

    First commit for using spirit parsers.
    Adds network iterators to be used with the parsers and a simple dumb
    example parser.

---
+++ src/spirit/num_adder.hpp
@@ -0,0 +1,12 @@
+#ifndef __SPIRIT_NUM_ADDER_HPP__
+#define __SPIRIT_NUM_ADDER_HPP__
+
+#include "spirit/boost_parser.hpp"
+#include "arch/arch.hpp"
+
+//struct adder_t : gi::grammar<tcp_conn_t::iterator>
+//{
+//    adder_t() : adder_t::base_type(start)
+//}
+
+#endif
