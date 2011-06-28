c4a61f5e5a92235844b64982a1352355fddd5bae
Sam Hughes <sam@rethinkdb.com>
2011-06-28 08:42:35 -0700

    Got delete_expired.cc compiling.

--- src/btree/delete_expired.cc
+++ src/btree/delete_expired.cc
@@ -5,10 +5,10 @@
 class btree_delete_expired_oper_t : public btree_modify_oper_t
 {
 public:
-    bool operate(UNUSED const boost::shared_ptr<transaction_t>& txn, UNUSED btree_value *old_value, UNUSED boost::scoped_ptr<large_buf_t>& old_large_buflock, UNUSED btree_value **new_value, UNUSED boost::scoped_ptr<large_buf_t>& new_large_buflock) {
-        /* Don't do anything. run_btree_modify_oper() will take advantage of
-         * the fact that we got to the leaf in write mode to automatically
-         * delete the expired key if necessary. */
+    bool operate(UNUSED const boost::shared_ptr<transaction_t>& txn, UNUSED scoped_malloc<btree_value_t>& value) {
+        /* Don't do anything.  run_btree_modify_oper() will take
+           advantage of the fact that we got to the leaf in write mode
+           to automatically delete the expired key if necessary. */
         return false;
     }
 
