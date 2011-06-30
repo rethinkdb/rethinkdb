4d52598ba552198ba91b180070237371a5898282
Sam Hughes <sam@rethinkdb.com>
2011-06-30 12:27:24 -0700

    Removed general node stupidity.

--- src/btree/leaf_node.hpp
+++ src/btree/leaf_node.hpp
@@ -8,7 +8,35 @@
 // See leaf_node_t in node.hpp
 
 /* EPSILON to prevent split then merge bug */
+// TODO BLIND: LEAF_EPSILON is b.s., use value_sizer_t.
 #define LEAF_EPSILON (sizeof(btree_key_t) + MAX_KEY_SIZE + MAX_BTREE_VALUE_SIZE)
+
+
+struct value_type_t { char unused; };
+
+class value_sizer_t {
+public:
+    // The number of bytes the value takes up.  Reference implementation:
+    //
+    // for (int i = 0; i < INT_MAX; ++i) {
+    //    if (fits(value, i)) return i;
+    // }
+    virtual int size(const char *value) = 0;
+
+    // True if size(value) would return no more than length_available.
+    // Does not read any bytes outside of [value, value +
+    // length_available).
+    virtual bool fits(const char *value, int length_available) = 0;
+
+    virtual int max_possible_size() const = 0;
+
+    // The magic that should be used for btree leaf nodes (or general
+    // nodes) with this kind of value.
+    virtual block_magic_t btree_leaf_magic() const = 0;
+protected:
+    virtual ~value_sizer_t() { }
+};
+
 
 
 // Note: This struct is stored directly on disk.  Changing it
