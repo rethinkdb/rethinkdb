d854a87093797acb09415aeb0f23ab860f4e60a4
Sam Hughes <sam@rethinkdb.com>
2011-06-30 12:18:07 -0700

    general_timestamps_t and general_node_t, basically stolen from leaf node definition.

--- src/unittest/general_node_test.cc
+++ src/unittest/general_node_test.cc
@@ -1,7 +1,6 @@
 #include "unittest/gtest.hpp"
 
 #include "btree/general_node.hpp"
-#include "buffer_cache/types.hpp"
 
 namespace unittest {
 
@@ -15,16 +14,28 @@ public:
         return length_available > 0 && length_available >= 1 + size(value);
     }
 
-    virtual block_magic_t btree_leaf_magic() const {
+    int max_possible_size() const {
+        return 251;
+    }
+
+    block_magic_t btree_leaf_magic() const {
         block_magic_t ret = { { 'u', 'n', 'i', 't' } };
         return ret;
     }
 };
 
+TEST(GeneralNodeTest, Offsets) {
+    EXPECT_EQ(0, offsetof(btree::general_node_t, magic));
+    EXPECT_EQ(4, offsetof(btree::general_node_t, times));
+    EXPECT_EQ(12, offsetof(btree::general_node_t, npairs));
+    EXPECT_EQ(14, offsetof(btree::general_node_t, frontmost_offset));
+    EXPECT_EQ(16, offsetof(btree::general_node_t, pair_offsets));
+    EXPECT_EQ(16, sizeof(btree::general_node_t));
 
+    EXPECT_EQ(0, offsetof(btree::general_timestamps_t, last_modified));
+    EXPECT_EQ(4, offsetof(btree::general_timestamps_t, earlier));
+    EXPECT_EQ(8, sizeof(btree::general_timestamps_t));
 
-TEST(GeneralNodeTest, HelloWorld) {
-    EXPECT_EQ(0, 0);
 }
 
 
