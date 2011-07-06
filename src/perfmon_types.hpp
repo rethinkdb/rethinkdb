826d438c8c0b6bf6ec3659402449be05539898e2
Sam Hughes <sam@rethinkdb.com>
2011-07-06 11:22:18 -0700

    class/struct correctness in perfmon_types.hpp.

--- src/perfmon_types.hpp
+++ src/perfmon_types.hpp
@@ -1,11 +1,11 @@
 #ifndef _PERFMON_TYPES_HPP_
 #define _PERFMON_TYPES_HPP_
 
-struct perfmon_counter_t;
-struct perfmon_sampler_t;
+class perfmon_counter_t;
+class perfmon_sampler_t;
 struct perfmon_stddev_t;
 struct perfmon_duration_sampler_t;
-struct perfmon_rate_monitor_t;
+class perfmon_rate_monitor_t;
 struct perfmon_function_t;
 
 #endif
