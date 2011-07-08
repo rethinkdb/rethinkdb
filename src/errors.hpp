6a7e9fae5e27b726f9eb8a8dd159a526841bff90
Sam Hughes <sam@rethinkdb.com>
2011-07-08 12:37:56 -0700

    Put debug.hpp stuff inside errors.hpp out of a misguided sense of refactoring.

--- src/errors.hpp
+++ src/errors.hpp
@@ -4,7 +4,20 @@
 #include <errno.h>
 #include <stdlib.h>
 
-#include "debug.hpp"
+#ifdef __linux__
+#if defined __i386 || defined __x86_64
+#define BREAKPOINT __asm__ volatile ("int3")
+#else   /* not x86/amd64 */
+#define BREAKPOINT raise(SIGTRAP)
+#endif  /* x86/amd64 */
+#endif /* __linux__ */
+
+
+#ifndef NDEBUG
+#define DEBUG_ONLY(expr) do { expr; } while (0)
+#else
+#define DEBUG_ONLY(expr) ((void)(0))
+#endif
 
 /* Error handling
  *
