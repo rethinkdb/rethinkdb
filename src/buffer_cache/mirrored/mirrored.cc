8e26a04bf4a3e7834f322dc9072ec0ad60f3833c
Michael Arntzenius <daekharel@gmail.com>
2011-07-11 16:08:22 -0700

    remove incorrect TODO

--- src/buffer_cache/mirrored/mirrored.cc
+++ src/buffer_cache/mirrored/mirrored.cc
@@ -405,7 +405,6 @@ void mc_buf_t::acquire_block(bool locked, mc_inner_buf_t::version_id_t version_t
     mc_inner_buf_t::version_id_t inner_version = inner_buf->version_id;
     // In case we don't have received a version yet (i.e. this is the first block we are acquiring, just access the most recent version)
     if (snapshotted && version_to_access != mc_inner_buf_t::faux_version_id) {
-        // TODO (rntz) needs to be updated for potentially-written-out snapshotted blocks, whose data may be NULL
         data = inner_version <= version_to_access ? inner_buf->data : inner_buf->get_snapshot_data(version_to_access);
         guarantee(data != NULL);
     } else {
