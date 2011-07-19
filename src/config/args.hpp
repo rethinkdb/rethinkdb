55ea1566c75fca89763c3bd6ede983e876596477
Slava Akhmechet <slava@rethinkdb.com>
2011-07-18 17:37:49 -0700

    Changing the default cache size to half the memory, as opposed to .7

--- src/config/args.hpp
+++ src/config/args.hpp
@@ -91,7 +91,7 @@
 #define LOG_WORKER 0
 
 // Ratio of free ram to use for the cache by default
-#define DEFAULT_MAX_CACHE_RATIO                   0.7f
+#define DEFAULT_MAX_CACHE_RATIO                   0.5f
 
 // Maximum number of threads we support
 // TODO: make this dynamic where possible
