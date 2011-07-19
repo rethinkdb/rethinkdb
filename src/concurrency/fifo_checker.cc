e97496367c6d9903874f21b83bc182a8af36dcd0
Sam Hughes <sam@rethinkdb.com>
2011-07-18 18:01:47 -0700

    Made btree_slice_t use order_checkpoint_ and not the defunct order_sink_, making everything go through a checkpoint.  What is the purpose of the dispatching_store_t substore_order_source_, one might wonder.

--- src/concurrency/fifo_checker.cc
+++ src/concurrency/fifo_checker.cc
@@ -219,7 +219,7 @@ void plain_sink_t::check_out(order_token_t token) {
             bucket_ = token.bucket_;
             have_bucket_ = true;
         } else {
-            rassert(token.bucket_ == bucket_, "plain_sink_t can only be used with one order_source_t. for %s", token.tag_.c_str());
+            rassert(token.bucket_ == bucket_, "plain_sink_t can only be used with one order_source_t. for %s (last seens %s, %s)", token.tag_.c_str(), ls_pair_.first.tag.c_str(), ls_pair_.second.tag.c_str());
         }
 
         order_sink_t::verify_token_value_and_update(token, &ls_pair_);
