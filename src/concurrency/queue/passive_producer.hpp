rethinkdb/rethinkdb

commit 4534abf4f1e689a74ea3d8a541baf168e9d04cc0

watchable_value_t was basically only useful for passive_producer_t because of its weird semantics, so I renamed it to availability_t and put it in the same file with passive_producer_t.

src/concurrency/queue/passive_producer.hpp
@@ -1,7 +1,9 @@
 #ifndef __CONCURRENCY_QUEUE_PASSIVE_PRODUCER_HPP__
 #define __CONCURRENCY_QUEUE_PASSIVE_PRODUCER_HPP__
 
-#include "concurrency/watchable_value.hpp"
+#include "errors.hpp"
+#include "utils.hpp"
+#include <boost/function.hpp>
 
 /* A passive producer is an object (often a queue) that a consumer can read from
 by calling a method. It's called "passive" because the consumer calls a method
@@ -11,14 +13,49 @@ the consumer to give it data.
 In general you must create the passive producer before you create the consumer,
 and destroy the consumer before you destroy the passive producer.
 
-`passive_producer_t` is not thread-safe. */
+`passive_producer_t` is not thread-safe.
+
+`availability_t` is used to communicate to the consumer whether or not data is
+available on the `passive_producer_t`. `availability_t` is separate from
+`passive_producer_t` because often there are several `passive_producer_t`s in a
+chain, where each one's availability is equal to the availability of the one
+before it, and when `availability_t` is a separate object they can all share the
+same `availability_t`. `availability_control_t` is a concrete subclass of
+`availability_t` that can actually be instantiated. */
+
+struct availability_t {
+    bool get() { return available; }
+    void set_callback(boost::function<void()> fun) {
+        rassert(!callback);
+        rassert(fun);
+        callback = fun;
+    }
+    void unset_callback() {
+        rassert(callback);
+        callback = 0;
+    }
+private:
+    friend class availability_control_t;
+    bool available;
+    boost::function<void()> callback;
+};
+
+struct availability_control_t : public availability_t {
+    availability_control_t() {
+        available = false;
+    }
+    void set_available(bool a) {
+        available = a;
+        if (callback) callback();
+    }
+};
 
 template<class value_t>
 struct passive_producer_t {
 
     /* true if any data is available. `available` is const so that nobody
     screws around with it. */
-    watchable_value_t<bool> * const available;
+    availability_t * const available;
 
     /* `pop()` removes a value from the producer and returns it. It is
     an error to call `pop()` when `available->get()` is `false`. */
@@ -28,12 +65,13 @@ struct passive_producer_t {
     }
 
 protected:
-    /* To implement a passive producer: Pass a `watchable_value_t<bool>*` to the
-    constructor; the current value of this `watchable_value_t<bool>` will be
-    taken to indicate whether data is available. Override
+    /* To implement a passive producer: Pass an `availability_t*` to the
+    constructor. You can get it either by creating an `availability_control_t`,
+    or by passing the `availability_t*` from some other queue. Override
     `produce_next_value()`; `produce_next_value()` will only be called when the
     `watchable_value_t<bool>` is true, but it must never fail or block. */
-    passive_producer_t(watchable_value_t<bool> *w) : available(w) { }
+
+    passive_producer_t(availability_t *a) : available(a) { }
     virtual value_t produce_next_value() = 0;
     virtual ~passive_producer_t() { };
 

src/arch/linux/disk/aio/submit_sync.cc
@@ -1,7 +1,6 @@
-#include <vector>
-
 #include "arch/linux/disk/aio/submit_sync.hpp"
 #include "logger.hpp"
+#include <boost/bind.hpp>
 
 linux_aio_submit_sync_t::linux_aio_submit_sync_t(
         linux_aio_context_t *context, passive_producer_t<iocb *> *source) :
@@ -11,12 +10,12 @@ linux_aio_submit_sync_t::linux_aio_submit_sync_t(
     if (source->available->get()) pump();
 
     /* Register so we get notified when requests come in */
-    source->available->add_watcher(this);
+    source->available->set_callback(boost::bind(&linux_aio_submit_sync_t::on_source_availability_changed, this));
 }
 
 linux_aio_submit_sync_t::~linux_aio_submit_sync_t() {
     rassert(n_pending == 0);
-    source->available->remove_watcher(this);
+    source->available->unset_callback();
 }
 
 void linux_aio_submit_sync_t::notify_done() {
@@ -27,7 +26,7 @@ void linux_aio_submit_sync_t::notify_done() {
     pump();
 }
 
-void linux_aio_submit_sync_t::on_watchable_value_changed() {
+void linux_aio_submit_sync_t::on_source_availability_changed() {
     /* This is called when data becomes available on the source-queue or when
     it stops being available. If it became available, we need to start the cycle
     of pulling operations off the source queue and running them. */

src/arch/linux/disk/aio/submit_sync.hpp
@@ -5,15 +5,14 @@
 #include <vector>
 
 struct linux_aio_submit_sync_t :
-    public linux_diskmgr_aio_t::submit_strategy_t,
-    public watchable_value_t<bool>::watcher_t
+    public linux_diskmgr_aio_t::submit_strategy_t
 {
     linux_aio_submit_sync_t(linux_aio_context_t *context, passive_producer_t<iocb *> *source);
     ~linux_aio_submit_sync_t();
     void notify_done();
 
 private:
-    void on_watchable_value_changed();
+    void on_source_availability_changed();
     void pump();
     linux_aio_context_t *context;
     passive_producer_t<iocb *> *source;

src/arch/linux/disk/pool.cc
@@ -1,6 +1,6 @@
 #include "arch/linux/disk/pool.hpp"
-
 #include "config/args.hpp"
+#include <boost/bind.hpp>
 
 #define BLOCKER_POOL_QUEUE_DEPTH (MAX_CONCURRENT_IO_REQUESTS * 2)
 
@@ -11,11 +11,11 @@ pool_diskmgr_t::pool_diskmgr_t(
     n_pending(0)
 {
     if (source->available->get()) pump();
-    source->available->add_watcher(this);
+    source->available->set_callback(boost::bind(&pool_diskmgr_t::on_source_availability_changed, this));
 }
 
 pool_diskmgr_t::~pool_diskmgr_t() {
-    source->available->remove_watcher(this);
+    source->available->unset_callback();
 }
 
 void pool_diskmgr_t::action_t::run() {
@@ -31,7 +31,7 @@ void pool_diskmgr_t::action_t::done() {
     parent->done_fun(this);
 }
 
-void pool_diskmgr_t::on_watchable_value_changed() {
+void pool_diskmgr_t::on_source_availability_changed() {
     /* This is called when the queue used to be empty but now has requests on
     it, and also when the queue's last request is consumed. */
     if (source->available->get()) pump();

src/arch/linux/disk/pool.hpp
@@ -9,8 +9,7 @@
 /* The pool disk manager uses a thread pool in conjunction with synchronous
 (blocking) IO calls to asynchronously run IO requests. */
 
-struct pool_diskmgr_t :
-    private watchable_value_t<bool>::watcher_t
+struct pool_diskmgr_t
 {
     struct action_t : private blocker_pool_t::job_t {
 
@@ -60,7 +59,7 @@ private:
     passive_producer_t<action_t *> *source;
     blocker_pool_t blocker_pool;
 
-    void on_watchable_value_changed();
+    void on_source_availability_changed();
     int n_pending;
     void pump();
 };

src/concurrency/coro_pool.cc
@@ -11,20 +11,20 @@ coro_pool_t::coro_pool_t(size_t worker_count_, passive_producer_t<boost::functio
     active_worker_count(0)
 {
     rassert(max_worker_count > 0);
-    on_watchable_value_changed();   // Start process if necessary
-    source->available->add_watcher(this);
+    on_source_availability_changed();   // Start process if necessary
+    source->available->set_callback(boost::bind(&coro_pool_t::on_source_availability_changed, this));
 }
 
 
 coro_pool_t::~coro_pool_t() {
     assert_thread();
-    source->available->remove_watcher(this);
+    source->available->unset_callback();
     coro_drain_semaphore.drain();
     rassert(active_worker_count == 0);
 }
 
 
-void coro_pool_t::on_watchable_value_changed() {
+void coro_pool_t::on_source_availability_changed() {
     assert_thread();
     while (source->available->get() && active_worker_count < max_worker_count) {
         coro_t::spawn_now(boost::bind(&coro_pool_t::worker_run,

src/concurrency/coro_pool.hpp
@@ -5,7 +5,6 @@
 #include <boost/function.hpp>
 
 #include "concurrency/drain_semaphore.hpp"
-#include "concurrency/watchable_value.hpp"
 
 template <class value_t>
 struct passive_producer_t;
@@ -21,8 +20,7 @@ returns. */
 class coro_t;
 
 class coro_pool_t :
-    public home_thread_mixin_t,
-    private watchable_value_t<bool>::watcher_t
+    public home_thread_mixin_t
 {
 public:
     coro_pool_t(size_t worker_count_, passive_producer_t<boost::function<void()> > *source);
@@ -37,7 +35,7 @@ public:
 private:
     passive_producer_t<boost::function<void()> > *source;
 
-    void on_watchable_value_changed();
+    void on_source_availability_changed();
 
     int max_worker_count, active_worker_count;
 

src/concurrency/queue/accounting.hpp
@@ -23,7 +23,7 @@ class accounting_queue_t :
 {
 public:
     accounting_queue_t(int batch_factor) :
-        passive_producer_t<value_t>(&available_var),
+        passive_producer_t<value_t>(&available_control),
         total_shares(0),
         selector(0),
         batch_factor(batch_factor) {
@@ -37,8 +37,7 @@ public:
     }
 
     class account_t :
-        public intrusive_list_node_t<account_t>,
-        private watchable_value_t<bool>::watcher_t
+        public intrusive_list_node_t<account_t>
     {
     public:
         account_t(accounting_queue_t *p, passive_producer_t<value_t> *s, int shares) :
@@ -48,22 +47,22 @@ public:
             rassert(shares > 0);
             if (source->available->get()) activate();
             else parent->inactive_accounts.push_back(this);
-            source->available->add_watcher(this);
-            parent->available_var.set(!parent->active_accounts.empty());
+            source->available->set_callback(boost::bind(&account_t::on_availability_changed, this));
+            parent->available_control.set_available(!parent->active_accounts.empty());
         }
         ~account_t() {
             on_thread_t thread_switcher(parent->home_thread());
             parent->assert_thread();
-            source->available->remove_watcher(this);
+            source->available->unset_callback();
             if (active) deactivate();
             else parent->inactive_accounts.remove(this);
-            parent->available_var.set(!parent->active_accounts.empty());
+            parent->available_control.set_available(!parent->active_accounts.empty());
         }
 
     private:
         friend class accounting_queue_t;
 
-        void on_watchable_value_changed() {
+        void on_availability_changed() {
             parent->assert_thread();
             if (source->available->get() && !active) {
                 parent->inactive_accounts.remove(this);
@@ -72,7 +71,7 @@ public:
                 deactivate();
                 parent->inactive_accounts.push_back(this);
             }
-            parent->available_var.set(!parent->active_accounts.empty());
+            parent->available_control.set_available(!parent->active_accounts.empty());
         }
 
         void activate() {
@@ -99,7 +98,7 @@ private:
 
     int total_shares, selector, batch_factor;
 
-    watchable_var_t<bool> available_var;
+    availability_control_t available_control;
     value_t produce_next_value() {
         assert_thread();
 

commit df14bbebcd7fc7f212c2554e47613f96f0c0c3a6

Fixed a bug introduced by 4534abf4f1e689a74ea3d8a541baf168e9d04cc0.

src/concurrency/queue/passive_producer.hpp
@@ -45,8 +45,10 @@ struct availability_control_t : public availability_t {
         available = false;
     }
     void set_available(bool a) {
-        available = a;
-        if (callback) callback();
+        if (available != a) {
+            available = a;
+            if (callback) callback();
+        }
     }
 };
 