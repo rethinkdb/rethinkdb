2fbccc9b49e000a0430977f3fc0d6e997d9c334e
Daniel Mewes <daniel@rethinkdb.com>
2011-07-28 16:38:34 -0700

    Make the redis value unit test actually work as a unit test

--- src/unittest/redis_values_test.cc
+++ src/unittest/redis_values_test.cc
@@ -28,43 +28,37 @@ private:
         hash_value.size = 0;
 
         // Perform some operations
-        // TODO! Convert all the rasserts and outputs into unit test assertions
         {
             on_thread_t thread(cache->home_thread());
 
             boost::shared_ptr<transaction_t> transaction(new transaction_t(cache, rwi_write, 1, repli_timestamp_t::invalid));
             value_sizer_t<redis_hash_value_t> sizer(cache->get_block_size());
 
-            rassert(hash_value.hset(&sizer, transaction, "field_1", "value_1"));
-            rassert(hash_value.hset(&sizer, transaction, "field_2", "value_2"));
+            EXPECT_TRUE(hash_value.hset(&sizer, transaction, "field_1", "value_1"));
+            EXPECT_TRUE(hash_value.hset(&sizer, transaction, "field_2", "value_2"));
             std::string large_value(1024 * 1024, 'X');
-            rassert(hash_value.hset(&sizer, transaction, "field_3", large_value));
+            EXPECT_TRUE(hash_value.hset(&sizer, transaction, "field_3", large_value));
 
-            rassert(hash_value.hexists(&sizer, transaction, "field_1"));
-            rassert(hash_value.hexists(&sizer, transaction, "field_2"));
-            rassert(hash_value.hexists(&sizer, transaction, "field_3"));
-            rassert(!hash_value.hexists(&sizer, transaction, "field_4"));
+            EXPECT_TRUE(hash_value.hexists(&sizer, transaction, "field_1"));
+            EXPECT_TRUE(hash_value.hexists(&sizer, transaction, "field_2"));
+            EXPECT_TRUE(hash_value.hexists(&sizer, transaction, "field_3"));
+            EXPECT_TRUE(!hash_value.hexists(&sizer, transaction, "field_4"));
 
-            fprintf(stderr, "Hash length: %d\n", (int)hash_value.hlen());
+            EXPECT_EQ(hash_value.hlen(), 3);
 
             boost::shared_ptr<one_way_iterator_t<std::pair<std::string, std::string> > > iter = hash_value.hgetall(&sizer, transaction, cache->home_thread());
-            fprintf(stderr, "\nHash contents:\n");
-            while (true) {
-                boost::optional<std::pair<std::string, std::string> > next = iter->next();
-                if (next) {
-                    fprintf(stderr, "\t%s -> %s\n", next->first.c_str(), next->second.c_str());
-                } else {
-                    break;
-                }
-            }
-            iter.reset();
+            EXPECT_EQ(iter->next(), (boost::optional<std::pair<std::string, std::string> >(std::pair<std::string, std::string>("field_1", "value_1"))));
+            EXPECT_EQ(iter->next(), (boost::optional<std::pair<std::string, std::string> >(std::pair<std::string, std::string>("field_2", "value_2"))));
+            EXPECT_EQ(iter->next(), (boost::optional<std::pair<std::string, std::string> >(std::pair<std::string, std::string>("field_3", large_value))));
+            EXPECT_EQ(iter->next(), (boost::none));
+            iter.reset(); // release locks
 
             hash_value.clear(&sizer, transaction, cache->home_thread());
-            rassert(!hash_value.hexists(&sizer, transaction, "field_1"));
-            rassert(!hash_value.hexists(&sizer, transaction, "field_2"));
-            rassert(!hash_value.hexists(&sizer, transaction, "field_3"));
+            EXPECT_TRUE(!hash_value.hexists(&sizer, transaction, "field_1"));
+            EXPECT_TRUE(!hash_value.hexists(&sizer, transaction, "field_2"));
+            EXPECT_TRUE(!hash_value.hexists(&sizer, transaction, "field_3"));
 
-            fprintf(stderr, "Hash length after clear(): %d\n", (int)hash_value.hlen());
+            EXPECT_EQ(hash_value.hlen(), 0);
         }
     }
 
@@ -75,46 +69,39 @@ private:
         set_value.size = 0;
 
         // Perform some operations
-        // TODO! Convert all the rasserts and outputs into unit test assertions
         {
             on_thread_t thread(cache->home_thread());
 
             boost::shared_ptr<transaction_t> transaction(new transaction_t(cache, rwi_write, 1, repli_timestamp_t::invalid));
             value_sizer_t<redis_set_value_t> sizer(cache->get_block_size());
 
-            guarantee(set_value.sadd(&sizer, transaction, "member_1"));
-            guarantee(set_value.sadd(&sizer, transaction, "member_2"));
-            rassert(!set_value.sadd(&sizer, transaction, "member_1"));
-            rassert(!set_value.sadd(&sizer, transaction, "member_2"));
+            EXPECT_TRUE(set_value.sadd(&sizer, transaction, "member_1"));
+            EXPECT_TRUE(set_value.sadd(&sizer, transaction, "member_2"));
+            EXPECT_TRUE(!set_value.sadd(&sizer, transaction, "member_1"));
+            EXPECT_TRUE(!set_value.sadd(&sizer, transaction, "member_2"));
 
-            rassert(set_value.sismember(&sizer, transaction, "member_1"));
-            rassert(set_value.sismember(&sizer, transaction, "member_2"));
-            rassert(!set_value.sismember(&sizer, transaction, "member_3"));
+            EXPECT_TRUE(set_value.sismember(&sizer, transaction, "member_1"));
+            EXPECT_TRUE(set_value.sismember(&sizer, transaction, "member_2"));
+            EXPECT_TRUE(!set_value.sismember(&sizer, transaction, "member_3"));
 
-            fprintf(stderr, "Set cardinality: %d\n", (int)set_value.scard());
+            EXPECT_EQ(set_value.scard(), 2);
 
             boost::shared_ptr<one_way_iterator_t<std::string> > iter = set_value.smembers(&sizer, transaction, cache->home_thread());
-            fprintf(stderr, "\nSet contents:\n");
-            while (true) {
-                boost::optional<std::string> next = iter->next();
-                if (next) {
-                    fprintf(stderr, "\t%s \n", next->c_str());
-                } else {
-                    break;
-                }
-            }
-            iter.reset();
+            EXPECT_EQ(iter->next(), (boost::optional<std::string>(std::string("member_1"))));
+            EXPECT_EQ(iter->next(), (boost::optional<std::string>(std::string("member_2"))));
+            EXPECT_EQ(iter->next(), (boost::none));
+            iter.reset(); // release locks
 
-            guarantee(set_value.sadd(&sizer, transaction, "member_3"));
-            rassert(set_value.sismember(&sizer, transaction, "member_3"));
-            guarantee(set_value.srem(&sizer, transaction, "member_3"));
-            rassert(!set_value.sismember(&sizer, transaction, "member_3"));
+            EXPECT_TRUE(set_value.sadd(&sizer, transaction, "member_3"));
+            EXPECT_TRUE(set_value.sismember(&sizer, transaction, "member_3"));
+            EXPECT_TRUE(set_value.srem(&sizer, transaction, "member_3"));
+            EXPECT_TRUE(!set_value.sismember(&sizer, transaction, "member_3"));
 
             set_value.clear(&sizer, transaction, cache->home_thread());
-            rassert(!set_value.sismember(&sizer, transaction, "member_1"));
-            rassert(!set_value.sismember(&sizer, transaction, "member_2"));
+            EXPECT_TRUE(!set_value.sismember(&sizer, transaction, "member_1"));
+            EXPECT_TRUE(!set_value.sismember(&sizer, transaction, "member_2"));
 
-            fprintf(stderr, "Set cardinality after clear(): %d\n", (int)set_value.scard());
+            EXPECT_EQ(set_value.scard(), 0);
         }
     }
 
@@ -126,61 +113,49 @@ private:
         sortedset_value.size = 0;
 
         // Perform some operations
-        // TODO! Convert all the rasserts and outputs into unit test assertions
         {
             on_thread_t thread(cache->home_thread());
 
             boost::shared_ptr<transaction_t> transaction(new transaction_t(cache, rwi_write, 1, repli_timestamp_t::invalid));
             value_sizer_t<redis_sortedset_value_t> sizer(cache->get_block_size());
 
-            guarantee(sortedset_value.zadd(&sizer, transaction, "member_1", -0.5f));
-            guarantee(sortedset_value.zadd(&sizer, transaction, "member_3", 0.1f));
-            guarantee(sortedset_value.zadd(&sizer, transaction, "member_2", 0.0f));
-            guarantee(sortedset_value.zrank(&sizer, transaction, cache->home_thread(), "member_1").get() == 0);
-            guarantee(sortedset_value.zrank(&sizer, transaction, cache->home_thread(), "member_2").get() == 1);
-            guarantee(sortedset_value.zrank(&sizer, transaction, cache->home_thread(), "member_3").get() == 2);
-            guarantee(sortedset_value.zscore(&sizer, transaction, "member_1").get() == -0.5f);
-            guarantee(sortedset_value.zscore(&sizer, transaction, "member_2").get() == 0.0f);
-            guarantee(sortedset_value.zscore(&sizer, transaction, "member_3").get() == 0.1f);
-            guarantee(!sortedset_value.zadd(&sizer, transaction, "member_1", -0.1f)); // Should update the score
-            guarantee(!sortedset_value.zadd(&sizer, transaction, "member_2", 0.0f));
-            guarantee(!sortedset_value.zadd(&sizer, transaction, "member_3", 0.1f));
-            guarantee(sortedset_value.zrank(&sizer, transaction, cache->home_thread(), "member_1").get() == 0);
-            guarantee(sortedset_value.zrank(&sizer, transaction, cache->home_thread(), "member_2").get() == 1);
-            guarantee(sortedset_value.zrank(&sizer, transaction, cache->home_thread(), "member_3").get() == 2);
+            EXPECT_TRUE(sortedset_value.zadd(&sizer, transaction, "member_1", -0.5f));
+            EXPECT_TRUE(sortedset_value.zadd(&sizer, transaction, "member_3", 0.1f));
+            EXPECT_TRUE(sortedset_value.zadd(&sizer, transaction, "member_2", 0.0f));
+            EXPECT_TRUE(sortedset_value.zrank(&sizer, transaction, cache->home_thread(), "member_1").get() == 0);
+            EXPECT_TRUE(sortedset_value.zrank(&sizer, transaction, cache->home_thread(), "member_2").get() == 1);
+            EXPECT_TRUE(sortedset_value.zrank(&sizer, transaction, cache->home_thread(), "member_3").get() == 2);
+            EXPECT_TRUE(sortedset_value.zscore(&sizer, transaction, "member_1").get() == -0.5f);
+            EXPECT_TRUE(sortedset_value.zscore(&sizer, transaction, "member_2").get() == 0.0f);
+            EXPECT_TRUE(sortedset_value.zscore(&sizer, transaction, "member_3").get() == 0.1f);
+            EXPECT_TRUE(!sortedset_value.zadd(&sizer, transaction, "member_1", -0.1f)); // Should update the score
+            EXPECT_TRUE(!sortedset_value.zadd(&sizer, transaction, "member_2", 0.0f));
+            EXPECT_TRUE(!sortedset_value.zadd(&sizer, transaction, "member_3", 0.1f));
+            EXPECT_TRUE(sortedset_value.zrank(&sizer, transaction, cache->home_thread(), "member_1").get() == 0);
+            EXPECT_TRUE(sortedset_value.zrank(&sizer, transaction, cache->home_thread(), "member_2").get() == 1);
+            EXPECT_TRUE(sortedset_value.zrank(&sizer, transaction, cache->home_thread(), "member_3").get() == 2);
 
-            fprintf(stderr, "Sorted set cardinality: %d\n", (int)sortedset_value.zcard());
+            EXPECT_EQ(sortedset_value.zcard(), 3);
 
             boost::shared_ptr<one_way_iterator_t<std::pair<float, std::string> > > iter = sortedset_value.zrange(&sizer, transaction, cache->home_thread(), 0);
-            fprintf(stderr, "\nSorted set contents:\n");
-            while (true) {
-                boost::optional<std::pair<float, std::string> > next = iter->next();
-                if (next) {
-                    fprintf(stderr, "\t%f %s \n", next->first, next->second.c_str());
-                } else {
-                    break;
-                }
-            }
-            iter.reset();
-
+            EXPECT_EQ(iter->next(), (boost::optional<std::pair<float, std::string> >(std::pair<float, std::string>(-0.1f, "member_1"))));
+            EXPECT_EQ(iter->next(), (boost::optional<std::pair<float, std::string> >(std::pair<float, std::string>(0.0f, "member_2"))));
+            EXPECT_EQ(iter->next(), (boost::optional<std::pair<float, std::string> >(std::pair<float, std::string>(0.1f, "member_3"))));
+            EXPECT_EQ(iter->next(), (boost::none));
+            iter.reset(); // release locks
 
             // Make all members ambiguous when it comes to score (they should then be sorted by the member string)
-            guarantee(!sortedset_value.zadd(&sizer, transaction, "member_1", 0.0));
-            guarantee(!sortedset_value.zadd(&sizer, transaction, "member_3", 0.0));
+            EXPECT_TRUE(!sortedset_value.zadd(&sizer, transaction, "member_1", 0.0));
+            EXPECT_TRUE(!sortedset_value.zadd(&sizer, transaction, "member_3", 0.0));
+            // Last two elements in the sorted set
             iter = sortedset_value.zrange(&sizer, transaction, cache->home_thread(), -2);
-            fprintf(stderr, "\nLast two elements int sorted set:\n");
-            while (true) {
-                boost::optional<std::pair<float, std::string> > next = iter->next();
-                if (next) {
-                    fprintf(stderr, "\t%f %s \n", next->first, next->second.c_str());
-                } else {
-                    break;
-                }
-            }
-            iter.reset();
+            EXPECT_EQ(iter->next(), (boost::optional<std::pair<float, std::string> >(std::pair<float, std::string>(0.0f, "member_2"))));
+            EXPECT_EQ(iter->next(), (boost::optional<std::pair<float, std::string> >(std::pair<float, std::string>(0.0f, "member_3"))));
+            EXPECT_EQ(iter->next(), (boost::none));
+            iter.reset(); // release locks
 
             sortedset_value.clear(&sizer, transaction, cache->home_thread());
-            fprintf(stderr, "Set cardinality after clear(): %d\n", (int)sortedset_value.zcard());
+            EXPECT_EQ(sortedset_value.zcard(), 0);
         }
     }
 };
