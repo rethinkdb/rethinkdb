8ed2178c285a0987439277089fb96072fcc2dd1e
Tim Maxwell <tim@rethinkdb.com>
2011-07-29 15:40:09 -0700

    Moved diskinfo.* to server/.

--- src/server/diskinfo.hpp
+++ src/server/diskinfo.hpp
@@ -1,5 +1,5 @@
-#ifndef __DISKINFO_HPP__
-#define __DISKINFO_HPP__
+#ifndef __SERVER_DISKINFO_HPP__
+#define __SERVER_DISKINFO_HPP__
 
 #include <vector>
 #include "server/cmd_args.hpp"
@@ -7,4 +7,4 @@
 /* Logs hdparm information about the disks that the serializers' files reside on. */
 void log_disk_info(std::vector<log_serializer_private_dynamic_config_t> const &serializers);
 
-#endif  // __DISKINFO_HPP__
+#endif  // __SERVER_DISKINFO_HPP__
