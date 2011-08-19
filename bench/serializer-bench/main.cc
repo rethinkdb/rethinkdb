23963097264d2c14dfc4ae2c73e23565281d9ec1
Sam Hughes <sam@rethinkdb.com>
2011-08-19 16:54:58 -0700

    Fixed trivial build errors in serializer-bench.

--- bench/serializer-bench/main.cc
+++ bench/serializer-bench/main.cc
@@ -1,3 +1,4 @@
+#include "arch/arch.hpp"
 #include "errors.hpp"
 #include "serializer/log/log_serializer.hpp"
 
@@ -7,6 +8,8 @@ namespace stress {
     #include "../stress-client/utils.cc"
     #include "../stress-client/random.cc"
 }
+
+class timer_token_t;
 
 struct txn_info_t {
     ticks_t start, end;
@@ -133,10 +136,10 @@ struct tester_t :
         }
 
         fprintf(stderr, "Creating a database...\n");
-        log_serializer_t::create(&config->ser_dynamic_config, &config->ser_private_dynamic_config, &config->ser_static_config);
+        log_serializer_t::create(config->ser_dynamic_config, config->ser_private_dynamic_config, config->ser_static_config);
         
         fprintf(stderr, "Starting serializer...\n");
-        ser = new log_serializer_t(&config->ser_dynamic_config, &config->ser_private_dynamic_config);
+        ser = new log_serializer_t(config->ser_dynamic_config, config->ser_private_dynamic_config);
         on_serializer_ready(ser);
     }
     
