69102f13b4719806fadd9a5ac575ec41f14e7cde
Sam Hughes <sam@rethinkdb.com>
2011-08-30 15:16:30 -0700

    Specified libQtCore.so.4 to be copied to the cloud upon full test.

--- lib/retester/cloud_config/ec2_configuration.py
+++ lib/retester/cloud_config/ec2_configuration.py
@@ -26,11 +26,12 @@ private_ssh_key_filename = os.path.join (cloud_config_path, "ec2_private_key.pem
 # Dependencies to automatically install on the cloud nodes
 
 cloudtest_lib_dependencies = [("/usr/local/lib/libtcmalloc_minimal.so.0", "libtcmalloc_minimal.so.0"),
-        ("/usr/local/lib/libmemcached.so.5", "libmemcached.so.5"),
-        ("/usr/local/lib/libmemcached.so.6", "libmemcached.so.6"),
-        ("/usr/local/lib/libgtest.so.0", "libgtest.so.0"),
-        ("/usr/lib/libstdc++.so.6", "libstdc++.so.6"),
-        ("/usr/lib/libgccpp.so.1", "libgccpp.so.1")]
+                              ("/usr/local/lib/libmemcached.so.5", "libmemcached.so.5"),
+                              ("/usr/local/lib/libmemcached.so.6", "libmemcached.so.6"),
+                              ("/usr/local/lib/libgtest.so.0", "libgtest.so.0"),
+                              ("/usr/lib/libstdc++.so.6", "libstdc++.so.6"),
+                              ("/usr/lib/libgccpp.so.1", "libgccpp.so.1"),
+                              ("/usr/lib/libQtCore.so.4", "libQtCore.so.4")]
 for valgrind_file in ["vgpreload_memcheck-amd64-linux.so", "vgpreload_core-amd64-linux.so", "memcheck-amd64-linux", "drd-amd64-linux", "helgrind-amd64-linux"]:
     cloudtest_lib_dependencies.append(("/usr/lib/valgrind/" + valgrind_file, "valgrind/" + valgrind_file))
 cloudtest_lib_dependencies.append((os.path.join (cloud_config_path, "valgrind-default.supp"), "valgrind/default.supp"))
