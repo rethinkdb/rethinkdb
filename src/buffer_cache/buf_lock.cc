9e8e3e7f2e6dc04db606b2ffec526f9de037aea5
Sam Hughes <sam@rethinkdb.com>
2011-09-06 09:53:17 -0700

    Made buf_lock_t disallow any calls from wrong threads.

--- src/buffer_cache/buf_lock.cc
+++ src/buffer_cache/buf_lock.cc
@@ -14,28 +14,38 @@ buf_t *co_acquire_block(transaction_t *transaction, block_id_t block_id, access_
 }
 
 buf_lock_t::buf_lock_t(transaction_t *txn, block_id_t block_id, access_t mode, cond_t *acquisition_cond) :
-    buf_(co_acquire_block(txn, block_id, mode, acquisition_cond)), home_thread_(get_thread_id()) { }
-
-void buf_lock_t::allocate(transaction_t *txn) {
+    buf_(co_acquire_block(txn, block_id, mode, acquisition_cond))
+#ifndef NDEBUG
+    , home_thread_(get_thread_id())
+#endif  // NDEBUG
+{
     txn->assert_thread();
-    guarantee(buf_ == NULL);
-    buf_ = txn->allocate();
+}
+void buf_lock_t::allocate(transaction_t *txn) {
+    assert_thread();
+#ifndef NDEBUG
     home_thread_ = get_thread_id();
+#endif  // NDEBUG
+    guarantee(buf_ == NULL);
+    txn->assert_thread();
+    buf_ = txn->allocate();
 }
 
 
 buf_lock_t::~buf_lock_t() {
+    assert_thread();
     release_if_acquired();
 }
 
 void buf_lock_t::release() {
+    assert_thread();
     guarantee(buf_ != NULL);
-    on_thread_t thread_switcher(home_thread_);
     buf_->release();
     buf_ = NULL;
 }
 
 void buf_lock_t::release_if_acquired() {
+    assert_thread();
     if (buf_)
         release();
 }
