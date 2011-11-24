3df3144e202c83ffcdc941b5d9df80620f1adf00
Tim Maxwell <tim@rethinkdb.com>
2011-11-23 17:26:52 -0800

    Use /home/teapot/slurm/slurm.conf, not /home/tim/slurm/slurm.conf.

--- scripts/nightly-test/launch_nightly_test.sh
+++ scripts/nightly-test/launch_nightly_test.sh
@@ -12,6 +12,6 @@ EMAILEE=$1; shift
 tar --create -z --file=- full_test_driver.py remotely.py simple_linear_db.py renderer | \
     curl -X POST http://$TEST_HOST/spawn/ \
         -F tarball=@- \
-        -F command="SLURM_CONF=/home/tim/slurm/slurm.conf ./full_test_driver.py $* >output.txt 2>&1" \
+        -F command="SLURM_CONF=/home/teapot/slurm/slurm.conf ./full_test_driver.py $* >output.txt 2>&1" \
         -F title="Nightly test" \
         -F emailee=$EMAILEE
