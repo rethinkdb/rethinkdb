280dfa523e034abe6eff945aaa82f3dc87eecf20
Tim Maxwell <tim@rethinkdb.com>
2012-01-11 16:00:55 -0800

    Add rwi_lock_assertion_t

--- src/concurrency/mutex_assertion.hpp
+++ src/concurrency/mutex_assertion.hpp
@@ -56,6 +56,80 @@ inline void swap(mutex_assertion_t::acq_t &a, mutex_assertion_t::acq_t &b) {
     std::swap(a.mutex, b.mutex);
 }
 
+class rwi_lock_assertion_t : public home_thread_mixin_t {
+public:
+    class read_acq_t {
+    public:
+        read_acq_t() : lock(NULL) { }
+        explicit read_acq_t(rwi_lock_assertion_t *l) : lock(NULL) {
+            reset(l);
+        }
+        ~read_acq_t() {
+            reset(NULL);
+        }
+        void reset(rwi_lock_assertion_t *l = NULL) {
+            if (lock) {
+                lock->assert_thread();
+                rassert(lock->state > 0);
+                lock->state--;
+            }
+            lock = l;
+            if (lock) {
+                lock->assert_thread();
+                rassert(lock->state != rwi_lock_assertion_t::write_locked);
+                lock->state++;
+            }
+        }
+        void assert_is_holding(rwi_lock_assertion_t *l) {
+            rassert(lock == l);
+        }
+    private:
+        rwi_lock_assertion_t *lock;
+        DISABLE_COPYING(read_acq_t);
+    };
+    class write_acq_t {
+    public:
+        write_acq_t() : lock(NULL) { }
+        explicit write_acq_t(rwi_lock_assertion_t *l) : lock(NULL) {
+            reset(l);
+        }
+        ~write_acq_t() {
+            reset(NULL);
+        }
+        void reset(rwi_lock_assertion_t *l = NULL) {
+            if (lock) {
+                lock->assert_thread();
+                rassert(lock->state == rwi_lock_assertion_t::write_locked);
+                lock->state = 0;
+            }
+            lock = l;
+            if (lock) {
+                lock->assert_thread();
+                rassert(lock->state == 0);
+                lock->state = rwi_lock_assertion_t::write_locked;
+            }
+        }
+        void assert_is_holding(rwi_lock_assertion_t *l) {
+            rassert(lock == l);
+        }
+    private:
+        rwi_lock_assertion_t *lock;
+        DISABLE_COPYING(write_acq_t);
+    };
+    explicit rwi_lock_assertion_t(int explicit_home_thread) :
+        home_thread_mixin_t(explicit_home_thread), state(0) { }
+    rwi_lock_assertion_t() : state(0) { }
+    ~rwi_lock_assertion_t() {
+        rassert(state == 0);
+    }
+private:
+    friend class read_acq_t;
+    friend class write_acq_t;
+    static const int write_locked = -1;
+    int state;
+    DISABLE_COPYING(rwi_lock_assertion_t);
+};
+
 #else
 
 class mutex_assertion_t {
@@ -77,6 +151,34 @@ private:
 
 inline void swap(mutex_assertion_t::acq_t &, mutex_assertion_t::acq_t &) {
 }
+
+class rwi_lock_assertion_t {
+public:
+    class read_acq_t {
+    public:
+        read_acq_t() { }
+        explicit read_acq_t(rwi_lock_assertion_t *) { }
+        void reset(rwi_lock_assertion_t * = NULL) { }
+        void assert_is_holding(rwi_lock_assertion_t *) { }
+    private:
+        DISABLE_COPYING(read_acq_t);
+    };
+    class write_acq_t {
+    public:
+        write_acq_t() { }
+        explicit write_acq_t(rwi_lock_assertion_t *) { }
+        void reset(rwi_lock_assertion_t * = NULL) { }
+        void assert_is_holding(rwi_lock_assertion_t *) { }
+    private:
+        DISABLE_COPYING(write_acq_t);
+    };
+    explicit rwi_lock_assertion_t(int) { }
+    rwi_lock_assertion_t() { }
+private:
+    friend class read_acq_t;
+    friend class write_acq_t;
+    DISABLE_COPYING(rwi_lock_assertion_t);
+};
 
 #endif /* NDEBUG */
 
