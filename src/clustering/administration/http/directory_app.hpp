43aa5a27fdf5e5f4bec2a499ce1bc4f7b50a48f6
Tim Maxwell <tim@rethinkdb.com>
2012-03-15 12:39:23 -0700

    Made directory_http_app_t constructor explicit.

--- src/clustering/administration/http/directory_app.hpp
+++ src/clustering/administration/http/directory_app.hpp
@@ -7,7 +7,7 @@
 
 class directory_http_app_t : public http_app_t {
 public:
-    directory_http_app_t(clone_ptr_t<directory_rview_t<cluster_directory_metadata_t> >& _directory_metadata);
+    explicit directory_http_app_t(clone_ptr_t<directory_rview_t<cluster_directory_metadata_t> >& _directory_metadata);
     http_res_t handle(const http_req_t &);
 private:
     clone_ptr_t<directory_rview_t<cluster_directory_metadata_t> > directory_metadata;
