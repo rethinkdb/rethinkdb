da65fe2e3824a9f063f94a71c14653d4d22efc5e
jdoliner <jdoliner@gmail.com>
2012-03-19 12:32:43 -0700

    Explicit machine selection seems to be working.

--- src/clustering/administration/http/semilattice_app.cc
+++ src/clustering/administration/http/semilattice_app.cc
@@ -119,19 +119,19 @@ http_res_t semilattice_http_app_t::handle(const http_req_t &req) {
                     json_adapter_head->apply(change.get(), json_ctx);
 
                     /* Fill in the blueprints */
-                    std::map<machine_id_t, datacenter_id_t> machine_assignments;
-                    for (std::map<machine_id_t, deletable_t<machine_semilattice_metadata_t> >::iterator it  = cluster_metadata.machines.machines.begin();
-                                                                                                        it != cluster_metadata.machines.machines.end();
-                                                                                                        it++) {
-                        if (!it->second.is_deleted()) {
-                            machine_assignments[it->first] = it->second.get().datacenter.get();
-                        }
-                    }
-                    fill_in_blueprints_for_protocol<memcached_protocol_t>(&cluster_metadata.memcached_namespaces,
-                            directory_metadata->subview(clone_ptr_t<read_lens_t<namespaces_directory_metadata_t<memcached_protocol_t>, cluster_directory_metadata_t> >(field_lens(&cluster_directory_metadata_t::memcached_namespaces))),
-                            directory_metadata->subview(clone_ptr_t<read_lens_t<machine_id_t, cluster_directory_metadata_t> >(field_lens(&cluster_directory_metadata_t::machine_id))),
-                            machine_assignments,
-                            us);
+                    //std::map<machine_id_t, datacenter_id_t> machine_assignments;
+                    //for (std::map<machine_id_t, deletable_t<machine_semilattice_metadata_t> >::iterator it  = cluster_metadata.machines.machines.begin();
+                    //                                                                                    it != cluster_metadata.machines.machines.end();
+                    //                                                                                    it++) {
+                    //    if (!it->second.is_deleted()) {
+                    //        machine_assignments[it->first] = it->second.get().datacenter.get();
+                    //    }
+                    //}
+                    //fill_in_blueprints_for_protocol<memcached_protocol_t>(&cluster_metadata.memcached_namespaces,
+                    //        directory_metadata->subview(clone_ptr_t<read_lens_t<namespaces_directory_metadata_t<memcached_protocol_t>, cluster_directory_metadata_t> >(field_lens(&cluster_directory_metadata_t::memcached_namespaces))),
+                    //        directory_metadata->subview(clone_ptr_t<read_lens_t<machine_id_t, cluster_directory_metadata_t> >(field_lens(&cluster_directory_metadata_t::machine_id))),
+                    //        machine_assignments,
+                    //        us);
 
                     semilattice_metadata->join(cluster_metadata);
 
