986183682cb06231deac54faa4a6875a3a070b69
Marc Hesse <marc@rethinkdb.com>
2012-04-04 16:18:28 -0700

    updating scenario framework, should be a lot closer to finished, but still has a few bugs

--- test/integration/workload_runner.py
+++ test/integration/workload_runner.py
@@ -1,93 +1,38 @@
 import subprocess, os, time, string, signal
 
-class InfiniteWorkloadRunner(object):
+class WorkloadRunner(object):
 	def __init__(self, command_line):
+		# Check for the executable
+		# TODO: this won't work with complex command-lines
+		executable = string.split(command_line)[0]
+		if not os.access(executable, os.X_OK):
+			raise RuntimeError("specified executable '%s' not found or permissions not set" % (executable))
 		self.command_line = string.split(command_line)
-		self.process = None
+	
+	def run(self, host, port, timeout):
+		start_time = time.time()
+		end_time = start_time + timeout
+		print "Running " + string.join(self.command_line)
 
-	def run(self, timeout):
-		# run the test in a loop until timeout has occurred, then return
-		endTime = time.time() + timeout
-		self.process = subprocess.Popen(self.command_line)
+		# Set up environment
+		new_environ = os.environ.copy()
+		new_environ["HOST"] = host
+		new_environ["PORT"] = str(port)
 
-		while time.time() < endTime:
-			time.sleep(1)
+		proc = subprocess.Popen(self.command_line, env = new_environ)
+
+		while time.time() < end_time:
 			result = self.process.poll()
 			if result is None:
-				continue
+				time.sleep(1)
 			elif result == 0:
-				self.process = subprocess.Popen(self.command_line)
+				print "Done (%d seconds)" % (time.time() - self.start_time)
+				self.process = None
+				return
 			else:
-				raise RuntimeError("workload failed with error code %d" % (result))
-
-	def pause(self):
-		assert self.process is not None
-		self.process.send_signal(signal.SIGSTOP)
-		# TODO: race condition if process died already?
-
-	def resume(self):
-		assert self.process is not None
-		self.process.send_signal(signal.SIGCONT)
-	
-	def finish(self):
-		# finish the current run of the test
-		assert self.process is not None
-		result = self.process.wait()
-		if result != 0:
-			raise RuntimeError("workload failed with error code %d" % (result))
-
-class PhasedWorkloadRunner(object):
-	def __init__(self, command_line, phaseCount):
-		self.command_line = string.split(command_line) + ["--phase"]
-		self.process = None
-		self.phase = 0
-		self.maxPhase = phaseCount - 1
-	
-	def _launch_next_phase(self):
-		assert self.process is None
-		print "Running " + string.join(self.command_line) + str(self.phase)
-		self.process = subprocess.Popen(self.command_line + [str(self.phase)])
-		self.phase += 1
-	
-	def _check_last_phase(self):
-		assert self.process is not None
-		result = self.process.wait()
-		if result != 0:
-			raise RuntimeError("workload '%s' failed in phase %d with error code %d" % (self.command_line, self.phase, result))
-		self.process = None
-
-	def run(self, timeout):
-		assert self.phase == 0
-		self._launch_next_phase()
-		# timeout is ignored, since we only return at a phase break
-	
-	def pause(self):
-		assert self.phase < self.maxPhase # if equal, we are on the last phase, pause will be after the test
-		self._check_last_phase()
-
-	def resume(self):
-		assert self.phase < self.maxPhase
-		self._launch_next_phase()
-
-	def finish(self):
-		assert self.phase != 0
-		self._check_last_phase()
-		while self.phase <= self.maxPhase:
-			self._launch_next_phase()
-			self._check_last_phase()
-
-def create_workload(command_line):
-	# Check for the executable
-	executable = string.split(command_line)[0]
-	if not os.access(executable, os.X_OK):
-		raise RuntimeError("specified executable '%s' not found or permissions not set" % (executable))
-
-	# Get the number of phases
-	phaseCount = subprocess.call(string.split(command_line) + ["--phase-count"])
-	if phaseCount == 0:
-		return InfiniteWorkloadRunner(command_line)
-	elif phaseCount > 0:
-		return PhasedWorkloadRunner(command_line, phaseCount)
-	else:
-		raise RuntimeError("error when determining phase count of executable '%s': %d" % (executable, phaseCount))
+				print "Failed (%d seconds)" % (time.time() - self.start_time)
+				raise RuntimeError("workload '%s' failed in phase %d with error code %d" % (self.command_line, self.phase, result))
+		print "Timed out (%d seconds)" % (time.time() - self.start_time)
+		self.process.send_signal(signal.SIGINT) # TODO: SIGKILL if stubborn
+		raise RuntimeError("workload timed out before completion")
 
