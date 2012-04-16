e60d80249920e19b63c846d9e469802bd4c7d706
Tim Maxwell <tim@rethinkdb.com>
2012-04-16 13:15:27 -0700

    Made test script handle timeouts better.

--- test/scenarios/workload_runner.py
+++ test/scenarios/workload_runner.py
@@ -10,22 +10,24 @@ def run(command_line, host, port, timeout):
 	new_environ["HOST"] = host
 	new_environ["PORT"] = str(port)
 
-	proc = subprocess.Popen(command_line, shell = True, env = new_environ, preexec_fn = os.setsid)
+	proc = subprocess.Popen(command_line, shell = True, env = new_environ, preexec_fn = lambda: os.setpgid(0, 0))
 
-	while time.time() < end_time:
-		result = proc.poll()
-		if result is None:
-			time.sleep(1)
-		elif result == 0:
-			print "Done (%d seconds)" % (time.time() - start_time)
-			return
-		else:
-			print "Failed (%d seconds)" % (time.time() - start_time)
-			raise RuntimeError("workload '%s' failed with error code %d" % (command_line, result))
-	print "Timed out (%d seconds)" % (time.time() - start_time)
 	try:
-            proc.send_signal(signal.SIGKILL)
-        except OSError:
-            pass
+		while time.time() < end_time:
+			result = proc.poll()
+			if result is None:
+				time.sleep(1)
+			elif result == 0:
+				print "Done (%d seconds)" % (time.time() - start_time)
+				return
+			else:
+				print "Failed (%d seconds)" % (time.time() - start_time)
+				raise RuntimeError("workload '%s' failed with error code %d" % (command_line, result))
+		print "Timed out (%d seconds)" % (time.time() - start_time)
+	finally:
+		try:
+			os.killpg(proc.pid, signal.SIGTERM)
+		except OSError:
+			pass
 	raise RuntimeError("workload timed out before completion")
 
