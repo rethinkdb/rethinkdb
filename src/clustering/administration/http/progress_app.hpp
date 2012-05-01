0a79264a5b2b7af39f5b1a43fd867891a67f8751
Sam Hughes <sam@rethinkdb.com>
2012-05-01 15:40:20 -0700

    Added a TODO comment about the name of progress_app_t.

--- src/clustering/administration/http/progress_app.hpp
+++ src/clustering/administration/http/progress_app.hpp
@@ -5,6 +5,8 @@
 #include "containers/clone_ptr.hpp"
 #include "http/http.hpp"
 
+// TODO: Name this progress_http_app_t like the rest of the
+// http_app_t's in this directory.
 class progress_app_t : public http_app_t {
 public:
     progress_app_t(clone_ptr_t<watchable_t<std::map<peer_id_t, cluster_directory_metadata_t> > > _directory_metadata, mailbox_manager_t *_mbox_manager);
