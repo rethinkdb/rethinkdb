4c27d68e78cc2d79061593e09a7c3a03c694423f
jdoliner <jdoliner@gmail.com>
2012-05-03 11:20:03 -0700

    Adds a perfmon_collection so both the parsers and the reactors can get at the same collection for the namespace.

---
+++ src/clustering/administration/perfmon_collection_repo.cc
@@ -0,0 +1,12 @@
+#include "clustering/administration/perfmon_collection_repo.hpp"
+
+perfmon_collection_repo_t::perfmon_collection_repo_t(perfmon_collection_t *_parent)
+    : parent(_parent)
+{ }
+
+perfmon_collection_t *perfmon_collection_repo_t::get_perfmon_collection_for_namespace(namespace_id_t n_id) {
+    if (!std_contains(perfmon_collections, n_id)) {
+        perfmon_collections.insert(n_id, new perfmon_collection_t(uuid_to_str(n_id), parent));
+    }
+    return perfmon_collections.find(n_id)->second;
+}
