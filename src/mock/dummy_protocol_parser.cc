900e89535d7656b2462c300e8340dee4ec9bea6e
Tim Maxwell <tim@rethinkdb.com>
2012-05-07 12:47:58 -0700

    Add a way to check that a cluster_namespace_interface_t has made initial master contact. Combine different parser makers into parser_maker_t.

--- src/mock/dummy_protocol_parser.cc
+++ src/mock/dummy_protocol_parser.cc
@@ -64,42 +64,4 @@ http_res_t query_http_app_t::handle(const http_req_t &req) {
     }
 }
 
-dummy_protocol_parser_maker_t::dummy_protocol_parser_maker_t(mailbox_manager_t *_mailbox_manager,
-                                                             boost::shared_ptr<semilattice_read_view_t<namespaces_semilattice_metadata_t<mock::dummy_protocol_t> > > _namespaces_semilattice_metadata,
-                                                             namespace_repo_t<dummy_protocol_t> *_repo)
-    : mailbox_manager(_mailbox_manager),
-      namespaces_semilattice_metadata(_namespaces_semilattice_metadata),
-      repo(_repo),
-      subscription(boost::bind(&dummy_protocol_parser_maker_t::on_change, this), namespaces_semilattice_metadata)
-{
-    on_change();
-}
-
-void dummy_protocol_parser_maker_t::on_change() {
-    namespaces_semilattice_metadata_t<dummy_protocol_t> snapshot = namespaces_semilattice_metadata->get();
-
-    for (namespaces_semilattice_metadata_t<dummy_protocol_t>::namespace_map_t::iterator it  = snapshot.namespaces.begin();
-                                                                                        it != snapshot.namespaces.end();
-                                                                                        it++) {
-        if (parsers.find(it->first) == parsers.end() && !it->second.is_deleted()) {
-            int port = 10000 + (random() % 10000);
-            //We're feeling lucky
-            namespace_id_t tmp = it->first;
-            parsers.insert(tmp, new parser_and_namespace_if_t(it->first, this, port));
-            logINF("Setup a parser on %d", port);
-        } else if (parsers.find(it->first) != parsers.end() && it->second.is_deleted()) {
-            //The namespace has been deleted... get rid of the parser
-            parsers.erase(it->first);
-        } else {
-            //do nothing
-        }
-    }
-}
-
-dummy_protocol_parser_maker_t::parser_and_namespace_if_t::parser_and_namespace_if_t(namespace_id_t id, dummy_protocol_parser_maker_t *parent, int port)
-    : namespace_access(parent->repo, id),
-      parser(namespace_access.get_namespace_if()),
-      server(port, &parser)
-{ }
-
 } //namespace mock
