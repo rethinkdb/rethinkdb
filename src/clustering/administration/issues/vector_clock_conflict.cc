e31916cd7b926bf7f04e5c8a40e7be7609940454
jdoliner <jdoliner@gmail.com>
2012-05-07 16:29:06 -0700

    Adds port offset as a vector clock issue.

--- src/clustering/administration/issues/vector_clock_conflict.cc
+++ src/clustering/administration/issues/vector_clock_conflict.cc
@@ -57,6 +57,7 @@ std::list<clone_ptr_t<global_issue_t> > vector_clock_conflict_issue_tracker_t::g
         if (!it->second.is_deleted()) {
             check("machine", it->first, "datacenter", it->second.get().datacenter, &issues);
             check("machine", it->first, "name", it->second.get().name, &issues);
+            DEBUG_ONLY_CODE(check("machine", it->first, "port_offset", it->second.get().port_offset, &issues););
         }
     }
 
