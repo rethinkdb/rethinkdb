8ad4563c37f9333265e2a7c0d37477bc60c06574
jdoliner <jdoliner@gmail.com>
2012-06-14 14:02:10 -0700

    Bud bump bump bump another on bites the dust.
    In this case rdb_backfill has bitten the dust.

--- src/rdb_protocol/btree.cc
+++ src/rdb_protocol/btree.cc
@@ -4,22 +4,11 @@
 #include "errors.hpp"
 #include <boost/shared_ptr.hpp>
 
+#include "btree/backfill.hpp"
 #include "containers/archive/vector_stream.hpp"
 #include "rdb_protocol/btree.hpp"
 
-bool btree_value_fits(block_size_t bs, int data_length, const rdb_value_t *value) {
-    return blob::ref_fits(bs, data_length, value->value_ref(), blob::btree_maxreflen);
-}
-
-point_read_response_t db_get(const store_key_t &store_key, btree_slice_t *slice, transaction_t *txn, superblock_t *superblock) {
-    keyvalue_location_t<rdb_value_t> kv_location;
-    find_keyvalue_location_for_read(txn, superblock, store_key.btree_key(), &kv_location, slice->root_eviction_priority, &slice->stats);
-
-    if (!kv_location.value) {
-        return point_read_response_t();
-    }
-
-    const rdb_value_t *value = kv_location.value.get();
+boost::shared_ptr<scoped_cJSON_t> get_data(const rdb_value_t *value, transaction_t *txn) {
     blob_t blob(const_cast<rdb_value_t *>(value)->value_ref(), blob::btree_maxreflen);
 
     boost::shared_ptr<scoped_cJSON_t> data;
@@ -36,6 +25,23 @@ point_read_response_t db_get(const store_key_t &store_key, btree_slice_t *slice,
 
     int res = deserialize(&read_stream, &data);
     guarantee_err(res == 0, "corruption detected... this should probably be an exception\n");
+
+    return data;
+}
+
+bool btree_value_fits(block_size_t bs, int data_length, const rdb_value_t *value) {
+    return blob::ref_fits(bs, data_length, value->value_ref(), blob::btree_maxreflen);
+}
+
+point_read_response_t db_get(const store_key_t &store_key, btree_slice_t *slice, transaction_t *txn, superblock_t *superblock) {
+    keyvalue_location_t<rdb_value_t> kv_location;
+    find_keyvalue_location_for_read(txn, superblock, store_key.btree_key(), &kv_location, slice->root_eviction_priority, &slice->stats);
+
+    if (!kv_location.value) {
+        return point_read_response_t();
+    }
+
+    boost::shared_ptr<scoped_cJSON_t> data = get_data(kv_location.value.get(), txn);
 
     return point_read_response_t(data);
 }
@@ -74,4 +80,40 @@ point_write_response_t rdb_set(const store_key_t &key, boost::shared_ptr<scoped_
     //                                                                     ^-- That means the key isn't expired.
 
     return point_write_response_t(already_existed ? DUPLICATE : STORED);
+}
+
+class agnostic_rdb_backfill_callback_t : public agnostic_backfill_callback_t {
+public:
+    agnostic_rdb_backfill_callback_t(backfill_callback_t *cb, const key_range_t &kr) : cb_(cb), kr_(kr) { }
+
+    void on_delete_range(const key_range_t &range) {
+        rassert(kr_.is_superset(range));
+        cb_->on_delete_range(range);
+    }
+
+    void on_deletion(const btree_key_t *key, repli_timestamp_t recency) {
+        rassert(kr_.contains_key(key->contents, key->size));
+        cb_->on_deletion(key, recency);
+    }
+
+    void on_pair(transaction_t *txn, repli_timestamp_t recency, const btree_key_t *key, const void *val) {
+        rassert(kr_.contains_key(key->contents, key->size));
+        const rdb_value_t *value = static_cast<const rdb_value_t *>(val);
+
+        backfill_atom_t atom;
+        atom.key.assign(key->size, key->contents);
+        atom.value = get_data(value, txn);
+        atom.recency = recency;
+        cb_->on_keyvalue(atom);
+    }
+
+    backfill_callback_t *cb_;
+    key_range_t kr_;
+};
+
+void rdb_backfill(btree_slice_t *slice, const key_range_t& key_range, repli_timestamp_t since_when, backfill_callback_t *callback,
+                    transaction_t *txn, superblock_t *superblock, traversal_progress_t *p) {
+    agnostic_rdb_backfill_callback_t agnostic_cb(callback, key_range);
+    value_sizer_t<rdb_value_t> sizer(slice->cache()->get_block_size());
+    do_agnostic_btree_backfill(&sizer, slice, key_range, since_when, &agnostic_cb, txn, superblock, p);
 }
