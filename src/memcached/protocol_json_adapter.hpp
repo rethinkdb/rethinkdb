7a42157d2a9bf4a8b699d047e3b43c01a3ca13d4
Ivan Tarasov <ivan@rethinkdb.com>
2012-06-18 18:43:10 -0700

    Add perfmon collections for different regions.
    Closes #736 again, but there still can be other places which show up as
    the same problem.

--- src/memcached/protocol_json_adapter.hpp
+++ src/memcached/protocol_json_adapter.hpp
@@ -30,6 +30,9 @@ void apply_json_to(cJSON *, memcached_protocol_t::region_t *, const ctx_t &);
 template <class ctx_t>
 void  on_subfield_change(memcached_protocol_t::region_t *, const ctx_t &);
 
+template <class ctx_t>
+std::string render_region_as_string(memcached_protocol_t::region_t *target, const ctx_t &ctx);
+
 #include "memcached/protocol_json_adapter.tcc"
 
 #endif /* MEMCACHED_PROTOCOL_JSON_ADAPTER_HPP_ */
