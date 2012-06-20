2caf430601cb2c0a8734360add59e70212d8ee4a
Slava Akhmechet <slava@rethinkdb.com>
2012-06-20 12:56:27 -0700

    More test typos

--- test/drivers/python/db.py
+++ test/drivers/python/db.py
@@ -10,13 +10,13 @@ class TestDB(unittest.TestCase):
     # Grabbing a db reference (the following tests are equivalent)
     def test_db_ref_attr(self):
         db = r.db_name
-        assertEqual(db.ast == ast)
+        assertEqual(db.ast == self.ast)
         
     def test_db_ref_dict(self):
         db = r['db_name']
-        assertEqual(db.ast == ast)
+        assertEqual(db.ast == self.ast)
         
     def test_db_ref_direct(self):
         db = r.db('db_name')
-        assertEqual(db.ast == ast)
+        assertEqual(db.ast == self.ast)
 
