e9324a2c343f02b3197adee0d16bd454cd9edb53
Slava Akhmechet <slava@rethinkdb.com>
2012-06-20 12:53:27 -0700

    First set of client driver tests allegedly ready

--- test/drivers/python/db.py
+++ test/drivers/python/db.py
@@ -4,18 +4,19 @@ import unittest
 class TestDB(unittest.TestCase):
     # Shared ast
     def setUp(self):
-        self.ast = ''
+        # The AST these tests should generate
+        self.ast = 'db_name'
         
     # Grabbing a db reference (the following tests are equivalent)
     def test_db_ref_attr(self):
         db = r.db_name
-        assertTrue(protoEq(db, ast))
+        assertEqual(db.ast == ast)
         
     def test_db_ref_dict(self):
         db = r['db_name']
-        assertTrue(protoEq(db, ast))
+        assertEqual(db.ast == ast)
         
     def test_db_ref_direct(self):
         db = r.db('db_name')
-        assertTrue(protoEq(db, ast))
+        assertEqual(db.ast == ast)
 
--- test/drivers/python/table.py
+++ test/drivers/python/table.py
@@ -1,23 +1,28 @@
 import rethinkdb as r
 import unittest
+import query_language_pb2 as p
 
 class TestTable(unittest.TestCase):
     # Shared db ref, ast
     def setUp(self):
+        # A database object
         self.db = r.db_name
-        self.ast = ''
+
+        # The AST these tests should generate
+        self.ast = p.TableRef()
+        self.ast.db_name = 'db_name'
+        self.ast.table_name = 'table_name'
     
     # Grabbing a table reference (the following tests are equivalent)
     def test_table_ref_attr(self):
         t = self.db.table_name
-        assertTrue(protoEq(t, ast))
+        assertEqual(t, ast)
         
     def test_table_ref_dict(self):
         t = self.db['table_name']
-        assertTrue(protoEq(t, ast))
+        assertEqual(t, ast)
         
     def test_table_ref_direct(self):
         t = r.table(self.db, 'table_name')
-        assertTrue(protoEq(t, ast))
+        assertEqual(t, ast)
         
-
