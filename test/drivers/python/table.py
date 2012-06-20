0b52d0c1b735bec3b4c74576b8c7c9644edd37ed
Slava Akhmechet <slava@rethinkdb.com>
2012-06-20 12:57:44 -0700

    yet more test typos, apologies for multiple commits, I'm working without compiling them for now

--- test/drivers/python/table.py
+++ test/drivers/python/table.py
@@ -16,13 +16,13 @@ class TestTable(unittest.TestCase):
     # Grabbing a table reference (the following tests are equivalent)
     def test_table_ref_attr(self):
         t = self.db.table_name
-        assertEqual(t, self.ast)
+        assertEqual(t.ast, self.ast)
         
     def test_table_ref_dict(self):
         t = self.db['table_name']
-        assertEqual(t, self.ast)
+        assertEqual(t.ast, self.ast)
         
     def test_table_ref_direct(self):
         t = r.table(self.db, 'table_name')
-        assertEqual(t, self.ast)
+        assertEqual(t.ast, self.ast)
         
