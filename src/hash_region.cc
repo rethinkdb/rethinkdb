4416209655b20f849f53bc0015aee429959c2f80
Ivan Tarasov <ivan@rethinkdb.com>
2012-06-21 12:57:22 -0700

    Fix explicits

--- src/hash_region.cc
+++ src/hash_region.cc
@@ -17,15 +17,15 @@ uint64_t hash_region_hasher(const uint8_t *s, ssize_t len) {
 
     uint64_t h = 0x47a59e381fb2dc06ULL;
     for (ssize_t i = 0; i < len; ++i) {
-	uint8_t ch = s[i];
+        uint8_t ch = s[i];
 
-	// By the power of magic, the 62nd, 61st, ..., 55th bits of d
-	// are equal to the 0th, 1st, 2nd, ..., 7th bits of ch.  This
-	// helps us meet the criterion specified above.
-	uint64_t d = (((ch * 0x80200802ULL) & 0x0884422110ULL) * 0x0101010101ULL) << 23;
+        // By the power of magic, the 62nd, 61st, ..., 55th bits of d
+        // are equal to the 0th, 1st, 2nd, ..., 7th bits of ch.  This
+        // helps us meet the criterion specified above.
+        uint64_t d = (((ch * 0x80200802ULL) & 0x0884422110ULL) * 0x0101010101ULL) << 23;
 
-	h += d;
-	h = h ^ (h >> 11) ^ (h << 21);
+        h += d;
+        h = h ^ (h >> 11) ^ (h << 21);
     }
 
     return h & 0x7fffffffffffffffULL;
@@ -142,7 +142,7 @@ private:
 };
 
 MUST_USE region_join_result_t region_join(const std::vector< hash_region_t<key_range_t> > &vec,
-					  hash_region_t<key_range_t> *out) {
+                                          hash_region_t<key_range_t> *out) {
 
     // Look at all this allocation we're doing.  Somebody shoot me.
     // These are weird indices pointing into vec, where 0 <=
