7304e41827b95ecdf7ebb019bb172e6ef652e647
Sam Hughes <sam@rethinkdb.com>
2012-07-05 12:09:34 -0700

    Made the debug-mockcache build succeed by removing boost::scoped_ptr usage in blob.cc.

--- src/buffer_cache/blob.cc
+++ src/buffer_cache/blob.cc
@@ -497,7 +497,7 @@ void blob_t::clear(transaction_t *txn) {
 
 void blob_t::write_from_string(const std::string &val, transaction_t *txn, int64_t offset) {
     buffer_group_t dest;
-    boost::scoped_ptr<blob_acq_t> acq(new blob_acq_t);
+    scoped_ptr_t<blob_acq_t> acq(new blob_acq_t);
     expose_region(txn, rwi_write, offset, val.size(), &dest, acq.get());
 
     buffer_group_t src;
@@ -512,7 +512,7 @@ void blob_t::read_to_string(std::string &s_out, transaction_t *txn, int64_t offs
     dest.add_buffer(length, s_out.c_str());
 
     buffer_group_t src;
-    boost::scoped_ptr<blob_acq_t> acq(new blob_acq_t);
+    scoped_ptr_t<blob_acq_t> acq(new blob_acq_t);
     expose_region(txn, rwi_read, offset, length, &src, acq.get());
     buffer_group_copy_data(&dest, const_view(&src));
 }
