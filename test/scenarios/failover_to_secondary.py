075f3b66f2226164206dead363f911cfada74df1
Tim Maxwell <tim@rethinkdb.com>
2012-07-09 16:21:40 -0700

    Add --serve-flags to every scenario.

--- test/scenarios/failover_to_secondary.py
+++ test/scenarios/failover_to_secondary.py
@@ -12,9 +12,11 @@ opts = op.parse(sys.argv)
 with driver.Metacluster() as metacluster:
     print "Starting cluster..."
     cluster = driver.Cluster(metacluster)
-    executable_path, command_prefix  = scenario_common.parse_mode_flags(opts)
-    primary = driver.Process(cluster, driver.Files(metacluster, db_path = "db-1", executable_path = executable_path, command_prefix = command_prefix), log_path = "serve-output-1", executable_path = executable_path, command_prefix = command_prefix)
-    secondary = driver.Process(cluster, driver.Files(metacluster, db_path = "db-2", executable_path = executable_path, command_prefix = command_prefix), log_path = "serve-output-2", executable_path = executable_path, command_prefix = command_prefix)
+    executable_path, command_prefix, serve_options = scenario_common.parse_mode_flags(opts)
+    primary = driver.Process(cluster, driver.Files(metacluster, db_path = "db-1", executable_path = executable_path, command_prefix = command_prefix), log_path = "serve-output-1",
+        executable_path = executable_path, command_prefix = command_prefix, extra_options = serve_options)
+    secondary = driver.Process(cluster, driver.Files(metacluster, db_path = "db-2", executable_path = executable_path, command_prefix = command_prefix), log_path = "serve-output-2",
+        executable_path = executable_path, command_prefix = command_prefix, extra_options = serve_options)
     primary.wait_until_started_up()
     secondary.wait_until_started_up()
 
