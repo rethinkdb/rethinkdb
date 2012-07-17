1bcbc2793ab16d98f327132055d87cc7eb6a64bf
Alex Chen <axcqwerty@gmail.com>
2012-07-16 18:06:05 -0700

    Fixed an error (forgot self)

--- scripts/VirtuaBuild/vm_access.py
+++ scripts/VirtuaBuild/vm_access.py
@@ -37,7 +37,7 @@ class VM(object):
         return proc
     def shut_down(self, remove_temp = False):
         if remove_temp:
-            command("rm -rf /tmp/test.*")
+            self.command("rm -rf /tmp/test.*")
         subprocess.Popen(["ssh %s 'VBoxManage controlvm %s poweroff'" % (control_user, self.uuid)], shell = True).wait()
 
 def sys_exit(message, exit_code, shut_down = False):
