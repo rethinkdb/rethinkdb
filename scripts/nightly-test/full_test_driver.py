8a61ddaaf21c7762b751b6b083ab3c904cbfd3ec
Alex Chen <axcqwerty@gmail.com>
2012-07-18 11:44:17 -0700

    Fixed a typo.

--- scripts/nightly-test/full_test_driver.py
+++ scripts/nightly-test/full_test_driver.py
@@ -219,7 +219,7 @@ with simple_linear_db.LinearDBWriter("result_log.txt") as result_log:
                 "target_os": "centos5_5"
                 }
             builds["suse"] = {
-                "kpommand_line": "cd rethinkdb/src && make rpm PACKAGING=1 PACKAGE_FOR_SUSE_10=1 SYMBOLS=1 DEBUG=1 UNIT_TESTS=0 LEGACY_LINUX=1 LEGACY_GCC=1 NO_EVENTFD=1 LEGACY_PROC_STAT=1 && mv ../build/packages ../build/suse",
+                "command_line": "cd rethinkdb/src && make rpm PACKAGING=1 PACKAGE_FOR_SUSE_10=1 SYMBOLS=1 DEBUG=1 UNIT_TESTS=0 LEGACY_LINUX=1 LEGACY_GCC=1 NO_EVENTFD=1 LEGACY_PROC_STAT=1 && mv ../build/packages ../build/suse",
                 "products": ["rethinkdb/build/suse"],
                 "target_os": "suse"
                 }
