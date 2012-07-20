ad73b86bd9f9e2fe3d59b7e2324cb9892e227e5a
Sam Hughes <sam@rethinkdb.com>
2012-07-20 16:25:34 -0700

    Removed scoped_ptr.hpp from socket_stream.hpp.

--- src/containers/archive/socket_stream.hpp
+++ src/containers/archive/socket_stream.hpp
@@ -1,13 +1,11 @@
 #ifndef CONTAINERS_ARCHIVE_SOCKET_STREAM_HPP_
 #define CONTAINERS_ARCHIVE_SOCKET_STREAM_HPP_
 
-#include "utils.hpp"
-#include <boost/scoped_ptr.hpp>
-
 #include "arch/io/event_watcher.hpp" // linux_event_watcher_t
 #include "arch/io/io_utils.hpp"      // scoped_fd_t
 #include "concurrency/cond_var.hpp"
 #include "containers/archive/archive.hpp"
+#include "containers/scoped.hpp"
 #include "errors.hpp"
 
 /* fd_watcher_t exists to factor the problem of "how to wait for I/O on an fd"
@@ -128,7 +126,7 @@ class socket_stream_t :
     // Member fields
   protected:
     scoped_fd_t fd_;
-    boost::scoped_ptr<fd_watcher_t> fd_watcher_;
+    scoped_ptr_t<fd_watcher_t> fd_watcher_;
 
   private:
     DISABLE_COPYING(socket_stream_t);
