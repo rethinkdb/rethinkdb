3e3496dede21ef8c9e22692a5a7f9a1a119aa0ca
Michael Lucy <mlucy@rethinkdb.com>
2012-08-03 12:17:46 -0700

    Slightly better error handling.

--- drivers/ruby/rethinkdb/rethinkdb.rb
+++ drivers/ruby/rethinkdb/rethinkdb.rb
@@ -4,8 +4,8 @@ require 'socket'
 require 'pp'
 load '/home/mlucy/rethinkdb_ruby/drivers/ruby/rethinkdb/parse.rb'
 
-class ToTerm
-  def _ x; SexpTerm.new x; end
+def _ x; SexpTerm.new x; end
+class Reql
   def attr a; _ [:call, [:implicit_getattr, a], []]; end
   def expr x
     return x.sexp if x.class() == SexpTerm
@@ -26,15 +26,15 @@ class ToTerm
   end
   def method_missing(meth, *args, &block)
     meth = $method_aliases[meth] || meth
-    if enum_type(Builtin::BuiltinType, meth)
-      _ [:call, [meth], args]
-    elsif enum_type(Builtin::Comparison, meth)
-      _ [:call, [:compare, meth], args]
-    else
-      _ [meth, *args]
+    case
+    when enum_type(Builtin::BuiltinType, meth) then _ [:call, [meth], args]
+    when enum_type(Builtin::Comparison, meth)  then _ [:call, [:compare, meth], args]
+    when enum_type(Term::TermType, meth)       then _ [meth, *args]
+    else super(meth, *args, &block)
     end
   end
 end
+$reql = Reql.new
 
 def clean_lst lst
   case lst.class.hash
@@ -65,10 +65,11 @@ class SexpTerm
     meth = $method_aliases[meth] || meth
     if enum_type(Builtin::Comparison, meth)
       _ [:call, [:compare, meth], [@body, *args]]
-    elsif enum_type(Builtin::BuiltinType, meth) and not message_field(Builtin, meth)
-      _ [:call, [meth], [@body, *args]]
-    else
-      _ [:call, [meth, *args], [@body]]
+    elsif enum_type(Builtin::BuiltinType, meth)
+      if message_field(Builtin, meth) then _ [:call, [meth, *args], [@body]]
+      else _ [:call, [meth], [@body, *args]]
+      end
+    else super(meth, *args, &block)
     end
   end
 end
@@ -97,36 +98,37 @@ $reql.all($reql.var('a'), $reql.var('b')).protob
 ################################################################################
 #                                  TERM TESTS                                  #
 ################################################################################
-$reql.var('x').protob ;p :VAR
-$reql.expr(:$x).protob ;p :VAR
+def p? x; x; end
+$reql.var('x').protob ;p? :VAR
+$reql.expr(:$x).protob ;p? :VAR
 $reql.let([["a", 1],
           ["b", 2]],
-          $reql.all($reql.var('a').eq(1), $reql.var('b').eq(2))).protob ;p :LET
+          $reql.all($reql.var('a').eq(1), $reql.var('b').eq(2))).protob ;p? :LET
 #CALL not done explicitly
-$reql.if($reql.var('a'), 1, '2').protob ;p :IF
-$reql.error('e').protob ;p :ERROR
-$reql.expr(5).protob ;p :NUMBER
-$reql.expr('s').protob ;p :STRING
-$reql.json('{type : "json"}').protob ;p :JSON
-$reql.expr(true).protob ;p :BOOL
-$reql.expr(false).protob ;p :BOOL
-$reql.expr(nil).protob ;p :JSON_NULL
-$reql.expr([1, 2, [4, 5]]).protob ;p :ARRAY
-$reql.expr({'a' => 1, 'b' => '2', 'c' => { 'nested' => true }}).protob ;p :OBJECT
-$reql.table('a', 'b').getbykey('attr', 5).protob ;p :GETBYKEY
-$reql.table('a', 'b').protob ;p :TABLE
-$reql.table({:table_name => 'b'}).protob ;p :TABLE
-$reql.javascript('javascript').protob ;p :JAVASCRIPT
+$reql.if($reql.var('a'), 1, '2').protob ;p? :IF
+$reql.error('e').protob ;p? :ERROR
+$reql.expr(5).protob ;p? :NUMBER
+$reql.expr('s').protob ;p? :STRING
+$reql.json('{type : "json"}').protob ;p? :JSON
+$reql.expr(true).protob ;p? :BOOL
+$reql.expr(false).protob ;p? :BOOL
+$reql.expr(nil).protob ;p? :JSON_NULL
+$reql.expr([1, 2, [4, 5]]).protob ;p? :ARRAY
+$reql.expr({'a' => 1, 'b' => '2', 'c' => { 'nested' => true }}).protob ;p? :OBJECT
+$reql.table('a', 'b').getbykey('attr', 5).protob ;p? :GETBYKEY
+$reql.table('a', 'b').protob ;p? :TABLE
+$reql.table({:table_name => 'b'}).protob ;p? :TABLE
+$reql.javascript('javascript').protob ;p? :JAVASCRIPT
 
 ################################################################################
 #                                 BUILTIN TESTS                                #
 ################################################################################
-$reql.not(true).protob ;p :NOT
-$reql.expr(true).not.protob ;p :NOT
-$reql.var('v')['a'].protob ;p :GETATTR
-$reql.var('v')[:a].protob ;p :GETATTR
-$reql.attr('a').protob ;p :IMPLICIT_GETATTR
-$reql.expr(:a).protob ;p :IMPLICIT_GETATTR
+$reql.not(true).protob ;p? :NOT
+$reql.expr(true).not.protob ;p? :NOT
+$reql.var('v')['a'].protob ;p? :GETATTR
+$reql.var('v')[:a].protob ;p? :GETATTR
+$reql.attr('a').protob ;p? :IMPLICIT_GETATTR
+$reql.expr(:a).protob ;p? :IMPLICIT_GETATTR
 #HASATTR
 #IMPLICIT_HASATTR
 #PICKATTRS
@@ -137,53 +139,53 @@ $reql.expr(:a).protob ;p :IMPLICIT_GETATTR
 #ARRAYSLICE
 #ARRAYNTH
 #ARRAYLENGTH
-$reql.add(1, 2).protob ;p :ADD
-$reql.expr(1).add(2).protob ;p :ADD
-($reql.expr(1) + 2).protob ;p :ADD
-$reql.subtract(1, 2).protob ;p :SUBTRACT
-$reql.expr(1).subtract(2).protob ;p :SUBTRACT
-($reql.expr(1) - 2).protob ;p :SUBTRACT
-$reql.multiply(1, 2).protob ;p :MULTIPLY
-$reql.expr(1).multiply(2).protob ;p :MULTIPLY
-($reql.expr(1) * 2).protob ;p :MULTIPLY
-$reql.divide(1, 2).protob ;p :DIVIDE
-$reql.expr(1).divide(2).protob ;p :DIVIDE
-($reql.expr(1) / 2).protob ;p :DIVIDE
-$reql.modulo(1, 2).protob ;p :MODULO
-$reql.expr(1).modulo(2).protob ;p :MODULO
-($reql.expr(1) % 2).protob ;p :MODULO
+$reql.add(1, 2).protob ;p? :ADD
+$reql.expr(1).add(2).protob ;p? :ADD
+($reql.expr(1) + 2).protob ;p? :ADD
+$reql.subtract(1, 2).protob ;p? :SUBTRACT
+$reql.expr(1).subtract(2).protob ;p? :SUBTRACT
+($reql.expr(1) - 2).protob ;p? :SUBTRACT
+$reql.multiply(1, 2).protob ;p? :MULTIPLY
+$reql.expr(1).multiply(2).protob ;p? :MULTIPLY
+($reql.expr(1) * 2).protob ;p? :MULTIPLY
+$reql.divide(1, 2).protob ;p? :DIVIDE
+$reql.expr(1).divide(2).protob ;p? :DIVIDE
+($reql.expr(1) / 2).protob ;p? :DIVIDE
+$reql.modulo(1, 2).protob ;p? :MODULO
+$reql.expr(1).modulo(2).protob ;p? :MODULO
+($reql.expr(1) % 2).protob ;p? :MODULO
 
-$reql.eq(1, 2).protob ;p :COMPARE_EQ
-$reql.expr(1).eq(2).protob ;p :COMPARE_EQ
-$reql.equals(1, 2).protob ;p :COMPARE_EQ
-$reql.expr(1).equals(2).protob ;p :COMPARE_EQ
+$reql.eq(1, 2).protob ;p? :COMPARE_EQ
+$reql.expr(1).eq(2).protob ;p? :COMPARE_EQ
+$reql.equals(1, 2).protob ;p? :COMPARE_EQ
+$reql.expr(1).equals(2).protob ;p? :COMPARE_EQ
 
-$reql.ne(1, 2).protob ;p :COMPARE_NE
-$reql.expr(1).ne(2).protob ;p :COMPARE_NE
-$reql.neq(1, 2).protob ;p :COMPARE_NE
-$reql.expr(1).neq(2).protob ;p :COMPARE_NE
+$reql.ne(1, 2).protob ;p? :COMPARE_NE
+$reql.expr(1).ne(2).protob ;p? :COMPARE_NE
+$reql.neq(1, 2).protob ;p? :COMPARE_NE
+$reql.expr(1).neq(2).protob ;p? :COMPARE_NE
 
-$reql.lt(1, 2).protob ;p :COMPARE_LT
-$reql.expr(1).lt(2).protob ;p :COMPARE_LT
-($reql.expr(1) < 2).protob ;p :COMPARE_LT
+$reql.lt(1, 2).protob ;p? :COMPARE_LT
+$reql.expr(1).lt(2).protob ;p? :COMPARE_LT
+($reql.expr(1) < 2).protob ;p? :COMPARE_LT
 
-$reql.le(1, 2).protob ;p :COMPARE_LE
-$reql.expr(1).le(2).protob ;p :COMPARE_LE
-($reql.expr(1) <= 2).protob ;p :COMPARE_LE
+$reql.le(1, 2).protob ;p? :COMPARE_LE
+$reql.expr(1).le(2).protob ;p? :COMPARE_LE
+($reql.expr(1) <= 2).protob ;p? :COMPARE_LE
 
-$reql.gt(1, 2).protob ;p :COMPARE_GT
-$reql.expr(1).gt(2).protob ;p :COMPARE_GT
-($reql.expr(1) > 2).protob ;p :COMPARE_GT
+$reql.gt(1, 2).protob ;p? :COMPARE_GT
+$reql.expr(1).gt(2).protob ;p? :COMPARE_GT
+($reql.expr(1) > 2).protob ;p? :COMPARE_GT
 
-$reql.ge(1, 2).protob ;p :COMPARE_GE
-$reql.expr(1).ge(2).protob ;p :COMPARE_GE
-($reql.expr(1) >= 2).protob ;p :COMPARE_GE
+$reql.ge(1, 2).protob ;p? :COMPARE_GE
+$reql.expr(1).ge(2).protob ;p? :COMPARE_GE
+($reql.expr(1) >= 2).protob ;p? :COMPARE_GE
 
-$reql.table('','Welcome').filter('row', $reql.eq(:id, 1)).protob ;p :FILTER
-$reql.table('','Welcome').map('row', $reql.expr(:$row)).protob ;p :MAP
+$reql.table('','Welcome').filter('row', $reql.eq(:id, 1)).protob ;p? :FILTER
+$reql.table('','Welcome').map('row', $reql.expr(:$row)).protob ;p? :MAP
 $reql.table('','Welcome').concatmap('row',
     $reql.table('','Other').filter('row2',
-        $reql.expr(:$row)[:id] > :id)).protob ;p :CONCATMAP
+        $reql.expr(:$row)[:id] > :id)).protob ;p? :CONCATMAP
 
 #ORDERBY
 #DISTINCT
@@ -200,22 +202,14 @@ $reql.table('','Welcome').concatmap('row',
 #RANGE
 #SKIP
 
-class Abc < ToTerm
-  @@i = 1
-  def f
-    @@i
-  end
-end
-
-class Tst
-  @@r = ToTerm.new
-  def self.r
-    @@r
-  end
-end
-
-class Foo < Tst
-  def f
-    r.expr(:id)
+class Tst5
+  def method_missing(meth, *args, &block)
+    if meth == :tst
+      PP.pp([meth, args, block])
+      $out = block
+      block.call(:block) if block
+    else
+      super(meth)
+    end
   end
 end
