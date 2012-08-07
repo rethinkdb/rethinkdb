7a4eaba8bc2928b49a31621625caf111f7dbcfbf
Michael Lucy <mlucy@rethinkdb.com>
2012-08-06 18:23:09 -0700

    Broke out code.

--- drivers/ruby/rethinkdb/query.rb
+++ drivers/ruby/rethinkdb/query.rb
@@ -1,12 +1,3 @@
-require 'rubygems'
-require 'query_language.pb.rb'
-require 'socket'
-require 'pp'
-
-load 'utils.rb'
-load 'query.rb'
-load 'protob_compiler.rb'
-
 module RethinkDB
   class S #Sexp
     @@gensym_counter = 0
