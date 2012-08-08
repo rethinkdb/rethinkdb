9671440246ada664535643a64da8c3d7a7f5f823
Michael Lucy <mlucy@rethinkdb.com>
2012-08-07 20:56:24 -0700

    More tests.

--- drivers/ruby/rethinkdb/test.rb
+++ drivers/ruby/rethinkdb/test.rb
@@ -1,49 +1,21 @@
 load '/home/mlucy/rethinkdb_ruby/drivers/ruby/rethinkdb/rethinkdb.rb'
 r = RethinkDB::RQL
-################################################################################
-#                                  TERM TESTS                                  #
-################################################################################
-def p? x; x; end
-r.javascript('javascript').query ;p? :JAVASCRIPT
-
-################################################################################
-#                                 BUILTIN TESTS                                #
-################################################################################
-#TODO: Does pickattrs even work?
-r.var('v').pickattrs('a').query ;p? :PICKATTRS
-r.var('v').attrs('a').query ;p? :PICKATTRS
-r.attrs('a').query ;p? :IMPLICIT_PICKATTRS
-
-# These are going away?
-#MAPMERGE
-#ARRAYAPPEND
-#ARRAYCONCAT
-#ARRAYSLICE
-#ARRAYNTH
-#ARRAYLENGTH
-
-r.limit(:$var).query ;p? :LIMIT
-r.length(:$var).query ;p? :LENGTH
-r.union(:$var1, :$var2).query ;p? :LENGTH
-r.nth(:$var1).query ;p? :LENGTH
-r.streamtoarray(r.table('','Welcome').all).query ;p? :STREAMTOARRAY
-
-#GROUPEDMAPREDUCE -- how does this work?
-
-r.or(true, true, false).query ;p? :ANY
-r.expr(true).or(false).query ;p? :ANY
-r.all(true, true, false).query ;p? :ALL
-r.expr(true).and(false).query ;p? :ALL
-
-r.table('','Welcome').range('id', 1, 2).query ;p? :RANGE
-r.table('','Welcome').range(:id, 1, 2).query ;p? :RANGE
-r.table('','Welcome').range({:attrname => :id, :upperbound => 2}).query ;p? :RANGE
-
-#SKIP -- how does this work?
+# TODO:
+# Confirm these are going away:
+#   * MAPMERGE
+#   * ARRAYAPPEND
+#   * ARRAYCONCAT
+#   * ARRAYSLICE
+#   * ARRAYNTH
+#   * ARRAYLENGTH
+# Figure out how to use:
+#   * JAVASCRIPT
+#   * GROUPEDMAPREDUCE
 
 ################################################################################
 #                                 WRITE QUERY                                  #
 ################################################################################
+def p? x; x; end
 
 r.db('').Welcome.update{|x| x}.query ;p? :UPDATE
 r.db('').Welcome.filter{|x| x[:id] < 5}.update{|x| x}.query ;p? :UPDATE
@@ -88,7 +60,7 @@ class ClientTest < Test::Unit::TestCase
   @@c = RethinkDB::Connection.new('localhost', 64346)
   def c; @@c; end
 
-  def test_ops #+,-,%,*,/,<,>,<=,>=,eq,ne
+  def test_ops #+,-,%,*,/,<,>,<=,>=,eq,ne,any,all
     assert_equal((r[5] + 3).run, 8)
     assert_equal((r[5].add(3)).run, 8)
     assert_equal(r.add(5,3).run, 8)
@@ -184,15 +156,15 @@ class ClientTest < Test::Unit::TestCase
     assert_equal((r[true].or(false)).run, true)
   end
 
-  def test_let #LET, CALL, ADD, VAR, NUMBER, STRING
+  def test_let #LET, CALL, VAR, NUMBER, STRING
     query = r.let([['a', r.add(1,2)], ['b', r.add(:$a,2)]], r.var('a') + :$b)
     assert_equal(query.run, 8)
   end
 
   def test_easy_read #TABLE
-    assert_equal($welcome_data, rdb.run)
-    assert_equal($welcome_data, r.table('', 'Welcome').run)
-    assert_equal($welcome_data, r.table({:table_name => 'Welcome'}).run)
+    assert_equal($data, rdb.run)
+    assert_equal($data, r.table('', 'Welcome').run)
+    assert_equal($data, r.table({:table_name => 'Welcome'}).run)
   end
 
   def test_error #IF, JSON, ERROR
@@ -210,11 +182,11 @@ class ClientTest < Test::Unit::TestCase
   def test_getbykey #OBJECT, GETBYKEY
     query = r.expr({'obj' => rdb.getbykey(:id, 0)})
     query2 = r[{'obj' => rdb.getbykey(:id, 0)}]
-    assert_equal(query.run['obj'], $welcome_data[0])
-    assert_equal(query2.run['obj'], $welcome_data[0])
+    assert_equal(query.run['obj'], $data[0])
+    assert_equal(query2.run['obj'], $data[0])
   end
 
-  def test_map #MAP, FILTER, LT, GETATTR, IMPLICIT_GETATTR
+  def test_map #MAP, FILTER, GETATTR, IMPLICIT_GETATTR
     query = rdb.map { |outer_row|
       r.streamtoarray(rdb.filter{r[:id] < outer_row[:id]})
     }
@@ -224,57 +196,97 @@ class ClientTest < Test::Unit::TestCase
     query3 = rdb.map { |outer_row|
       r.streamtoarray(rdb.filter{r.attr('id').lt(outer_row.getattr(:id))})
     }
-    assert_equal(query.run[2], $welcome_data[0..1])
-    assert_equal(query2.run[2], $welcome_data[0..1])
-    assert_equal(query3.run[2], $welcome_data[0..1])
+    assert_equal(query.run[2], $data[0..1])
+    assert_equal(query2.run[2], $data[0..1])
+    assert_equal(query3.run[2], $data[0..1])
   end
 
-  def test_reduce #REDUCE, NOT, HASATTR, IMPLICIT_HASATTR
+  def test_reduce #REDUCE, HASATTR, IMPLICIT_HASATTR
     #REDUCE NOT IMPLEMENTED
     #query = rdb.map{r.hasattr(:id)}.reduce(true){|a,b| r.all a,b}
-    assert_equal(rdb.map{r.hasattr(:id)}.run, $welcome_data.map{true})
-    assert_equal(rdb.map{|row| r.not row.hasattr('id')}.run, $welcome_data.map{false})
-    assert_equal(rdb.map{r.attr?(:id)}.run, $welcome_data.map{true})
-    assert_equal(rdb.map{r.attr?('id').not}.run, $welcome_data.map{false})
+    assert_equal(rdb.map{r.hasattr(:id)}.run, $data.map{true})
+    assert_equal(rdb.map{|row| r.not row.hasattr('id')}.run, $data.map{false})
+    assert_equal(rdb.map{r.attr?(:id)}.run, $data.map{true})
+    assert_equal(rdb.map{r.attr?('id').not}.run, $data.map{false})
   end
 
-  def test_filter #FILTER, ANY, ALL, EQ
+  def test_filter #FILTER
     #BROKEN: Can't filter on strings
     #query_5 = rdb.filter{r[:name].eq('5')}
     query_2345 = rdb.filter{|row| r.and r[:id] >= 2,row[:id] <= 5}
     query_234 = query_2345.filter{r[:num].neq(5)}
     query_23 = query_234.filter{|row| r.any row[:num].eq(2),row[:num].equals(3)}
-    assert_equal(query_2345.run, $welcome_data[2..5])
-    assert_equal(query_234.run, $welcome_data[2..4])
-    assert_equal(query_23.run, $welcome_data[2..3])
+    assert_equal(query_2345.run, $data[2..5])
+    assert_equal(query_234.run, $data[2..4])
+    assert_equal(query_23.run, $data[2..3])
   end
 
   def test_orderby #ORDERBY, MAP
-    assert_equal(rdb.orderby(:id).run, $welcome_data)
-    assert_equal(rdb.orderby('id').run, $welcome_data)
-    assert_equal(rdb.orderby([:id]).run, $welcome_data)
-    assert_equal(rdb.orderby([:id,true]).run, $welcome_data)
-    assert_equal(rdb.orderby([:id,false]).run, $welcome_data.reverse)
+    assert_equal(rdb.orderby(:id).run, $data)
+    assert_equal(rdb.orderby('id').run, $data)
+    assert_equal(rdb.orderby([:id]).run, $data)
+    assert_equal(rdb.orderby([:id,true]).run, $data)
+    assert_equal(rdb.orderby([:id,false]).run, $data.reverse)
     query = rdb.map{r[{:id => r[:id],:num => r[:id].mod(2)}]}.orderby(:num,['id',false])
-    want = $welcome_data.map{|o| o['id']}.sort_by{|n| (n%2)*$welcome_data.length - n}
+    want = $data.map{|o| o['id']}.sort_by{|n| (n%2)*$data.length - n}
     assert_equal(query.run.map{|o| o['id']}, want)
   end
 
-  def test_concatmap #CONCATMAP, DISTINCT, MULTIPLY
+  def test_concatmap #CONCATMAP, DISTINCT
     query = rdb.concatmap{|row| rdb.map{r[:id] * row[:id]}}.distinct
-    nums = $welcome_data.map{|o| o['id']}
+    nums = $data.map{|o| o['id']}
     want = nums.map{|n| nums.map{|m| n*m}}.flatten(1).uniq
     assert_equal(query.run.sort, want.sort)
   end
 
+  def test_range #RANGE
+    assert_equal(rdb.range(:id, 1, 3).run, $data[1..3])
+    assert_equal(rdb.range({:attrname => :id, :upperbound => 4}).run,$data[0..4])
+  end
+
+  def test_limit #LIMIT, SKIP
+    assert_equal(rdb.limit(5).skip(2).run, $data[2..4])
+  end
+
+  def test_nth #NTH
+    assert_equal(rdb.nth(2).run, $data[2])
+  end
+
+  def test_pickattrs #PICKATTRS, #UNION, #LENGTH
+    q1=r.union(rdb.map{r.pickattrs(:id,:name)}, rdb.map{|row| row.pickattrs(:id,:num)})
+    q2=r.union(rdb.map{r.attrs(:id,:name)}, rdb.map{|row| row.attrs(:id,:num)})
+    q1v = q1.run
+    assert_equal(q1v, q2.run)
+    len = $data.length
+    assert_equal(q2.length.run, 2*len)
+    assert_equal(q1v.map{|o| o['num']}[0..len-1], Array.new(len,nil))
+    assert_equal(q1v.map{|o| o['name']}[len..2*len-1], Array.new(len,nil))
+  end
+
+  def test__write #two underscores so it runs first
+    $data = []
+    len = 10
+    Array.new(len).each_index{|i| $data << {'id'=>i,'num'=>i,'name'=>i.to_s}}
+    assert_equal(rdb.insert($data).run[0]['inserted'], $data.length)
+    assert_equal(rdb.run, $data)
+    assert_equal(rdb.insert({:id => 0, :broken => true}).run[0]['inserted'], 1)
+    assert_equal(rdb.run[1..len-1], $data[1..len-1])
+    assert_equal(rdb.run[0]['broken'], true)
+    #PP.pp rdb.update{|x| r.if(x.attr?(:broken), $data[0], nil)}.run
+    update = rdb.update{|x| r.if(x.attr?(:broken), $data[0], x)}.run
+    PP.pp 'here'
+    assert_equal(update[0], {'errors' => 0, 'updated' => 10})
+    assert_equal(rdb.run, $data)
+
+    assert_equal(rdb.run, $data)
+  end
 end
 
-
-c = RethinkDB::Connection.new('localhost', 64346)
-rdb = r.db('').Welcome
-$welcome_data = []
-Array.new(10).each_index{|i| $welcome_data << {'id' => i, 'num' => i, 'name' => i.to_s}}
-c.run(rdb.insert($welcome_data).query)
-rdb.map{|row|row.pickattrs(:id,:name)}.query
+# c = RethinkDB::Connection.new('localhost', 64346)
+# rdb = r.db('').Welcome
+# $welcome_data = []
+# Array.new(10).each_index{|i| $welcome_data << {'id' => i, 'num' => i, 'name' => i.to_s}}
+# c.run(rdb.insert($welcome_data).query)
+# rdb.map{|row|row.pickattrs(:id,:name)}.query
 #r[{:a => 1}].pickattrs(:id).run
 
