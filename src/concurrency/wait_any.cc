0f8d20aade6e6f6d39e75b92d0f793e7a6e56033
Marc Hesse <marc@rethinkdb.com>
2012-08-15 17:53:13 -0700

    attempt to reduce dynamic allocation by adding preallocated wait_any_subscribtion_ts to the wait_any_t class

--- src/concurrency/wait_any.cc
+++ src/concurrency/wait_any.cc
@@ -1,18 +1,13 @@
 #include "concurrency/wait_any.hpp"
 
-class wait_any_subscription_t : public signal_t::subscription_t, public intrusive_list_node_t<wait_any_subscription_t> {
-public:
-    explicit wait_any_subscription_t(wait_any_t *parent) : parent_(parent) { }
-    virtual void run();
-private:
-    wait_any_t *parent_;
-    DISABLE_COPYING(wait_any_subscription_t);
-};
-
-void wait_any_subscription_t::run() {
-    parent_->pulse_if_not_already_pulsed();
+void wait_any_t::wait_any_subscription_t::init(wait_any_t *parent) {
+    parent_ = parent;
 }
 
+void wait_any_t::wait_any_subscription_t::run() {
+    rassert(parent_ != NULL);
+    parent_->pulse_if_not_already_pulsed();
+}
 
 wait_any_t::wait_any_t() {
 }
@@ -51,13 +46,25 @@ wait_any_t::~wait_any_t() {
     while (!subs.empty()) {
         wait_any_subscription_t *p = subs.head();
         subs.remove(p);
-        delete p;
+
+        // Only delete the subscription if it was allocated in dynamic memory
+        if (p < &sub_storage[0] || p > &sub_storage[default_preallocated_subs - 1]) {
+            delete p;
+        }
     }
 }
 
 void wait_any_t::add(signal_t *s) {
     rassert(s);
-    wait_any_subscription_t *sub = new wait_any_subscription_t(this);
+    wait_any_subscription_t *sub;
+
+    // Use preallocated subscriptions, if possible, to save on dynamic memory usage
+    if (subs.size() < default_preallocated_subs) {
+        sub = &sub_storage[subs.size()];
+    } else {
+        sub = new wait_any_subscription_t();
+    }
+    sub->init(this);
     sub->reset(s);
     subs.push_back(sub);
 }
