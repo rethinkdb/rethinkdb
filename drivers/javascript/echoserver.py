80b1bccd7520901cda014792cc5a5c56bd5e6872
Bill <bill@rethinkdb.com>
2012-08-21 13:00:57 -0700

    gets rid of file from earlier javascript testing

--- drivers/javascript/echoserver.py
+++
@@ -1,9 +0,0 @@
-import SocketServer
-
-class EchoHandler(SocketServer.BaseRequestHandler):
-    def handle(self):
-        print "Recv'd"
-        self.request.sendall("bob")
-
-server = SocketServer.TCPServer(('localhost', 11211), EchoHandler)
-server.serve_forever()
