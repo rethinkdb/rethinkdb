9aad16a031d43712cce21bc742f96c923f79f3cd
Michael Lucy <mlucy@rethinkdb.com>
2012-08-22 19:23:01 -0700

    updated docs

--- drivers/ruby/rethinkdb/rethinkdb_shortcuts.rb
+++ drivers/ruby/rethinkdb/rethinkdb_shortcuts.rb
@@ -1,13 +1,11 @@
 load 'rethinkdb.rb'
 # Right now, this is a place to record high-level spec changes that need to
 # happen.  TODO:
-# * UPDATE needs to be changed to do an implicit mapmerge on its righthand side
-#   (this will make its behavior line up with the Python documentation).
 # * MUTATE needs to be renamed to REPLACE (Joe and Tim and I just talked about
 #   this) and maintain its current behavior rather than changing to match
 #   UPDATE.
 # * The following are unimplemented: REDUCE, GROUPEDMAPREDUCE, POINTMUTATE
-# * The following are buggy: UPDATE, POINTUPDATE, FOREACH
+# * The following are buggy: FOREACH
 # * I don't understand how GROUPEDMAPREDUCE works.
 module RethinkDB
   # Shortcuts to easily build RQL queries.
