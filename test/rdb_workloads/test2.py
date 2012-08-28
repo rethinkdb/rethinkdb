21637818ced5de41debbb780492ae6a3da5faaf0
Tim Maxwell <tim@rethinkdb.com>
2012-08-28 14:28:39 -0700

    Update Python client to expect error count.

--- test/rdb_workloads/test2.py
+++ test/rdb_workloads/test2.py
@@ -482,7 +482,7 @@ class RDBTest(unittest.TestCase):
         docs = [{"id": 100 + n, "a": n, "b": n % 3} for n in range(10)]
         self.do_insert(docs)
 
-        self.expect(self.table.mutate(fn('x', R('$x'))), {"modified": len(docs), "deleted": 0})
+        self.expect(self.table.mutate(fn('x', R('$x'))), {"modified": len(docs), "deleted": 0, "errors": 0})
         self.expect(self.table.orderby("id"), docs)
 
         self.expect(self.table.update(None), {'updated': 0, 'skipped': 10, 'errors': 0})
