c04f16bb81928673e70dc12cf0f29878bf986541
jdoliner <jdoliner@gmail.com>
2012-08-28 17:21:26 -0700

    Hooks up the distribution query... and reveals the fact that the unshard methods are missing.

--- src/clustering/administration/http/server.cc
+++ src/clustering/administration/http/server.cc
@@ -38,6 +38,7 @@ administrative_http_server_manager_t::administrative_http_server_manager_t(
         boost::shared_ptr<semilattice_readwrite_view_t<cluster_semilattice_metadata_t> > _semilattice_metadata,
         clone_ptr_t<watchable_t<std::map<peer_id_t, cluster_directory_metadata_t> > > _directory_metadata,
         namespace_repo_t<memcached_protocol_t> *_namespace_repo,
+        namespace_repo_t<rdb_protocol_t> *_rdb_namespace_repo,
         admin_tracker_t *_admin_tracker,
         local_issue_tracker_t *_local_issue_tracker,
         http_app_t *reql_app,
@@ -117,7 +118,8 @@ administrative_http_server_manager_t::administrative_http_server_manager_t(
         _directory_metadata->subview(&get_log_mailbox),
         _directory_metadata->subview(&get_machine_id)));
     progress_app.init(new progress_app_t(_directory_metadata, mbox_manager));
-    distribution_app.init(new distribution_app_t(metadata_field(&cluster_semilattice_metadata_t::memcached_namespaces, _semilattice_metadata), _namespace_repo));
+    distribution_app.init(new distribution_app_t(metadata_field(&cluster_semilattice_metadata_t::memcached_namespaces, _semilattice_metadata), _namespace_repo,
+                                                 metadata_field(&cluster_semilattice_metadata_t::rdb_namespaces, _semilattice_metadata), _rdb_namespace_repo));
 
 #ifndef NDEBUG
     cyanide_app.init(new cyanide_http_app_t);
