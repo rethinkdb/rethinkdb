d9cafe4ef95712012627052dc0dc369fe3370cd9
Slava Akhmechet <slava@rethinkdb.com>
2012-09-21 12:51:02 -0700

    Fixing up clients to account for renamed objects

--- drivers/ruby/quickstart.rb
+++ drivers/ruby/quickstart.rb
@@ -3,10 +3,8 @@ load 'rethinkdb.rb'
 extend RethinkDB::Shortcuts
 port_offset = ENV['PORT_OFFSET'].to_i || 0 if not port_offset
 print "WARNING: No `PORT_OFFSET` environment variable, using 0\n" if port_offset == 0
-$c = RethinkDB::Connection.new('localhost', port_offset + 12346, 'Welcome-db')
-$rdb = r.db('Welcome-db').table('Welcome-rdb')
+$c = RethinkDB::Connection.new('localhost', port_offset + 12346, 'test')
 print "Loaded shortcut: r\n"
 print "Connection: $c\n"
-print "Welcome-rdb table: $rdb (i.e. `$rdb = r.db('Welcome-db','Welcome-rdb')`)\n"
 print "Examples: `r.list.run`, `$rdb.insert({:id => 0}).run`, `$rdb.run`\n"
 
