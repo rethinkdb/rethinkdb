284230192cdf08936a64b66bedffcf7e4dbf316c
Sam Hughes <sam@rethinkdb.com>
2012-09-24 17:51:17 -0700

    Reviewed assertions in other memcached code.

--- src/memcached/memcached_btree/value.hpp
+++ src/memcached/memcached_btree/value.hpp
@@ -68,8 +68,7 @@ public:
         // We shouldn't be asking for a value's CAS unless we know it has one.
         cas_t ret;
         bool hascas = metadata_memcached_cas(metadata_flags, contents, &ret);
-        (void)hascas;
-        rassert_unreviewed(hascas);
+        guarantee_reviewed(hascas);
         return ret;
     }
 
