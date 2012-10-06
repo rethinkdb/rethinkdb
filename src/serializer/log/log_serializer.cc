f1464041c6479e312e18f68b9ddcb1eb790f161f
Sam Hughes <sam@rethinkdb.com>
2012-10-06 00:35:44 -0700

    Removed spurious initializer list entry in log_serializer_t (which was there just to make sure that was the first declared variable in the class).

--- src/serializer/log/log_serializer.cc
+++ src/serializer/log/log_serializer.cc
@@ -218,8 +218,7 @@ struct ls_start_existing_fsm_t :
 };
 
 log_serializer_t::log_serializer_t(dynamic_config_t _dynamic_config, io_backender_t *io_backender_, private_dynamic_config_t _private_config, perfmon_collection_t *_perfmon_collection)
-    : token_offsets(),
-      stats(new log_serializer_stats_t(_perfmon_collection)),
+    : stats(new log_serializer_stats_t(_perfmon_collection)),
       disk_stats_collection(),
       disk_stats_membership(_perfmon_collection, &disk_stats_collection, "disk"),
 #ifndef NDEBUG
