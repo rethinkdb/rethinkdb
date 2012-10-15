17cf268376d2f228c49d6755afaba7ce4bc09513
Sam Hughes <sam@rethinkdb.com>
2012-10-14 23:53:25 -0700

    Used clock_monotonic function in log_message.cc.

--- src/unittest/log_message.cc
+++ src/unittest/log_message.cc
@@ -7,12 +7,10 @@ namespace unittest {
 TEST(LogMessageTest, ParseFormat) {
 
     struct timespec timestamp;
-    int res = clock_gettime(CLOCK_REALTIME, &timestamp);
-    ASSERT_EQ(0, res);
+    clock_monotonic(&timestamp);
 
     struct timespec uptime;
-    res = clock_gettime(CLOCK_MONOTONIC, &uptime);
-    ASSERT_EQ(0, res);
+    clock_monotonic(&uptime);
 
     log_message_t message(timestamp, uptime, log_level_info, "test message *(&Q(!#@LJVO");
     std::string formatted = format_log_message(message);
