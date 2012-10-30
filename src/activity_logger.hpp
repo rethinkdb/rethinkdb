ea648c78b46958a56f713a827f180e353d7e09aa
Sam Hughes <sam@rethinkdb.com>
2012-10-30 15:40:53 -0700

    Specified inheritance privileges and used DISABLE_COPYING in activity_logger.hpp.

--- src/activity_logger.hpp
+++ src/activity_logger.hpp
@@ -17,13 +17,16 @@ class log_event_t {
 public:
     log_event_t(const std::string &_msg, bool log_bt = true);
     std::string print(bool print_bt);
+
 private:
     microtime_t timestamp;
     std::string msg;
     scoped_ptr_t<lazy_backtrace_t> bt;
+
+    DISABLE_COPYING(log_event_t);
 };
 
-class activity_logger_t : home_thread_mixin_t {
+class activity_logger_t : private home_thread_mixin_t {
 public:
     explicit activity_logger_t(bool _log_bt = true);
     void add(const std::string &msg); // Use value of [log_bt] from constructor
@@ -31,18 +34,25 @@ public:
     size_t size();
     std::string print(bool print_bt = true);
     std::string print_range(size_t start, size_t end, bool print_bt = true);
+
 private:
     bool log_bt_;
     boost::ptr_vector<log_event_t> events;
+
+    DISABLE_COPYING(activity_logger_t);
 };
 
-struct fake_activity_logger_t : home_thread_mixin_t {
+struct fake_activity_logger_t : private home_thread_mixin_t {
     explicit fake_activity_logger_t(UNUSED bool b = true) { }
     void add(const std::string &msg) {
         assert_thread();
         events.push_back(msg);
     }
+
+private:
     std::vector<std::string> events;
+
+    DISABLE_COPYING(fake_activity_logger_t);
 };
 
 #define debugf_log(log, args...) {                                      \
