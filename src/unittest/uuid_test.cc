44c826bebfe886794e4865aeca92e3a35f506c76
Sam Hughes <sam@rethinkdb.com>
2012-10-30 00:11:38 -0700

    Revert "Added UuidTest.Sha1."
    This reverts commit bca55df6760d297e7c8b93cd74ebe410f0898aed.

--- src/unittest/uuid_test.cc
+++ src/unittest/uuid_test.cc
@@ -1,14 +1,6 @@
 #include "unittest/gtest.hpp"
 
-#include <arpa/inet.h>
-
 #include "containers/uuid.hpp"
-
-// We keep the sha1 function hidden to avoid encouraging others from using it.
-namespace sha1 {
-// hash is a pointer to 20-byte array, unfortunately.
-void calc(const void* src, const int bytelength, unsigned char* hash);
-}
 
 namespace unittest {
 
@@ -59,33 +51,6 @@ TEST(UuidTest, StrToUuid) {
     std::string w2 = "f47ac10b-58cc-4372-a567-0e02b2c3d47";
     failure = str_to_uuid(w2, &y);
     ASSERT_FALSE(failure);
-}
-
-void check_sha(const std::string &str, uint32_t expected[5]) {
-    union {
-        uint8_t hash[24];
-        uint32_t ghetto[6];
-    } u;
-    u.ghetto[5] = 0xcafeb4b3;
-
-    sha1::calc(str.data(), str.size(), u.hash);
-
-    for (int i = 0; i < 5; ++i) {
-        EXPECT_EQ(expected[i], htonl(u.ghetto[i]));
-    }
-    EXPECT_EQ(0xcafeb4b3, u.ghetto[5]);
-}
-
-TEST(UuidTest, Sha1) {
-    std::string sentence = "The quick brown fox jumps over the lazy dog";
-    uint32_t expected[5] = { 0x2fd4e1c6, 0x7a2d28fc, 0xed849ee1, 0xbb76e739, 0x1b93eb12 };
-
-    check_sha(sentence, expected);
-
-    std::string empty = "";
-    uint32_t empty_expected[5] = { 0xda39a3ee, 0x5e6b4b0d, 0x3255bfef, 0x95601890, 0xafd80709 };
-
-    check_sha(empty, empty_expected);
 }
 
 
