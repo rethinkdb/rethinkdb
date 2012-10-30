58c49c0643cc3884176e3e7606e1684526d16f08
Bill Rowan <bill@rethinkdb.com>
2012-10-29 18:27:05 -0700

    adds python distutils setup file for distribution

---
+++ drivers/python/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name="rethinkdb"
+     ,version="1.2.0-0"
+     ,description="This package provides the Python driver library for the RethinkDB database server."
+     ,url="http://rethinkdb.com"
+     ,maintainer="RethinkDB Inc."
+     ,maintainer_email="bugs@rethinkdb.com"
+     ,packages=['rethinkdb']
+     )
