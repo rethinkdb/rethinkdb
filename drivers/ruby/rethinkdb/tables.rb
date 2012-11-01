683127233d3c11cdb2cf701f78edc0b3b3e7ec73
Michael Lucy <mlucy@rethinkdb.com>
2012-10-31 18:20:11 -0700

    Added some optional argument checking.

--- drivers/ruby/rethinkdb/tables.rb
+++ drivers/ruby/rethinkdb/tables.rb
@@ -68,6 +68,9 @@ module RethinkDB
       @table_name = name;
       @opts = opts
       @context = caller
+      if (bad_opts = (@opts.map{|k,_|k}) - [:use_outdated]) != []
+        raise ArgumentError,"Unrecognized options: #{bad_opts.inspect}"
+      end
       use_od = (@opts.include? :use_outdated) ? @opts[:use_outdated] : S.conn_outdated
       @body = [:table, @db_name, @table_name, use_od]
     end
