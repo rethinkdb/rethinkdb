fb901e8c8d027dbbf44211d2b3fc057fb2e9bb04
Sam Hughes <sam@rethinkdb.com>
2012-11-12 14:29:07 -0800

    Made spinlock_t use a pthread_mutex_t when __APPLE__.

--- src/arch/spinlock.hpp
+++ src/arch/spinlock.hpp
@@ -7,30 +7,58 @@
 
 #include "errors.hpp"
 
+// TODO(OSX) Reconsider how to detect spinlock feature.
+#if __APPLE__
+#define PTHREAD_HAS_SPINLOCK 0
+#else
+#define PTHREAD_HAS_SPINLOCK 1
+#endif
+
+
 class spinlock_t {
 public:
     friend class spinlock_acq_t;
 
     spinlock_t() {
+#if PTHREAD_HAS_SPINLOCK
         pthread_spin_init(&l, PTHREAD_PROCESS_PRIVATE);
+#else
+        pthread_mutex_init(&l, NULL);
+#endif
     }
     ~spinlock_t() {
+#if PTHREAD_HAS_SPINLOCK
         pthread_spin_destroy(&l);
+#else
+        pthread_mutex_destroy(&l);
+#endif
     }
 
 private:
     void lock() {
+#if PTHREAD_HAS_SPINLOCK
         int res = pthread_spin_lock(&l);
+#else
+        int res = pthread_mutex_lock(&l);
+#endif
         guarantee_err(res == 0, "could not lock spin lock");
     }
     void unlock() {
+#if PTHREAD_HAS_SPINLOCK
         int res = pthread_spin_unlock(&l);
+#else
+        int res = pthread_mutex_unlock(&l);
+#endif
         guarantee_err(res == 0, "could not unlock spin lock");
     }
 
+#if PTHREAD_HAS_SPINLOCK
     pthread_spinlock_t l;
-    spinlock_t(const spinlock_t&);
-    void operator=(const spinlock_t&);
+#else
+    pthread_mutex_t l;
+#endif
+
+    DISABLE_COPYING(spinlock_t);
 };
 
 class spinlock_acq_t {
@@ -44,8 +72,8 @@ public:
 
 private:
     spinlock_t *the_lock_;
-    spinlock_acq_t(const spinlock_acq_t&);
-    void operator=(const spinlock_acq_t&);
+
+    DISABLE_COPYING(spinlock_acq_t);
 };
 
 #endif /* ARCH_SPINLOCK_HPP_ */
