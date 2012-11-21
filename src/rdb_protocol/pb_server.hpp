d1cc40fa990ac364bb5ba286b501ee2d517474e5
Sam Hughes <sam@rethinkdb.com>
2012-11-21 13:48:18 -0800

    Adjusted includes to include-what-you-use, etc.

--- src/rdb_protocol/pb_server.hpp
+++ src/rdb_protocol/pb_server.hpp
@@ -3,10 +3,8 @@
 #define RDB_PROTOCOL_PB_SERVER_HPP_
 
 #include <map>
+#include <set>
 #include <string>
-
-#include "errors.hpp"
-#include <boost/shared_ptr.hpp>
 
 #include "clustering/administration/metadata.hpp"
 #include "clustering/administration/namespace_interface_repository.hpp"
@@ -36,6 +34,8 @@ private:
     rdb_protocol_t::context_t *ctx;
     uuid_t parser_id;
     one_per_thread_t<int> thread_counters;
+
+    DISABLE_COPYING(query_server_t);
 };
 
 Response on_unparsable_query(Query *q, std::string msg);
