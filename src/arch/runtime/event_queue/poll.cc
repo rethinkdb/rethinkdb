f583df0f985fc800d6e9740ed8cf4d3030d42488
Sam Hughes <sam@rethinkdb.com>
2012-11-26 14:46:01 -0800

    Temporarily gave our poll system call a 1ms timeout.

--- src/arch/runtime/event_queue/poll.cc
+++ src/arch/runtime/event_queue/poll.cc
@@ -77,7 +77,8 @@ void poll_event_queue_t::run() {
 #ifdef RDB_USE_TIMER_SIGNAL_PROVIDER
         res = ppoll(&watched_fds[0], watched_fds.size(), NULL, &sigmask_restricted);
 #else
-        res = poll(&watched_fds[0], watched_fds.size(), -1);
+        // TODO(OSX) Replace 1 (milliseconds) with -1, and.. get the timer working?  Or something.
+        res = poll(&watched_fds[0], watched_fds.size(), 1);
 #endif
         // ppoll might return with EINTR in some cases (in particular
         // under GDB), we just need to retry.
