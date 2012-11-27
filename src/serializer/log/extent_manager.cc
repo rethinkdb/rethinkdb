73840a2181914814814a16f5dddf92c25bc7539f
Frank Trampe <frank.trampe@gmail.com>
2012-11-27 01:23:50 -0600

    We sign some stuff.

--- src/serializer/log/extent_manager.cc
+++ src/serializer/log/extent_manager.cc
@@ -177,10 +177,10 @@ extent_manager_t::extent_manager_t(file_t *file, const log_serializer_on_disk_st
         which is determined by a configuration parameter. */
         size_t zone_size = ceil_aligned(dynamic_config->file_zone_size, extent_size);
         off64_t end = 0;
-        while (end != floor_aligned((off64_t)file->get_size(), extent_size)) {
+        while (end != (off64_t)floor_aligned(file->get_size(), extent_size)) {
             off64_t start = end;
             // Frank changed this from std::min to RT_MIN (from utils.hpp) since std::min fails to match on i386.
-            end = RT_MIN(start + zone_size, floor_aligned(file->get_size(), extent_size));
+            end = RT_MIN(start + zone_size, (off64_t)floor_aligned(file->get_size(), extent_size));
             zones.push_back(new extent_zone_t(start, end, extent_size));
         }
     } else {
