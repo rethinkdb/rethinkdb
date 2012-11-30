4a580d7a83e0b3d887e3cc2c02de92e99ba02f14
Sam Hughes <sam@rethinkdb.com>
2012-11-29 17:03:35 -0800

    Removed old commented code in timer_itimer_provider.cc.

--- src/arch/io/timer/timer_itimer_provider.cc
+++ src/arch/io/timer/timer_itimer_provider.cc
@@ -38,49 +38,13 @@ timer_itimer_provider_t::timer_itimer_provider_t(linux_event_queue_t *queue,
                                                  UNUSED time_t secs, UNUSED int32_t nsecs)
     : queue_(queue), callback_(callback) {
     add_to_itimer_callback_list(this);
-
-    // Tell the event queue to start watching the signal.
-    // sigevent evp;
-    // evp.sigev_signo = SIGALRM;
-    // queue_->watch_signal(&evp, this);
-
-    // Create the timer.
-    // TODO(OSX) Actually support timing things.
-    // struct itimerval value;
-    // value.it_interval.tv_sec = secs;
-    // value.it_interval.tv_usec = nsecs / 1000;
-    // value.it_value = value.it_interval;
-
-    // struct itimerval old_value;
-
-    // int res = setitimer(ITIMER_REAL, &value, &old_value);
-    // guarantee_err(res == 0, "setitimer failed");
-    // guarantee(old_value.it_interval.tv_sec == 0 && old_value.it_interval.tv_usec == 0
-    //           && old_value.it_value.tv_sec == 0 && old_value.it_value.tv_usec == 0,
-    //           "setitimer reported that the old timer was present");
 }
 
 timer_itimer_provider_t::~timer_itimer_provider_t() {
     remove_from_itimer_callback_list(this);
-    // TODO(OSX) Actually support timing things.
-    // struct itimerval value;
-    // value.it_interval.tv_sec = 0;
-    // value.it_interval.tv_usec = 0;
-    // value.it_value = value.it_interval;
-
-    // struct itimerval old_value;
-    // int res = setitimer(ITIMER_REAL, &value, &old_value);
-    // guarantee_err(res == 0, "setitimer failed");
-    // guarantee(old_value.it_interval.tv_sec != 0 || old_value.it_interval.tv_usec != 0,
-    //           "setitimer reported that the old timer was present");
 }
 
 void timer_itimer_provider_t::on_alrm() {
-    // TODO(OSX): This change is bullshit so far, in order to trick ourselves into compiling at all.
-    // This callback never actually gets called.
-
-    // TODO(OSX): See the comment in timer_signal_provider_t, we just made the same assumption here
-    // and passed events + 1.
     callback_->on_timer(1);
 }
 
