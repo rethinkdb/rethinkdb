c84f7659802cee13a435bb3137a21b167a0a6aef
Frank Trampe <frank.trampe@gmail.com>
2012-12-10 12:23:31 -0600

    Some fixes.

--- src/compatibility/boost.cc
+++ src/compatibility/boost.cc
@@ -1,18 +1,11 @@
 // Copyright 2010-2012 RethinkDB, all rights reserved.
 #include "compatibility/boost.hpp"
 
-enum boost_program_options_validation_error_type {
-    multiple_values_not_allowed = 30 , 
-    at_least_one_value_required ,
-    invalid_bool_value , 
-    invalid_option_value,
-    invalid_option
-} ;
-
 #if BOOST_VERSION >= 104200
 boost::program_options::validation_error::kind_t boost_program_options_validation_error_type_to_kind(
      enum boost_program_options_validation_error_type input
 ) {
+    // This function converts from the internal enum to the boost enum.
     switch ( input ) {
         case multiple_values_not_allowed :
              return boost::program_options::validation_error::multiple_values_not_allowed ; break ;
@@ -24,15 +17,19 @@ boost::program_options::validation_error::kind_t boost_program_options_validatio
              return boost::program_options::validation_error::invalid_option_value ; break ;
         case invalid_option :
              return boost::program_options::validation_error::invalid_option ; break ;
-        default : return "Unknown error" ; break ;
+        default :
+             // This seems a decent if not desirable fallback.
+             return boost::program_options::validation_error::invalid_option ; break ;
     }
-    return "Error handling error" ;
+    // We have a return value here so as to help older compilers to figure things out.
+    return boost::program_options::validation_error::invalid_option ;
 }
 #endif
 
 std::string boost_program_options_validation_error_type_to_string(
     enum boost_program_options_validation_error_type input
 ) {
+    // This function converts from the internal enum to a string.
     switch ( input ) {
         case multiple_values_not_allowed : return STR_multiple_values_not_allowed ; break ;
         case at_least_one_value_required : return STR_at_least_one_value_required ; break ;
@@ -43,11 +40,6 @@ std::string boost_program_options_validation_error_type_to_string(
     }
     return "Error handling error" ;
 }
-
-void boost_program_options_validation_error_wrap(
-    enum boost_program_options_validation_error_type error_type ,
-    std::string error_desc , std::string error_item
-) ;
 
 void boost_program_options_validation_error_wrap(
     enum boost_program_options_validation_error_type error_type ,
