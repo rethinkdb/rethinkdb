43fd516e962dec841562f19bcda4846ddbf4f6fb
neumino <michel@rethinkdb.com>
2012-12-21 16:24:02 -0800

    Revert to old version to handlebars 1.0.0.beta.6 + remove code that was injecting handlebars template

--- src/clustering/administration/http/server.cc
+++ src/clustering/administration/http/server.cc
@@ -72,7 +72,7 @@ administrative_http_server_manager_t::administrative_http_server_manager_t(
     white_list.insert("/js/jquery.cookie.js");
     white_list.insert("/js/flot/jquery.flot.js");
     white_list.insert("/js/flot/jquery.flot.resize.js");
-    white_list.insert("/js/handlebars.runtime-1.0.rc.1.js");
+    white_list.insert("/js/handlebars.runtime-1.0.0.beta.6.js");
     white_list.insert("/js/jquery-1.7.2.min.js");
     white_list.insert("/js/jquery.color.js");
     white_list.insert("/js/jquery.dataTables.min.js");
