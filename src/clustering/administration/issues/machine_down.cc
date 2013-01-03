93d367afc27e4dc00364612c3aada29dd6c7dbfc
Sam Hughes <sam@rethinkdb.com>
2013-01-03 11:51:20 -0800

    Moved implementations from machine_down.hpp to machine_down.cc.

--- src/clustering/administration/issues/machine_down.cc
+++ src/clustering/administration/issues/machine_down.cc
@@ -2,6 +2,51 @@
 #include "clustering/administration/issues/machine_down.hpp"
 #include "clustering/administration/machine_id_to_peer_id.hpp"
 
+std::string machine_down_issue_t::get_description() const {
+    return "Machine " + uuid_to_str(machine_id) + " is inaccessible.";
+}
+
+cJSON *machine_down_issue_t::get_json_description() {
+    issue_json_t json;
+    json.critical = true;
+    json.description = "Machine " + uuid_to_str(machine_id) + " is inaccessible.";
+    json.type = "MACHINE_DOWN";
+    json.time = get_secs();
+
+    cJSON *res = render_as_json(&json);
+    cJSON_AddItemToObject(res, "victim", render_as_json(&machine_id));
+
+    return res;
+}
+
+machine_down_issue_t *machine_down_issue_t::clone() const {
+    return new machine_down_issue_t(machine_id);
+}
+
+std::string machine_ghost_issue_t::get_description() const {
+    return "Machine " + uuid_to_str(machine_id) + " was declared dead, but it's connected to the cluster.";
+}
+
+
+cJSON *machine_ghost_issue_t::get_json_description() {
+    issue_json_t json;
+    json.critical = false;
+    json.description = get_description();
+    json.type = "MACHINE_GHOST";
+    json.time = get_secs();
+
+    cJSON *res = render_as_json(&json);
+    cJSON_AddItemToObject(res, "ghost", render_as_json(&machine_id));
+
+    return res;
+}
+
+machine_ghost_issue_t *machine_ghost_issue_t::clone() const {
+    return new machine_ghost_issue_t(machine_id);
+}
+
+
+
 machine_down_issue_tracker_t::machine_down_issue_tracker_t(
         boost::shared_ptr<semilattice_read_view_t<cluster_semilattice_metadata_t> > _semilattice_view,
         const clone_ptr_t<watchable_t<std::map<peer_id_t, machine_id_t> > > &_machine_id_translation_table) :
