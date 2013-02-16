080729dd71efaaf356adef984e79650f96306d4f
Michael Lucy <mlucy@rethinkdb.com>
2013-02-15 17:47:02 -0800

    pb_utils style fixes

--- src/rdb_protocol/pb_utils.hpp
+++ src/rdb_protocol/pb_utils.hpp
@@ -3,16 +3,6 @@
 
 #include "rdb_protocol/ql2.pb.h"
 
-// TO: CYBERPUNK SAM
-// SUBJECT: APOLOGY
-//
-// I'm sorry I'm sorry I'm sorry...
-//
-// I think that the correct way to generate protobufs for our rewrite terms is
-// to implement a C++ driver analagous to the ruby driver and use its protobuf
-// construction functionality.  I didn't have time to do that, so I did this
-// instead.
-
 namespace ql {
 namespace pb {
 
@@ -41,40 +31,41 @@ void set(Term2 *out, Term2_TermType type, std::vector<Term2 *> *args_out, int nu
 // These macros expect `arg` to point to the Term2 they should modify.  They set
 // its type to PB, and then for each of their arguments, rebind `arg` to point
 // to the appropriate portion of the new Term2 and execute that argument.
-#define N0(PB)                                  \
-    arg->set_type(Term2_TermType_##PB);
-#define N1(PB, ARG1) {                          \
+#define N0(PB) do {                             \
         arg->set_type(Term2_TermType_##PB);     \
-        Term2 *__arg1 = arg->add_args();        \
-        Term2 *arg = __arg1; ARG1;              \
-    }
-#define N2(PB, ARG1, ARG2) {                    \
+    } while (0);
+#define N1(PB, ARG1) do {                       \
         arg->set_type(Term2_TermType_##PB);     \
-        Term2 *__arg1 = arg->add_args();        \
-        Term2 *__arg2 = arg->add_args();        \
-        Term2 *arg = __arg1; ARG1;              \
-        arg = __arg2; ARG2;                     \
-    }
-#define N3(PB, ARG1, ARG2, ARG3) {              \
+        Term2 *PB_arg1 = arg->add_args();       \
+        Term2 *arg = PB_arg1; ARG1;             \
+    } while (0);
+#define N2(PB, ARG1, ARG2) do {                 \
         arg->set_type(Term2_TermType_##PB);     \
-        Term2 *__arg1 = arg->add_args();        \
-        Term2 *__arg2 = arg->add_args();        \
-        Term2 *__arg3 = arg->add_args();        \
-        Term2 *arg = __arg1; ARG1;              \
-        arg = __arg2; ARG2;                     \
-        arg = __arg3; ARG3;                     \
-    }
-#define N4(PB, ARG1, ARG2, ARG3, ARG4) {        \
+        Term2 *PB_arg1 = arg->add_args();       \
+        Term2 *PB_arg2 = arg->add_args();       \
+        Term2 *arg = PB_arg1; ARG1;             \
+        arg = PB_arg2; ARG2;                    \
+    } while (0);
+#define N3(PB, ARG1, ARG2, ARG3) do {           \
         arg->set_type(Term2_TermType_##PB);     \
-        Term2 *__arg1 = arg->add_args();        \
-        Term2 *__arg2 = arg->add_args();        \
-        Term2 *__arg3 = arg->add_args();        \
-        Term2 *__arg4 = arg->add_args();        \
-        Term2 *arg = __arg1; ARG1;              \
-        arg = __arg2; ARG2;                     \
-        arg = __arg3; ARG3;                     \
-        arg = __arg4; ARG4;                     \
-    }
+        Term2 *PB_arg1 = arg->add_args();       \
+        Term2 *PB_arg2 = arg->add_args();       \
+        Term2 *PB_arg3 = arg->add_args();       \
+        Term2 *arg = PB_arg1; ARG1;             \
+        arg = PB_arg2; ARG2;                    \
+        arg = PB_arg3; ARG3;                    \
+    } while (0);
+#define N4(PB, ARG1, ARG2, ARG3, ARG4) do {     \
+        arg->set_type(Term2_TermType_##PB);     \
+        Term2 *PB_arg1 = arg->add_args();       \
+        Term2 *PB_arg2 = arg->add_args();       \
+        Term2 *PB_arg3 = arg->add_args();       \
+        Term2 *PB_arg4 = arg->add_args();       \
+        Term2 *arg = PB_arg1; ARG1;             \
+        arg = PB_arg2; ARG2;                    \
+        arg = PB_arg3; ARG3;                    \
+        arg = PB_arg4; ARG4;                    \
+    } while (0);
 
 // Wrappers around `set_var` and `write_to_protobuf` that work with the `N*` macros.
 #define NVAR(varnum) ql::pb::set_var(arg, varnum)
@@ -88,26 +79,27 @@ template<class U>
 void run(const datum_t *d, U arg) { run(*d, arg); }
 template<class T, class U>
 void run(T t, U arg) { run(datum_t(t), arg); }
-}
+} // ndatum_impl
+
 #define NDATUM(val) ql::pb::ndatum_impl::run(val, arg)
 
 // Like `N*` but for optional arguments.  See rewrites.hpp for examples if you
 // need to use them.
-#define OPT1(PB, STR1, ARG1) {                          \
+#define OPT1(PB, STR1, ARG1) do {                       \
         arg->set_type(Term2_TermType_##PB);             \
-        Term2_AssocPair *__ap1 = arg->add_optargs();    \
-        __ap1->set_key(STR1);                           \
-        arg = __ap1->mutable_val(); ARG1;               \
-    }
-#define OPT2(PB, STR1, ARG1, STR2, ARG2) {              \
+        Term2_AssocPair *PB_ap1 = arg->add_optargs();   \
+        PB_ap1->set_key(STR1);                          \
+        arg = PB_ap1->mutable_val(); ARG1;              \
+    } while (0);
+#define OPT2(PB, STR1, ARG1, STR2, ARG2) do {           \
         arg->set_type(Term2_TermType_##PB);             \
-        Term2_AssocPair *__ap1 = arg->add_optargs();    \
-        __ap1->set_key(STR1);                           \
-        Term2_AssocPair *__ap2 = arg->add_optargs();    \
-        __ap2->set_key(STR2);                           \
-        arg = __ap1->mutable_val(); ARG1;               \
-        arg = __ap2->mutable_val(); ARG2;               \
-    }
+        Term2_AssocPair *PB_ap1 = arg->add_optargs();   \
+        PB_ap1->set_key(STR1);                          \
+        Term2_AssocPair *PB_ap2 = arg->add_optargs();   \
+        PB_ap2->set_key(STR2);                          \
+        arg = PB_ap1->mutable_val(); ARG1;              \
+        arg = PB_ap2->mutable_val(); ARG2;              \
+    } while (0);
 
 // Used to empty portions of protobufs when we're modifying them in-place.
 template<class T>
