9d05e79ba8f710ca9ba9d855c41300440b8d1d3b
Sam Hughes <sam@rethinkdb.com>
2013-02-16 23:30:45 -0800

    Updated some copyright notices to 2013, which was fun.

--- src/buffer_cache/semantic_checking.tcc
+++ src/buffer_cache/semantic_checking.tcc
@@ -1,4 +1,4 @@
-// Copyright 2010-2012 RethinkDB, all rights reserved.
+// Copyright 2010-2013 RethinkDB, all rights reserved.
 #include "buffer_cache/semantic_checking.hpp"
 
 #include <algorithm>
--- src/serializer/serializer.hpp
+++ src/serializer/serializer.hpp
@@ -1,4 +1,4 @@
-// Copyright 2010-2012 RethinkDB, all rights reserved.
+// Copyright 2010-2013 RethinkDB, all rights reserved.
 #ifndef SERIALIZER_SERIALIZER_HPP_
 #define SERIALIZER_SERIALIZER_HPP_
 
