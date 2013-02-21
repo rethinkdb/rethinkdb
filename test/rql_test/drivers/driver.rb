bc2afb0047ad3cb5b515b302a1323dcc55c03e43
Etienne Laurin <etienne@rethinkdb.com>
2013-02-21 14:05:12 -0800

    Test for exceptions in the ruby polyglot tests

--- test/rql_test/drivers/driver.rb
+++ test/rql_test/drivers/driver.rb
@@ -7,34 +7,72 @@ extend RethinkDB::Shortcuts
 JSPORT = ARGV[0]
 CPPPORT = ARGV[1]
 
-def eq_test(one, two)
-
-  case "#{two.class}"
-  when "Array"
-    return false if one.class != two.class
-    return false if one.length != two.length
-    return one.zip(two).map{ |a, b| eq_test(a, b) }.all?
-
-  when "Hash"
-    return false if one.class != two.class
-    one = Hash[ one.map{ |k,v| [k.to_s, v] } ]
-    two = Hash[ two.map{ |k,v| [k.to_s, v] } ]
-    return false if one.keys.sort != two.keys.sort
-    return one.keys.map{|k| eq_test(one[k], two[k])}.all?
-
-  when "Bag"
-    return eq_test(one.sort, two.items.sort)
-
-  else
-    if not [Fixnum, Float].member? one.class
-      return false if one.class != two.class
-    end
-    return one == two
-  end
-end
-
 def show x
   return (PP.pp x, "").chomp
+end
+
+NoError = "nope"
+Err = Struct.new(:type, :message, :backtrace)
+Bag = Struct.new(:items)
+
+def bag list
+  Bag.new(list)
+end
+
+def err(type, message, backtrace)
+  Err.new(type, message, backtrace)
+end
+
+def eq_test(one, two)
+  return cmp_test(one, two) == 0
+end
+
+def cmp_test(one, two)
+
+  if two.object_id == NoError.object_id
+    return -1 if one.class == Err
+    return 0
+  end
+
+  case "#{two.class}"
+  when "Err"
+    cmp = one.class.name <=> two.class.name
+    return cmp if cmp != 0
+    [one.type, one.message] <=> [two.type, two.message]
+
+  when "Array"
+    cmp = one.class.name <=> two.class.name
+    return cmp if cmp != 0
+    cmp = one.length <=> two.length
+    return cmp if cmp != 0
+    return one.zip(two).reduce(0){ |acc, pair|
+      acc == 0 ? cmp_test(pair[0], pair[1]) : acc
+    }
+
+  when "Hash"
+    cmp = one.class.name <=> two.class.name
+    return cmp if cmp != 0
+    one = Hash[ one.map{ |k,v| [k.to_s, v] } ]
+    two = Hash[ two.map{ |k,v| [k.to_s, v] } ]
+    cmp = one.keys.sort <=> two.keys.sort
+    return cmp if cmp != 0
+    return one.keys.reduce(0){ |acc, k|
+      acc == 0 ? cmp_test(one[k], two[k]) : acc
+    }
+
+  when "Bag"
+    return cmp_test(one.sort{ |a, b| cmp_test a, b },
+                    two.items.sort{ |a, b| cmp_test a, b })
+    
+  else
+    begin
+      cmp = one <=> two
+      return cmp if cmp != nil
+      return one.class.name <=> two.class.name
+    rescue
+      return one.class.name <=> two.class.name
+    end
+  end
 end
 
 def eval_env; binding; end
@@ -75,13 +113,16 @@ end
 def do_test query, expected, con, name, src
   begin
     res = query.run(con)
-  rescue Exception => e
-    puts "#{name}: Error: #{e} running query #{src}"
-    return false
-    end
+  rescue Exception => exc
+    res = err("Rql" + exc.class.name, exc.message.split("\n")[0], "TODO")
+  end
   
   begin
-    expected = eval expected.to_s, $defines
+    if expected != ''
+      expected = eval expected.to_s, $defines
+    else
+      expected = NoError
+    end
     if ! eq_test(res, expected)
       puts "#{name}: Equality comparison failed for #{src}"
       puts "Value: #{show res}, Expected: #{show expected}"
@@ -97,12 +138,6 @@ end
 
 def define expr
   eval expr, $defines
-end
-
-Bag = Struct.new(:items)
-
-def bag list
-  Bag.new(list)
 end
 
 True=true
