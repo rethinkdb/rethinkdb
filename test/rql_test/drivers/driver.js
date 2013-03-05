4ec5fc997ad4cc73a4a6c2759611442008b84ebe
Bill Rowan <bill@rethinkdb.com>
2013-03-05 15:20:39 -0800

    fixes failure of JS error printing

--- test/rql_test/drivers/driver.js
+++ test/rql_test/drivers/driver.js
@@ -169,6 +169,11 @@ r.connect({port:CPPPORT}, function(cpp_conn_err, cpp_conn) {
                                                 "Error running test on CPP server not equal to expected err:"+
                                                 "\n\tERROR: "+cpp_err.toString()+
                                                 "\n\tEXPECTED "+exp_fun.toString());
+                                        } else {
+                                            printTestFailure(testName, src,
+                                                "Error running test on CPP server:"+
+                                                "\n\tERROR: "+cpp_err.toString()+
+                                                "\n\tEXPECTED "+exp_fun.toString());
                                         }
                                     } else if (!exp_fun(cpp_res)) {
                                         printTestFailure(testName, src,
@@ -182,6 +187,11 @@ r.connect({port:CPPPORT}, function(cpp_conn_err, cpp_conn) {
                                             printTestFailure(testName, src,
                                                 "Error running test on JS server not equal to expected err:"+
                                                 "\n\tERROR: "+js_err.toString()+
+                                                "\n\tEXPECTED "+exp_fun.toString());
+                                        } else {
+                                            printTestFailure(testName, src,
+                                                "Error running test on JS server:"+
+                                                "\n\tERROR: "+cpp_err.toString()+
                                                 "\n\tEXPECTED "+exp_fun.toString());
                                         }
                                     } else if (!exp_fun(js_res)) {
