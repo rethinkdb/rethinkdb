1444dc659bdedf20fedef2bf0e9dba4e61c6f281
Sam Hughes <sam@rethinkdb.com>
2013-03-25 13:24:55 -0700

    Removed the mock cache.

--- src/buffer_cache/buffer_cache.hpp
+++ src/buffer_cache/buffer_cache.hpp
@@ -1,38 +1,19 @@
-// Copyright 2010-2012 RethinkDB, all rights reserved.
+// Copyright 2010-2013 RethinkDB, all rights reserved.
 #ifndef BUFFER_CACHE_BUFFER_CACHE_HPP_
 #define BUFFER_CACHE_BUFFER_CACHE_HPP_
 
 // Predeclarations are in here.
 #include "buffer_cache/types.hpp"
 
-/* Choose our cache */
 
 // This file should be kept synced up with buffer_cache/types.hpp,
 // which defines what buf_lock_t, cache_t, etc, are.
 
-#ifndef MOCK_CACHE_CHECK
-
 #include "buffer_cache/mirrored/mirrored.hpp"
 
 #if !defined(VALGRIND) && !defined(NDEBUG)
-
 #include "buffer_cache/semantic_checking.hpp"
-
-#else  // !defined(VALGRIND) && !defined(NDEBUG)
-
-// include nothing else
-
-#endif  // !defined(VALGRIND) && !defined(NDEBUG)
-
-#else  // MOCK_CACHE_CHECK
-
-#include "buffer_cache/mock.hpp"
-
-#if !defined(VALGRIND)
-#include "buffer_cache/semantic_checking.hpp"  // NOLINT
-#endif  // !defined(VALGRIND)
-
-#endif  // MOCK_CACHE_CHECK
+#endif
 
 
 #endif /* BUFFER_CACHE_BUFFER_CACHE_HPP_ */
--- src/buffer_cache/types.hpp
+++ src/buffer_cache/types.hpp
@@ -89,8 +89,6 @@ protected:
 
 // Keep this part below synced up with buffer_cache.hpp.
 
-#ifndef MOCK_CACHE_CHECK
-
 class mc_cache_t;
 class mc_buf_lock_t;
 class mc_transaction_t;
@@ -118,37 +116,5 @@ typedef mc_cache_account_t cache_account_t;
 
 #endif  // !defined(VALGRIND) && !defined(NDEBUG)
 
-#else
-
-class mock_cache_t;
-class mock_cache_account_t;
-
-#if !defined(VALGRIND)
-
-template <class inner_cache_type> class scc_cache_t;
-template <class inner_cache_type> class scc_buf_lock_t;
-template <class inner_cache_type> class scc_transaction_t;
-
-typedef scc_cache_t<mock_cache_t> cache_t;
-typedef scc_buf_lock_t<mock_cache_t> buf_lock_t;
-typedef scc_transaction_t<mock_cache_t> transaction_t;
-typedef mock_cache_account_t cache_account_t;
-
-#else  // !defined(VALGRIND)
-
-class mock_buf_lock_t;
-class mock_transaction_t;
-class mock_cache_account_t;
-
-typedef mock_cache_t cache_t;
-typedef mock_buf_lock_t buf_lock_t;
-typedef mock_transaction_t transaction_t;
-typedef mock_cache_account_t cache_account_t;
-
-#endif  // !defined(VALGRIND)
-
-#endif // MOCK_CACHE_CHECK
-
-// Don't put anything down here, put it above the line labeled "BLAH".
 
 #endif /* BUFFER_CACHE_TYPES_HPP_ */
