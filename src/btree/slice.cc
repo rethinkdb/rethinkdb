ff856817929bc5f4a340afdef27b75f8974e72b1
Sam Hughes <sam@rethinkdb.com>
2013-03-26 15:24:11 -0700

    Made btree_slice_t::create wait for its disk_ack_signal.

--- src/btree/slice.cc
+++ src/btree/slice.cc
@@ -10,29 +10,28 @@
 #define BACKFILL_CACHE_PRIORITY 10
 
 void btree_slice_t::create(cache_t *cache, const std::vector<char> &metainfo_key, const std::vector<char> &metainfo_value) {
-
-    // SAMRSI: Should we wait on this signal?  Pass it in as a parameter?
     sync_callback_t disk_ack_signal;
+    {
+        /* Initialize the btree superblock and the delete queue */
+        transaction_t txn(cache, rwi_write, 1, repli_timestamp_t::distant_past, order_token_t::ignore, &disk_ack_signal);
 
-    /* Initialize the btree superblock and the delete queue */
-    transaction_t txn(cache, rwi_write, 1, repli_timestamp_t::distant_past, order_token_t::ignore, &disk_ack_signal);
+        buf_lock_t superblock(&txn, SUPERBLOCK_ID, rwi_write);
 
-    buf_lock_t superblock(&txn, SUPERBLOCK_ID, rwi_write);
+        // Initialize replication time barrier to 0 so that if we are a slave, we will begin by pulling
+        // ALL updates from master.
+        superblock.touch_recency(repli_timestamp_t::distant_past);
 
-    // Initialize replication time barrier to 0 so that if we are a slave, we will begin by pulling
-    // ALL updates from master.
-    superblock.touch_recency(repli_timestamp_t::distant_past);
+        btree_superblock_t *sb = reinterpret_cast<btree_superblock_t *>(superblock.get_data_write());
+        bzero(sb, cache->get_block_size().value());
 
-    btree_superblock_t *sb = reinterpret_cast<btree_superblock_t *>(superblock.get_data_write());
-    bzero(sb, cache->get_block_size().value());
+        // sb->metainfo_blob has been properly zeroed.
+        sb->magic = btree_superblock_t::expected_magic;
+        sb->root_block = NULL_BLOCK_ID;
+        sb->stat_block = NULL_BLOCK_ID;
 
-    // sb->metainfo_blob has been properly zeroed.
-
-    sb->magic = btree_superblock_t::expected_magic;
-    sb->root_block = NULL_BLOCK_ID;
-    sb->stat_block = NULL_BLOCK_ID;
-
-    set_superblock_metainfo(&txn, &superblock, metainfo_key, metainfo_value);
+        set_superblock_metainfo(&txn, &superblock, metainfo_key, metainfo_value);
+    }
+    disk_ack_signal.wait();
 }
 
 btree_slice_t::btree_slice_t(cache_t *c, perfmon_collection_t *parent)
