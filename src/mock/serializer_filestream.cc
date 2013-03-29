9e0300490154168fceb9f6d0ec554d1fafef6d61
Sam Hughes <sam@rethinkdb.com>
2013-03-29 13:21:37 -0700

    Removed the last SAMRSI comment in serializer_filestream.cc.

--- src/mock/serializer_filestream.cc
+++ src/mock/serializer_filestream.cc
@@ -86,9 +86,6 @@ MUST_USE int64_t serializer_file_write_stream_t::write(const void *p, int64_t n)
     const char *chp = static_cast<const char *>(p);
     const int block_size = cache_->get_block_size().value();
 
-    // SAMRSI: Construct a disk_ack_signal here?  As a parameter?  (No, we can't.)  Wait on the
-    // signal?  Construct and wait on the disk ack signal as configured by a boolean parameter
-    // to the serializer_file_write_stream constructor?
     sync_callback_t disk_ack_signal;
 
     transaction_t txn(cache_.get(), rwi_write, 2 + n / block_size, repli_timestamp_t::invalid, order_token_t::ignore, &disk_ack_signal);
