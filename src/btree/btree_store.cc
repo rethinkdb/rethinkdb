e8cac2dcc8fb7d8d9561cc97be186acaace93b1f
Michael Lucy <mlucy@rethinkdb.com>
2013-04-09 14:23:48 -0700

    Adds error handling for sindex create and drop (cf. Issue #578).

--- src/btree/btree_store.cc
+++ src/btree/btree_store.cc
@@ -274,7 +274,7 @@ bool has_homogenous_value(const region_map_t &metainfo, typename region_map_t::m
 }
 
 template <class protocol_t>
-void btree_store_t<protocol_t>::add_sindex(
+MUST_USE bool btree_store_t<protocol_t>::add_sindex(
         write_token_pair_t *token_pair,
         const std::string &id,
         const secondary_index_t::opaque_definition_t &definition,
@@ -287,13 +287,13 @@ void btree_store_t<protocol_t>::add_sindex(
     /* Get the sindex block which we will need to modify. */
     scoped_ptr_t<buf_lock_t> sindex_block;
 
-    add_sindex(token_pair, id, definition, txn,
-               super_block, &sindex_block,
-               interruptor);
+    return add_sindex(token_pair, id, definition, txn,
+                      super_block, &sindex_block,
+                      interruptor);
 }
 
 template <class protocol_t>
-void btree_store_t<protocol_t>::add_sindex(
+MUST_USE bool btree_store_t<protocol_t>::add_sindex(
         write_token_pair_t *token_pair,
         const std::string &id,
         const secondary_index_t::opaque_definition_t &definition,
@@ -304,12 +304,12 @@ void btree_store_t<protocol_t>::add_sindex(
     THROWS_ONLY(interrupted_exc_t) {
     assert_thread();
 
-    acquire_sindex_block_for_write(token_pair, txn, sindex_block_out, super_block->get_sindex_block_id(), interruptor);
+    acquire_sindex_block_for_write(token_pair, txn, sindex_block_out,
+                                   super_block->get_sindex_block_id(), interruptor);
 
     secondary_index_t sindex;
     if (::get_secondary_index(txn, sindex_block_out->get(), id, &sindex)) {
-        /* sindex was already created. */
-        return;
+        return false; // sindex was already created
     } else {
         {
             buf_lock_t sindex_superblock(txn);
@@ -325,14 +325,17 @@ void btree_store_t<protocol_t>::add_sindex(
          * something that would give a better error if someone did try to use
          * it... on the other hand this code isn't exactly idiot proof even
          * with that. */
-        btree_slice_t::create(txn->get_cache(), sindex.superblock, txn, std::vector<char>(), std::vector<char>());
-        secondary_index_slices.insert(id, new btree_slice_t(cache.get(), &perfmon_collection, id));
+        btree_slice_t::create(txn->get_cache(), sindex.superblock,
+                              txn, std::vector<char>(), std::vector<char>());
+        secondary_index_slices.insert(
+            id, new btree_slice_t(cache.get(), &perfmon_collection, id));
 
         // TODO when we have post construction this will be set to false and
         // we'll begin postconstruction from here.
         sindex.post_construction_complete = false;
 
         ::set_secondary_index(txn, sindex_block_out->get(), id, sindex);
+        return true;
     }
 }
 
@@ -422,7 +425,7 @@ THROWS_NOTHING {
 }
 
 template <class protocol_t>
-bool btree_store_t<protocol_t>::drop_sindex(
+MUST_USE bool btree_store_t<protocol_t>::drop_sindex(
         write_token_pair_t *token_pair,
         const std::string &id,
         transaction_t *txn,
