8541a505a1fc46a280030afd51760f741d3daf76
Michael Lucy <mlucy@rethinkdb.com>
2013-04-09 11:45:05 -0700

    fixed crashing bug discovered while fixing issue 579

--- src/rdb_protocol/terms/db_table.hpp
+++ src/rdb_protocol/terms/db_table.hpp
@@ -372,6 +372,7 @@ private:
             db = arg(0)->as_db();
             name = arg(1)->as_str();
         }
+        virtual bool is_deterministic_impl() const { return false; }
         return new_val(new table_t(env, db, name, use_outdated, this));
     }
     virtual const char *name() const { return "table"; }
