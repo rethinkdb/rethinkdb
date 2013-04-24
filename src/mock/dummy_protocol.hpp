c1ebf3ae36b2b3f4571a2278c9d544e81e1538d6
Sam Hughes <sam@rethinkdb.com>
2013-04-24 16:09:05 -0700

    Removed dummy_protocol_t::backfill_chunk_t::shard.

--- src/mock/dummy_protocol.hpp
+++ src/mock/dummy_protocol.hpp
@@ -80,11 +80,6 @@ public:
         std::string key, value;
         state_timestamp_t timestamp;
 
-        backfill_chunk_t shard(DEBUG_VAR const region_t &r) const THROWS_NOTHING {
-            rassert(r.keys.find(key) != r.keys.end());
-            return *this;
-        }
-
         RDB_MAKE_ME_SERIALIZABLE_3(key, value, timestamp);
     };
 
