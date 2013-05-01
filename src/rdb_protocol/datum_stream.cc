dd740f9cd9936ad29d87f45e554ea95909dbe1da
Sam Hughes <sam@rethinkdb.com>
2013-05-01 16:40:38 -0700

    Removed comment about unit tests being debug-only.

--- src/rdb_protocol/datum_stream.cc
+++ src/rdb_protocol/datum_stream.cc
@@ -18,7 +18,6 @@ counted_t<datum_stream_t> datum_stream_t::zip() {
 }
 
 counted_t<const datum_t> datum_stream_t::next() {
-    // SAMRSI: What the fuck?  Unit tests aren't debug-only.
     // This is a hook for unit tests to change things mid-query.
     DEBUG_ONLY_CODE(env->do_eval_callback());
     env->throw_if_interruptor_pulsed();
