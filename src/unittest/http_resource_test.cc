d00609322dc5d01e7c650b9ecf5d584f3c52b330
Sam Hughes <sam@rethinkdb.com>
2013-05-30 00:49:47 -0700

    Added a bunch of HttpResourceTest tests, documenting the behavior of http_req_t::resource_t.

--- src/unittest/http_resource_test.cc
+++ src/unittest/http_resource_test.cc
@@ -4,10 +4,10 @@
 
 namespace unittest {
 
-TEST(HttpResourceTest, EmptyResource) {
+void RunThrowingResourceTest(const char *path) {
     bool caught_exception = false;
     try {
-        http_req_t::resource_t resource("");
+        http_req_t::resource_t resource(path);
         auto it = resource.begin();
         auto jt = resource.end();
     } catch (const std::invalid_argument &ex) {
@@ -15,7 +15,132 @@ TEST(HttpResourceTest, EmptyResource) {
     }
 
     ASSERT_TRUE(caught_exception);
-};
+}
+
+TEST(HttpResourceTest, EmptyPath) {
+    RunThrowingResourceTest("");
+}
+
+TEST(HttpResourceTest, DoesNotStartWithSlash) {
+    RunThrowingResourceTest("foo/");
+}
+
+TEST(HttpResourceTest, Slash) {
+    http_req_t::resource_t resource("/");
+    auto it = resource.begin();
+    auto jt = resource.end();
+    ASSERT_EQ(it, jt);
+}
+
+TEST(HttpResourceTest, OneElt) {
+    http_req_t::resource_t resource("/foo");
+    auto it = resource.begin();
+    auto jt = resource.end();
+    ASSERT_EQ("foo", *it);
+    ++it;
+    ASSERT_EQ(jt, it);
+}
+
+TEST(HttpResourceTest, OneEltSlash) {
+    http_req_t::resource_t resource("/foo/");
+    auto it = resource.begin();
+    auto jt = resource.end();
+    ASSERT_EQ("foo", *it);
+    ++it;
+    ASSERT_NE(jt, it);
+    ASSERT_EQ("", *it);
+    ++it;
+    ASSERT_EQ(jt, it);
+}
+
+TEST(HttpResourceTest, TwoElt) {
+    http_req_t::resource_t resource("/foo/bar");
+    auto it = resource.begin();
+    auto jt = resource.end();
+    ASSERT_EQ("foo", *it);
+    ++it;
+    ASSERT_EQ("bar", *it);
+    ++it;
+    ASSERT_EQ(jt, it);
+}
+
+TEST(HttpResourceTest, TwoEltSlash) {
+    http_req_t::resource_t resource("/foo/bar/");
+    auto it = resource.begin();
+    auto jt = resource.end();
+    ASSERT_EQ("foo", *it);
+    ++it;
+    ASSERT_EQ("bar", *it);
+    ++it;
+    ASSERT_NE(jt, it);
+    ASSERT_EQ("", *it);
+    ++it;
+    ASSERT_EQ(jt, it);
+}
+
+TEST(HttpResourceTest, TwoEltSlashSlash) {
+    http_req_t::resource_t resource("/foo/bar//");
+    auto it = resource.begin();
+    auto jt = resource.end();
+    ASSERT_EQ("foo", *it);
+    ++it;
+    ASSERT_EQ("bar", *it);
+    ++it;
+    ASSERT_NE(jt, it);
+    ASSERT_EQ("", *it);
+    ++it;
+    ASSERT_NE(jt, it);
+    ASSERT_EQ("", *it);
+    ++it;
+    ASSERT_EQ(jt, it);
+}
+
+
+TEST(HttpResourceTest, TwoEltSlashSlashSlash) {
+    http_req_t::resource_t resource("/foo/bar///");
+    auto it = resource.begin();
+    auto jt = resource.end();
+    ASSERT_EQ("foo", *it);
+    ++it;
+    ASSERT_EQ("bar", *it);
+    ++it;
+    ASSERT_NE(jt, it);
+    ASSERT_EQ("", *it);
+    ++it;
+    ASSERT_NE(jt, it);
+    ASSERT_EQ("", *it);
+    ++it;
+    ASSERT_NE(jt, it);
+    ASSERT_EQ("", *it);
+    ++it;
+    ASSERT_EQ(jt, it);
+}
+
+TEST(HttpResourceTest, TwoEltSlashSlashElt) {
+    http_req_t::resource_t resource("/foo/bar//baz");
+    auto it = resource.begin();
+    auto jt = resource.end();
+    ASSERT_EQ("foo", *it);
+    ++it;
+    ASSERT_EQ("bar", *it);
+    ++it;
+    ASSERT_NE(jt, it);
+    ASSERT_EQ("", *it);
+    ++it;
+    ASSERT_EQ("baz", *it);
+    ++it;
+    ASSERT_EQ(jt, it);
+}
+
+TEST(HttpResourceTest, SlashSlash) {
+    http_req_t::resource_t resource("//");
+    auto it = resource.begin();
+    auto jt = resource.end();
+    ASSERT_NE(jt, it);
+    ASSERT_EQ("", *it);
+    ++it;
+    ASSERT_EQ(jt, it);
+}
 
 
 
