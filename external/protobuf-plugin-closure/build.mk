a060d45204d3e83ce04888c9b7f03cb259e8baad
Etienne Laurin <atnnn@rethinkdb.com>
2013-06-05 20:15:48 -0400

    Tweak protoc-js build

--- external/protobuf-plugin-closure/build.mk
+++ external/protobuf-plugin-closure/build.mk
@@ -8,7 +8,7 @@ PROTOC_JS_BUILD_DIR := $(BUILD_DIR)/protobuf-plugin-closure
 PROTOC_JS_PLUGIN := $(PROTOC_JS_BUILD_DIR)/protoc-gen-js
 
 PROTOC_LDFLAGS := $(LDFLAGS) $(PROTOBUF_LIBS) $(PTHREAD_LIBS)
-PROTOC_CXXFLAGS := $(CXXFLAGS) -I $(PROTOC_JS_DIR) -I $(PROTOC_JS_DIR)/js
+PROTOC_CXXFLAGS := $(patsubst -isystem,-I,$(CXXFLAGS)) -I $(PROTOC_JS_DIR) -I $(PROTOC_JS_DIR)/js
 PROTOC_CXXFLAGS += -I $(PROTOC_JS_BUILD_DIR) -I /usr/include
 
 # TODO: don't assume that libprotoc is installed right next to libprotobuf
@@ -16,7 +16,7 @@ LIBPROTOC_LDFLAGS = $(subst protobuf,protoc,$(PROTOBUF_LIBS))
 
 .PRECIOUS: $(PROTOC_JS_BUILD_DIR)/.
 
-$(PROTOC_JS_BUILD_DIR)/js/%.pb.cc: $(PROTOC_JS_DIR)/js/%.proto | $(PROTOC_JS_BUILD_DIR)/.
+$(PROTOC_JS_BUILD_DIR)/js/%.pb.cc: $(PROTOC_JS_DIR)/js/%.proto | $(PROTOC_JS_BUILD_DIR)/. $(PROTOC_DEP)
 	$P PROTOC
 	$(PROTOC) \
 	  $(PROTOC_CXXFLAGS) \
@@ -24,12 +24,13 @@ $(PROTOC_JS_BUILD_DIR)/js/%.pb.cc: $(PROTOC_JS_DIR)/js/%.proto | $(PROTOC_JS_BUI
 	  $<
 
 $(PROTOC_JS_PLUGIN): $(PROTOC_JS_BUILD_DIR)/js/javascript_package.pb.cc
-$(PROTOC_JS_PLUGIN): $(PROTOC_JS_BUILD_DIR)/js/int64_encoding.pb.cc
+$(PROTOC_JS_PLUGIN): $(PROTOC_JS_BUILD_DIR)/js/int64_encoding.pb.cc | $(PROTOC_DEP)
 	$P CC
-	$(CXX) $(PROTOC_LDFLAGS) $(PROTOC_CXXFLAGS) \
+	$(CXX) \
 	  $(PROTOC_JS_DIR)/js/code_generator.cc \
 	  $(PROTOC_JS_DIR)/js/protoc_gen_js.cc \
 	  $^ \
+	  $(PROTOC_LDFLAGS) $(PROTOC_CXXFLAGS) \
 	  $(LIBPROTOC_LDFLAGS) \
 	  -o $@
 
