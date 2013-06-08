d9b9809aaa741fa73d70b573ea53d7d691868969
Etienne Laurin <atnnn@rethinkdb.com>
2013-06-07 20:30:12 -0700

    Tweak protoc-js build even more

--- external/protobuf-plugin-closure/build.mk
+++ external/protobuf-plugin-closure/build.mk
@@ -8,8 +8,7 @@ PROTOC_JS_BUILD_DIR := $(BUILD_DIR)/protobuf-plugin-closure
 PROTOC_JS_PLUGIN := $(PROTOC_JS_BUILD_DIR)/protoc-gen-js
 
 PROTOC_LDFLAGS := $(LDFLAGS) $(PROTOBUF_LIBS) $(PTHREAD_LIBS)
-PROTOC_CXXFLAGS := $(patsubst -isystem,-I,$(CXXFLAGS)) -I $(PROTOC_JS_DIR) -I $(PROTOC_JS_DIR)/js
-PROTOC_CXXFLAGS += -I $(PROTOC_JS_BUILD_DIR) -I /usr/include
+PROTOC_CXXFLAGS := $(CXXFLAGS) -I $(PROTOC_JS_BUILD_DIR)
 
 # TODO: don't assume that libprotoc is installed right next to libprotobuf
 LIBPROTOC_LDFLAGS = $(subst protobuf,protoc,$(PROTOBUF_LIBS))
@@ -19,7 +18,7 @@ LIBPROTOC_LDFLAGS = $(subst protobuf,protoc,$(PROTOBUF_LIBS))
 $(PROTOC_JS_BUILD_DIR)/js/%.pb.cc: $(PROTOC_JS_DIR)/js/%.proto | $(PROTOC_JS_BUILD_DIR)/. $(PROTOC_DEP)
 	$P PROTOC
 	$(PROTOC) \
-	  $(PROTOC_CXXFLAGS) \
+	  -I $(PROTOC_JS_DIR) -I $(PROTOC_JS_DIR)/js \
 	  --cpp_out=$(PROTOC_JS_BUILD_DIR) \
 	  $<
 
@@ -30,6 +29,7 @@ $(PROTOC_JS_PLUGIN): $(PROTOC_JS_BUILD_DIR)/js/int64_encoding.pb.cc | $(PROTOC_D
 	  $(PROTOC_JS_DIR)/js/code_generator.cc \
 	  $(PROTOC_JS_DIR)/js/protoc_gen_js.cc \
 	  $^ \
+	  -I $(PROTOC_JS_DIR) -I $(PROTOC_JS_DIR)/js \
 	  $(PROTOC_LDFLAGS) $(PROTOC_CXXFLAGS) \
 	  $(LIBPROTOC_LDFLAGS) \
 	  -o $@
