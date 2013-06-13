840a4b6849a7be66bc066d16973dffd3b09fbeec
Sam Hughes <sam@rethinkdb.com>
2013-06-13 16:43:09 -0700

    Removed redundent setting of entries after entry->destroy.

--- src/serializer/log/data_block_manager.cc
+++ src/serializer/log/data_block_manager.cc
@@ -302,7 +302,6 @@ void data_block_manager_t::check_and_handle_empty_extent(unsigned int extent_id)
         ++stats->pm_serializer_data_extents_reclaimed;
         entry->destroy();
         rassert(entries.get(extent_id) == NULL);
-        entries.set(extent_id, NULL);
 
     } else if (entry->state == gc_entry_t::state_old) {
         entry->our_pq_entry->update();
