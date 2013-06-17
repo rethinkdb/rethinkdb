055232ee632fefc634426c3de86bc1aef49a84bb
Bill Rowan <bill@rethinkdb.com>
2013-06-17 13:26:25 -0700

    tells python protobuf at library import to use C++ implementation

--- drivers/python/rethinkdb/__init__.py
+++ drivers/python/rethinkdb/__init__.py
@@ -1,4 +1,12 @@
 # This file includes all public facing Python API functions
+
+# Set an environment variable telling the protobuf library
+# to use the fast C++ based serializer implementation
+# over the pure python one if it is available.
+from os import environ
+environ['PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION'] = 'cpp'
+
+
 from net import connect, Connection, Cursor
 from query import js, error, do, row, table, db, db_create, db_drop, db_list, table_create, table_drop, table_list, branch, count, sum, avg, asc, desc, eq, ne, le, ge, lt, gt, any, all, add, sub, mul, div, mod, type_of, info
 from errors import RqlError, RqlClientError, RqlCompileError, RqlRuntimeError, RqlDriverError
