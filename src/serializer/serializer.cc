dfdfa05c15f1404102742bb242100b40aecb9f0a
Sam Hughes <sam@rethinkdb.com>
2013-07-10 15:58:51 -0700

    Removed last use of block_write (we think).

--- src/serializer/serializer.cc
+++ src/serializer/serializer.cc
@@ -131,10 +131,15 @@ counted_t<standard_block_token_t> serializer_block_write(serializer_t *ser, cons
         void on_io_complete() { pulse(); }
     } cb;
     ser_buffer_t *ser_buf = convert_buffer_cache_buf_to_ser_buffer(buf);
-    counted_t<standard_block_token_t> result
-        = ser->block_write(ser_buf, block_id, io_account, &cb);
+
+    // RSI: use the real block size thank you.
+    std::vector<counted_t<standard_block_token_t> > tokens
+        = ser->block_writes({ buf_write_info_t(ser_buf,
+                                               ser->get_block_size().ser_value(),
+                                               block_id) }, io_account, &cb);
+    guarantee(tokens.size() == 1);
     cb.wait();
-    return result;
+    return tokens[0];
 
 }
 
