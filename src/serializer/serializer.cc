21442f72c685dc367e44c2ffb8c5250236a9a103
Sam Hughes <sam@rethinkdb.com>
2013-07-12 13:44:54 -0700

    Fixed serializer_block_write bug.

--- src/serializer/serializer.cc
+++ src/serializer/serializer.cc
@@ -144,10 +144,9 @@ counted_t<standard_block_token_t> serializer_block_write(serializer_t *ser, ser_
     struct : public cond_t, public iocallback_t {
         void on_io_complete() { pulse(); }
     } cb;
-    ser_buffer_t *ser_buf = convert_buffer_cache_buf_to_ser_buffer(buf);
 
     std::vector<counted_t<standard_block_token_t> > tokens
-        = ser->block_writes({ buf_write_info_t(ser_buf, block_size, block_id) },
+        = ser->block_writes({ buf_write_info_t(buf, block_size, block_id) },
                             io_account, &cb);
     guarantee(tokens.size() == 1);
     cb.wait();
