3cf84e2ffc7051a3da7c26573406f133dc0a3c1d
Etienne Laurin <atnnn@rethinkdb.com>
2013-07-22 15:38:40 -0700

    Tweak build of closureless js driver

--- drivers/javascript/build.mk
+++ drivers/javascript/build.mk
@@ -1,7 +1,5 @@
 # Copyright 2010-2013 RethinkDB, all rights reserved.
 
-EXTERNAL := $(TOP)/external
-
 JS_SRC_DIR=$(TOP)/drivers/javascript
 DRIVER_COFFEE_BUILD_DIR=$(JS_BUILD_DIR)/coffee
 
@@ -17,10 +15,12 @@ DRIVER_COMPILED_COFFEE := $(patsubst $(JS_SRC_DIR)/%.coffee,$(DRIVER_COFFEE_BUIL
 JS_PKG_DIR := $(PACKAGES_DIR)/js
 
 $(PB_BIN_FILE): $(PROTO_FILE) | $(JS_BUILD_DIR)/.
+	$P PROTOC
 	$(PROTOC) -I $(PROTO_FILE_DIR) -o $(JS_BUILD_DIR)/ql2.desc $(PROTO_FILE)
 
-$(PROTO_MODULE): $(PROTO_FILE)
-	proto2js $< -commonjs > $@
+$(PROTO_MODULE): $(PROTO_FILE) | $(PROTO2JS)
+	$P PROTO2JS
+	$(PROTO2JS) $< -commonjs > $@
 
 # Must be synced with the list in package.json
 JS_PKG_FILES := $(DRIVER_COMPILED_COFFEE) $(JS_SRC_DIR)/README.md $(PROTO_MODULE) $(PB_BIN_FILE) $(JS_SRC_DIR)/package.json
@@ -30,16 +30,16 @@ $(DRIVER_COFFEE_BUILD_DIR)/%.js: $(JS_SRC_DIR)/%.coffee | $(DRIVER_COFFEE_BUILD_
 	$P COFFEE
 	$(COFFEE) -b -p -c $< > $@
 
-.PHONY: js-dist
-js-dist: $(JS_PKG_FILES)
+$(JS_PKG_DIR): $(JS_PKG_FILES)
 	$P DIST-JS $(JS_PKG_DIR)
 	rm -rf $(JS_PKG_DIR)
 	mkdir -p $(JS_PKG_DIR)
 	cp $(JS_PKG_FILES) $(JS_PKG_DIR)
 
 .PHONY: js-publish
-js-publish: js-dist
+js-publish: $(JS_PKG_DIR)
 	$P PUBLISH-JS $(JS_PKG_DIR)
+	# TODO: why use --force?
 	cd $(JS_PKG_DIR) && npm publish --force
 
 .PHONY: js-clean
@@ -47,17 +47,16 @@ js-clean:
 	$P RM $(JS_BUILD_DIR)
 	rm -rf $(JS_BUILD_DIR)
 
-.PHONY: js-driver
-js-driver: js-dist
-	$P NPM INSTALL $(JS_PKG_DIR)
-	#TODO This currently won't run without erroring because it recursively calls make
-	# which results in a make error
-	npm install $(JS_PKG_DIR)
+.PHONY: js-install
+js-install: NPM_PREFIX=.
+js-install: $(JS_PKG_DIR)
+	$P NPM-INSTALL $(JS_PKG_DIR)
+	MAKEFLAGS= npm install $(JS_PKG_DIR) --prefix $(NPM_PREFIX)
 
-$(JS_BUILD_DIR)/rethinkdb.js: $(JS_BUILD_DIR)/. js-driver
-	#echo "throw new Error('Web version of JS driver not available');" > $(JS_BUILD_DIR)/rethinkdb.js
+$(JS_BUILD_DIR)/rethinkdb.js: $(JS_PKG_DIR) | $(BROWSERIFY)
 	$P BROWSERIFY
-	browserify --require rethinkdb --outfile $@
+	cd $(JS_PKG_DIR) && \
+	  $(abspath $(BROWSERIFY)) --require rethinkdb --outfile $(abspath $@)
 
-.PHONY: all
-all: js-driver
+.PHONY: js-driver
+js-driver: $(JS_BUILD_DIR)/rethinkdb.js
