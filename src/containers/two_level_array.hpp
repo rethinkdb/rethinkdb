4d69c24a44e0ad1e740ad603756b6c1fbaa4b3b5
Sam Hughes <sam@rethinkdb.com>
2013-07-29 18:36:35 -0700

    Made two_level_array_t and two_level_nevershrink_array_t use std::vector instead of having a max size.

--- src/containers/two_level_array.hpp
+++ src/containers/two_level_array.hpp
@@ -4,6 +4,8 @@
 
 #include <limits.h>
 
+#include <vector>
+
 #include "errors.hpp"
 
 /* two_level_array_t is a tree that always has exactly two levels. Its computational complexity is
@@ -25,50 +27,39 @@ It is also parameterized at compile-time on the maximum number of elements in th
 size of the chunks to use.
 */
 
-#define DEFAULT_TWO_LEVEL_ARRAY_CHUNK_SIZE (1 << 16)
+#define TWO_LEVEL_ARRAY_CHUNK_SIZE (1 << 16)
 
-// RSI: Remove this parameter.
-#define FAKE_UNRUNNABLE_MAX_TWO_LEVEL_ARRAY_SIZE (INT_MAX)
-
-template <class value_t, size_t max_size = FAKE_UNRUNNABLE_MAX_TWO_LEVEL_ARRAY_SIZE, size_t chunk_size = DEFAULT_TWO_LEVEL_ARRAY_CHUNK_SIZE>
+template <class value_t>
 class two_level_array_t {
 private:
-    static const size_t num_chunks = max_size / chunk_size + 1;
-
     struct chunk_t {
         chunk_t()
             : count(0), values()   // default-initialize each value in values
             { }
         size_t count;
-        value_t values[chunk_size];
+        value_t values[TWO_LEVEL_ARRAY_CHUNK_SIZE];
     };
-    chunk_t **chunks;
+    std::vector<chunk_t *> chunks;
 
     static size_t chunk_for_key(key_t key) {
-        size_t chunk_id = key / chunk_size;
-        rassert(chunk_id < num_chunks, "chunk_id < num_chunks: %zu < %zu", chunk_id, num_chunks);
+        size_t chunk_id = key / TWO_LEVEL_ARRAY_CHUNK_SIZE;
         return chunk_id;
     }
     static size_t index_for_key(key_t key) {
-        return key % chunk_size;
+        return key % TWO_LEVEL_ARRAY_CHUNK_SIZE;
     }
 
 public:
-    two_level_array_t() : chunks(new chunk_t *[num_chunks]) {
-        for (size_t i = 0; i < num_chunks; i++) {
-            chunks[i] = NULL;
-        }
-    }
+    two_level_array_t() { }
     ~two_level_array_t() {
-        for (size_t i = 0; i < num_chunks; i++) {
-            delete chunks[i];
+        for (auto it = chunks.begin(); it != chunks.end(); ++it) {
+            delete *it;
         }
-        delete[] chunks;
     }
 
     value_t get(size_t key) const {
         size_t chunk_id = chunk_for_key(key);
-        if (chunks[chunk_id]) {
+        if (chunk_id < chunks.size() && chunks[chunk_id]) {
             return chunks[chunk_id]->values[index_for_key(key)];
         } else {
             return value_t();
@@ -76,21 +67,24 @@ public:
     }
 
     void set(size_t key, value_t value) {
-        size_t chunk_id = chunk_for_key(key);
-        chunk_t *chunk = chunks[chunk_id];
-
-        if (!value && !chunk) {
-            /* If the user is inserting a zero value into an already-empty chunk, exit early so we
-            don't create a new empty chunk */
-            return;
+        const size_t chunk_id = chunk_for_key(key);
+        if (chunk_id >= chunks.size() || chunks[chunk_id] == NULL) {
+            if (!value) {
+                return;
+            } else {
+                if (chunk_id >= chunks.size()) {
+                    chunks.resize(chunk_id + 1, NULL);
+                }
+                chunks[chunk_id] = new chunk_t;
+            }
         }
 
-        if (!chunk) chunk = chunks[chunk_id] = new chunk_t;
-
-        if (chunk->values[index_for_key(key)]) {
+        chunk_t *chunk = chunks[chunk_id];
+        const size_t index = index_for_key(key);
+        if (chunk->values[index]) {
             --chunk->count;
         }
-        chunk->values[index_for_key(key)] = value;
+        chunk->values[index] = value;
         if (value) {
             ++chunk->count;
         }
@@ -98,54 +92,53 @@ public:
         if (chunk->count == 0) {
             chunks[chunk_id] = NULL;
             delete chunk;
+
+            while (!chunks.empty() && chunks.back() == NULL) {
+                chunks.pop_back();
+            }
         }
     }
 };
 
 
-template <class value_t, size_t max_size = FAKE_UNRUNNABLE_MAX_TWO_LEVEL_ARRAY_SIZE, size_t chunk_size = DEFAULT_TWO_LEVEL_ARRAY_CHUNK_SIZE>
+template <class value_t>
 class two_level_nevershrink_array_t {
 private:
-    static const size_t num_chunks = max_size / chunk_size + 1;
-
     struct chunk_t {
         chunk_t()
             : values()   // default-initialize each value in values
             { }
-        value_t values[chunk_size];
+        value_t values[TWO_LEVEL_ARRAY_CHUNK_SIZE];
     };
-    chunk_t **chunks;
+    std::vector<chunk_t *> chunks;
 
     static size_t chunk_for_key(key_t key) {
-        size_t chunk_id = key / chunk_size;
-        rassert(chunk_id < num_chunks, "chunk_id < num_chunks: %zu < %zu", chunk_id, num_chunks);
+        size_t chunk_id = key / TWO_LEVEL_ARRAY_CHUNK_SIZE;
         return chunk_id;
     }
     static size_t index_for_key(key_t key) {
-        return key % chunk_size;
+        return key % TWO_LEVEL_ARRAY_CHUNK_SIZE;
     }
 
 public:
-    two_level_nevershrink_array_t() : chunks(new chunk_t *[num_chunks]) {
-        for (size_t i = 0; i < num_chunks; i++) {
-            chunks[i] = NULL;
-        }
-    }
+    two_level_nevershrink_array_t() { }
     ~two_level_nevershrink_array_t() {
-        for (size_t i = 0; i < num_chunks; i++) {
-            delete chunks[i];
+        for (auto it = chunks.begin(); it != chunks.end(); ++it) {
+            delete *it;
         }
-        delete[] chunks;
     }
 
     value_t &operator[](size_t key) {
-        size_t chunk_id = chunk_for_key(key);
-        if (chunks[chunk_id]) {
-            return chunks[chunk_id]->values[index_for_key(key)];
-        } else {
-            chunk_t *chunk = chunks[chunk_id] = new chunk_t;
-            return chunk->values[index_for_key(key)];
+        const size_t chunk_id = chunk_for_key(key);
+        if (chunk_id > chunks.size()) {
+            chunks.resize(chunk_id + 1, NULL);
         }
+
+        if (chunks[chunk_id] == NULL) {
+            chunks[chunk_id] = new chunk_t;
+        }
+
+        return chunks[chunk_id]->values[index_for_key(key)];
     }
 };
 
