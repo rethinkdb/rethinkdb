054c382f0fd76a18f872e0a9467a887354f83897
jdoliner <jdoliner@gmail.com>
2013-08-05 13:53:04 -0700

    Adds pseudo typing for literal.

---
+++ src/rdb_protocol/pseudo_literal.cc
@@ -0,0 +1,24 @@
+#include "rdb_protocol/pseudo_literal.hpp"
+
+namespace ql {
+namespace pseudo {
+
+const char *const literal_string = "literal";
+const char *const value_string = "value";
+
+
+void rcheck_literal_valid(const datum_t *lit) {
+    for (auto it = lit->as_object().begin(); it != lit->as_object().end(); ++it) {
+        if (it->first == datum_t::reql_type_string) {
+            r_sanity_check(it->second->as_str() == literal_string);
+        } else if (it->first == value_string) {
+        } else {
+            rfail_target(lit, base_exc_t::GENERIC,
+                         "Invalid literal term with illegal key `%s`.",
+                         it->first.c_str());
+        }
+    }
+}
+
+} // namespace pseudo
+} // namespace ql
