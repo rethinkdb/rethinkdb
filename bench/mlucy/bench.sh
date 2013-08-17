0165de7b3ee1332f9421491a20b9f959f4e882ef
Michael Lucy <mlucy@rethinkdb.com>
2013-08-16 22:53:40 -0700

    added rsync_to

--- bench/mlucy/bench.sh
+++ bench/mlucy/bench.sh
@@ -5,15 +5,15 @@ load_conf "$1"
 # Used by `admin` command.
 export RETHINKDB_FOR_ADMIN=$RETHINKDB_DIR/rethinkdb
 
-new_cluster "server" $SERVERS > server_hosts &
-new_cluster "client" $CLIENTS > client_hosts &
+# new_cluster "$PREFIX-server" $SERVERS > server_hosts &
+# new_cluster "$PREFIX-client" $CLIENTS > client_hosts &
 wait
 
 # clients=":::: client_hosts ::: `seq $CLIENT_INSTANCES`"
 
-init_rdb_cluster `cat server_hosts` $SERVER_INSTANCES $RETHINKDB_DIR &
-cluster_port=`tunnel_to_poc $(head -1 server_hosts) 1`
-export CLUSTER_PORT=$cluster_port
+init_rdb_cluster "`cat server_hosts`" $SERVER_INSTANCES $RETHINKDB_DIR $STAGING &
+tunnel_to_poc $(head -1 server_hosts) 1 >cluster_port
+export CLUSTER_PORT=`cat cluster_port`
 wait
 
 for i in `seq $TABLES`; do cat /proc/sys/kernel/random/uuid | tr - _; done >tables
