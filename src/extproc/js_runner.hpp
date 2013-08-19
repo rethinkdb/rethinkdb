0e142e7c70a697d1b647488677cec7e3517220dc
Marc Hesse <marc@rethinkdb.com>
2013-08-19 15:40:14 -0700

    changing js extprocs to pass around ql::datum_t instead of cJSON

--- src/extproc/js_runner.hpp
+++ src/extproc/js_runner.hpp
@@ -11,6 +11,8 @@
 #include <boost/variant.hpp>
 
 #include "containers/scoped.hpp"
+#include "containers/counted.hpp"
+#include "rdb_protocol/datum.hpp"
 #include "concurrency/wait_any.hpp"
 #include "arch/timing.hpp"
 #include "http/json.hpp"
@@ -21,7 +23,7 @@ const js_id_t INVALID_ID = 0;
 
 // JS calls result either in a DATUM return value, a function id (which we can
 // use to call the function later), or an error string
-typedef boost::variant<std::shared_ptr<const scoped_cJSON_t>, js_id_t, std::string> js_result_t;
+typedef boost::variant<counted_t<const ql::datum_t>, js_id_t, std::string> js_result_t;
 
 class extproc_pool_t;
 class js_runner_t;
@@ -59,7 +61,7 @@ public:
 
     // Calls a previously compiled function.
     js_result_t call(const std::string &source,
-                     const std::vector<std::shared_ptr<const scoped_cJSON_t> > &args,
+                     const std::vector<counted_t<const ql::datum_t> > &args,
                      const req_config_t &config);
 
 private:
