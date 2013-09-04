1037fbc4b7a025a0c8bcd08d2ab1a774e02456ba
Michael Lucy <mlucy@rethinkdb.com>
2013-09-03 19:58:37 -0700

    1389

---
+++ bench/mlucy/1389-postshard/client/main.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+set -o nounset
+#!/bin/bash
+set -e
+set -o nounset
+
+run_at=$1
+nodes=$2
+
+cd `dirname "$0"`
+while [[ `date +%s` -lt $run_at ]]; do
+    sleep 0.1
+done
+echo "$run_at $nodes"
---
+++ bench/mlucy/1389-postshard/conf.sh
@@ -0,0 +1,11 @@
+# This is a benchmark configuraiton file.  It's just a shell script,
+# so write whatever you want as long as you export the right
+# variables.
+
+SERVERS=2
+SERVER_INSTANCES=2
+SERVER_OPTS=""
+SERVER_STAGING=/mnt/ssd
+
+CLIENTS=2
+CLIENT_STAGING=/home/ubuntu/client
