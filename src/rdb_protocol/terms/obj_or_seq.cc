ef1bc0815ebb715e49bd6884609e258655e56bc0
Sam Hughes <sam@rethinkdb.com>
2013-09-09 23:22:24 -0700

    Made obj_or_seq_op_term_t use hard-coded gensym.

--- src/rdb_protocol/terms/obj_or_seq.cc
+++ src/rdb_protocol/terms/obj_or_seq.cc
@@ -28,7 +28,7 @@ public:
                          poly_type_t _poly_type, argspec_t argspec)
         : op_term_t(env, term, argspec, optargspec_t({"_NO_RECURSE_"})),
           poly_type(_poly_type), func(make_counted_term()) {
-        const sym_t varnum = env->symgen->gensym();
+        const sym_t varnum = GENSYM_A();
         Term *arg = pb::set_func(func.get(), varnum);
         Term *body = NULL;
         switch (poly_type) {
