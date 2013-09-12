ac1c756ce5195f21d2a4413847655be983dd76fd
jdoliner <jdoliner@gmail.com>
2013-09-12 11:16:29 -0700

    Adds more adapters.. this might no compile.

---
+++ src/compression/read_write_stream_adapter.cc
@@ -0,0 +1,8 @@
+#include "compression/read_write_stream_adapter.hpp"
+
+write_stream_sink_t::write_stream_sink_t(write_stream_t *write_stream)
+    : internal(write_stream) { }
+
+void write_stream_sink_t::Append(const char *bytes, size_t n) {
+    guarantee(internal->write(bytes, n) != -1);
+}
---
+++ src/compression/read_write_stream_adapter.hpp
@@ -0,0 +1,17 @@
+#ifndef COMPRESSION_READ_WRITE_STREAM_ADAPTER_HPP_
+#define COMPRESSION_READ_WRITE_STREAM_ADAPTER_HPP_
+
+#include <snappy-sinksource.h>
+#include "containers/archive/archive.hpp"
+
+class write_stream_sink_t : public snappy::Sink {
+public:
+    write_stream_sink_t(write_stream_t *write_stream);
+    virtual void Append(const char *bytes, size_t n);
+private:
+    write_stream_t *internal;
+};
+
+class read_stream_source_t
+
+#endif
