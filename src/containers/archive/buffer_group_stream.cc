aaa32f8cf090ff022dccc68d0a0249e20892a088
Sam Hughes <sam@rethinkdb.com>
2013-09-14 21:48:56 -0700

    Added buffer_group_write_stream_t type.

--- src/containers/archive/buffer_group_stream.cc
+++ src/containers/archive/buffer_group_stream.cc
@@ -28,3 +28,32 @@ int64_t buffer_group_read_stream_t::read(void *p, int64_t n) {
 
     return original_n - n;
 }
+
+buffer_group_write_stream_t::buffer_group_write_stream_t(const buffer_group_t *group)
+    : group_(group), bufnum_(0), bufpos_(0) { }
+
+buffer_group_write_stream_t::~buffer_group_write_stream_t() { }
+
+int64_t buffer_group_write_stream_t::write(const void *p, int64_t n) {
+    const char *v = static_cast<const char *>(p);
+    const int64_t original_n = n;
+
+    while (bufnum_ < group_->num_buffers() && n > 0) {
+        buffer_group_t::buffer_t buf = group_->get_buffer(bufnum_);
+        int64_t bytes_to_copy = std::min(buf.size - bufpos_, n);
+        memcpy(static_cast<char *>(buf.data) + bufpos_, v, bytes_to_copy);
+        n -= bytes_to_copy;
+        v += bytes_to_copy;
+        bufpos_ += bytes_to_copy;
+
+        if (bufpos_ == buf.size) {
+            ++bufnum_;
+            bufpos_ = 0;
+        }
+    }
+
+    return n == 0 ? original_n : -1;
+}
+
+
+
--- src/containers/archive/buffer_group_stream.hpp
+++ src/containers/archive/buffer_group_stream.hpp
@@ -6,6 +6,7 @@
 #include "errors.hpp"
 
 class const_buffer_group_t;
+class buffer_group_t;
 
 class buffer_group_read_stream_t : public read_stream_t {
 public:
@@ -20,6 +21,21 @@ private:
     int64_t bufpos_;
 
     DISABLE_COPYING(buffer_group_read_stream_t);
+};
+
+class buffer_group_write_stream_t : public write_stream_t {
+public:
+    explicit buffer_group_write_stream_t(const buffer_group_t *group);
+    virtual ~buffer_group_write_stream_t();
+
+    virtual MUST_USE int64_t write(const void *p, int64_t n);
+
+private:
+    const buffer_group_t *group_;
+    size_t bufnum_;
+    int64_t bufpos_;
+
+    DISABLE_COPYING(buffer_group_write_stream_t);
 };
 
 
