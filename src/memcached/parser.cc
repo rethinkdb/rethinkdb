73d7e60b707d83f41c5db1b266de0c7b675e6d74
Sam Hughes <sam@rethinkdb.com>
2013-09-17 00:39:58 -0700

    Removed boost::bind-related hack storage_metadata_t.

--- src/memcached/parser.cc
+++ src/memcached/parser.cc
@@ -566,20 +566,14 @@ enum storage_command_t {
     prepend_command
 };
 
-struct storage_metadata_t {
-    const mcflags_t mcflags;
-    const exptime_t exptime;
-    const cas_t unique;
-    storage_metadata_t(mcflags_t _mcflags, exptime_t _exptime, cas_t _unique)
-        : mcflags(_mcflags), exptime(_exptime), unique(_unique) { }
-};
-
 void run_storage_command(txt_memcached_handler_t *rh,
                          pipeliner_acq_t *pipeliner_acq_raw,
                          storage_command_t sc,
                          store_key_t key,
-                         const counted_t<data_buffer_t>& data,
-                         storage_metadata_t metadata,
+                         const counted_t<data_buffer_t> &data,
+                         mcflags_t mcflags,
+                         exptime_t exptime,
+                         cas_t unique,
                          bool noreply,
                          order_token_t token) {
 
@@ -619,8 +613,8 @@ void run_storage_command(txt_memcached_handler_t *rh,
         bool ok;
 
         try {
-            sarc_mutation_t sarc_mutation(key, data, metadata.mcflags, metadata.exptime,
-                add_policy, replace_policy, metadata.unique);
+            sarc_mutation_t sarc_mutation(key, data, mcflags, exptime,
+                add_policy, replace_policy, unique);
             memcached_protocol_t::write_t write(sarc_mutation, rh->generate_cas(), time(NULL));
             memcached_protocol_t::write_response_t result;
             rh->nsi->write(write, &result, token, rh->interruptor);
@@ -848,14 +842,9 @@ void do_storage(txt_memcached_handler_t *rh, pipeliner_t *pipeliner, storage_com
         return;
     }
 
-    /* Bundle metadata into one object so we don't try to pass too many arguments
-    to `std::bind()` */
-    // RSI: is this no longer necessary?
-    storage_metadata_t metadata(mcflags, exptime, unique);
-
     pipeliner_acq->done_argparsing();
 
-    coro_t::spawn_now_dangerously(std::bind(&run_storage_command, rh, pipeliner_acq.release(), sc, key, dp, metadata, noreply, token));
+    coro_t::spawn_now_dangerously(std::bind(&run_storage_command, rh, pipeliner_acq.release(), sc, key, dp, mcflags, exptime, unique, noreply, token));
 }
 
 /* "incr" and "decr" commands */
