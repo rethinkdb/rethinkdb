ec5b8756dc978faf521441582d29ca2288ef2d8b
Sam Hughes <sam@rethinkdb.com>
2013-09-16 22:34:27 -0700

    Removed boost::bind in cross_thread_watchable.cc, clustering_branch.cc, rpc_connectivity.cc, fifo_enforcer.cc, memcached_protocol.cc, and rpc_semilattice.cc unit tests.

--- src/unittest/clustering_branch.cc
+++ src/unittest/clustering_branch.cc
@@ -1,9 +1,6 @@
 // Copyright 2010-2013 RethinkDB, all rights reserved.
 #include "unittest/gtest.hpp"
 
-#include "errors.hpp"
-#include <boost/bind.hpp>
-
 #include "clustering/immediate_consistency/branch/broadcaster.hpp"
 #include "clustering/immediate_consistency/branch/listener.hpp"
 #include "clustering/immediate_consistency/branch/replier.hpp"
@@ -17,6 +14,7 @@
 #include "unittest/unittest_utils.hpp"
 
 using mock::dummy_protocol_t;
+using namespace std::placeholders;
 
 namespace unittest {
 
@@ -99,7 +97,7 @@ void run_in_thread_pool_with_broadcaster(
                              scoped_ptr_t<listener_t<dummy_protocol_t> > *,
                              order_source_t *)> fun)
 {
-    unittest::run_in_thread_pool(boost::bind(&run_with_broadcaster, fun));
+    unittest::run_in_thread_pool(std::bind(&run_with_broadcaster, fun));
 }
 
 }   /* anonymous namespace */
@@ -207,7 +205,7 @@ void run_backfill_test(io_backender_t *io_backender,
     /* Start sending operations to the broadcaster */
     std::map<std::string, std::string> inserter_state;
     test_inserter_t inserter(
-        boost::bind(&write_to_broadcaster, broadcaster->get(), _1, _2, _3, _4),
+        std::bind(&write_to_broadcaster, broadcaster->get(), _1, _2, _3, _4),
         NULL,
         &dummy_key_gen,
         order_source,
@@ -272,7 +270,7 @@ void run_partial_backfill_test(io_backender_t *io_backender,
     /* Start sending operations to the broadcaster */
     std::map<std::string, std::string> inserter_state;
     test_inserter_t inserter(
-        boost::bind(&write_to_broadcaster, broadcaster->get(), _1, _2, _3, _4),
+        std::bind(&write_to_broadcaster, broadcaster->get(), _1, _2, _3, _4),
         NULL,
         &dummy_key_gen,
         order_source,
--- src/unittest/fifo_enforcer.cc
+++ src/unittest/fifo_enforcer.cc
@@ -2,7 +2,6 @@
 #include "concurrency/fifo_enforcer.hpp"
 
 #include "errors.hpp"
-#include <boost/bind.hpp>
 #include <boost/variant.hpp>
 
 #include "arch/timing.hpp"
@@ -18,14 +17,14 @@ class test_shared_variable_t {
 public:
     explicit test_shared_variable_t(int initial) : variable(initial) { }
     void spawn_read(int expected_value) {
-        coro_t::spawn_sometime(boost::bind(
+        coro_t::spawn_sometime(std::bind(
             &test_shared_variable_t::attempt_read, this,
             expected_value,
             source.enter_read(),
             auto_drainer_t::lock_t(&auto_drainer)));
     }
     void spawn_write(int expected_value, int new_value) {
-        coro_t::spawn_sometime(boost::bind(
+        coro_t::spawn_sometime(std::bind(
             &test_shared_variable_t::attempt_write, this,
             expected_value, new_value,
             source.enter_write(),
