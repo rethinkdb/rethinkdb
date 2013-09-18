69ef24b7c9b938df37b05cc97a36d9047b874089
Sam Hughes <sam@rethinkdb.com>
2013-09-17 23:04:29 -0700

    Made one_per_thread_t use simple bind objects instead of manually declared callables.

--- src/concurrency/one_per_thread.hpp
+++ src/concurrency/one_per_thread.hpp
@@ -1,4 +1,4 @@
-// Copyright 2010-2012 RethinkDB, all rights reserved.
+// Copyright 2010-2013 RethinkDB, all rights reserved.
 #ifndef CONCURRENCY_ONE_PER_THREAD_HPP_
 #define CONCURRENCY_ONE_PER_THREAD_HPP_
 
@@ -10,67 +10,24 @@
 template <class T>
 class one_per_thread_t {
 public:
-    struct construct_0_t {
-        one_per_thread_t *parent_;
-        explicit construct_0_t(one_per_thread_t *p) : parent_(p) { }
-        void operator()(int thread) const {
-            on_thread_t th((threadnum_t(thread)));
-            parent_->array[thread].init(new T());
-        }
-    };
-
     one_per_thread_t() : array(get_num_threads()) {
-        pmap(get_num_threads(), construct_0_t(this));
+        pmap(get_num_threads(), std::bind(&one_per_thread_t::construct_0, this, _1));
     }
 
     template <class arg1_t>
-    struct construct_1_t {
-        one_per_thread_t *parent_;
-        const arg1_t &arg1_;
-        construct_1_t(one_per_thread_t *p, const arg1_t &arg1) : parent_(p), arg1_(arg1) { }
-
-        void operator()(int thread) const {
-            on_thread_t th((threadnum_t(thread)));
-            parent_->array[thread].init(new T(arg1_));
-        }
-    };
-
-
-    template<class arg1_t>
     explicit one_per_thread_t(const arg1_t &arg1) : array(get_num_threads()) {
-        pmap(get_num_threads(), construct_1_t<arg1_t>(this, arg1));
+        pmap(get_num_threads(), std::bind(&one_per_thread_t::construct_1<arg1_t>,
+                                          this, _1, std::ref(arg1)));
     }
-
-    template <class arg1_t, class arg2_t>
-    struct construct_2_t {
-        one_per_thread_t *parent_;
-        const arg1_t &arg1_;
-        const arg2_t &arg2_;
-        construct_2_t(one_per_thread_t *p, const arg1_t &arg1, const arg2_t &arg2) : parent_(p), arg1_(arg1), arg2_(arg2) { }
-
-        void operator()(int thread) const {
-            on_thread_t th((threadnum_t(thread)));
-            parent_->array[thread].init(new T(arg1_, arg2_));
-        }
-    };
 
     template<class arg1_t, class arg2_t>
     one_per_thread_t(const arg1_t &arg1, const arg2_t &arg2) : array(get_num_threads()) {
-        pmap(get_num_threads(), construct_2_t<arg1_t, arg2_t>(this, arg1, arg2));
+        pmap(get_num_threads(), std::bind(&one_per_thread_t::construct_2<arg1_t, arg2_t>,
+                                          this, _1, std::ref(arg1), std::ref(arg2)));
     }
 
-
-    struct destruct_t {
-        one_per_thread_t *parent_;
-        explicit destruct_t(one_per_thread_t *p) : parent_(p) { }
-        void operator()(int thread) const {
-            on_thread_t th((threadnum_t(thread)));
-            parent_->array[thread].reset();
-        }
-    };
-
     ~one_per_thread_t() {
-        pmap(get_num_threads(), destruct_t(this));
+        pmap(get_num_threads(), std::bind(&one_per_thread_t::destruct, this, _1));
     }
 
     T *get() {
@@ -78,6 +35,28 @@ public:
     }
 
 private:
+    void construct_0(int thread) {
+        on_thread_t th((threadnum_t(thread)));
+        array[thread].init(new T());
+    }
+
+    template <class arg1_t>
+    void construct_1(int thread, const arg1_t &arg1) {
+        on_thread_t th((threadnum_t(thread)));
+        array[thread].init(new T(arg1));
+    }
+
+    template <class arg1_t, class arg2_t>
+    void construct_2(int thread, const arg1_t &arg1, const arg2_t &arg2) {
+        on_thread_t th((threadnum_t(thread)));
+        array[thread].init(new T(arg1, arg2));
+    }
+
+    void destruct(int thread) {
+        on_thread_t th((threadnum_t(thread)));
+        array[thread].reset();
+    }
+
     // An array of size get_num_threads().  Pointees are allocated and destroyed on
     // their respective threads.  Because this is an array of pointers and not
     // object_buffer_t<T>, objects live on separate cache lines.
