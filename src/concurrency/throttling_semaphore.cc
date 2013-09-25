88e1ac01b9bffcbf4e969805bbe8558d592567f3
Daniel Mewes <daniel@rethinkdb.com>
2013-09-24 17:25:34 -0700

    Throttling semaphore initial implementation.

---
+++ src/concurrency/throttling_semaphore.cc
@@ -0,0 +1,126 @@
+// Copyright 2010-2013 RethinkDB, all rights reserved.
+#include "concurrency/throttling_semaphore.hpp"
+
+#include "arch/runtime/coroutines.hpp"
+#include "concurrency/cond_var.hpp"
+
+
+void throttling_semaphore_t::on_waiters_changed() {
+    // Start or stop the timer depending on whether waiters is empty or not
+    if (waiters.empty() && timer.has()) {
+        timer.reset();
+    } else if (!waiters.empty() && !timer.has()) {
+        timer.init(new repeating_timer_t(delay_granularity, this));
+    }
+}
+
+void throttling_semaphore_t::on_ring() {
+    lock_request_t *request = waiters.head();
+    while (request != NULL) {
+        lock_request_t *next_request = waiters.next(request);
+        
+        request->progress(delay_granularity);
+        
+        const bool might_have_waited_long_enough = request->total_time_waited >= request->target_delay;
+        const bool recalc_delay_met = request->time_since_recalc >= request->recalc_delay;
+        if (recalc_delay_met || might_have_waited_long_enough) {
+            request->target_delay = compute_target_delay();
+            // Wait longer before we perform the next re-calculation
+            request->recalc_delay *= 2; // TODO (daniel): Should we limit this to some reasonable value?
+        }
+        
+        const bool has_waited_long_enough = request->total_time_waited >= request->target_delay;
+        if (has_waited_long_enough) {
+            current += request->count;
+            waiters.remove(request);
+            on_waiters_changed();
+            request->on_available();
+        } else {
+            // If we want to maintain the ordering of requests,
+            // we must *not* skip ahead and accept a later request first.
+            if (maintain_ordering) break;
+        }
+        
+        request = next_request;
+    }
+}
+
+void throttling_semaphore_t::lock(semaphore_available_callback_t *cb, int count) {
+    int64_t target_delay = compute_target_delay();
+    if (target_delay == 0) {
+        // Continue immediately
+        current += count;
+        cb->on_semaphore_available();
+    } else {
+        // Put it on the queue
+        lock_request_t *r = new lock_request_t;
+        r->count = count;
+        r->cb = cb;
+        r->recalc_delay = delay_granularity; // Re-calculate the delay for the first time at delay_granularity
+        r->target_delay = target_delay;
+        r->time_since_recalc = 0;
+        r->total_time_waited = 0;
+        waiters.push_back(r);
+        on_waiters_changed();
+    }
+}
+
+void throttling_semaphore_t::unlock(int count) {
+    rassert(current >= count);
+    current -= count;
+}
+
+void throttling_semaphore_t::force_lock(int count) {
+    current += count;
+}
+
+void throttling_semaphore_t::co_lock(int count) {
+    struct : public semaphore_available_callback_t, public one_waiter_cond_t {
+        void on_semaphore_available() { pulse(); }
+    } cb;
+    lock(&cb, count);
+    cb.wait_eagerly_deprecated();
+    coro_t::yield();
+}
+
+void throttling_semaphore_t::co_lock_interruptible(signal_t *interruptor, int count) {
+    struct : public semaphore_available_callback_t, public cond_t {
+        void on_semaphore_available() { pulse(); }
+    } cb;
+    lock(&cb, count);
+
+    try {
+        wait_interruptible(&cb, interruptor);
+    } catch (const interrupted_exc_t& ex) {
+        // Remove our lock request from the queue
+        for (lock_request_t *request = waiters.head(); request != NULL; request = waiters.next(request)) {
+            if (request->cb == &cb) {
+                waiters.remove(request);
+                on_waiters_changed();
+                delete request;
+                break;
+            }
+        }
+        throw;
+    }
+}
+
+int64_t throttling_semaphore_t::compute_target_delay() const {
+    // Do not delay if we are below the throttling_threshold
+    const int64_t threshold = static_cast<int64_t>(static_cast<double>(capacity) * throttling_threshold);
+    if (current < threshold) return 0;
+    
+    rassert(current > threshold);
+    rassert(threshold < capacity);
+    const double delay_level = static_cast<double>(current - threshold) / static_cast<double>(capacity - threshold);
+    rassert(delay_level >= 0.0 && delay_level <= 1.0);
+    // This is the function we are using to calculate delays: http://www.wolframalpha.com/input/?i=1%2F%281-x%29+-+1+for+x+from+0+to+1
+    // If necessary, the steepness can be adjusted by taking a power of delay_level. This
+    // is not currently implemented here though.
+    if (delay_level > 0.999999) // Fall-through for (close enough to) infinite
+        return 1000000;
+    // We multiply the value of the function by a delay_at_half constant.
+    // delay_at_half is the delay in ms when delay_level = 0.5
+    const double target_delay = static_cast<double>(delay_at_half) * (1.0/(1.0 - delay_level) - 1.0);
+    return static_cast<int64_t>(target_delay);
+}
---
+++ src/concurrency/throttling_semaphore.hpp
@@ -0,0 +1,115 @@
+// Copyright 2010-2013 RethinkDB, all rights reserved.
+#ifndef THROTTLING_SEMAPHORE_HPP_
+#define THROTTLING_SEMAPHORE_HPP_
+
+#include "containers/scoped.hpp"
+#include "concurrency/semaphore.hpp"
+#include "concurrency/signal.hpp"
+#include "concurrency/wait_any.hpp"
+#include "containers/intrusive_list.hpp"
+#include "arch/timing.hpp"
+
+/* TODO (daniel): Document
+ * It attempts to meet the following design goals:
+ * - No lock is ever granted if the given capacity is currently violated (except when using lock_force).
+ *   If necessary, lock requests are delayed indefinitely. Note that the capacity requirements
+ *   of the issued lock itself are not considered in this guarantee (so no request is
+ *   hold back indefinitely if its own count is higher than the capacity).
+ * - throttling_semaphore_t does adapt to changing conditions. If locks are released
+ *   earlier or later than expected, the delay times of queued lock requests are re-evaluated
+ *   automatically.
+ * - The configuration of delay times is robust, i.e. throttling_semaphore_t works reasonably well
+ *   for a wide range of `delay_at_half` values.
+ */
+class throttling_semaphore_t : public repeating_timer_callback_t {    
+    struct lock_request_t : public intrusive_list_node_t<lock_request_t> {
+        semaphore_available_callback_t *cb;
+        int count;
+        int64_t target_delay;
+        int64_t total_time_waited;
+        int64_t time_since_recalc;
+        int64_t recalc_delay;
+        void on_available() {
+            cb->on_semaphore_available();
+            delete this;
+        }
+        void progress(int64_t time_passed) {
+            total_time_waited += time_passed;            
+            time_since_recalc += time_passed;            
+        }
+    };
+
+    int capacity, current;
+    double throttling_threshold; // No throttling if current < capacity * throttling_threshold.
+    int64_t delay_granularity;
+    int64_t delay_at_half;
+    intrusive_list_t<lock_request_t> waiters;
+    scoped_ptr_t<repeating_timer_t> timer;
+    bool maintain_ordering;
+
+
+public:
+    /* throttling_semaphore_t starts throttling transactions when current >= cap * thre.
+     * The granularity in which delays are processed is gran (smaller = higher resolution, more overhead).
+     * If current is halfway in between (cap - (cap * thre)) and cap, the delay will be d_half.
+     */
+    explicit throttling_semaphore_t(int cap, double thre = 0.5, int64_t gran = 25, int64_t d_half = 100) :
+        capacity(cap),
+        current(0),
+        throttling_threshold(thre),
+        delay_granularity(gran),
+        delay_at_half(d_half),
+        maintain_ordering(true)
+    {
+        rassert(throttling_threshold >= 0.0 && throttling_threshold <= 1.0);
+        rassert(delay_granularity > 0);
+        rassert(capacity >= 0 || capacity == SEMAPHORE_NO_LIMIT);
+    }
+
+    void lock(semaphore_available_callback_t *cb, int count = 1);
+
+    void co_lock(int count = 1);
+    void co_lock_interruptible(signal_t *interruptor, int count = 1);
+
+    void unlock(int count = 1);
+
+    void force_lock(int count = 1);
+
+    // This does not trigger any request to be re-scheduled immediately.
+    // Instead the new capacity will become effective as part of the regular
+    // re-calculation schedule.
+    void set_capacity(int new_capacity) { capacity = new_capacity; }
+    int get_capacity() const { return capacity; }
+    
+    virtual void on_ring(); // Called periodically by timer
+
+private:
+    void on_waiters_changed(); // Should be called after a change to waiters
+    
+    int64_t compute_target_delay() const; // The "heart" of throttling_semaphore_t.
+};
+
+
+class throttling_semaphore_acq_t {
+public:
+    throttling_semaphore_acq_t(throttling_semaphore_t *_acquiree, int c = 1) : acquiree(_acquiree), count(c) {
+        acquiree->co_lock(count);
+    }
+
+    throttling_semaphore_acq_t(throttling_semaphore_acq_t &&movee) : acquiree(movee.acquiree) {
+        movee.acquiree = NULL;
+    }
+
+    ~throttling_semaphore_acq_t() {
+        if (acquiree) {
+            acquiree->unlock(count);
+        }
+    }
+
+private:
+    throttling_semaphore_t *acquiree;
+    int count;
+    DISABLE_COPYING(throttling_semaphore_acq_t);
+};
+
+#endif /* THROTTLING_SEMAPHORE_HPP_ */
