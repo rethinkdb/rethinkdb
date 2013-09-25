4ba41b7fda76ed4a6444e3165dfdc8cc714b9f1e
Sam Hughes <sam@rethinkdb.com>
2013-09-25 09:53:57 -0700

    Make current_page_t and current_page_acq_t seem mostly complete in this WIP.

--- src/buffer_cache/alt/page.cc
+++ src/buffer_cache/alt/page.cc
@@ -1,6 +1,172 @@
 #include "buffer_cache/alt/page.hpp"
 
+#include "concurrency/auto_drainer.hpp"
+
 namespace alt {
+
+page_cache_t::page_cache_t(serializer_t *serializer)
+    : serializer_(serializer),
+      free_list_(serializer),
+      drainer_(new auto_drainer_t) {
+    // RSI: We'll use the serializer soon to load pages with.
+    (void)serializer_;
+}
+
+page_cache_t::~page_cache_t() {
+    drainer_.reset();
+    for (auto it = current_pages_.begin(); it != current_pages_.end(); ++it) {
+        delete *it;
+    }
+}
+
+current_page_t *page_cache_t::page_for_block_id(block_id_t block_id) {
+    if (current_pages_.size() <= block_id) {
+        current_pages_.resize(block_id + 1, NULL);
+    }
+
+    if (current_pages_[block_id] == NULL) {
+        current_pages_[block_id] = new current_page_t(block_id, serializer_);
+    }
+
+    return current_pages_[block_id];
+}
+
+current_page_t *page_cache_t::page_for_new_block_id(block_id_t *block_id_out) {
+    block_id_t block_id = free_list_.acquire_block_id();
+    current_page_t *ret = page_for_block_id(block_id);
+    *block_id_out = block_id;
+    return ret;
+}
+
+current_page_acq_t::current_page_acq_t(current_page_t *current_page,
+                                       alt_access_t access)
+    : access_(access),
+      current_page_(current_page),
+      snapshotted_page_(NULL) {
+    current_page_->add_acquirer(this);
+}
+
+current_page_acq_t::~current_page_acq_t() {
+    if (current_page_ != NULL) {
+        current_page_->remove_acquirer(this);
+    }
+    if (snapshotted_page_ != NULL) {
+        snapshotted_page_->remove_snapshotter(this);
+    }
+}
+
+void current_page_acq_t::declare_snapshotted() {
+    rassert(access_ == alt_access_t::read);
+    // Allow redeclaration of snapshottedness.
+    if (!declared_snapshotted_) {
+        declared_snapshotted_ = true;
+        rassert(current_page_ != NULL);
+        current_page_->pulse_pulsables(this);
+    }
+}
+
+signal_t *current_page_acq_t::read_acq_signal() {
+    return &read_cond_;
+}
+
+signal_t *current_page_acq_t::write_acq_signal() {
+    rassert(access_ == alt_access_t::write);
+    return &write_cond_;
+}
+
+page_t *current_page_acq_t::page_for_read() {
+    rassert(snapshotted_page_ != NULL || current_page_ != NULL);
+    read_cond_.wait();
+    if (snapshotted_page_ != NULL) {
+        return snapshotted_page_;
+    }
+    rassert(current_page_ != NULL);
+    return current_page_->the_page_for_read();
+}
+
+page_t *current_page_acq_t::page_for_write() {
+    rassert(access_ == alt_access_t::write);
+    rassert(current_page_ != NULL);
+    write_cond_.wait();
+    rassert(current_page_ != NULL);
+    return current_page_->the_page_for_write();
+}
+
+current_page_t::current_page_t(block_id_t block_id, serializer_t *serializer)
+    : block_id_(block_id), serializer_(serializer), page_(NULL) {
+}
+
+current_page_t::~current_page_t() {
+    rassert(acquirers_.empty());
+}
+
+void current_page_t::add_acquirer(current_page_acq_t *acq) {
+    acquirers_.push_back(acq);
+    pulse_pulsables(acq);
+}
+
+void current_page_t::remove_acquirer(current_page_acq_t *acq) {
+    current_page_acq_t *next = acquirers_.next(acq);
+    acquirers_.remove(acq);
+    if (next != NULL) {
+        pulse_pulsables(next);
+    }
+}
+
+void current_page_t::pulse_pulsables(current_page_acq_t *const acq) {
+    // First, avoid pulsing when there's nothing to pulse.
+    {
+        current_page_acq_t *prev = acquirers_.prev(acq);
+        if (!(prev == NULL || (prev->access_ == alt_access_t::read
+                               && prev->read_cond_.is_pulsed()))) {
+            return;
+        }
+    }
+
+    // Second, avoid re-pulsing already-pulsed chains.
+    if (acq->access_ == alt_access_t::read && acq->read_cond_.is_pulsed()) {
+        return;
+    }
+
+    // It's time to pulse the pulsables.
+    current_page_acq_t *cur = acq;
+    while (cur != NULL) {
+        // We know that the previous node has read access and has been pulsed as
+        // readable, so we pulse the current node as readable.
+        cur->read_cond_.pulse_if_not_already_pulsed();
+
+        if (cur->access_ == alt_access_t::read) {
+            current_page_acq_t *next = acquirers_.next(cur);
+            if (cur->declared_snapshotted_) {
+                // Snapshotters get kicked out of the queue, to make way for
+                // write-acquirers.
+                cur->snapshotted_page_ = the_page_for_read();
+                cur->current_page_ = NULL;
+                acquirers_.remove(cur);
+            }
+            cur = next;
+        } else {
+            // Even the first write-acquirer gets read access (there's no need for an
+            // "intent" mode).  But subsequent acquirers need to wait, because the
+            // write-acquirer might modify the value.
+            if (acquirers_.prev(cur) == NULL) {
+                // (It gets exclusive write access if there's no preceding reader.)
+                cur->write_cond_.pulse_if_not_already_pulsed();
+            }
+            break;
+        }
+    }
+}
+
+page_t *current_page_t::the_page_for_read() {
+    if (page_ == NULL) {
+        page_ = new page_t(block_id_, serializer_);
+        serializer_ = NULL;
+    }
+
+    return page_;
+}
+
 
 
 
--- src/buffer_cache/alt/page.hpp
+++ src/buffer_cache/alt/page.hpp
@@ -5,6 +5,8 @@
 #include "containers/intrusive_list.hpp"
 #include "serializer/types.hpp"
 
+class auto_drainer_t;
+
 namespace alt {
 
 class current_page_acq_t;
@@ -15,11 +17,13 @@ enum class alt_access_t { read, write };
 
 class page_t {
 public:
-    page_t();
+    page_t(block_id_t block_id, serializer_t *serializer);
     ~page_t();
 
     void *get_buf();
     uint32_t get_buf_size();
+
+    void remove_snapshotter(current_page_acq_t *acq);
 
 private:
     bool *destroy_ptr_;
@@ -48,10 +52,29 @@ private:
 
 class current_page_t {
 public:
-    current_page_t();
+    current_page_t(block_id_t block_id, serializer_t *serializer);
     ~current_page_t();
 
+
 private:
+    // current_page_acq_t should not access our fields directly.
+    friend class current_page_acq_t;
+    void add_acquirer(current_page_acq_t *acq);
+    void remove_acquirer(current_page_acq_t *acq);
+    void pulse_pulsables(current_page_acq_t *acq);
+
+    page_t *the_page_for_write();
+    page_t *the_page_for_read();
+
+    // Our block id.
+    block_id_t block_id_;
+    // Either page_ is null or serializer_ is null.  block_id_ and serializer_ can be
+    // used to construct (and load) the page when it's null.
+    // RSP:  This is a waste of memory.
+    serializer_t *serializer_;
+    page_t *page_;
+
+    // All list elements have current_page_ != NULL, snapshotted_page_ == NULL.
     intrusive_list_t<current_page_acq_t> acquirers_;
     DISABLE_COPYING(current_page_t);
 };
@@ -61,6 +84,8 @@ public:
     current_page_acq_t(current_page_t *current_page, alt_access_t access);
     ~current_page_acq_t();
 
+    void declare_snapshotted();
+
     signal_t *read_acq_signal();
     signal_t *write_acq_signal();
 
@@ -68,7 +93,11 @@ public:
     page_t *page_for_write();
 
 private:
+    friend class current_page_t;
+
     alt_access_t access_;
+    bool declared_snapshotted_ = false;  // RSI this won't compile
+    // At most one of current_page_ is NULL or snapshotted_page_ is NULL.
     current_page_t *current_page_;
     page_t *snapshotted_page_;
     cond_t read_cond_;
@@ -77,15 +106,40 @@ private:
     DISABLE_COPYING(current_page_acq_t);
 };
 
+class free_list_t {
+public:
+    free_list_t(serializer_t *serializer);
+    ~free_list_t();
+
+    block_id_t acquire_block_id();
+    void release_block_id(block_id_t block_id);
+
+private:
+    block_id_t next_new_block_id_;
+    // RSP: std::vector performance.
+    std::vector<block_id_t> free_ids_;
+    DISABLE_COPYING(free_list_t);
+};
+
 class page_cache_t {
 public:
     page_cache_t(serializer_t *serializer);
+    ~page_cache_t();
     current_page_t *page_for_block_id(block_id_t block_id);
     current_page_t *page_for_new_block_id(block_id_t *block_id_out);
 
 
 
 private:
+    serializer_t *serializer_;
+
+    // RSP: std::vector bad growth performance
+    std::vector<current_page_t *> current_pages_;
+
+    free_list_t free_list_;
+
+    scoped_ptr_t<auto_drainer_t> drainer_;
+
     DISABLE_COPYING(page_cache_t);
 };
 
