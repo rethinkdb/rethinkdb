a8e77f8dadf23758d48ac931a8f74d15783f1964
Sam Hughes <sam@rethinkdb.com>
2013-09-26 10:01:52 -0700

    Seemingly implemented non-flushing aspects of page cache.

--- src/buffer_cache/alt/page.cc
+++ src/buffer_cache/alt/page.cc
@@ -41,7 +41,7 @@ current_page_t *page_cache_t::page_for_block_id(block_id_t block_id) {
 
     if (current_pages_[block_id] == NULL) {
         current_pages_[block_id] = new current_page_t(block_id, this);
-    }
+   }
 
     return current_pages_[block_id];
 }
@@ -50,7 +50,8 @@ current_page_t *page_cache_t::page_for_new_block_id(block_id_t *block_id_out) {
     block_id_t block_id = free_list_.acquire_block_id();
     // RSI: Have a user-specifiable block size.
     current_page_t *ret = new current_page_t(serializer_->get_block_size(),
-                                             serializer_->malloc());
+                                             serializer_->malloc(),
+                                             this);
     *block_id_out = block_id;
     return ret;
 }
@@ -68,7 +69,7 @@ current_page_acq_t::~current_page_acq_t() {
         current_page_->remove_acquirer(this);
     }
     if (snapshotted_page_ != NULL) {
-        snapshotted_page_->remove_snapshotter(this);
+        snapshotted_page_->remove_snapshotter();
     }
 }
 
@@ -116,9 +117,10 @@ current_page_t::current_page_t(block_id_t block_id, page_cache_t *page_cache)
 }
 
 current_page_t::current_page_t(block_size_t block_size,
-                               scoped_malloc_t<ser_buffer_t> buf)
+                               scoped_malloc_t<ser_buffer_t> buf,
+                               page_cache_t *page_cache)
     : block_id_(NULL_BLOCK_ID),
-      page_cache_(NULL),
+      page_cache_(page_cache),
       page_(new page_t(block_size, std::move(buf))) {
 }
 
@@ -168,7 +170,7 @@ void current_page_t::pulse_pulsables(current_page_acq_t *const acq) {
                 // write-acquirers.
                 cur->snapshotted_page_ = the_page_for_read();
                 cur->current_page_ = NULL;
-                cur->snapshotted_page_->add_snapshotter(cur);
+                cur->snapshotted_page_->add_snapshotter();
                 acquirers_.remove(cur);
             }
             cur = next;
@@ -188,7 +190,6 @@ void current_page_t::pulse_pulsables(current_page_acq_t *const acq) {
 void current_page_t::convert_from_serializer_if_necessary() {
     if (page_ == NULL) {
         page_ = new page_t(block_id_, page_cache_);
-        page_cache_ = NULL;
         block_id_ = NULL_BLOCK_ID;
     }
 }
@@ -201,7 +202,7 @@ page_t *current_page_t::the_page_for_read() {
 page_t *current_page_t::the_page_for_write() {
     convert_from_serializer_if_necessary();
     if (page_->has_snapshot_references()) {
-        page_ = page_->make_copy();
+        page_ = page_->make_copy(page_cache_);
     }
     return page_;
 }
@@ -222,6 +223,67 @@ page_t::page_t(block_size_t block_size, scoped_malloc_t<ser_buffer_t> buf)
       buf_(std::move(buf)),
       snapshot_refcount_(0) {
     rassert(buf_.has());
+}
+
+page_t::page_t(page_t *copyee, page_cache_t *page_cache)
+    : destroy_ptr_(NULL),
+      buf_size_(block_size_t::undefined()),
+      snapshot_refcount_(0) {
+    coro_t::spawn_now_dangerously(std::bind(&page_t::load_from_copyee,
+                                            this,
+                                            copyee,
+                                            page_cache));
+}
+
+page_t::~page_t() {
+    if (destroy_ptr_ != NULL) {
+        *destroy_ptr_ = true;
+    }
+}
+
+void page_t::load_from_copyee(page_t *page, page_t *copyee,
+                              page_cache_t *page_cache) {
+    // This is called using spawn_now_dangerously.  We need to atomically set
+    // destroy_ptr_ (and add a snapshotter, why not).
+    copyee->add_snapshotter();
+    bool page_destroyed = false;
+    rassert(page->destroy_ptr_ == NULL);
+    page->destroy_ptr_ = &page_destroyed;
+
+    // Okay, it's safe to block.
+    auto_drainer_t::lock_t lock(page_cache->drainer_.get());
+
+    {
+        page_acq_t acq;
+        acq.init(copyee);
+        acq.buf_ready_signal()->wait();
+
+        ASSERT_FINITE_CORO_WAITING;
+        if (!page_destroyed) {
+            // RSP: If somehow there are no snapshotters of copyee now (besides ourself),
+            // maybe we could avoid copying this memory.  We need to carefully track
+            // snapshotters anyway, once we're comfortable with that, we could do it.
+
+            block_size_t buf_size = copyee->buf_size_;
+            rassert(copyee->buf_.has());
+            // RSI: Support mallocking the specific buf size.
+            scoped_malloc_t<ser_buffer_t> buf = page_cache->serializer_->malloc();
+
+            // RSI: Are we sure we want to copy the whole ser buffer, and not the cache's
+            // part?  Is it _necessary_?  If unnecessary, let's not do it.
+            memcpy(buf.get(), copyee->buf_.get(), buf_size.ser_value());
+
+            page->buf_size_ = buf_size;
+            page->buf_ = std::move(buf);
+            page->destroy_ptr_ = NULL;
+
+            page->pulse_waiters();
+        }
+    }
+
+    // RSI: Shouldn't there be some better way to acquire and release a snapshot
+    // reference?  Maybe not.
+    copyee->remove_snapshotter();
 }
 
 
@@ -266,24 +328,118 @@ void page_t::load_with_block_id(page_t *page, block_id_t block_id,
     page->buf_size_ = block_token->block_size();
     page->buf_ = std::move(buf);
     page->block_token_ = std::move(block_token);
+
+    page->pulse_waiters();
 }
 
-void page_t::add_snapshotter(current_page_acq_t *acq) {
-    (void)acq;  // RSI: remove param.
+void page_t::add_snapshotter() {
+    // This may not block, because it's called at the beginning of
+    // page_t::load_from_copyee.
+    ASSERT_NO_CORO_WAITING;
     ++snapshot_refcount_;
 }
 
-void page_t::remove_snapshotter(current_page_acq_t *acq) {
-    (void)acq;  // RSI: remove param.
+void page_t::remove_snapshotter() {
     rassert(snapshot_refcount_ > 0);
     --snapshot_refcount_;
+    // RSI: We might need to delete the page here.
 }
 
 bool page_t::has_snapshot_references() {
     return snapshot_refcount_ > 0;
 }
 
+page_t *page_t::make_copy(page_cache_t *page_cache) {
+    return new page_t(this, page_cache);
+}
 
+void page_t::pulse_waiters() {
+    for (page_acq_t *p = waiters_.head(); p != NULL; p = waiters_.next(p)) {
+        // The waiter's not already going to have been pulsed.
+        p->buf_ready_signal_.pulse();
+    }
+}
+
+void page_t::add_waiter(page_acq_t *acq) {
+    waiters_.push_back(acq);
+    if (buf_.has()) {
+        acq->buf_ready_signal_.pulse();
+    }
+}
+
+void *page_t::get_buf() {
+    // RSI: definitely make private, used through page_acq_t.
+    rassert(buf_.has());
+    return buf_->cache_data;
+}
+
+uint32_t page_t::get_buf_size() {
+    // RSI: definitely make private, used through page_acq_t.
+    rassert(buf_.has());
+    return buf_size_.value();
+}
+
+void page_t::remove_waiter(page_acq_t *acq) {
+    waiters_.remove(acq);
+    if (waiters_.empty()) {
+        // RSI: We might need to delete the page here.
+    }
+}
+
+page_acq_t::page_acq_t() : page_(NULL) {
+}
+
+void page_acq_t::init(page_t *page) {
+    rassert(page_ == NULL);
+    rassert(!buf_ready_signal_.is_pulsed());
+    page_ = page;
+    page_->add_waiter(this);
+}
+
+bool page_acq_t::has() const {
+    return page_ != NULL;
+}
+
+signal_t *page_acq_t::buf_ready_signal() {
+    return &buf_ready_signal_;
+}
+
+page_acq_t::~page_acq_t() {
+    if (page_ != NULL) {
+        page_->remove_waiter(this);
+    }
+}
+
+free_list_t::free_list_t(serializer_t *serializer) {
+    // RSI: Maybe this should be specifically constructed when we visit the
+    // serializer's home thread (and setting the file accounts).
+    on_thread_t th(serializer->home_thread());
+
+    next_new_block_id_ = serializer->max_block_id();
+    for (block_id_t i = 0; i < next_new_block_id_; ++i) {
+        if (serializer->get_delete_bit(i)) {
+            free_ids_.push_back(i);
+        }
+    }
+}
+
+free_list_t::~free_list_t() { }
+
+block_id_t free_list_t::acquire_block_id() {
+    if (free_ids_.empty()) {
+        block_id_t ret = next_new_block_id_;
+        ++next_new_block_id_;
+        return ret;
+    } else {
+        block_id_t ret = free_ids_.back();
+        free_ids_.pop_back();
+        return ret;
+    }
+}
+
+void free_list_t::release_block_id(block_id_t block_id) {
+    free_ids_.push_back(block_id);
+}
 
 
 }  // namespace alt
--- src/buffer_cache/alt/page.hpp
+++ src/buffer_cache/alt/page.hpp
@@ -21,20 +21,29 @@ class page_t {
 public:
     page_t(block_size_t block_size, scoped_malloc_t<ser_buffer_t> buf);
     page_t(block_id_t block_id, page_cache_t *page_cache);
+    page_t(page_t *copyee, page_cache_t *page_cache);
     ~page_t();
 
     void *get_buf();
     uint32_t get_buf_size();
 
-    void add_snapshotter(current_page_acq_t *acq);
-    void remove_snapshotter(current_page_acq_t *acq);
+    void add_snapshotter();
+    void remove_snapshotter();
     bool has_snapshot_references();
-    page_t *make_copy();
+    page_t *make_copy(page_cache_t *page_cache);
+
+    void add_waiter(page_acq_t *acq);
+    void remove_waiter(page_acq_t *acq);
 
 private:
+    void pulse_waiters();
+
     static void load_with_block_id(page_t *page,
                                    block_id_t block_id,
                                    page_cache_t *page_cache);
+
+    static void load_from_copyee(page_t *page, page_t *copyee,
+                                 page_cache_t *page_cache);
 
     // One of destroy_ptr_, buf_, or block_token_ is non-null.
     bool *destroy_ptr_;
@@ -45,6 +54,7 @@ private:
     // How many pointers to this value are expecting it to be snapshotted?
     size_t snapshot_refcount_;
 
+    // RSP: This could be a single pointer instead of two.
     intrusive_list_t<page_acq_t> waiters_;
     DISABLE_COPYING(page_t);
 };
@@ -60,6 +70,9 @@ public:
     bool has() const;
 
 private:
+    friend class page_t;
+
+    page_t *page_;
     cond_t buf_ready_signal_;
     DISABLE_COPYING(page_acq_t);
 };
@@ -67,7 +80,8 @@ private:
 class current_page_t {
 public:
     // Constructs a fresh, empty page.
-    current_page_t(block_size_t block_size, scoped_malloc_t<ser_buffer_t> buf);
+    current_page_t(block_size_t block_size, scoped_malloc_t<ser_buffer_t> buf,
+                   page_cache_t *page_cache);
     // Constructs a page to be loaded from the serializer.
     current_page_t(block_id_t block_id, page_cache_t *page_cache);
     ~current_page_t();
@@ -87,9 +101,9 @@ private:
 
     // Our block id.
     block_id_t block_id_;
-    // Either page_ is null or page_cache_ is null.  block_id_ and page_cache_ can be
-    // used to construct (and load) the page when it's null.
-    // RSP:  This is a waste of memory.
+    // Either page_ is null or page_cache_ is null (except that page_cache_ is never
+    // null).  block_id_ and page_cache_ can be used to construct (and load) the page
+    // when it's null.  RSP: This is a waste of memory.
     page_cache_t *page_cache_;
     page_t *page_;
 
