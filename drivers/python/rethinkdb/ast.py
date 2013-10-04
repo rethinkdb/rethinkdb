3e38d69a725e3729f912926d5abaa6aa25711277
Xiuming Chen <cc@cxm.cc>
2013-10-04 01:28:57 -0700

    fix undefined variable error for tzname()

--- drivers/python/rethinkdb/ast.py
+++ drivers/python/rethinkdb/ast.py
@@ -496,7 +496,7 @@ class RqlTzinfo(datetime.tzinfo):
         return self.delta
 
     def tzname(self, dt):
-        return offsetstr
+        return self.offsetstr
 
     def dst(self, dt):
         return datetime.timedelta(0)
