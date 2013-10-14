5a5e3b3afed52f784a83be6d88139a9f035d3eed
Sam Hughes <sam@rethinkdb.com>
2013-10-13 19:11:41 -0700

    Added basic CreateDestroy page_cache_t test.

---
+++ src/unittest/alt_test.cc
@@ -0,0 +1,47 @@
+#include "buffer_cache/alt/page.hpp"
+#include "serializer/config.hpp"
+#include "unittest/gtest.hpp"
+#include "unittest/mock_file.hpp"
+#include "unittest/unittest_utils.hpp"
+
+namespace unittest {
+
+using alt::page_cache_t;
+
+struct mock_ser_t {
+    mock_file_opener_t opener;
+    scoped_ptr_t<standard_serializer_t> ser;
+
+    mock_ser_t()
+        : opener() {
+        standard_serializer_t::create(&opener,
+                                      standard_serializer_t::static_config_t());
+        ser = make_scoped<standard_serializer_t>(log_serializer_t::dynamic_config_t(),
+                                                 &opener,
+                                                 &get_global_perfmon_collection());
+
+    }
+};
+
+void run_Control() {
+    mock_ser_t ser;
+}
+
+TEST(PageTest, Control) {
+    run_in_thread_pool(run_Control, 4);
+}
+
+void run_CreateDestroy() {
+    mock_ser_t mock;
+    page_cache_t page_cache(mock.ser.get());
+}
+
+TEST(PageTest, CreateDestroy) {
+    run_in_thread_pool(run_CreateDestroy, 4);
+}
+
+
+
+
+
+}  // namespace unittest
