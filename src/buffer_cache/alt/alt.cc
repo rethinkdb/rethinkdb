56ce7ce7f5054475c6a0f914e83185f2c31d973e
Sam Hughes <sam@rethinkdb.com>
2013-11-13 10:14:19 -0800

    Made alt_txn_t constructor handle preceding_txn being NULL.

--- src/buffer_cache/alt/alt.cc
+++ src/buffer_cache/alt/alt.cc
@@ -18,7 +18,8 @@ block_size_t alt_cache_t::max_block_size() const {
 
 alt_txn_t::alt_txn_t(alt_cache_t *cache, alt_txn_t *preceding_txn)
     : cache_(cache),
-      page_txn_(&cache->page_cache_, &preceding_txn->page_txn_),
+      page_txn_(&cache->page_cache_,
+                preceding_txn == NULL ? NULL : &preceding_txn->page_txn_),
       this_txn_timestamp_(repli_timestamp_t::invalid) {
     // RSI: Use this_txn_timestamp_ for something.
     (void)this_txn_timestamp_;
