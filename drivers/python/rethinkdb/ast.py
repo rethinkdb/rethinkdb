663ca955fa985e24a8389a3c15b0887cb45ad6aa
Etienne Laurin <atnnn@rethinkdb.com>
2013-11-15 01:11:28 +0000

    Python - better error messages when using dates
    Review 1033 by @tryneus
    Closes #1509

--- drivers/python/rethinkdb/ast.py
+++ drivers/python/rethinkdb/ast.py
@@ -22,14 +22,14 @@ def expr(val, nesting_depth=20):
 
     if isinstance(val, RqlQuery):
         return val
-    elif isinstance(val, datetime.datetime):
-        if not val.tzinfo:
-            raise RqlDriverError("""Cannot convert datetime to ReQL time object
+    elif isinstance(val, datetime.datetime) or isinstance(val, datetime.date):
+        if not hasattr(val, 'tzinfo') or not val.tzinfo:
+            raise RqlDriverError("""Cannot convert %s to ReQL time object
             without timezone information. You can add timezone information with
             the third party module \"pytz\" or by constructing ReQL compatible
             timezone values with r.make_timezone(\"[+-]HH:MM\"). Alternatively,
             use one of ReQL's bultin time constructors, r.now, r.time, or r.iso8601.
-            """)
+            """ % (type(val).__name__))
         return ISO8601(val.isoformat())
     elif isinstance(val, list):
         val = [expr(v, nesting_depth - 1) for v in val]
