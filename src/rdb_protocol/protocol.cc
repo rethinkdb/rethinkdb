2ff52fb8de539abdd2db9d9a18d4c771fb4c999f
jdoliner <jdoliner@gmail.com>
2013-11-22 17:47:35 -0800

    blocks_remaining -> blocks_processed.

--- src/rdb_protocol/protocol.cc
+++ src/rdb_protocol/protocol.cc
@@ -392,7 +392,7 @@ typedef boost::variant<rdb_modification_report_t,
 
 void add_status(const single_sindex_status_t &new_status,
     single_sindex_status_t *status_out) {
-    status_out->blocks_remaining += new_status.blocks_remaining;
+    status_out->blocks_processed += new_status.blocks_processed;
     status_out->blocks_total += new_status.blocks_total;
     status_out->ready &= new_status.ready;
 }
@@ -1398,10 +1398,10 @@ struct rdb_read_visitor_t : public boost::static_visitor<void> {
                 s->ready = it->second.post_construction_complete;
                 if (!s->ready) {
                     if (frac.estimate_of_total_nodes == -1) {
-                        s->blocks_remaining = 0;
+                        s->blocks_processed = 0;
                         s->blocks_total = 0;
                     } else {
-                        s->blocks_remaining = frac.estimate_of_released_nodes;
+                        s->blocks_processed = frac.estimate_of_released_nodes;
                         s->blocks_total = frac.estimate_of_total_nodes;
                     }
                 }
@@ -1968,7 +1968,7 @@ region_t rdb_protocol_t::cpu_sharding_subspace(int subregion_number,
 
 RDB_IMPL_ME_SERIALIZABLE_3(rdb_protocol_details::rget_item_t, key, sindex_key, data);
 RDB_IMPL_ME_SERIALIZABLE_3(rdb_protocol_details::single_sindex_status_t,
-                           blocks_total, blocks_remaining, ready);
+                           blocks_total, blocks_processed, ready);
 
 RDB_IMPL_ME_SERIALIZABLE_1(rdb_protocol_t::point_read_response_t, data);
 RDB_IMPL_ME_SERIALIZABLE_4(rdb_protocol_t::rget_read_response_t,
