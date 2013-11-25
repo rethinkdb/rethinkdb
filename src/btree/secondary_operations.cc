dda9a8235ebb685e932cbb016938865781f5d248
Sam Hughes <sam@rethinkdb.com>
2013-11-25 14:43:05 -0800

    Fixed alt cache linking problems related to secondary_operations.cc (integrated alt cache code into the whole file).

--- src/btree/secondary_operations.cc
+++ src/btree/secondary_operations.cc
@@ -1,25 +1,65 @@
 // Copyright 2010-2013 RethinkDB, all rights reserved.
 #include "btree/secondary_operations.hpp"
 
+#include "btree/slice.hpp"  // RSI: for SLICE_ALT
 #include "btree/operations.hpp"
+#if SLICE_ALT
+#include "buffer_cache/alt/alt.hpp"
+#include "buffer_cache/alt/alt_blob.hpp"
+#include "buffer_cache/alt/alt_serialize_onto_blob.hpp"
+#endif
 #include "buffer_cache/blob.hpp"
 #include "buffer_cache/serialize_onto_blob.hpp"
 #include "containers/archive/vector_stream.hpp"
 #include "protocol_api.hpp"
 
+using namespace alt;  // RSI
+
 RDB_IMPL_ME_SERIALIZABLE_4(secondary_index_t, superblock, opaque_definition,
                            post_construction_complete, id);
 
+#if SLICE_ALT
+void get_secondary_indexes_internal(alt_buf_lock_t *sindex_block,
+                                    std::map<std::string, secondary_index_t> *sindexes_out) {
+#else
 void get_secondary_indexes_internal(transaction_t *txn, buf_lock_t *sindex_block, std::map<std::string, secondary_index_t> *sindexes_out) {
+#endif
+#if SLICE_ALT
+    alt_buf_read_t read(sindex_block);
+    const btree_sindex_block_t *data
+        = static_cast<const btree_sindex_block_t *>(read.get_data_read());
+#else
     const btree_sindex_block_t *data = static_cast<const btree_sindex_block_t *>(sindex_block->get_data_read());
+#endif
 
+#if SLICE_ALT
+    alt::blob_t sindex_blob(sindex_block->cache()->get_block_size(),
+                            const_cast<char *>(data->sindex_blob),
+                            btree_sindex_block_t::SINDEX_BLOB_MAXREFLEN);
+
+    deserialize_from_blob(alt_buf_parent_t(sindex_block), &sindex_blob, sindexes_out);
+#else
     blob_t sindex_blob(txn->get_cache()->get_block_size(),
                        const_cast<char *>(data->sindex_blob),
                        btree_sindex_block_t::SINDEX_BLOB_MAXREFLEN);
 
     deserialize_from_blob(txn, &sindex_blob, sindexes_out);
+#endif
 }
 
+#if SLICE_ALT
+void set_secondary_indexes_internal(alt_buf_lock_t *sindex_block,
+                                    const std::map<std::string, secondary_index_t> &sindexes) {
+    alt_buf_write_t write(sindex_block);
+    btree_sindex_block_t *data
+        = static_cast<btree_sindex_block_t *>(write.get_data_write());
+
+    alt::blob_t sindex_blob(sindex_block->cache()->get_block_size(),
+                            data->sindex_blob,
+                            btree_sindex_block_t::SINDEX_BLOB_MAXREFLEN);
+    serialize_onto_blob(alt_buf_parent_t(sindex_block), &sindex_blob, sindexes);
+}
+#else
 void set_secondary_indexes_internal(transaction_t *txn, buf_lock_t *sindex_block, const std::map<std::string, secondary_index_t> &sindexes) {
     btree_sindex_block_t *data = static_cast<btree_sindex_block_t *>(sindex_block->get_data_write());
 
@@ -28,7 +68,20 @@ void set_secondary_indexes_internal(transaction_t *txn, buf_lock_t *sindex_block
                        btree_sindex_block_t::SINDEX_BLOB_MAXREFLEN);
     serialize_onto_blob(txn, &sindex_blob, sindexes);
 }
+#endif
 
+#if SLICE_ALT
+void initialize_secondary_indexes(alt_buf_lock_t *sindex_block) {
+    alt_buf_write_t write(sindex_block);
+    btree_sindex_block_t *data
+        = static_cast<btree_sindex_block_t *>(write.get_data_write());
+    data->magic = btree_sindex_block_t::expected_magic;
+    memset(data->sindex_blob, 0, btree_sindex_block_t::SINDEX_BLOB_MAXREFLEN);
+
+    set_secondary_indexes_internal(sindex_block,
+                                   std::map<std::string, secondary_index_t>());
+}
+#else
 void initialize_secondary_indexes(transaction_t *txn, buf_lock_t *sindex_block) {
     btree_sindex_block_t *data = static_cast<btree_sindex_block_t *>(sindex_block->get_data_write());
     data->magic = btree_sindex_block_t::expected_magic;
@@ -36,7 +89,24 @@ void initialize_secondary_indexes(transaction_t *txn, buf_lock_t *sindex_block) 
 
     set_secondary_indexes_internal(txn, sindex_block, std::map<std::string, secondary_index_t>());
 }
+#endif
 
+#if SLICE_ALT
+bool get_secondary_index(alt_buf_lock_t *sindex_block, const std::string &id,
+                         secondary_index_t *sindex_out) {
+    std::map<std::string, secondary_index_t> sindex_map;
+
+    get_secondary_indexes_internal(sindex_block, &sindex_map);
+
+    auto it = sindex_map.find(id);
+    if (it != sindex_map.end()) {
+        *sindex_out = it->second;
+        return true;
+    } else {
+        return false;
+    }
+}
+#else
 bool get_secondary_index(transaction_t *txn, buf_lock_t *sindex_block, const std::string &id, secondary_index_t *sindex_out) {
     std::map<std::string, secondary_index_t> sindex_map;
 
@@ -50,7 +120,23 @@ bool get_secondary_index(transaction_t *txn, buf_lock_t *sindex_block, const std
         return false;
     }
 }
+#endif
 
+#if SLICE_ALT
+bool get_secondary_index(alt_buf_lock_t *sindex_block, uuid_u id,
+                         secondary_index_t *sindex_out) {
+    std::map<std::string, secondary_index_t> sindex_map;
+
+    get_secondary_indexes_internal(sindex_block, &sindex_map);
+    for (auto it = sindex_map.begin(); it != sindex_map.end(); ++it) {
+        if (it->second.id == id) {
+            *sindex_out = it->second;
+            return true;
+        }
+    }
+    return false;
+}
+#else
 bool get_secondary_index(transaction_t *txn, buf_lock_t *sindex_block, uuid_u id, secondary_index_t *sindex_out) {
     std::map<std::string, secondary_index_t> sindex_map;
 
@@ -63,11 +149,30 @@ bool get_secondary_index(transaction_t *txn, buf_lock_t *sindex_block, uuid_u id
     }
     return false;
 }
+#endif
 
+#if SLICE_ALT
+void get_secondary_indexes(alt_buf_lock_t *sindex_block,
+                           std::map<std::string, secondary_index_t> *sindexes_out) {
+    get_secondary_indexes_internal(sindex_block, sindexes_out);
+}
+#else
 void get_secondary_indexes(transaction_t *txn, buf_lock_t *sindex_block, std::map<std::string, secondary_index_t> *sindexes_out) {
     get_secondary_indexes_internal(txn, sindex_block, sindexes_out);
 }
+#endif
 
+#if SLICE_ALT
+void set_secondary_index(alt_buf_lock_t *sindex_block, const std::string &id,
+                         const secondary_index_t &sindex) {
+    std::map<std::string, secondary_index_t> sindex_map;
+    get_secondary_indexes_internal(sindex_block, &sindex_map);
+
+    /* We insert even if it already exists overwriting the old value. */
+    sindex_map[id] = sindex;
+    set_secondary_indexes_internal(sindex_block, sindex_map);
+}
+#else
 void set_secondary_index(transaction_t *txn, buf_lock_t *sindex_block, const std::string &id, const secondary_index_t &sindex) {
     std::map<std::string, secondary_index_t> sindex_map;
     get_secondary_indexes_internal(txn, sindex_block, &sindex_map);
@@ -76,7 +181,23 @@ void set_secondary_index(transaction_t *txn, buf_lock_t *sindex_block, const std
     sindex_map[id] = sindex;
     set_secondary_indexes_internal(txn, sindex_block, sindex_map);
 }
+#endif
 
+#if SLICE_ALT
+void set_secondary_index(alt_buf_lock_t *sindex_block, uuid_u id,
+                         const secondary_index_t &sindex) {
+    std::map<std::string, secondary_index_t> sindex_map;
+    get_secondary_indexes_internal(sindex_block, &sindex_map);
+
+    for (auto it = sindex_map.begin(); it != sindex_map.end(); ++it) {
+        if (it->second.id == id) {
+            guarantee(sindex.id == id, "This shouldn't change the id.");
+            it->second = sindex;
+        }
+    }
+    set_secondary_indexes_internal(sindex_block, sindex_map);
+}
+#else
 void set_secondary_index(transaction_t *txn, buf_lock_t *sindex_block, uuid_u id, const secondary_index_t &sindex) {
     std::map<std::string, secondary_index_t> sindex_map;
     get_secondary_indexes_internal(txn, sindex_block, &sindex_map);
@@ -89,7 +210,21 @@ void set_secondary_index(transaction_t *txn, buf_lock_t *sindex_block, uuid_u id
     }
     set_secondary_indexes_internal(txn, sindex_block, sindex_map);
 }
+#endif
 
+#if SLICE_ALT
+bool delete_secondary_index(alt_buf_lock_t *sindex_block, const std::string &id) {
+    std::map<std::string, secondary_index_t> sindex_map;
+    get_secondary_indexes_internal(sindex_block, &sindex_map);
+
+    if (sindex_map.erase(id) == 1) {
+        set_secondary_indexes_internal(sindex_block, sindex_map);
+        return true;
+    } else {
+        return false;
+    }
+}
+#else
 bool delete_secondary_index(transaction_t *txn, buf_lock_t *sindex_block, const std::string &id) {
     std::map<std::string, secondary_index_t> sindex_map;
     get_secondary_indexes_internal(txn, sindex_block, &sindex_map);
@@ -101,8 +236,16 @@ bool delete_secondary_index(transaction_t *txn, buf_lock_t *sindex_block, const 
         return false;
     }
 }
+#endif
 
+#if SLICE_ALT
+void delete_all_secondary_indexes(alt_buf_lock_t *sindex_block) {
+    set_secondary_indexes_internal(sindex_block,
+                                   std::map<std::string, secondary_index_t>());
+}
+#else
 void delete_all_secondary_indexes(transaction_t *txn, buf_lock_t *sindex_block) {
     set_secondary_indexes_internal(txn, sindex_block, std::map<std::string, secondary_index_t>());
 }
+#endif
 
