29447b474f5491b46a4413c51df8e8be17c12974
Daniel Mewes <daniel@rethinkdb.com>
2013-12-05 16:07:45 -0800

    Simplified custom TLS. Might not work on ICC anymore, I'm not sure (but guess it should).

--- src/thread_local.hpp
+++ src/thread_local.hpp
@@ -61,41 +61,14 @@
 
 #ifndef THREADED_COROUTINES
 #define TLS_with_init(type, name, initial)                              \
-    static pthread_key_t TLS_ ## name ## _key;                          \
-    static pthread_once_t TLS_ ## name ## _key_once;                    \
-                                                                        \
-    static void TLS_ ## name ## _destructor(void *ptr) {                \
-        delete static_cast<type *>(ptr);                                \
-    }                                                                   \
-                                                                        \
-    NOINLINE static void TLS_make_ ## name ## _key() {                  \
-        int res = pthread_key_create(& TLS_ ## name ## _key,            \
-                                     TLS_ ## name ## _destructor);      \
-        guarantee_xerr(res == 0, res, "could not pthread_key_create");  \
-    }                                                                   \
-                                                                        \
-    NOINLINE static void TLS_intialize_ ## name () {                    \
-        int res = pthread_once(& TLS_ ## name ## _key_once,             \
-                               TLS_make_ ## name ## _key);              \
-        guarantee_xerr(res == 0, res, "could not pthread_once");        \
-                                                                        \
-        if (pthread_getspecific(TLS_ ## name ## _key) == NULL) {        \
-            type *ptr = new type;                                       \
-            *ptr = initial;                                             \
-            res = pthread_setspecific(TLS_ ## name ## _key, ptr);       \
-            guarantee_xerr(res == 0, res,                               \
-                           "pthread_setspecific failed");               \
-        }                                                               \
-    }                                                                   \
+    static __thread type TLS_ ## name = initial;                        \
                                                                         \
     NOINLINE type TLS_get_ ## name () {                                 \
-        TLS_intialize_ ## name();                                       \
-            return *static_cast<type *>(pthread_getspecific(TLS_ ## name ## _key)); \
+        return TLS_ ## name;                                            \
     }                                                                   \
                                                                         \
     NOINLINE void TLS_set_ ## name (type const &val) {                  \
-        TLS_intialize_ ## name();                                       \
-            *static_cast<type *>(pthread_getspecific(TLS_ ## name ## _key)) = val; \
+        TLS_ ## name = val;                                             \
     }
 
 #else  // THREADED_COROUTINES
@@ -114,38 +87,14 @@
 
 #ifndef THREADED_COROUTINES
 #define TLS(type, name)                                                 \
-    static pthread_key_t TLS_ ## name ## _key;                          \
-    static pthread_once_t TLS_ ## name ## _key_once;                    \
-                                                                        \
-    static void TLS_ ## name ## _destructor(void *ptr) {                \
-        delete static_cast<type *>(ptr);                                \
-    }                                                                   \
-                                                                        \
-    NOINLINE static void TLS_make_ ## name ## _key() {                  \
-        int res = pthread_key_create(& TLS_ ## name ## _key,            \
-                                     TLS_ ## name ## _destructor);      \
-        guarantee_xerr(res == 0, res, "pthread_key_create failed");     \
-    }                                                                   \
-                                                                        \
-    NOINLINE static void TLS_intialize_ ## name () {                    \
-        int res = pthread_once(& TLS_ ## name ## _key_once,             \
-                               TLS_make_ ## name ## _key);              \
-        guarantee_xerr(res == 0, res, "pthread_once failed");           \
-                                                                        \
-        if (pthread_getspecific(TLS_ ## name ## _key) == NULL) {        \
-            type *ptr = new type;                                       \
-            pthread_setspecific(TLS_ ## name ## _key, ptr);             \
-        }                                                               \
-    }                                                                   \
+    static __thread type TLS_ ## name;                                  \
                                                                         \
     NOINLINE type TLS_get_ ## name () {                                 \
-        TLS_intialize_ ## name();                                       \
-            return *static_cast<type *>(pthread_getspecific(TLS_ ## name ## _key)); \
+        return TLS_ ## name;                                            \
     }                                                                   \
                                                                         \
     NOINLINE void TLS_set_ ## name (type const &val) {                  \
-        TLS_intialize_ ## name();                                       \
-            *static_cast<type *>(pthread_getspecific(TLS_ ## name ## _key)) = val; \
+        TLS_ ## name = val;                                             \
     }
 
 #else // THREADED_COROUTINES
