cbe70c3ce278dedf69208286b00784d2cf32541f
Michael Lucy <mlucy@rethinkdb.com>
2013-12-05 16:27:30 -0800

    switched to using templates

--- src/rdb_protocol/terms/case.cc
+++ src/rdb_protocol/terms/case.cc
@@ -6,30 +6,35 @@
 
 namespace ql {
 
+template<int (*const F)(int)>
+const char *case_name();
+template<>
+const char *case_name<::toupper>() { return "upcase"; }
+template<>
+const char *case_name<::tolower>() { return "downcase"; }
+
+template<int (*const F)(int)>
 class case_term_t : public op_term_t {
 public:
-    case_term_t(compile_env_t *env, const protob_t<const Term> &term,
-                const char *name, int (*_f)(int))
-        : op_term_t(env, term, argspec_t(1)), name_(name), f(_f) { }
+    case_term_t(compile_env_t *env, const protob_t<const Term> &term)
+        : op_term_t(env, term, argspec_t(1)) { }
 private:
     virtual counted_t<val_t> eval_impl(scope_env_t *env, UNUSED eval_flags_t flags) {
+        debugf("%s\n", name());
         std::string s = arg(env, 0)->as_str();
-        std::transform(s.begin(), s.end(), s.begin(), f);
+        std::transform(s.begin(), s.end(), s.begin(), F);
         return new_val(make_counted<const datum_t>(std::move(s)));
     }
-    virtual const char *name() const { return name_; }
-
-    const char *const name_;
-    int (*const f)(int);
+    virtual const char *name() const { return case_name<F>(); }
 };
 
 counted_t<term_t> make_upcase_term(compile_env_t *env,
                                    const protob_t<const Term> &term) {
-    return make_counted<case_term_t>(env, term, "upcase", ::toupper);
+    return make_counted<case_term_t<::toupper> >(env, term);
 }
 counted_t<term_t> make_downcase_term(compile_env_t *env,
                                      const protob_t<const Term> &term) {
-    return make_counted<case_term_t>(env, term, "downcase", ::tolower);
+    return make_counted<case_term_t<::tolower> >(env, term);
 }
 
 }
