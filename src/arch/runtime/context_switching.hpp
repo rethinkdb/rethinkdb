6d04d6cf16b5c555222e5f9c632dd9a90d4c0c5e
Daniel Mewes <daniel@rethinkdb.com>
2013-12-13 15:46:30 -0800

    Removed a redundant destructor

--- src/arch/runtime/context_switching.hpp
+++ src/arch/runtime/context_switching.hpp
@@ -111,7 +111,6 @@ class linux_thread_t;
 class threaded_context_ref_t {
 public:
     threaded_context_ref_t() : do_rethread(false) { store_virtual_thread(); }
-    ~threaded_context_ref_t() { lock.reset(); }
 
     /* Every context reference has a condition that can be used to wake it up.
     While it is active, it also holds a lock on the virtual_thread_mutexes of
