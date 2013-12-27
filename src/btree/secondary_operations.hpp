6c989c8cc11400555b6caa62dfe362f6eb451c98
Daniel Mewes <daniel@rethinkdb.com>
2013-12-27 14:36:15 -0800

    Regression test for issue 1789

--- src/btree/secondary_operations.hpp
+++ src/btree/secondary_operations.hpp
@@ -71,11 +71,11 @@ void set_secondary_index(transaction_t *txn, buf_lock_t *sindex_block, const std
 void set_secondary_index(transaction_t *txn, buf_lock_t *sindex_block, uuid_u id, const secondary_index_t &sindex);
 
 //XXX note this just drops the entry. It doesn't cleanup the btree that it
-//points to. drop_secondary_index. Does both and should be used publicly.
+//points to. `drop_sindex` does both and should be used publicly.
 bool delete_secondary_index(transaction_t *txn, buf_lock_t *sindex_block, const std::string &id);
 
 //XXX note this just drops the enties. It doesn't cleanup the btree that it
-//points to. drop_secondary_indexes. Does both and should be used publicly.
+//points to. `drop_all_sindexes` does both and should be used publicly.
 void delete_all_secondary_indexes(transaction_t *txn, buf_lock_t *sindex_block);
 
 #endif /* BTREE_SECONDARY_OPERATIONS_HPP_ */
