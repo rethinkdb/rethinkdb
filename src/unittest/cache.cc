4e39a6183f98ca3ce920718ddea4128f2e018376
jdoliner <jdoliner@gmail.com>
2014-01-03 10:43:49 -0800

    Adds a test that does a tree and block chains.

--- src/unittest/cache.cc
+++ src/unittest/cache.cc
@@ -82,30 +82,32 @@ TEST(Cache, DISABLED_TxnPredecessors) {
 class block_grinder_t {
 public:
     block_grinder_t() : cache(mock.ser.get()) { }
-    alt_buf_lock_t *read(block_id_t i, alt_txn_t *txn, alt_buf_lock_t *parent) {
+    alt_buf_lock_t read(block_id_t i, alt_buf_parent_t p) {
         guarantee(std_contains(in_memory_record, i));
-        alt_buf_parent_t p = parent ? alt_buf_parent_t(parent) : alt_buf_parent_t(txn);
-        alt_buf_lock_t *lock = new alt_buf_lock_t(p, i, alt_access_t::read);
-        alt_buf_read_t data(lock);
-        EXPECT_EQ(in_memory_record[i], *static_cast<const int *>(data.get_data_read()));
+        alt_buf_lock_t lock(p, i, alt_access_t::read);
+        {
+            alt_buf_read_t data(&lock);
+            EXPECT_EQ(in_memory_record[i], *static_cast<const int *>(data.get_data_read()));
+        }
         return lock;
     }
-    alt_buf_lock_t *write(block_id_t i, alt_txn_t *txn, alt_buf_lock_t *parent) {
-        alt_buf_parent_t p = parent ? alt_buf_parent_t(parent) : alt_buf_parent_t(txn);
+    alt_buf_lock_t write(block_id_t i,  alt_buf_parent_t p) {
         if (std_contains(in_memory_record, i)) {
-            alt_buf_lock_t *lock = new alt_buf_lock_t(p, i, alt_access_t::write);
-            alt_buf_write_t data(lock);
-            int *val = static_cast<int *>(data.get_data_write());
-            EXPECT_EQ(in_memory_record[i], *val);
-            debugf("%ld -> %d\n", i, *val);
-            *val = ++in_memory_record[i];
+            alt_buf_lock_t lock(p, i, alt_access_t::write);
+            {
+                alt_buf_write_t data(&lock);
+                int *val = static_cast<int *>(data.get_data_write());
+                EXPECT_EQ(in_memory_record[i], *val);
+                *val = ++in_memory_record[i];
+            }
             return lock;
         } else {
-            alt_buf_lock_t *lock = new alt_buf_lock_t(p, i, alt_create_t::create);
-            alt_buf_write_t data(lock);
-            int *val = static_cast<int *>(data.get_data_write());
-            *val = in_memory_record[i] = 0;
-            debugf("%ld <- %d\n", i, *val);
+            alt_buf_lock_t lock(p, i, alt_create_t::create);
+            {
+                alt_buf_write_t data(&lock);
+                int *val = static_cast<int *>(data.get_data_write());
+                *val = in_memory_record[i] = 0;
+            }
             return lock;
         }
     }
@@ -120,12 +122,14 @@ class block_thread_t {
 public:
     /* Creates a new read txn. */
     block_thread_t(block_grinder_t *_parent)
-        : parent(_parent), txn(new alt_txn_t(&parent->cache)) { }
+        : parent(_parent), txn(new alt_txn_t(&parent->cache)),
+          first_acq(true) { }
 
     /* Creates a new write txn. */
     block_thread_t(block_grinder_t *_parent, write_durability_t durability)
         : parent(_parent),
-          txn(new alt_txn_t(&parent->cache, durability)) { }
+          txn(new alt_txn_t(&parent->cache, durability)),
+          first_acq(true) { }
 
     /* Spins off a new thread of acquisition from an acquisition from an
      * existing one. */
@@ -133,19 +137,29 @@ public:
         : parent(sibling->parent), txn(sibling->txn) { }
 
     void read(block_id_t bid) {
-        buf_lock.reset(parent->read(bid, txn.get(), buf_lock.get()));
+        if (first_acq) {
+            first_acq = false;
+            buf_lock = parent->read(bid, alt_buf_parent_t(txn.get()));
+        } else {
+            buf_lock = parent->read(bid, alt_buf_parent_t(&buf_lock));
+        }
     }
     void write(block_id_t bid) {
-        buf_lock.reset(parent->write(bid, txn.get(), buf_lock.get()));
+        if (first_acq) {
+            first_acq = false;
+            buf_lock = parent->write(bid, alt_buf_parent_t(txn.get()));
+        } else {
+            buf_lock = parent->write(bid, alt_buf_parent_t(&buf_lock));
+        }
     }
     void snapshot() {
-        guarantee(buf_lock);
-        buf_lock->snapshot_subtree();
+        buf_lock.snapshot_subtree();
     }
 
     block_grinder_t *parent;
     boost::shared_ptr<alt_txn_t> txn;
-    boost::shared_ptr<alt_buf_lock_t> buf_lock;
+    alt_buf_lock_t buf_lock;
+    bool first_acq;
 };
 
 void runSingleBlock() {
@@ -164,19 +178,30 @@ TEST(Cache, SingleBlock) {
     run_in_thread_pool(runSingleBlock, 4);
 }
 
+void write_chain(block_grinder_t *g, block_id_t start,
+        block_id_t end, auto_drainer_t::lock_t) {
+    block_thread_t thread(g, write_durability_t::HARD);
+    for (block_id_t i = start; i < end; ++i) {
+        thread.write(i);
+        coro_t::yield();
+    }
+}
+
+void read_chain(block_grinder_t *g, block_id_t start,
+        block_id_t end, auto_drainer_t::lock_t) {
+    block_thread_t thread(g);
+    for (block_id_t i = start; i < end; ++i) {
+        thread.read(i);
+        coro_t::yield();
+    }
+}
+
 void runBlockChain() {
     block_grinder_t grinder;
-    {
-        block_thread_t thread(&grinder, write_durability_t::HARD);
-        for (block_id_t i = 0; i < 100; ++i) {
-            thread.write(i);
-        }
-    }
-    {
-        block_thread_t thread(&grinder);
-        for (block_id_t i = 0; i < 100; ++i) {
-            thread.read(i);
-        }
+    auto_drainer_t drainer;
+    for (int i = 0; i < 100; ++i) {
+        coro_t::spawn(std::bind(&write_chain, &grinder, 0, 100, drainer.lock()));
+        coro_t::spawn(std::bind(&read_chain, &grinder, 0, 100, drainer.lock()));
     }
 }
 
@@ -238,6 +263,76 @@ void runSnapshotTest() {
     block_id_t read_id = 1;
     read_thread.read(read_id);
     read_id = next_random_tree_node(read_id);
+
+    auto_drainer_t drainer;
+    for (int i = 0; i < 100; ++i) {
+        coro_t::spawn(std::bind(&write_tree, &grinder, drainer.lock()));
+    }
+}
+
+/* This test hangs right now, turn it back on when snapshotting works. */
+TEST(Cache, DISABLED_Snapshot) {
+    run_in_thread_pool(runSnapshotTest, 4);
+}
+
+void init_tree_and_chains(block_grinder_t *g) {
+    block_thread_t thread(g, write_durability_t::HARD);
+    for (block_id_t id = 1; id < 1 << tree_levels; ++id) {
+        thread.write(id);
+        auto_drainer_t dummy;
+        write_chain(g, (id + (1 << tree_levels)) * 100,
+                (id + (1 << tree_levels) + 1) * 100, dummy.lock());
+    }
+}
+
+void write_tree_maybe_chain(block_grinder_t *g, auto_drainer_t::lock_t) {
+    block_thread_t thread(g, write_durability_t::HARD);
+    for (block_id_t id = 1; id < (1 << tree_levels); id = next_random_tree_node(id)) {
+        thread.write(id);
+        if (randint(2) == 0) {
+            auto_drainer_t drainer;
+            for (int i = 0; i < 5; ++i) {
+                coro_t::spawn(std::bind(&write_chain, g,
+                            (id + (1 << tree_levels)) * 100,
+                            (id + (1 << tree_levels) + 1) * 100, drainer.lock()));
+            }
+        }
+        coro_t::yield();
+    }
+}
+
+void read_tree_maybe_chain(block_grinder_t *g, auto_drainer_t::lock_t) {
+    block_thread_t thread(g, write_durability_t::HARD);
+    for (block_id_t id = 1; id < (1 << tree_levels); id = next_random_tree_node(id)) {
+        thread.write(id);
+        if (randint(2) == 0) {
+            auto_drainer_t drainer;
+            for (int i = 0; i < 5; ++i) {
+                coro_t::spawn(std::bind(&read_chain, g,
+                            (id + (1 << tree_levels)) * 100,
+                            (id + (1 << tree_levels) + 1) * 100, drainer.lock()));
+            }
+        }
+        coro_t::yield();
+    }
+}
+
+void runTreeAndChains() {
+    block_grinder_t grinder;
+    init_tree_and_chains(&grinder);
+
+    auto_drainer_t drainer;
+
+    for (int i = 0; i < 40; ++i) {
+        coro_t::spawn(std::bind(&write_tree_maybe_chain, &grinder, drainer.lock()));
+        coro_t::spawn(std::bind(&read_tree_maybe_chain, &grinder, drainer.lock()));
+    }
+}
+
+/* This test hangs right now, not sure if it's a bug in the test or in the new
+ * cache. */
+TEST(Cache, DISABLED_TreeAndChains) {
+    run_in_thread_pool(runTreeAndChains, 4);
 }
 
 } //namespace unittest 
