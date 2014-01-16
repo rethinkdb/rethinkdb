5a2b7a76e91134078810fee73081c4f5f3c7a5a6
Sam Hughes <sam@rethinkdb.com>
2014-01-15 19:57:48 -0800

    Removed SLICE_ALT from unittest/btree_metainfo.cc.

--- src/unittest/btree_metainfo.cc
+++ src/unittest/btree_metainfo.cc
@@ -57,12 +57,7 @@ void run_metainfo_test() {
 
     cache_t::create(&serializer);
 
-#if SLICE_ALT
     alt_cache_t cache(&serializer);
-#else
-    const mirrored_cache_config_t cache_dynamic_config;
-    cache_t cache(&serializer, cache_dynamic_config, &get_global_perfmon_collection());
-#endif
 
     btree_slice_t::create(&cache, std::vector<char>(), std::vector<char>());
     std::map<std::string, std::string> mirror;
@@ -77,33 +72,17 @@ void run_metainfo_test() {
 
         order_token_t otok = order_source.check_in("metainfo unittest");
 
-#if SLICE_ALT
         scoped_ptr_t<alt_txn_t> txn;
-#else
-        scoped_ptr_t<transaction_t> txn;
-#endif
         scoped_ptr_t<real_superblock_t> superblock;
-#if SLICE_ALT
         get_btree_superblock_and_txn(&btree, alt_access_t::write, 1,
                                      repli_timestamp_t::invalid, otok,
                                      write_durability_t::SOFT,
                                      &superblock, &txn);
-#else
-        get_btree_superblock_and_txn(&btree, rwi_write, rwi_write, 1, repli_timestamp_t::invalid, otok, write_durability_t::SOFT, &superblock, &txn);
-#endif
-#if SLICE_ALT
         alt_buf_lock_t *sb_buf = superblock->get();
-#else
-        buf_lock_t *sb_buf = superblock->get();
-#endif
 
         int op = random() % 100;
         if (op == 0) {
-#if SLICE_ALT
             clear_superblock_metainfo(sb_buf);
-#else
-            clear_superblock_metainfo(txn.get(), sb_buf);
-#endif
             mirror.clear();
             if (print_log_messages) {
                 puts("clear");
@@ -114,12 +93,8 @@ void run_metainfo_test() {
             }
             std::string key = random_existing_key(mirror);
             std::vector<char> value_out;
-#if SLICE_ALT
             bool found = get_superblock_metainfo(sb_buf, string_to_vector(key),
                                                  &value_out);
-#else
-            bool found = get_superblock_metainfo(txn.get(), sb_buf, string_to_vector(key), &value_out);
-#endif
             EXPECT_TRUE(found);
             if (found) {
                 EXPECT_EQ(mirror[key], vector_to_string(value_out));
@@ -134,12 +109,8 @@ void run_metainfo_test() {
                 continue;
             }
             std::vector<char> value_out;
-#if SLICE_ALT
             const bool found = get_superblock_metainfo(sb_buf, string_to_vector(key),
                                                        &value_out);
-#else
-            const bool found = get_superblock_metainfo(txn.get(), sb_buf, string_to_vector(key), &value_out);
-#endif
             EXPECT_FALSE(found);
             if (found) {
                 EXPECT_EQ(mirror[key], vector_to_string(value_out));
@@ -154,12 +125,8 @@ void run_metainfo_test() {
             }
             std::string key = random_existing_key(mirror);
             std::string value = random_string();
-#if SLICE_ALT
             set_superblock_metainfo(sb_buf, string_to_vector(key),
                                     string_to_vector(value));
-#else
-            set_superblock_metainfo(txn.get(), sb_buf, string_to_vector(key), string_to_vector(value));
-#endif
             mirror[key] = value;
             if (print_log_messages) {
                 printf("update '%s' = '%s'\n", key.c_str(), value.c_str());
@@ -170,12 +137,8 @@ void run_metainfo_test() {
                 continue;
             }
             std::string value = random_string();
-#if SLICE_ALT
             set_superblock_metainfo(sb_buf, string_to_vector(key),
                                     string_to_vector(value));
-#else
-            set_superblock_metainfo(txn.get(), sb_buf, string_to_vector(key), string_to_vector(value));
-#endif
             mirror[key] = value;
             if (print_log_messages) {
                 printf("insert '%s' = '%s'\n", key.c_str(), value.c_str());
@@ -185,22 +148,14 @@ void run_metainfo_test() {
                 continue;
             }
             std::string key = random_existing_key(mirror);
-#if SLICE_ALT
             delete_superblock_metainfo(sb_buf, string_to_vector(key));
-#else
-            delete_superblock_metainfo(txn.get(), sb_buf, string_to_vector(key));
-#endif
             mirror.erase(key);
             if (print_log_messages) {
                 printf("delete '%s'\n", key.c_str());
             }
         } else {
             std::vector<std::pair<std::vector<char>, std::vector<char> > > pairs;
-#if SLICE_ALT
             get_superblock_metainfo(sb_buf, &pairs);
-#else
-            get_superblock_metainfo(txn.get(), sb_buf, &pairs);
-#endif
             std::map<std::string, std::string> mirror_copy = mirror;
             if (print_log_messages) {
                 puts("scan...");
