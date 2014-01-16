c09448101bbde7933277e3025292cd9b94d61ea0
Sam Hughes <sam@rethinkdb.com>
2014-01-15 19:43:54 -0800

    Removed SLICE_ALT from lazy_json.hpp and .cc.

--- src/rdb_protocol/lazy_json.cc
+++ src/rdb_protocol/lazy_json.cc
@@ -1,41 +1,22 @@
-// Copyright 2010-2013 RethinkDB, all rights reserved.
+// Copyright 2010-2014 RethinkDB, all rights reserved.
 #include "rdb_protocol/lazy_json.hpp"
 
 #include "containers/archive/buffer_group_stream.hpp"
 #include "rdb_protocol/blob_wrapper.hpp"
 
-#if SLICE_ALT
 using namespace alt;  // RSI
-#endif
 
-#if SLICE_ALT
 counted_t<const ql::datum_t> get_data(const rdb_value_t *value,
                                       alt_buf_parent_t parent) {
-#else
-counted_t<const ql::datum_t> get_data(const rdb_value_t *value,
-                                      transaction_t *txn) {
-#endif
-#if SLICE_ALT
     rdb_blob_wrapper_t blob(parent.cache()->get_block_size(),
                             const_cast<rdb_value_t *>(value)->value_ref(),
                             alt::blob::btree_maxreflen);
-#else
-    rdb_blob_wrapper_t blob(txn->get_cache()->get_block_size(),
-                            const_cast<rdb_value_t *>(value)->value_ref(),
-                            blob::btree_maxreflen);
-#endif
 
     counted_t<const ql::datum_t> data;
 
-#if SLICE_ALT
     alt::blob_acq_t acq_group;
     buffer_group_t buffer_group;
     blob.expose_all(parent, alt_access_t::read, &buffer_group, &acq_group);
-#else
-    blob_acq_t acq_group;
-    buffer_group_t buffer_group;
-    blob.expose_all(txn, rwi_read, &buffer_group, &acq_group);
-#endif
     buffer_group_read_stream_t read_stream(const_view(&buffer_group));
     archive_result_t res = deserialize(&read_stream, &data);
     guarantee_deserialization(res, "rdb value");
@@ -46,17 +27,9 @@ counted_t<const ql::datum_t> get_data(const rdb_value_t *value,
 const counted_t<const ql::datum_t> &lazy_json_t::get() const {
     guarantee(pointee.has());
     if (!pointee->ptr.has()) {
-#if SLICE_ALT
         pointee->ptr = get_data(pointee->rdb_value, pointee->parent);
-#else
-        pointee->ptr = get_data(pointee->rdb_value, pointee->txn);
-#endif
         pointee->rdb_value = NULL;
-#if SLICE_ALT
         pointee->parent = alt_buf_parent_t();
-#else
-        pointee->txn = NULL;
-#endif
     }
     return pointee->ptr;
 }
--- src/rdb_protocol/lazy_json.hpp
+++ src/rdb_protocol/lazy_json.hpp
@@ -1,10 +1,9 @@
-// Copyright 2010-2013 RethinkDB, all rights reserved.
+// Copyright 2010-2014 RethinkDB, all rights reserved.
 #ifndef RDB_PROTOCOL_LAZY_JSON_HPP_
 #define RDB_PROTOCOL_LAZY_JSON_HPP_
 
-#include "btree/slice.hpp"  // RSI: for SLICE_ALT
-#include "buffer_cache/blob.hpp"
-#include "buffer_cache/types.hpp"
+#include "buffer_cache/alt/alt.hpp"
+#include "buffer_cache/alt/alt_blob.hpp"
 #include "rdb_protocol/datum.hpp"
 
 struct rdb_value_t {
@@ -12,11 +11,11 @@ struct rdb_value_t {
 
 public:
     int inline_size(block_size_t bs) const {
-        return blob::ref_size(bs, contents, blob::btree_maxreflen);
+        return alt::blob::ref_size(bs, contents, alt::blob::btree_maxreflen);
     }
 
     int64_t value_size() const {
-        return blob::value_size(contents, blob::btree_maxreflen);
+        return alt::blob::value_size(contents, alt::blob::btree_maxreflen);
     }
 
     const char *value_ref() const {
@@ -28,57 +27,30 @@ public:
     }
 };
 
-#if SLICE_ALT
 counted_t<const ql::datum_t> get_data(const rdb_value_t *value,
                                       alt::alt_buf_parent_t parent);
-#else
-counted_t<const ql::datum_t> get_data(const rdb_value_t *value,
-                                      transaction_t *txn);
-#endif
 
 class lazy_json_pointee_t : public single_threaded_countable_t<lazy_json_pointee_t> {
-#if SLICE_ALT
     // RSI: Make sure callers/constructors get the lifetime of the buf parent right.
     lazy_json_pointee_t(const rdb_value_t *_rdb_value, alt::alt_buf_parent_t _parent)
         : rdb_value(_rdb_value), parent(_parent) {
-#else
-    lazy_json_pointee_t(const rdb_value_t *_rdb_value, transaction_t *_txn)
-        : rdb_value(_rdb_value), txn(_txn) {
-#endif
         guarantee(rdb_value != NULL);
-#if !SLICE_ALT
-        guarantee(txn != NULL);
-#endif
     }
 
-#if SLICE_ALT
     explicit lazy_json_pointee_t(const counted_t<const ql::datum_t> &_ptr)
         : ptr(_ptr), rdb_value(NULL), parent() {
         guarantee(ptr);
     }
-#else
-    explicit lazy_json_pointee_t(const counted_t<const ql::datum_t> &_ptr)
-        : ptr(_ptr), rdb_value(NULL), txn(NULL) {
-        guarantee(ptr);
-    }
-#endif
 
     friend class lazy_json_t;
 
     // If empty, we haven't loaded the value yet.
     counted_t<const ql::datum_t> ptr;
 
-#if SLICE_ALT
     // A pointer to the rdb value buffer in the leaf node (or perhaps a copy), and
     // the transaction with which to load it.  Non-NULL only if ptr is empty.
     const rdb_value_t *rdb_value;
     alt::alt_buf_parent_t parent;
-#else
-    // A pointer to the rdb value buffer in the leaf node (or perhaps a copy), and
-    // the transaction with which to load it.  Non-NULL only if ptr is empty.
-    const rdb_value_t *rdb_value;
-    transaction_t *txn;
-#endif
 
     DISABLE_COPYING(lazy_json_pointee_t);
 };
@@ -88,13 +60,8 @@ public:
     explicit lazy_json_t(const counted_t<const ql::datum_t> &ptr)
         : pointee(new lazy_json_pointee_t(ptr)) { }
 
-#if SLICE_ALT
     lazy_json_t(const rdb_value_t *rdb_value, alt::alt_buf_parent_t parent)
         : pointee(new lazy_json_pointee_t(rdb_value, parent)) { }
-#else
-    lazy_json_t(const rdb_value_t *rdb_value, transaction_t *txn)
-        : pointee(new lazy_json_pointee_t(rdb_value, txn)) { }
-#endif
 
     const counted_t<const ql::datum_t> &get() const;
     void reset();
