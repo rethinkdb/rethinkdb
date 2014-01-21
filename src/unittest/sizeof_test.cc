958ed9d54e4dce5bc7e4adb6dab81da1b702d8a3
Sam Hughes <sam@rethinkdb.com>
2014-01-21 01:50:12 -0800

    Made the SizeofTest use the alt cache.

--- src/unittest/sizeof_test.cc
+++ src/unittest/sizeof_test.cc
@@ -1,17 +1,18 @@
 // Copyright 2010-2012 RethinkDB, all rights reserved.
 #include "unittest/gtest.hpp"
 
-#include "buffer_cache/buffer_cache.hpp"
+#include "buffer_cache/alt/alt.hpp"
 #include "serializer/config.hpp"
 
+using namespace alt;  // RSI
 
 namespace unittest {
 
 TEST(SizeofTest, Sizes) {
     EXPECT_GT(1000u, sizeof(standard_serializer_t));
-    EXPECT_GT(1000u, sizeof(cache_t));
-    EXPECT_GT(1000u, sizeof(buf_lock_t));
-    EXPECT_GT(1000u, sizeof(transaction_t));
+    EXPECT_GT(1000u, sizeof(alt_cache_t));
+    EXPECT_GT(1000u, sizeof(alt_buf_lock_t));
+    EXPECT_GT(1000u, sizeof(alt_txn_t));
 }
 
 TEST(SizeofTest, SerBuffer) {
