969f3b3535d401a8e5761363b265da9627d23921
Etienne Laurin <atnnn@rethinkdb.com>
2014-01-24 02:11:27 +0000

    Change python shebangs to use /usr/bin/env
    Review 1149 by @neumino
    Closes #1889

--- external/re2/re2/make_unicode_casefold.py
+++ external/re2/re2/make_unicode_casefold.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # coding=utf-8
 #
 # Copyright 2008 The RE2 Authors.  All Rights Reserved.
--- external/re2/re2/make_unicode_groups.py
+++ external/re2/re2/make_unicode_groups.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # Copyright 2008 The RE2 Authors.  All Rights Reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
