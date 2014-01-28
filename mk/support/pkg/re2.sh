0c1cf3f2ee9670483b5632dcb022511625af3f26
Etienne Laurin <atnnn@rethinkdb.com>
2014-01-28 13:43:44 -0800

    Fix re2 install-include rule
    Review 1158 by @neumino

--- mk/support/pkg/re2.sh
+++ mk/support/pkg/re2.sh
@@ -5,7 +5,7 @@ src_url=http://re2.googlecode.com/files/re2-$version.tgz
 
 pkg_install-include () {
     mkdir -p "$install_dir/include/re2"
-    cp "$src_dir/re2/re2.h" "$install_dir/include/re2"
+    cp "$src_dir/re2"/*.h "$install_dir/include/re2"
 }
 
 pkg_install () {
