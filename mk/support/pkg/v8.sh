a65a28aa135daafe1e64fd5e2caada1997500e2b
Etienne Laurin <atnnn@rethinkdb.com>
2014-02-06 17:44:05 -0800

    Do not make dependencies for v8

--- mk/support/pkg/v8.sh
+++ mk/support/pkg/v8.sh
@@ -10,17 +10,6 @@ version=3.22.24.17
 
 src_url=https://commondatastorage.googleapis.com/chromium-browser-official/v8-$version.tar.bz2
 
-pkg_fetch () {
-    pkg_make_tmp_fetch_dir
-    local archive="${src_url##*/}"
-    geturl "$src_url" > "$tmp_dir/$archive"
-    in_dir "$tmp_dir" tar -xjf "$archive"
-    local unpacked="$tmp_dir/${archive%.tar.bz2}"
-    make -C "$unpacked" dependencies
-    mv "$unpacked" "$src_dir"
-    pkg_remove_tmp_fetch_dir
-}
-
 pkg_install () {
     pkg_copy_src_to_build
     mkdir -p "$install_dir/lib"
