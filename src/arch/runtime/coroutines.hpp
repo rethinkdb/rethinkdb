bcf43cf5406717758da382fbd21751b00a54193b
Sam Hughes <sam@rethinkdb.com>
2014-02-17 17:32:38 -0800

    Removed trailing whitespace.

--- src/arch/runtime/coroutines.hpp
+++ src/arch/runtime/coroutines.hpp
@@ -134,7 +134,7 @@ public:
     static void set_coroutine_stack_size(size_t size);
 
     coro_stack_t *get_stack();
-    
+
     void set_priority(int _priority) {
         linux_thread_message_t::set_priority(_priority);
     }
