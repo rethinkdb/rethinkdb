d05a75baf66eac30d5380c602b3793d35e661235
Etienne Laurin <atnnn@rethinkdb.com>
2014-02-20 19:34:45 -0800

    Fix build on lucid
    Review 1232 by @srh
    Closes #1967

--- mk/support/pkg/zlib.sh
+++ mk/support/pkg/zlib.sh
@@ -2,3 +2,8 @@
 version=1.2.8
 
 src_url=http://zlib.net/zlib-$version.tar.gz
+
+pkg_install-include () {
+    mkdir -p "$install_dir/include"
+    cp "$src_dir/zconf.h" "$src_dir/zlib.h" "$install_dir/include"
+}
