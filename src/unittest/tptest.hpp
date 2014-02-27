71c52ced7148b9897aa132ae38d7ad888d29d3c9
Sam Hughes <sam@rethinkdb.com>
2014-02-27 00:01:36 -0800

    Made TPTEST use group in the run_ function name.

--- src/unittest/tptest.hpp
+++ src/unittest/tptest.hpp
@@ -4,10 +4,10 @@
 #include "unittest/gtest.hpp"
 #include "unittest/unittest_utils.hpp"
 
-#define TPTEST(group, name, ...) void run_##name();                     \
+#define TPTEST(group, name, ...) void run_##group##_##name();           \
     TEST(group, name) {                                                 \
-        ::unittest::run_in_thread_pool(run_##name, ##__VA_ARGS__);      \
+        ::unittest::run_in_thread_pool(run_##group##_##name, ##__VA_ARGS__);      \
     }                                                                   \
-    void run_##name()
+    void run_##group##_##name()
 
 #endif  // UNITTEST_TPTEST_HPP_
