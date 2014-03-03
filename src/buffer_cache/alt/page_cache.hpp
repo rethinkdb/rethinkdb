c790111d3b062ca7d19def973275e4e96d412e56
Sam Hughes <sam@rethinkdb.com>
2014-03-02 20:50:39 -0800

    Fixed miscellaneous include problems.

--- src/buffer_cache/alt/page_cache.hpp
+++ src/buffer_cache/alt/page_cache.hpp
@@ -4,10 +4,9 @@
 
 #include <functional>
 #include <map>
+#include <set>
 #include <utility>
 #include <vector>
-#include <set>
-#include <functional>
 
 #include "buffer_cache/alt/block_version.hpp"
 #include "buffer_cache/alt/cache_account.hpp"
