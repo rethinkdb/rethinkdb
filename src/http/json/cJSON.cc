f5eb62457a946523d588f50c41f74110178d349e
Sam Hughes <sam@rethinkdb.com>
2014-03-02 19:59:23 -0800

    Removed commented function cJSON_InitHooks.

--- src/http/json/cJSON.cc
+++ src/http/json/cJSON.cc
@@ -64,18 +64,6 @@ static char* cJSON_strdup(const char* str)
       return copy;
 }
 
-//void cJSON_InitHooks(cJSON_Hooks* hooks)
-//{
-//    if (!hooks) { /* Reset hooks */
-//        cJSON_malloc = malloc;
-//        cJSON_free = free;
-//        return;
-//    }
-//
-//        cJSON_malloc = (hooks->malloc_fn)?hooks->malloc_fn:malloc;
-//        cJSON_free         = (hooks->free_fn)?hooks->free_fn:free;
-//}
-
 /* Internal constructor. */
 static cJSON *cJSON_New_Item()
 {
