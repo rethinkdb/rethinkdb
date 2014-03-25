b828b66090714e5af73cf98a95dac17ac663ed77
Michael Lucy <mlucy@rethinkdb.com>
2014-03-25 15:27:39 -0700

    get rid of extractor_t

--- src/protob/json_shim.cc
+++ src/protob/json_shim.cc
@@ -19,27 +19,36 @@ public:
 };
 
 void check_type(cJSON *json, int expected) {
-    if (json->type != expected) throw exc_t(json);
+    if (json->type != expected) throw exc_t();
 }
 
-template<class T, class = void>
-struct extractor_t;
+template<class T>
+typename std::enable_if<!((std::is_enum<T>::value || std::is_fundamental<T>::value)
+                          && !std::is_same<T, bool>::value)>::type
+extract(cJSON *, T *);
 
 template<class T>
-struct safe_extractor_t : public extractor_t<T> {
-    template<class U>
-    void operator()(cJSON *json, U *t) {
-        if (json != NULL && t != NULL) {
-            (*static_cast<extractor_t<T> *>(this))(json, t);
-        }
+typename std::enable_if<(std::is_enum<T>::value || std::is_fundamental<T>::value)
+                        && !std::is_same<T, bool>::value>::type
+extract(cJSON *field, T *dest) {
+    if (field->type != cJSON_Number) throw exc_t();
+    T t = static_cast<T>(field->valuedouble);
+    if (static_cast<double>(t) != field->valuedouble) throw exc_t();
+    *dest = t;
+}
+
+template<class T>
+void safe_extract(cJSON *json, T *t) {
+    if (json != NULL && t != NULL) {
+        extract(json, t);
     }
-};
+}
 
 template<class T, class U>
 void transfer(cJSON *json, T *dest, void (T::*setter)(U), const char *name) {
     U tmp;
     if (cJSON *item = cJSON_GetObjectItem(json, name)) {
-        safe_extractor_t<U>()(item, &tmp);
+        safe_extract(item, &tmp);
         (dest->*setter)(std::move(tmp));
     }
 }
@@ -47,7 +56,7 @@ void transfer(cJSON *json, T *dest, void (T::*setter)(U), const char *name) {
 template<class T, class U>
 void transfer(cJSON *json, T *dest, U *(T::*mut)(), const char *name) {
     if (cJSON *item = cJSON_GetObjectItem(json, name)) {
-        safe_extractor_t<U>()(item, (dest->*mut)());
+        safe_extract(item, (dest->*mut)());
     }
 }
 
@@ -59,133 +68,119 @@ void transfer_arr(cJSON *json, T *dest, U *(T::*adder)(), const char *name) {
         }
         int sz = cJSON_GetArraySize(arr);
         for (int i = 0; i < sz; ++i) {
-            safe_extractor_t<U>()(cJSON_GetArrayItem(arr, i), (dest->*adder)());
+            safe_extract(cJSON_GetArrayItem(arr, i), (dest->*adder)());
         }
     }
 }
 
 template<class T>
-struct extractor_t<
-    T, typename std::enable_if<std::is_enum<T>::value
-                               || std::is_fundamental<T>::value>::type> {
-    void operator()(cJSON *field, T *dest) {
-        if (field->type != cJSON_Number) throw exc_t(field);
-        T t = static_cast<T>(field->valuedouble);
-        if (static_cast<double>(t) != field->valuedouble) throw exc_t(field);
-        *dest = t;
+typename std::enable_if<(std::is_enum<T>::value || std::is_fundamental<T>::value)
+                        && !std::is_same<T, bool>::value>::type
+tst(cJSON *field, T *dest) {
+    if (field->type != cJSON_Number) throw exc_t();
+    T t = static_cast<T>(field->valuedouble);
+    if (static_cast<double>(t) != field->valuedouble) throw exc_t();
+    *dest = t;
+}
+void foo() {
+    Term_TermType x;
+    tst(NULL, &x);
+}
+
+template<>
+void extract(cJSON *field, std::string *s) {
+    check_type(field, cJSON_String);
+    *s = field->valuestring;
+};
+
+template<>
+void extract(cJSON *field, bool *dest) {
+    if (field->type == cJSON_False) {
+        *dest = false;
+    } else if (field->type == cJSON_True) {
+        *dest = true;
+    } else {
+        throw exc_t();
     }
-};
+}
 
 template<>
-struct extractor_t<std::string> {
-    void operator()(cJSON *field, std::string *s) {
-        check_type(field, cJSON_String);
-        *s = field->valuestring;
-    };
-};
+void extract(cJSON *json, Term *t) {
+    transfer(json, t, &Term::set_type, "t");
+    transfer(json, t, &Term::mutable_datum, "d");
+    transfer_arr(json, t, &Term::add_args, "a");
+    transfer_arr(json, t, &Term::add_optargs, "o");
+}
 
 template<>
-struct extractor_t<bool> {
-    void operator()(cJSON *field, bool *dest) {
-        if (field->type == cJSON_False) {
-            *dest = false;
-        } else if (field->type == cJSON_True) {
-            *dest = true;
-        } else {
-            throw exc_t(field);
+void extract(cJSON *json, Datum *d) {
+    switch (json->type) {
+    case cJSON_False: {
+        d->set_type(Datum::R_BOOL);
+        d->set_r_bool(false);
+    } break;
+    case cJSON_True: {
+        d->set_type(Datum::R_BOOL);
+        d->set_r_bool(true);
+    } break;
+    case cJSON_NULL: {
+        d->set_type(Datum::R_NULL);
+    } break;
+    case cJSON_Number: {
+        d->set_type(Datum::R_NUM);
+        d->set_r_num(json->valuedouble);
+    } break;
+    case cJSON_String: {
+        d->set_type(Datum::R_STR);
+        d->set_r_str(json->valuestring);
+    } break;
+    case cJSON_Array: {
+        d->set_type(Datum::R_ARRAY);
+        int sz = cJSON_GetArraySize(json);
+        for (int i = 0; i < sz; ++i) {
+            cJSON *item = cJSON_GetArrayItem(json, i);
+            extract(item, d->add_r_array());
         }
-    }
-};
-
-template<>
-struct extractor_t<Term> {
-    void operator()(cJSON *json, Term *t) {
-        transfer(json, t, &Term::set_type, "t");
-        transfer(json, t, &Term::mutable_datum, "d");
-        transfer_arr(json, t, &Term::add_args, "a");
-        transfer_arr(json, t, &Term::add_optargs, "o");
-    }
-};
-
-template<>
-struct extractor_t<Datum> {
-    void operator()(cJSON *json, Datum *d) {
-        switch(json->type) {
-        case cJSON_False: {
-            d->set_type(Datum::R_BOOL);
-            d->set_r_bool(false);
-        } break;
-        case cJSON_True: {
-            d->set_type(Datum::R_BOOL);
-            d->set_r_bool(true);
-        } break;
-        case cJSON_NULL: {
-            d->set_type(Datum::R_NULL);
-        } break;
-        case cJSON_Number: {
-            d->set_type(Datum::R_NUM);
-            d->set_r_num(json->valuedouble);
-        } break;
-        case cJSON_String: {
-            d->set_type(Datum::R_STR);
-            d->set_r_str(json->valuestring);
-        } break;
-        case cJSON_Array: {
-            d->set_type(Datum::R_ARRAY);
-            int sz = cJSON_GetArraySize(json);
-            for (int i = 0; i < sz; ++i) {
-                cJSON *item = cJSON_GetArrayItem(json, i);
-                (*this)(item, d->add_r_array());
-            }
-        } break;
-        case cJSON_Object: {
-            d->set_type(Datum::R_OBJECT);
-            int sz = cJSON_GetArraySize(json);
-            for (int i = 0; i < sz; ++i) {
-                cJSON *item = cJSON_GetArrayItem(json, i);
-                Datum::AssocPair *ap = d->add_r_object();
-                ap->set_key(item->string);
-                (*this)(item, ap->mutable_val());
-            }
-        } break;
-        default: unreachable();
+    } break;
+    case cJSON_Object: {
+        d->set_type(Datum::R_OBJECT);
+        int sz = cJSON_GetArraySize(json);
+        for (int i = 0; i < sz; ++i) {
+            cJSON *item = cJSON_GetArrayItem(json, i);
+            Datum::AssocPair *ap = d->add_r_object();
+            ap->set_key(item->string);
+            extract(item, ap->mutable_val());
         }
+    } break;
+    default: unreachable();
     }
+}
+
+template<>
+void extract(cJSON *json, Query::AssocPair *ap) {
+    ap->set_key(json->string);
+    extract(json, ap->mutable_val());
 };
 
 template<>
-struct extractor_t<Query::AssocPair> {
-    void operator()(cJSON *json, Query::AssocPair *ap) {
-        ap->set_key(json->string);
-        extractor_t<Term>()(json, ap->mutable_val());
-    };
+void extract(cJSON *json, Term::AssocPair *ap) {
+    ap->set_key(json->string);
+    extract(json, ap->mutable_val());
 };
 
 template<>
-struct extractor_t<Term::AssocPair> {
-    void operator()(cJSON *json, Term::AssocPair *ap) {
-        ap->set_key(json->string);
-        extractor_t<Term>()(json, ap->mutable_val());
-    };
+void extract(cJSON *json, Datum::AssocPair *ap) {
+    ap->set_key(json->string);
+    extract(json, ap->mutable_val());
 };
 
 template<>
-struct extractor_t<Datum::AssocPair> {
-    void operator()(cJSON *json, Datum::AssocPair *ap) {
-        ap->set_key(json->string);
-        extractor_t<Datum>()(json, ap->mutable_val());
-    };
-};
-
-template<>
-struct extractor_t<Query> {
-    void operator()(cJSON *json, Query *q) {
-        transfer(json, q, &Query::set_type, "t");
-        transfer(json, q, &Query::mutable_query, "q");
-        transfer(json, q, &Query::set_token, "k");
-        q->set_accepts_r_json(true);
-        transfer_arr(json, q, &Query::add_global_optargs, "g");
-    };
+void extract(cJSON *json, Query *q) {
+    transfer(json, q, &Query::set_type, "t");
+    transfer(json, q, &Query::mutable_query, "q");
+    transfer(json, q, &Query::set_token, "k");
+    q->set_accepts_r_json(true);
+    transfer_arr(json, q, &Query::add_global_optargs, "g");
 };
 
 bool parse_json_pb(Query *q, char *str) throw () {
@@ -194,7 +189,7 @@ bool parse_json_pb(Query *q, char *str) throw () {
     cJSON *json = json_holder.get();
     // debugf("%s\n", json_holder.Print().c_str());
     if (json == NULL) return false;
-    extractor_t<Query>()(json, q);
+    extract(json, q);
     // debugf("%s\n", q->DebugString().c_str());
     return true;
 }
