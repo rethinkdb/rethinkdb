263854d1b4637761c3cde1c0ea99b51a3192398e
Sam Hughes <sam@rethinkdb.com>
2014-03-31 16:39:35 -0700

    Made the RegionMap tests use the rdb protocol.

--- src/unittest/region_map.cc
+++ src/unittest/region_map.cc
@@ -1,21 +1,27 @@
 // Copyright 2010-2012 RethinkDB, all rights reserved.
 #include "unittest/gtest.hpp"
 
-#include "mock/dummy_protocol.hpp"
+#include "hash_region.hpp"
+#include "rdb_protocol/protocol.hpp"
 #include "protocol_api.hpp"
 
 namespace unittest {
-using mock::dummy_protocol_t;
+
+hash_region_t<key_range_t> make_region(std::string low, std::string high) {
+    return hash_region_t<key_range_t>(key_range_t(key_range_t::closed, store_key_t(low),
+                                                  key_range_t::open, store_key_t(high)));
+}
+
 TEST(RegionMap, BasicSets) {
-    region_map_t<dummy_protocol_t, int> rmap(dummy_protocol_t::region_t('a', 'z'), 0);
-    rmap.set(dummy_protocol_t::region_t('a', 'm'), 1);
-    rmap.set(dummy_protocol_t::region_t('n', 'z'), 2);
-    for (region_map_t<dummy_protocol_t, int>::iterator it  = rmap.begin();
+    region_map_t<rdb_protocol_t, int> rmap(make_region("a", "z"), 0);
+    rmap.set(make_region("a", "n"), 1);
+    rmap.set(make_region("n", "z"), 2);
+    for (region_map_t<rdb_protocol_t, int>::iterator it  = rmap.begin();
          it != rmap.end();
          ++it) {
-        if (region_is_superset(dummy_protocol_t::region_t('a', 'm'), it->first)) {
+        if (region_is_superset(make_region("a", "n"), it->first)) {
             EXPECT_EQ(1, it->second);
-        } else if (region_is_superset(dummy_protocol_t::region_t('n', 'z'), it->first)) {
+        } else if (region_is_superset(make_region("n", "z"), it->first)) {
             EXPECT_EQ(2, it->second);
         } else {
             ADD_FAILURE() << "Got unexpected region from region map.";
@@ -24,20 +30,20 @@ TEST(RegionMap, BasicSets) {
 }
 
 TEST(RegionMap, Mask) {
-    region_map_t<dummy_protocol_t, int> rmap(dummy_protocol_t::region_t('a', 'z'), 0);
-    rmap.set(dummy_protocol_t::region_t('a', 'm'), 1);
-    rmap.set(dummy_protocol_t::region_t('n', 'z'), 2);
+    region_map_t<rdb_protocol_t, int> rmap(make_region("a", "z"), 0);
+    rmap.set(make_region("a", "n"), 1);
+    rmap.set(make_region("n", "z"), 2);
 
-    region_map_t<dummy_protocol_t, int> masked_map = rmap.mask(dummy_protocol_t::region_t('g', 'r'));
+    region_map_t<rdb_protocol_t, int> masked_map = rmap.mask(make_region("g", "s"));
 
-    EXPECT_TRUE(masked_map.get_domain() == dummy_protocol_t::region_t('g', 'r'));
+    EXPECT_TRUE(masked_map.get_domain() == make_region("g", "s"));
 
-    for (region_map_t<dummy_protocol_t, int>::iterator it  = masked_map.begin();
+    for (region_map_t<rdb_protocol_t, int>::iterator it  = masked_map.begin();
          it != masked_map.end();
          ++it) {
-        if (region_is_superset(dummy_protocol_t::region_t('a', 'm'), it->first)) {
+        if (region_is_superset(make_region("a", "n"), it->first)) {
             EXPECT_EQ(1, it->second);
-        } else if (region_is_superset(dummy_protocol_t::region_t('n', 'z'), it->first)) {
+        } else if (region_is_superset(make_region("n", "z"), it->first)) {
             EXPECT_EQ(2, it->second);
         } else {
             ADD_FAILURE() << "Got unexpected region from region map.";
