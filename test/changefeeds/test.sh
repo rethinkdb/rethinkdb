4269fc7fe85063be8a32eb5bae2d18e220afc296
Michael Lucy <mlucy@rethinkdb.com>
2014-05-05 18:17:14 -0700

    Adding testing code.

--- test/changefeeds/test.sh
+++ test/changefeeds/test.sh
@@ -5,11 +5,19 @@ set -o nounset
 
 hostname=`hostname`
 tag() {
-    echo "$hostname/`date +%s.%N`"
+    echo 1
 }
 
+QUIET=
+deeplog() {
+    tag="$hostname@`date +%s.%N`"
+    space=`printf "%-$1s" ""`
+    shift
+    { [[ -z "$@" ]] && sed "s/^/$tag$space /" || echo "$tag$space $@"; } \
+        | tee -a log.t | { [[ -z $QUIET ]] && cat || true; } 1>&2
+}
 log() {
-    echo "`tag` $@" | tee -a log.t 1>&2
+    deeplog 0 "$@"
 }
 
 abort() {
@@ -22,11 +30,16 @@ abort() {
 servers="arclight arclight"
 nodes_per_server=2
 id=~/.ssh/test_rsa
+[[ ! -e $id ]] && abort "No file $id."
+eval "`ssh-agent | head -2` 2>&1 | QUIET=1 log"
+ssh-add $id 2>&1 | QUIET=1 log
 each_node() {
+    msg=$1
+    shift
+    log "$msg START"
     for server in $servers; do
         for n in `seq $nodes_per_server`; do
-            [[ ! -e $id ]] && abort "No file $id."
-            ssh -T -oBatchMode=yes -i $id test@$server <<EOF
+            ssh -T -oBatchMode=yes test@$server <<EOF 2>&1 | deeplog 2 &
 export PORT_OFFSET=$n
 mkdir -p /mnt/ssd/test/$n
 cd /mnt/ssd/test/$n
@@ -34,7 +47,20 @@ $@
 EOF
         done
     done
+    wait
+    log "$msg DONE"
 }
 
-log "Setting up..."
-each_node 'pwd; nohup rethinkdb -o $PORT_OFFSET --bind all >log1.t 2>log2.t &'
+log "syncing..."
+for server in $servers; do
+    for n in `seq $nodes_per_server`; do
+        rsync -avz \
+            ~/rethinkdb/build/debug/rethinkdb \
+            test@$server:/mnt/ssd/test/$n/rethinkdb 2>&1 | QUIET=1 log &
+    done
+done
+wait
+log "synced"
+
+each_node "pwd" \
+    'pwd; nohup rethinkdb -o $PORT_OFFSET --bind all >log1.t 2>log2.t &'
