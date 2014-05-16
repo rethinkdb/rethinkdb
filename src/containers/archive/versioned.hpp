3d817c8782be075abeefbbb128ccc459b7e23e8c
Sam Hughes <sam@rethinkdb.com>
2014-05-15 17:51:29 -0700

    Moved serialization of cluster_version_t to its definition.

--- src/containers/archive/versioned.hpp
+++ src/containers/archive/versioned.hpp
@@ -6,14 +6,22 @@
 // An enumeration of the current and previous recognized versions.  Every
 // serialization / deserialization happens relative to one of these versions.
 enum class cluster_version_t {
+    // The versions are currently contiguously numbered.  If this becomes untrue,
+    // redefine the ARCHIVE_PRIM_MAKE_RANGED_SERIALIZABLE line below.
     v1_13 = 0,
+
     // See CLUSTER_VERSION, which should always be the latest version.
     LATEST_VERSION = v1_13,
+
     // ONLY_VERSION should only exist as long as there's only one version.  A few
     // assertions check it -- maybe that code should be fixed to handle multiple
     // versions, once that's possible.
     ONLY_VERSION = v1_13,
 };
+
+ARCHIVE_PRIM_MAKE_RANGED_SERIALIZABLE(cluster_version_t, uint8_t,
+                                      cluster_version_t::v1_13,
+                                      cluster_version_t::LATEST_VERSION);
 
 // Serializes a value for a given version.  DOES NOT SERIALIZE THE VERSION NUMBER!
 template <class T>
