b5f29ff39add75d42c7bee470143897f82026f40
Karl Kuehn <karl@rethinkdb.com>
2014-05-19 16:59:07 -0700

    fixing a missing part of a commit

--- drivers/javascript/build.mk
+++ drivers/javascript/build.mk
@@ -14,7 +14,7 @@ DRIVER_COMPILED_COFFEE := $(patsubst $(JS_SRC_DIR)/%.coffee,$(DRIVER_COFFEE_BUIL
 JS_PKG_DIR := $(PACKAGES_DIR)/js
 
 $(PROTO_MODULE): $(PROTO_FILE)
-    ./proto-to-js < $(PROTO_FILE) > $@
+	$(JS_SRC_DIR)/proto-to-js < $(PROTO_FILE) > $@
 
 # Must be synced with the list in package.json
 JS_PKG_FILES := $(DRIVER_COMPILED_COFFEE) $(JS_SRC_DIR)/README.md $(PROTO_MODULE) $(JS_SRC_DIR)/package.json
