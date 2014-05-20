bb5fc04e2228dc3eb0c1c806dddb189260a91787
Sam Hughes <sam@rethinkdb.com>
2014-05-20 12:40:20 -0700

    Made cluster_version_t serialization macro use int8_t, dropped pragmas.

--- src/containers/archive/versioned.hpp
+++ src/containers/archive/versioned.hpp
@@ -4,19 +4,10 @@
 #include "version.hpp"
 #include "containers/archive/archive.hpp"
 
-// Silences complaints about spurious comparisons in the macro expansion on gcc-4.4.
-#if defined(__GNUC__) && (100 * __GNUC__ + __GNUC_MINOR__ < 406)
-#pragma GCC diagnostic ignored "-Wtype-limits"
-#endif
-
 // This is only correct as long as cluster_version_t values form a contiguous range.
-ARCHIVE_PRIM_MAKE_RANGED_SERIALIZABLE(cluster_version_t, uint8_t,
+ARCHIVE_PRIM_MAKE_RANGED_SERIALIZABLE(cluster_version_t, int8_t,
                                       cluster_version_t::v1_13,
                                       cluster_version_t::ONLY_VERSION);
-
-#if defined(__GNUC__) && (100 * __GNUC__ + __GNUC_MINOR__ < 406)
-#pragma GCC diagnostic error "-Wtype-limits"
-#endif
 
 // Serializes a value for a given version.  DOES NOT SERIALIZE THE VERSION NUMBER!
 template <class T>
