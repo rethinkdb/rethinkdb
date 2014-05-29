fcbb7b539fd7c12a68963a139e8f116f626604b0
Daniel Mewes <daniel@rethinkdb.com>
2014-05-29 15:18:51 -0700

    Removed superfluous backslash

--- src/btree/node.cc
+++ src/btree/node.cc
@@ -11,7 +11,7 @@ const block_magic_t btree_sindex_block_t::expected_magic = { { 's', 'i', 'n', 'd
 void btree_superblock_ct_asserts() {
     // Just some place to put the CT_ASSERTs
     CT_ASSERT(btree_superblock_t::METAINFO_BLOB_MAXREFLEN > 0);
-    CT_ASSERT(from_cache_block_size_t<sizeof(btree_superblock_t)>::ser_size \
+    CT_ASSERT(from_cache_block_size_t<sizeof(btree_superblock_t)>::ser_size
               == DEVICE_BLOCK_SIZE);
 }
 
