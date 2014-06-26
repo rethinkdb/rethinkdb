c0ebd5862de96c57e9104b203cbac546c4f3645f
Sam Hughes <sam@rethinkdb.com>
2014-06-25 17:58:54 -0700

    Removed the unused serialization implementation of binary_blob_t.

--- src/containers/binary_blob.cc
+++ src/containers/binary_blob.cc
@@ -3,9 +3,6 @@
 #include "containers/printf_buffer.hpp"
 #include "containers/archive/stl_types.hpp"
 
-RDB_IMPL_ME_SERIALIZABLE_1(binary_blob_t, storage);
-
-
 void debug_print(printf_buffer_t *buf, const binary_blob_t &blob) {
     const uint8_t *data = static_cast<const uint8_t *>(blob.data());
     const uint8_t *end = data + blob.size();
