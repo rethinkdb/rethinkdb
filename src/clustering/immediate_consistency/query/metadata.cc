2d39942fec803d93720d7830461d6970de250350
Michael Lucy <mlucy@rethinkdb.com>
2014-06-30 21:05:07 -0700

    Working on serialization.

--- src/clustering/immediate_consistency/query/metadata.cc
+++ src/clustering/immediate_consistency/query/metadata.cc
@@ -6,8 +6,10 @@
 RDB_IMPL_SERIALIZABLE_1_SINCE_v1_13(direct_reader_business_card_t, read_mailbox);
 RDB_IMPL_EQUALITY_COMPARABLE_1(direct_reader_business_card_t, read_mailbox);
 
-RDB_IMPL_SERIALIZABLE_4_SINCE_v1_13(
-        master_business_card_t::read_request_t, read, order_token, fifo_token, cont_addr);
+RDB_IMPL_SERIALIZABLE_4(
+        master_business_card_t::read_request_t,
+        read, order_token, fifo_token, cont_addr);
+INSTANTIATE_SERIALIZABLE_FOR_CLUSTER(master_business_card_t::read_request_t);
 
 RDB_IMPL_SERIALIZABLE_4_SINCE_v1_13(
         master_business_card_t::write_request_t, write, order_token, fifo_token, cont_addr);
