202f619f697ca136e665a387e7fee1a02906cb3b
Graham Hughes <graham@rethinkdb.com>
2014-07-14 18:33:39 -0700

    Sketch implementation.
    This is nothing better than a sketch, because of the NULL env pointer.

--- src/rdb_protocol/configured_limits.cc
+++ src/rdb_protocol/configured_limits.cc
@@ -1,11 +1,17 @@
 #include "rdb_protocol/configured_limits.hpp"
 #include "rdb_protocol/wire_func.hpp"
+#include "rdb_protocol/func.hpp"
 
 namespace ql {
 configured_limits_t
-from_optargs(const std::map<std::string, wire_func_t> &)
+from_optargs(const std::map<std::string, wire_func_t> &arguments)
 {
-    return configured_limits_t();
+    auto p = arguments.find("arrayLimit");
+    if (p != arguments.end()) {
+        return configured_limits_t((*p).second.compile_wire_func()->call(NULL)->as_int());
+    } else {
+        return configured_limits_t();
+    }
 }
 
 } // namespace ql
