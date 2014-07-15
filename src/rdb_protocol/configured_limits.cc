122158b22febac70486031da4e266bd062e4da0c
Graham Hughes <graham@rethinkdb.com>
2014-07-14 18:47:07 -0700

    Better sketch.  Let's see if this works.

--- src/rdb_protocol/configured_limits.cc
+++ src/rdb_protocol/configured_limits.cc
@@ -8,7 +8,13 @@ from_optargs(const std::map<std::string, wire_func_t> &arguments)
 {
     auto p = arguments.find("arrayLimit");
     if (p != arguments.end()) {
-        return configured_limits_t((*p).second.compile_wire_func()->call(NULL)->as_int());
+        // Fake an environment with no arguments.
+        cond_t cond;
+        rdb_context_t context;
+        // Can't use the original arguments, because it will cause an infinite loop.
+        env_t env(&context, &cond, std::map<std::string, wire_func_t>(),
+                  profile_bool_t::DONT_PROFILE);
+        return configured_limits_t((*p).second.compile_wire_func()->call(&env)->as_int());
     } else {
         return configured_limits_t();
     }
