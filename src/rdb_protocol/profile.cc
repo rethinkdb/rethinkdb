805fa74f944b9ddd2ba9f90bc00aaa476d52bc04
Graham Hughes <graham@rethinkdb.com>
2014-07-15 19:48:36 -0700

    Per @srh use a pointer for this visitor.

--- src/rdb_protocol/profile.cc
+++ src/rdb_protocol/profile.cc
@@ -79,15 +79,21 @@ counted_t<const ql::datum_t> construct_datum(
 
 class construct_datum_visitor_t : public boost::static_visitor<void> {
 public:
+    // N.B.: it is important that the lifetime of this visitor not
+    // exceed the lifetime of the limits reference: for example
+    // writing construct_datum_visitor_t v(begin, end,
+    // ql::configured_limits_t(), &res) is a terrible idea.  When in
+    // doubt, use construct_datum, which ensures that its lifetime is
+    // a subset of the limits lifetime.
     construct_datum_visitor_t(
         event_log_t::const_iterator *begin, event_log_t::const_iterator end,
-        const ql::configured_limits_t &limits,
+        const ql::configured_limits_t *limits,
         std::vector<counted_t<const ql::datum_t> > *res)
         : begin_(begin), end_(end), limits_(limits), res_(res) { }
 
     void operator()(const start_t &start) const {
         (*begin_)++;
-        counted_t<const ql::datum_t> sub_tasks = construct_datum(begin_, end_, limits_);
+        counted_t<const ql::datum_t> sub_tasks = construct_datum(begin_, end_, *limits_);
         auto stop = boost::get<stop_t>(&**begin_);
         guarantee(stop);
         res_->push_back(construct_start(
@@ -98,12 +104,12 @@ public:
         (*begin_)++;
         std::vector<counted_t<const ql::datum_t> > parallel_tasks;
         for (size_t i = 0; i < split.n_parallel_jobs_; ++i) {
-            parallel_tasks.push_back(construct_datum(begin_, end_, limits_));
+            parallel_tasks.push_back(construct_datum(begin_, end_, *limits_));
             guarantee(boost::get<stop_t>(&**begin_));
             (*begin_)++;
         }
         res_->push_back(construct_split(
-            make_counted<const ql::datum_t>(std::move(parallel_tasks), limits_)));
+            make_counted<const ql::datum_t>(std::move(parallel_tasks), *limits_)));
     }
     void operator()(const sample_t &sample) const {
         (*begin_)++;
@@ -116,7 +122,7 @@ public:
 private:
     event_log_t::const_iterator *begin_;
     event_log_t::const_iterator end_;
-    const ql::configured_limits_t &limits_;
+    const ql::configured_limits_t *limits_;
     std::vector<counted_t<const ql::datum_t> > *res_;
 };
 
@@ -126,7 +132,7 @@ counted_t<const ql::datum_t> construct_datum(
         const ql::configured_limits_t &limits) {
     std::vector<counted_t<const ql::datum_t> > res;
 
-    construct_datum_visitor_t visitor(begin, end, limits, &res);
+    construct_datum_visitor_t visitor(begin, end, &limits, &res);
     while (*begin != end && !boost::get<stop_t>(&**begin)) {
         boost::apply_visitor(visitor, **begin);
     }
