049db99e87c0a1c041ef8f80f9ca9a0bf2b7eb4d
Daniel Mewes <daniel@rethinkdb.com>
2014-07-24 12:58:38 -0700

    Fixed stylistic issues in ellispoid.hpp

--- src/geo/ellipsoid.hpp
+++ src/geo/ellipsoid.hpp
@@ -7,31 +7,32 @@
 class ellipsoid_spec_t {
 public:
     ellipsoid_spec_t() { } // For deserialization
-    ellipsoid_spec_t(double _a, double _f) : a(_a), f(_f) { }
+    ellipsoid_spec_t(double equator_radius, double flattening)
+        : equator_radius_(equator_radius), flattening_(flattening) { }
 
     // In meters
     inline double equator_radius() const {
-        return a;
+        return equator_radius_;
     }
     // In meters
     inline double poles_radius() const {
-        return (1.0 - f) * a;
+        return (1.0 - flattening_) * equator_radius_;
     }
     // Flattening, see http://en.wikipedia.org/w/index.php?title=Flattening&oldid=602517763
     inline double flattening() const {
-        return f;
+        return flattening_;
     }
 
+    RDB_DECLARE_ME_SERIALIZABLE;
+
 private:
-    template<cluster_version_t W>
-    friend void serialize(write_message_t *, const ellipsoid_spec_t &);
-    template<cluster_version_t W>
-    friend archive_result_t deserialize(read_stream_t *, ellipsoid_spec_t *);
-
-    double a, f;
+    double equator_radius_;
+    double flattening_;
 };
-RDB_DECLARE_SERIALIZABLE(ellipsoid_spec_t);
+RDB_SERIALIZE_OUTSIDE(ellipsoid_spec_t);
 
+// WGS 84 is a commonly used standard for earth geometry, see
+// http://en.wikipedia.org/w/index.php?title=World_Geodetic_System&oldid=614370148
 static const ellipsoid_spec_t WGS84_ELLIPSOID(6378137.0, 1.0/298.257223563);
 static const ellipsoid_spec_t UNIT_SPHERE(1.0, 0.0);
 
