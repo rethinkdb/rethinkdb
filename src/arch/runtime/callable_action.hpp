d28cb014bd7618d1e7618d77663abc5547a1120a
Sam Hughes <sam@rethinkdb.com>
2014-08-14 11:57:37 -0700

    Made coro_t::spawn... functions use rvalue references and std::forward.

--- src/arch/runtime/callable_action.hpp
+++ src/arch/runtime/callable_action.hpp
@@ -23,7 +23,8 @@ private:
 template<class Callable>
 class callable_action_instance_t : public callable_action_t {
 public:
-    explicit callable_action_instance_t(const Callable& callable) : callable_(callable) { }
+    explicit callable_action_instance_t(Callable &&callable)
+        : callable_(std::forward<Callable>(callable)) { }
 
     void run_action() { callable_(); }
 
@@ -37,7 +38,7 @@ public:
     ~callable_action_wrapper_t();
 
     template<class Callable>
-    void reset(const Callable& action)
+    void reset(Callable &&action)
     {
         rassert(action_ == NULL);
 
@@ -47,10 +48,12 @@ public:
 #endif
         // Allocate the action inside this object, if possible, or the heap otherwise
         if (sizeof(callable_action_instance_t<Callable>) > CALLABLE_CUTOFF_SIZE) {
-            action_ = new callable_action_instance_t<Callable>(action);
+            action_ = new callable_action_instance_t<Callable>(
+                    std::forward<Callable>(action));
             action_on_heap = true;
         } else {
-            action_ = new (action_data) callable_action_instance_t<Callable>(action);
+            action_ = new (action_data) callable_action_instance_t<Callable>(
+                    std::forward<Callable>(action));
             action_on_heap = false;
         }
 #ifdef __clang__
