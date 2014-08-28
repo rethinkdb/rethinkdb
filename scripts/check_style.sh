4063c334dabaf1b7c25e71628e7d88c6973d5369
Sam Hughes <sam@rethinkdb.com>
2014-08-28 14:27:01 -0700

    Fixed some indentation problems.

--- scripts/check_style.sh
+++ scripts/check_style.sh
@@ -4,12 +4,9 @@ DIR=`dirname $0`
 SRC_DIR=`dirname "$DIR"`/src
 IGNORE='/(linenoise\.cc|cJSON\.(cc|hpp)|thread_stack_pcs\.cc|http_parser\.(cc|hpp)|rdb_protocol/geo/s2/.*|rdb_protocol/geo/karney/.*)$'
 
-find "$SRC_DIR" -name \*.cc -o -name \*.hpp -o -name \*.tcc | grep -Ev $IGNORE | xargs $DIR/cpplint --verbose=2 --root="$SRC_DIR" --extensions=hpp,cc,tcc --filter=-build/c++11,-build/include_what_you_use,-whitespace/forcolon,-whitespace/end_of_line,-whitespace/indent,-whitespace/parens,-whitespace/line_length,+readability/casting,-whitespace/braces,-readability/todo,-legal/copyright,-whitespace/comments,-whitespace/labels,-whitespace/blank_line,-readability/function,-readability/braces,-runtime/explicit,-readability/namespace 2>&1
+find "$SRC_DIR" -name \*.cc -o -name \*.hpp -o -name \*.tcc | grep -Ev $IGNORE | xargs $DIR/cpplint --verbose=2 --root="$SRC_DIR" --extensions=hpp,cc,tcc --filter=-build/c++11,-build/include_what_you_use,-whitespace/forcolon,-whitespace/end_of_line,-whitespace/parens,-whitespace/line_length,+readability/casting,-whitespace/braces,-readability/todo,-legal/copyright,-whitespace/comments,-whitespace/labels,-whitespace/blank_line,-readability/function,-readability/braces,-runtime/explicit,-readability/namespace 2>&1
 
 # TODO(2014-09):
-
-# whitespace/indent - disabled temporarily until we can silence
-# complaints about needing a space before "private:" or "public:".
 
 # whitespace/parens - what is this?  Maybe it's something to enable
 # now.
@@ -17,7 +14,7 @@ find "$SRC_DIR" -name \*.cc -o -name \*.hpp -o -name \*.tcc | grep -Ev $IGNORE |
 # whitespace/braces - what is this?  Should we enable it and see what
 # happens?
 
-# readibility/braces - disabled temporarily until somebody goes
+# readability/braces - disabled temporarily until somebody goes
 # through and fixes all the problems
 
 # runtime/explicit - disabled temporarily, until we remove "explicit"
