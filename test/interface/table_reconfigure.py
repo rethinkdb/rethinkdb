2eb6bcfc4a03a46bf97bd49c178701dc7aa9ca78
Tim Maxwell <tim@rethinkdb.com>
2014-09-09 21:48:42 -0700

    Beef up table.reconfigure test, so it should now act as a regression test for issue #2966.

--- test/interface/table_reconfigure.py
+++ test/interface/table_reconfigure.py
@@ -14,26 +14,35 @@ scenario_common.prepare_option_parser_mode_flags(op)
 opts = op.parse(sys.argv)
 
 with driver.Metacluster() as metacluster:
-    cluster1 = driver.Cluster(metacluster)
+    cluster = driver.Cluster(metacluster)
     executable_path, command_prefix, serve_options = scenario_common.parse_mode_flags(opts)
-    print "Spinning up two processes..."
-    files1 = driver.Files(metacluster, log_path = "create-output-1",
-                          machine_name = "a", server_tags = ["a_tag"],
-                          executable_path = executable_path, command_prefix = command_prefix)
-    proc1 = driver.Process(cluster1, files1, log_path = "serve-output-1",
-        executable_path = executable_path, command_prefix = command_prefix, extra_options = serve_options)
-    files2 = driver.Files(metacluster, log_path = "create-output-2",
-                          machine_name = "b", server_tags = ["b_tag"],
-                          executable_path = executable_path, command_prefix = command_prefix)
-    proc2 = driver.Process(cluster1, files2, log_path = "serve-output-2",
-        executable_path = executable_path, command_prefix = command_prefix, extra_options = serve_options)
-    proc1.wait_until_started_up()
-    proc2.wait_until_started_up()
-    cluster1.check()
-    tag_table = {"default": ["a","b"], "a_tag": ["a"], "b_tag": ["b"]}
+
+    num_servers = 5
+    print "Spinning up %d processes..." % num_servers
+    files = [driver.Files(metacluster,
+                          log_path = "create-output-%d" % (i+1),
+                          machine_name = "server_%d" % (i+1),
+                          server_tags = ["tag_%d" % (i+1)],
+                          executable_path = executable_path,
+                          command_prefix = command_prefix)
+        for i in xrange(num_servers)]
+    procs = [driver.Process(cluster,
+                            files[i],
+                            log_path = "serve-output-%d" % (i+1),
+                            executable_path = executable_path,
+                            command_prefix = command_prefix,
+                            extra_options = serve_options)
+        for i in xrange(num_servers)]
+    for p in procs:
+        p.wait_until_started_up()
+    cluster.check()
+
+    tag_table = {"default": ["server_%d" % (i+1) for i in xrange(num_servers)]}
+    for i in xrange(num_servers):
+        tag_table["tag_%d" % (i+1)] = ["server_%d" % (i+1)]
 
     print "Creating a table..."
-    conn = r.connect("localhost", proc1.driver_port)
+    conn = r.connect("localhost", procs[0].driver_port)
     r.db_create("test").run(conn)
     r.table_create("foo").run(conn)
 
@@ -61,13 +70,12 @@ with driver.Metacluster() as metacluster:
                 servers_in_tag = [s for s in shard["replicas"] if s in tag_table[tag]]
                 assert len(servers_in_tag) == count
             assert len(shard["replicas"]) == sum(num_replicas.values())
-    test_reconfigure(1, {"default": 1}, "default")
-    test_reconfigure(2, {"default": 1}, "default")
-    test_reconfigure(1, {"default": 2}, "default")
-    test_reconfigure(2, {"default": 2}, "default")
-    test_reconfigure(1, {"a_tag": 1, "b_tag": 1}, "a_tag")
-    test_reconfigure(1, {"a_tag": 1, "b_tag": 1}, "b_tag")
-    test_reconfigure(1, {"a_tag": 1}, "a_tag")
+    for num_shards in [1, 2, num_servers-1, num_servers, num_servers+1, num_servers*2]:
+        for num_replicas in [1, 2, num_servers-1, num_servers]:
+            test_reconfigure(num_shards, {"default": num_replicas}, "default")
+    test_reconfigure(1, {"tag_1": 1, "tag_2": 1}, "tag_1")
+    test_reconfigure(1, {"tag_1": 1, "tag_2": 1}, "tag_2")
+    test_reconfigure(1, {"tag_1": 1}, "tag_1")
 
     # Test to make sure that `dry_run` is respected; the config should only be stored in
     # the semilattices if `dry_run` is `False`.
@@ -78,11 +86,11 @@ with driver.Metacluster() as metacluster:
         del row["uuid"]
         return row
     prev_config = get_config()
-    new_config = r.table("foo").reconfigure(1, {"b_tag": 1}, director_tag="b_tag",
+    new_config = r.table("foo").reconfigure(1, {"tag_2": 1}, director_tag="tag_2",
         dry_run=True).run(conn)
     assert prev_config != new_config
     assert get_config() == prev_config
-    new_config_2 = r.table("foo").reconfigure(1, {"b_tag": 1}, director_tag="b_tag",
+    new_config_2 = r.table("foo").reconfigure(1, {"tag_2": 1}, director_tag="tag_2",
         dry_run=False).run(conn)
     assert prev_config != new_config_2
     print "get_config()", get_config()
@@ -90,6 +98,6 @@ with driver.Metacluster() as metacluster:
     print "prev_config", prev_config
     assert get_config() == new_config_2
 
-    cluster1.check_and_stop()
+    cluster.check_and_stop()
 print "Done."
 
