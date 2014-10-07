c0873d4aea09469622e95b65e6385bcb0846764e
Tim Maxwell <tim@rethinkdb.com>
2014-10-07 13:37:55 -0700

    WIP on tim_raft_network (3)

--- src/clustering/generic/raft_network.hpp
+++ src/clustering/generic/raft_network.hpp
@@ -33,9 +33,12 @@ public:
     raft_networked_member_t(
         const raft_member_id_t &this_member_id,
         mailbox_manager_t *mailbox_manager,
-        clone_ptr_t<watchable_t<std::map<raft_member_id_t, 
+        watchable_map_t<raft_member_id_t, raft_business_card_t<state_t> > *peers,
         raft_storage_interface_t<state_t> *storage,
-        
+
+    
+
+private:
     /* The `send_*_rpc()`, `get_connected_members()`, and `write_persistent_state()`
     methods implement the `raft_network_and_storage_interface_t` interface. */
     bool send_request_vote_rpc(
@@ -45,18 +48,18 @@ public:
         raft_request_vote_reply_t *reply_out);
     bool send_install_snapshot_rpc(
         const raft_member_id_t &dest,
-        const raft_install_snapshot_rpc_t<table_raft_state_t, table_raft_change_t> &rpc,
+        const raft_install_snapshot_rpc_t<state_t> &rpc,
         signal_t *interruptor,
         raft_install_snapshot_reply_t *reply_out);
     bool send_append_entries_rpc(
         const raft_member_id_t &dest,
-        const raft_append_entries_rpc_t<table_raft_change_t> &rpc,
+        const raft_append_entries_rpc_t<state_t> &rpc,
         signal_t *interruptor,
         raft_append_entries_reply_t *reply_out);
     clone_ptr_t<watchable_t<std::set<raft_member_id_t> > >
         get_connected_members();
     void write_persistent_state(
-        const raft_persistent_state_t<table_raft_state_t, table_raft_change_t>
+        const raft_persistent_state_t<state_t>
             &persistent_state,
         signal_t *interruptor);
 
@@ -66,24 +69,24 @@ public:
         const mailbox_t<void(raft_request_vote_reply_t)>::address_t &reply_addr,
         auto_drainer_t::lock_t keepalive);
     void on_install_snapshot_rpc(
-        const raft_install_snapshot_rpc_t<table_raft_state_t, table_raft_change_t> &rpc,
+        const raft_install_snapshot_rpc_t<state_t> &rpc,
         const mailbox_t<void(raft_install_snapshot_reply_t)>::address_t &reply_addr,
         auto_drainer_t::lock_t keepalive);
     void on_append_entries_rpc(
-        const raft_append_entries_rpc_t<table_raft_change_t> &rpc,
+        const raft_append_entries_rpc_t<state_t> &rpc,
         const mailbox_t<void(raft_append_entries_reply_t)>::address_t &reply_addr,
         auto_drainer_t::lock_t keepalive);
 
     const machine_id_t machine_id;
     mailbox_manager_t *mailbox_manager;
 
-    raft_member_t<table_raft_state_t, table_raft_change_t> member;
+    raft_member_t<state_t> member;
 
     auto_drainer_t drainer;
 
-    table_raft_business_card_t::request_vote_mailbox_t request_vote_mailbox;
-    table_raft_business_card_t::install_snapshot_mailbox_t install_snapshot_mailbox;
-    table_raft_business_card_t::append_entries_mailbox_t append_entries_mailbox;
+    typename raft_business_card_t<state_t>::request_vote_mailbox_t request_vote_mailbox;
+    typename raft_business_card_t<state_t>::install_snapshot_mailbox_t install_snapshot_mailbox;
+    typename raft_business_card_t<state_t>::append_entries_mailbox_t append_entries_mailbox;
 };
 
 #endif   /* CLUSTERING_GENERIC_RAFT_NETWORK_HPP_ */
