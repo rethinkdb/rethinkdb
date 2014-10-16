7f38b6d3bead834311c9879853a6c3e7f098028e
Karl Kuehn <karl@rethinkdb.com>
2014-10-16 10:21:58 -0700

    merge of #2998 Python connection test failures

--- drivers/python/rethinkdb/net.py
+++ drivers/python/rethinkdb/net.py
@@ -151,20 +151,28 @@ class Connection(object):
             self.socket = socket.create_connection((self.host, self.port), self.timeout)
         except Exception as err:
             raise RqlDriverError("Could not connect to %s:%s. Error: %s" % (self.host, self.port, err))
-
-        self._sock_sendall(
-            struct.pack(
-                "<2L", p.VersionDummy.Version.V0_3, len(self.auth_key)
-            ) + self.auth_key +
-            struct.pack("<L", p.VersionDummy.Protocol.JSON))
-
-        # Read out the response from the server, which will be a null-terminated string
-        response = b""
-        while True:
-            char = self._sock_recv(1)
-            if char == b"\0":
-                break
-            response += char
+        
+        try:
+            # Send our initial handshake
+            
+            self._sock_sendall(
+                struct.pack("<2L", p.VersionDummy.Version.V0_3), len(self.auth_key)) +
+                self.auth_key +
+                struct.pack("<L", p.VersionDummy.Protocol.JSON)
+            )
+            
+            # Read out the response from the server, which will be a null-terminated string
+        
+            response = b""
+            while True:
+                char = self._sock_recv(1)
+                if char == b"\0":
+                    break
+                response += char
+        except socket.timeout:
+            raise RqlDriverError("Timed out during handshake with %s:%d." % (self.host, self.port))
+        except socket.error as err:
+            raise RqlDriverError("Error during handshake with %s:%d - %s" % (self.host, self.port, err))
 
         if response != b"SUCCESS":
             self.close(noreply_wait=False)
