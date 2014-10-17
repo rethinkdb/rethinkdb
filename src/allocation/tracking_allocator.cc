b87892feee38004890a06a4766d4a1f436721ea9
Graham Hughes <graham@rethinkdb.com>
2014-10-17 14:36:18 -0700

    Use weak pointer properly.

--- src/allocation/tracking_allocator.cc
+++ src/allocation/tracking_allocator.cc
@@ -5,19 +5,32 @@
 
 template <class T, class Allocator>
 T* tracking_allocator<T, Allocator>::allocate(size_t n) {
-    if (parent->debit(sizeof(T) * n))
-        return this->original.allocate(n);
-    else
-        rfail_toplevel(ql::base_exc_t::GENERIC, "Failed to allocate memory; exceeded allocation threshold");
+    if (auto p = parent.lock()) {
+        if (!(p->debit(sizeof(T) * n)))
+            rfail_toplevel(ql::base_exc_t::GENERIC,
+                           "Failed to allocate memory; exceeded allocation threshold");
+    }
+    return this->original.allocate(n);
+}
+
+template <class T, class Allocator>
+T* tracking_allocator<T, Allocator>::allocate(size_t n, const void *hint) {
+    if (auto p = parent.lock()) {
+        if (!(p->debit(sizeof(T) * n)))
+            rfail_toplevel(ql::base_exc_t::GENERIC,
+                           "Failed to allocate memory; exceeded allocation threshold");
+    }
+    return this->original.allocate(n, hint);
 }
 
 template <class T, class Allocator>
 void tracking_allocator<T, Allocator>::deallocate(T* region, size_t n) {
-    parent->credit(sizeof(T) * n);
+    if (auto p = parent.lock()) p->credit(sizeof(T) * n);
     this->original.deallocate(region, n);
 }
 
 template <class T, class Allocator>
-size_t tracking_allocator<T, Allocator>::max_size() {
-    return parent->left();
+size_t tracking_allocator<T, Allocator>::max_size() const {
+    if (auto p = parent.lock()) return p->left();
+    return std::numeric_limits<size_t>::max();
 }
--- src/allocation/tracking_allocator.hpp
+++ src/allocation/tracking_allocator.hpp
@@ -3,6 +3,7 @@
 
 #include <cstddef>
 #include <memory>
+#include <functional>
 
 class tracking_allocator_factory;
 
@@ -12,87 +13,130 @@ class tracking_allocator_factory;
 // `std::map` and `std::string`.  As a result, if we were to use `tracking_allocator`
 // and `unbounded_allocator` directly, the type of `datum_t` would need to be
 // parameterized by the allocator used, which would suck.
-template <class T, class Allocator=std::allocator<T>>
+template <class T, class Allocator=std::allocator<T> >
 class rethinkdb_allocator {
 public:
     typedef T value_type;
+    typedef std::allocator_traits<rethinkdb_allocator<T, Allocator> > traits;
 
-    rethinkdb_allocator(Allocator _original) : original(_original) {}
-    
+    explicit rethinkdb_allocator(Allocator _original) : original(_original) {}
+    virtual ~rethinkdb_allocator() {}
+
     virtual T* allocate(size_t n) = 0;
+    virtual T* allocate(size_t n, const void *hint) = 0;
     virtual void deallocate(T* region, size_t n) = 0;
 
-    virtual bool operator == (const rethinkdb_allocator<T, Allocator> &other) = 0;
-    bool operator != (const rethinkdb_allocator<T, Allocator> &other) { return !(*this == other); }
+    void dispose_of(T *ptr) {
+        traits::destroy(*this, ptr);
+        traits::deallocate(*this, ptr, 1);
+    }
 
-    virtual size_t max_size() = 0;
-protected:
+    virtual bool operator == (const rethinkdb_allocator<T, Allocator> &other) const = 0;
+    bool operator != (const rethinkdb_allocator<T, Allocator> &other) const {
+        return !(*this == other);
+    }
+
+    virtual size_t max_size() const = 0;
+
     enum class allocator_types {
         UNBOUNDED,
         TRACKING,
     };
-    virtual allocator_types type() = 0;
+    virtual allocator_types type() const = 0;
 
+    // function interface
+    void operator() (T *ptr) { deallocate(ptr, 1); }
+protected:
     Allocator original;
 };
 
 // Minimal definitions required to use `std::allocator_traits` and also keep track of
 // the amount of memory allocated.
-template <class T, class Allocator=std::allocator<T>>
+template <class T, class Allocator=std::allocator<T> >
 class tracking_allocator : public rethinkdb_allocator<T, Allocator> {
 public:
-    tracking_allocator(Allocator _original, tracking_allocator_factory *_parent) : rethinkdb_allocator<T, Allocator>(_original), parent(_parent) {}
-    tracking_allocator(const tracking_allocator<T, Allocator> &allocator) : rethinkdb_allocator<T, Allocator>(allocator), parent(allocator._parent) {}
-    tracking_allocator(tracking_allocator<T, Allocator> &&allocator) : rethinkdb_allocator<T, Allocator>(allocator), parent(allocator._parent) {}
-    
+    tracking_allocator(Allocator _original,
+                       std::shared_ptr<tracking_allocator_factory> _parent)
+        : rethinkdb_allocator<T, Allocator>(_original), parent(_parent) {}
+    tracking_allocator(Allocator _original,
+                       std::weak_ptr<tracking_allocator_factory> _parent)
+        : rethinkdb_allocator<T, Allocator>(_original), parent(_parent) {}
+    explicit tracking_allocator(const tracking_allocator<T, Allocator> &allocator)
+        : rethinkdb_allocator<T, Allocator>(allocator), parent(allocator._parent) {}
+    explicit tracking_allocator(tracking_allocator<T, Allocator> &&allocator)
+        : rethinkdb_allocator<T, Allocator>(allocator), parent(allocator._parent) {}
+    virtual ~tracking_allocator() {}
+
     virtual T* allocate(size_t n);
+    virtual T* allocate(size_t n, const void *hint);
     virtual void deallocate(T* region, size_t n);
 
-    bool operator == (const tracking_allocator<T, Allocator> &other) { return parent == other.parent; }
-    bool operator != (const tracking_allocator<T, Allocator> &other) { return parent != other.parent; }
-    virtual bool operator == (const rethinkdb_allocator<T, Allocator> &other)
+    bool operator == (const tracking_allocator<T, Allocator> &other) const {
+        return parent.lock() == other.parent.lock();
+    }
+    bool operator != (const tracking_allocator<T, Allocator> &other) const {
+        return parent.lock() != other.parent.lock();
+    }
+    virtual bool operator == (const rethinkdb_allocator<T, Allocator> &other) const
     {
         if (other.type() == rethinkdb_allocator<T, Allocator>::allocator_types::TRACKING)
-            return parent == static_cast<tracking_allocator<T, Allocator>>(other).parent;
+            return parent.lock() ==
+                static_cast<tracking_allocator<T, Allocator> &>
+                (const_cast<rethinkdb_allocator<T, Allocator> &>(other)).parent.lock();
         else
             return false;
     }
 
-    virtual size_t max_size();
-private:
-    tracking_allocator_factory *parent;
+    virtual size_t max_size() const;
+    virtual typename rethinkdb_allocator<T, Allocator>::allocator_types type() const {
+        return rethinkdb_allocator<T, Allocator>::allocator_types::TRACKING;
+    }
+
+    std::weak_ptr<tracking_allocator_factory> parent;
 };
 
-// Thin shell around the standard allocator.  We need this so the type of `datum_t` doesn't change.
-template <class T, class Allocator=std::allocator<T>>
+// Thin shell around the standard allocator.  We need this so the type
+// of `datum_t` doesn't change.
+template <class T, class Allocator=std::allocator<T> >
 class unbounded_allocator : public rethinkdb_allocator<T, Allocator> {
 public:
-    unbounded_allocator() {}
-    unbounded_allocator(const unbounded_allocator<T> &allocator) : rethinkdb_allocator<T, Allocator>(allocator) {}
-    unbounded_allocator(unbounded_allocator<T> &&allocator) : rethinkdb_allocator<T, Allocator>(allocator) {}
-    
-    virtual T* allocate(size_t n) { return this->original.allocate(n); }
-    virtual void deallocate(T* region, size_t n) { this->original.deallocate(region, n); }
+    unbounded_allocator(Allocator _original)
+        : rethinkdb_allocator<T, Allocator>(_original) {}
+    unbounded_allocator(const unbounded_allocator<T> &allocator)
+        : rethinkdb_allocator<T, Allocator>(allocator.original) {}
+    unbounded_allocator(unbounded_allocator<T> &&allocator)
+        : rethinkdb_allocator<T, Allocator>(allocator.original) {}
+    virtual ~unbounded_allocator() {}
 
-    bool operator == (const tracking_allocator<T, Allocator> &) { return true; }
-    bool operator != (const tracking_allocator<T, Allocator> &) { return false; }
-    virtual bool operator == (const rethinkdb_allocator<T, Allocator> &other)
+    virtual T* allocate(size_t n) {
+        return this->original.allocate(n);
+    }
+    virtual T* allocate(size_t n, const void *hint) {
+        return this->original.allocate(n, hint);
+    }
+    virtual void deallocate(T* region, size_t n) {
+        this->original.deallocate(region, n);
+    }
+
+    bool operator == (const tracking_allocator<T, Allocator> &) const { return true; }
+    bool operator != (const tracking_allocator<T, Allocator> &) const { return false; }
+    virtual bool operator == (const rethinkdb_allocator<T, Allocator> &other) const
     {
-        if (other.type() == rethinkdb_allocator<T, Allocator>::allocator_types::UNBOUNDED)
+        if (other.type()
+            == rethinkdb_allocator<T, Allocator>::allocator_types::UNBOUNDED)
             return true;
         else
             return false;
     }
-    virtual size_t max_size() { return std::numeric_limits<size_t>::max(); }
+    virtual size_t max_size() const { return std::numeric_limits<size_t>::max(); }
+    virtual typename rethinkdb_allocator<T, Allocator>::allocator_types type() const {
+        return rethinkdb_allocator<T, Allocator>::allocator_types::UNBOUNDED;
+    }
 };
 
 class tracking_allocator_factory {
 public:
     tracking_allocator_factory(size_t total_bytes) : available(total_bytes) {}
-    template <class T, class Allocator>
-    tracking_allocator<T, Allocator> allocator_for(Allocator original) {
-        return tracking_allocator<T, Allocator>(original, this);
-    }
 
     bool debit(size_t bytes) {
         if (available >= bytes) {
@@ -105,11 +149,23 @@ public:
     void credit(size_t bytes) {
         available += bytes;
     }
-    size_t left() {
+    size_t left() const {
         return available;
     }
 private:
     size_t available;
 };
+
+template <class T, class Allocator>
+tracking_allocator<T, Allocator>
+allocator_for(Allocator original, std::shared_ptr<tracking_allocator_factory> factory) {
+    return tracking_allocator<T, Allocator>(original, factory);
+}
+
+template <class T>
+tracking_allocator<T, std::allocator<T> >
+allocator(std::shared_ptr<tracking_allocator_factory> factory) {
+    return tracking_allocator<T, std::allocator<T>>(std::allocator<T>(), factory);
+}
 
 #endif /* TRACKING_ALLOCATOR_HPP_ */
