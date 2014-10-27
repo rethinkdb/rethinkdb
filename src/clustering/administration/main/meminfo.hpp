b6b785af09d18979a146fdb8b954d94112c337df
Sam Hughes <sam@rethinkdb.com>
2014-10-27 16:39:18 -0700

    Moved meminfo parsing code to meminfo.hpp/cc.

---
+++ src/clustering/administration/main/meminfo.hpp
@@ -0,0 +1,10 @@
+#ifndef CLUSTERING_ADMINISTRATION_MAIN_MEMINFO_HPP_
+#define CLUSTERING_ADMINISTRATION_MAIN_MEMINFO_HPP_
+
+#include <stdint.h>
+
+// Returns false if it can't get the available memory size.
+bool get_proc_meminfo_available_memory_size(uint64_t *mem_avail_out);
+
+#endif  // CLUSTERING_ADMINISTRATION_MAIN_MEMINFO_HPP_
+
