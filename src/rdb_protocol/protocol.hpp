2d7f18bc00c4ba523a98b459db94ed113e301a7a
Sam Hughes <sam@rethinkdb.com>
2014-10-28 16:35:30 -0700

    Adjusted read_t::use_snapshot comment.

--- src/rdb_protocol/protocol.hpp
+++ src/rdb_protocol/protocol.hpp
@@ -544,7 +544,8 @@ struct read_t {
         : read(std::forward<T>(_read)), profile(_profile) { }
 
 
-    // Only use snapshotting if we're doing a range get.
+    // We use snapshotting for queries that acquire-and-hold large portions of the
+    // table, so that they don't block writes.
     bool use_snapshot() const THROWS_NOTHING;
 
     // Returns true if this read should be sent to every replica.
