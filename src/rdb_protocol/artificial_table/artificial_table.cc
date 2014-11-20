713942299bd9032761be2a7b7e98a12713b4a9e5
Tim Maxwell <tim@rethinkdb.com>
2014-11-19 18:40:05 -0800

    Implement read_row_changes() on artificial_table_t.

--- src/rdb_protocol/artificial_table/artificial_table.cc
+++ src/rdb_protocol/artificial_table/artificial_table.cc
@@ -75,13 +75,17 @@ counted_t<ql::datum_stream_t> artificial_table_t::read_all(
 }
 
 counted_t<ql::datum_stream_t> artificial_table_t::read_row_changes(
-        UNUSED ql::env_t *env,
-        UNUSED ql::datum_t pval,
-        UNUSED const ql::protob_t<const Backtrace> &bt,
-        UNUSED const std::string &table_name) {
-    /* RSI(reql_admin): Artificial tables will eventually support change feeds. */
-    rfail_datum(ql::base_exc_t::GENERIC,
-        "Artificial tables don't support changefeeds.");
+        ql::env_t *env,
+        ql::datum_t pval,
+        const ql::protob_t<const Backtrace> &bt,
+        const std::string &table_name) {
+    return read_changes(
+        env,
+        ql::changefeed::keyspec_t::spec_t(
+            ql::changefeed::keyspec_t::point_t{
+                store_key_t(pval.print_primary())}),
+        bt,
+        table_name);
 }
 
 counted_t<ql::datum_stream_t> artificial_table_t::read_changes(
