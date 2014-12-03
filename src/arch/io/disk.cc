b695e33385232205f336b91d295e299014d95ed9
Daniel Mewes <daniel@rethinkdb.com>
2014-12-02 19:39:51 -0800

    Disable read-ahead on OS X as well.

--- src/arch/io/disk.cc
+++ src/arch/io/disk.cc
@@ -485,26 +485,29 @@ file_open_result_t open_file(const char *path, const int mode, io_backender_t *b
                                       0);
     } break;
     case file_direct_io_mode_t::buffered_desired: {
-#ifdef __linux__
-        // On linux we can often improve read performance by disabling read-ahead.
+        // We can typically improve read performance by disabling read-ahead.
         // Our access patterns are usually pretty random, and on startup we already
         // do read-ahead internally in our cache.
-        //
+        int disable_readahead_res = -1;
+#ifdef __linux__
         // From the man-page:
         //  Under Linux, POSIX_FADV_NORMAL sets the readahead window to the
         //  default size for the backing device; POSIX_FADV_SEQUENTIAL doubles
         //  this size, and POSIX_FADV_RANDOM disables file readahead entirely.
         //  These changes affect the entire file, not just the specified region
         //  (but other open file handles to the same file are unaffected)
-        const int fadvise_res = posix_fadvise(fd.get(), 0, 0, POSIX_FADV_RANDOM);
-        if (fadvise_res != 0) {
+        disable_readahead_res = posix_fadvise(fd.get(), 0, 0, POSIX_FADV_RANDOM);
+#elif defined(__APPLE__)
+        const int fcntl_res = fcntl(fd.get(), F_RDAHEAD, 0);
+        disable_readahead_res = fcntl_res == -1
+                                ? get_errno()
+                                : 0;
+#endif
+        if (disable_readahead_res != 0) {
             // Non-critical error. Just print a warning and keep going.
             logWRN("Failed to disable read-ahead on '%s' (errno %d). You might see "
-                   "decreased read performance.", path, fadvise_res);
+                   "decreased read performance.", path, disable_readahead_res);
         }
-#endif
-        // TODO: OS X doesn't have posix_fadvise(). Is there another way to
-        // disable read-ahead with buffered i/o on OS X?
 
         open_res = file_open_result_t(file_open_result_t::BUFFERED, 0);
     } break;
