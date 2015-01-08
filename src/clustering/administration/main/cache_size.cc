7e7422b49b7ce19cc6baee8edca70a3dac1f1e1c
Graham Hughes <graham@rethinkdb.com>
2015-01-07 16:48:31 -0800

    Derp, get conditional right.

--- src/clustering/administration/main/cache_size.cc
+++ src/clustering/administration/main/cache_size.cc
@@ -159,12 +159,12 @@ uint64_t get_avail_mem_size() {
     // up in 10.8, but is definitely not in 10.7.  Per availability.h,
     // we use a raw number rather than the corresponding #define.
 #if __MAC_OS_X_VERSION_MIN_REQUIRED < 1090
+    uint64_t ret = vmstat.free_count * page_size;
+#else
     // external_page_count is the number of pages that are file-backed (non-swap) --
     // see /usr/include/mach/vm_statistics.h, see also vm_stat.c, the implementation
     // of vm_stat, in Darwin.
     uint64_t ret = (vmstat.free_count + vmstat.external_page_count) * page_size;
-#else
-    uint64_t ret = vmstat.free_count * page_size;
 #endif // __MAC_OS_X_VERSION_MIN_REQUIRED < 1090
 #endif // __MAC_OS_X_VERSION_MIN_REQUIRED
     return ret;
