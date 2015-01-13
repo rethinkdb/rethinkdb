3f8bbd2a24d1580f5ca5d5c04fa650d1532fcbdd
Karl Kuehn <karl@rethinkdb.com>
2015-01-13 10:37:37 -0800

    minor cleanup on interface.log
    commit c83075e069cc27b8ebc8b6fef1cbd9684133f31c
    Author: Karl Kuehn <karl@rethinkdb.com>
    Date:   Mon Jan 12 17:29:20 2015 -0800
    
        minor cleanup on interface.log

--- test/interface/log.py
+++ test/interface/log.py
@@ -1,32 +1,38 @@
 #!/usr/bin/env python
-# Copyright 2010-2012 RethinkDB, all rights reserved.
-import sys, os, time, pprint, datetime
+# Copyright 2010-2015 RethinkDB, all rights reserved.
+
+from __future__ import print_function
+
+import datetime, pprint, os, sys, time
+
+startTime = time.time()
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, 'common')))
-import driver, http_admin, scenario_common, utils
-from vcoptparse import *
+import driver, scenario_common, utils, vcoptparse
+
+# --
+
+op = vcoptparse.OptParser()
+scenario_common.prepare_option_parser_mode_flags(op)
+_, command_prefix, serve_options = scenario_common.parse_mode_flags(op.parse(sys.argv))
+
 r = utils.import_python_driver()
 
-op = OptParser()
-scenario_common.prepare_option_parser_mode_flags(op)
-opts = op.parse(sys.argv)
+# --
 
-with driver.Metacluster() as metacluster:
-    cluster = driver.Cluster(metacluster)
-    _, command_prefix, serve_options = scenario_common.parse_mode_flags(opts)
-
-    print "Spinning up a process..."
-    files = driver.Files(metacluster, db_path="db", console_output="create-output",
-        server_name="the_server", command_prefix=command_prefix)
-    proc = driver.Process(cluster, files, console_output="serve-output", command_prefix=command_prefix, extra_options=serve_options)
-    proc.wait_until_started_up()
-    cluster.check()
-    conn = r.connect(proc.host, proc.driver_port)
-
-    print "Making a log entry..."
+print("Starting a server (%.2fs)" % (time.time() - startTime))
+with driver.Process(files="the_server", output_folder='.') as server:
+    
+    server.check()
+    conn = r.connect(host=server.host, port=server.driver_port)
+    
+    print("Making a log entry (%.2fs)" % (time.time() - startTime))
+    
     res = r.db("rethinkdb").table("server_config").update({"tags": ["xyz"]}).run(conn)
     assert res["errors"] == 0 and res["replaced"] == 1, res
-
-    print "Verifying the log entry..."
+    
+    print("Verifying the log entry (%.2fs)" % (time.time() - startTime))
+    
     log = list(r.db("rethinkdb").table("logs").order_by("timestamp").run(conn))
     assert len(log) > 5
 
@@ -39,12 +45,13 @@ with driver.Metacluster() as metacluster:
     now = datetime.datetime.now(entry["timestamp"].tzinfo)
     assert entry["timestamp"] <= now
     assert entry["timestamp"] >= now - datetime.timedelta(minutes = 1)
-
-    print "Verifying that point gets work..."
+    
+    print("Verifying that point gets work (%.2fs)" % (time.time() - startTime))
+    
     for e in log:
         # Point gets use a separate code path, so we want to test them specifically
         e2 = r.db("rethinkdb").table("logs").get(e["id"]).run(conn)
         assert e == e2, (e, e2)
 
-    cluster.check_and_stop()
-print "Done."
+    print("Cleaning up (%.2fs)" % (time.time() - startTime))
+print("Done (%.2fs)" % (time.time() - startTime))
