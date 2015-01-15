58e15f7aaf0f5826f3a7e2da3adc79b90f7d6def
Michael Lucy <mlucy@rethinkdb.com>
2015-01-14 17:05:48 -0800

    Fixed bug in tests.

--- drivers/ruby/cfeed_test.rb
+++ drivers/ruby/cfeed_test.rb
@@ -17,12 +17,15 @@ $c = r.connect(:host => 'localhost', :port => $port_offset + 28015,
                :db => 'test').repl.auto_reconnect
 # RSI: %032x
 # RSI: keys of different lengths
+# Make sure to keep the "\x01"s in there, they test some key unmangling logic.
 def frac2uuid frac
   val = ("%030x" % (0xffffffffffffffffffffffffffffffff * frac).to_i).chars
-  "00000000-0000-0000-0000-000000000000".chars.map{|c|
-    c == '-' ? c : val.shift
+  "00000000-00\x0100-00\x0200-0000-000000000000".chars.map{|c|
+    c != '0' ? c : val.shift
   }.join
 end
+# RSI: add truncated keys to `m`?
+# RSI: some pkeys should contain \1 and \2 to test unmangling logic.
 def pop(i, max)
   { 'id' => frac2uuid((i*1.0)/max),
     'a' => i,
@@ -89,16 +92,18 @@ class Emulator
     @t[field.to_s].size
   end
   def state
-    @t['id'].flat_map{|x| x[1]}
+    @t['id'].flat_map{|x| x[1]}.map{|x| x.to_a.sort}.sort
   end
   def in_state?(rows)
-    state.sort == rows.sort
+    state == rows
   end
-  def assert_state(rows)
+  def assert_state(raw_rows)
+    rows = raw_rows.map{|x| x.to_a.sort}.sort
+    $assert_state = [state, rows]
     raise "Incorrect state." if !in_state?(rows)
   end
   def sync(query)
-    rows = query.current_val
+    rows = query.current_val.map{|x| x.to_a.sort}.sort
     while !in_state?(rows)
       $synclog << [:sync, state.sort, rows.sort]
       change = query.wait_for_change
@@ -110,10 +115,16 @@ end
 
 class Query
   attr_reader :size
-  def initialize(pop_size, limit_size, query)
-    @pop_size = pop_size
+  def initialize(raw_pop_size, limit_size, query)
+    @raw_pop_size = raw_pop_size
+    if query.inspect.match('"m"')
+      @pop_size = @raw_pop_size * 4
+    else
+      @pop_size = @raw_pop_size
+    end
     @limit_size = limit_size
-    @init_size = [pop_size, limit_size].min
+
+    @init_size = [@pop_size, limit_size].min
 
     @query = query
     @changes = query.changes.run_safe.each
@@ -127,6 +138,7 @@ class Query
   def wait_for_change(tm=5)
     Timeout::timeout(tm) {
       change = @changes.next
+      $bad_change = change
       raise "Bad change." if change['new_val'] == change['old_val']
       return change
     }
@@ -143,7 +155,7 @@ $shards.each {|name, nshards|
     PP.pp [:limit_size, limit_size]
     pop_sizes = [0, limit_size - 1, limit_size, $max_pop_size].uniq
     pop_sizes.each_with_index {|pop_size, pop_size_index|
-      # next if pop_size_index < 1
+      next if pop_size_index < 1
       PP.pp [:pop_size, pop_size]
       begin
         sub_pop = (0...pop_size).map{|i| pop(i, pop_size)}
@@ -154,12 +166,15 @@ $shards.each {|name, nshards|
            $t.orderby(index: r.desc(field)).limit(limit_size)]
         }
         queries.each_with_index {|raw_query, query_index|
+          next if query_index < 9
+          # next if query_index < 1
           $rqlast = raw_query
           $qlast = q = Query.new(pop_size, limit_size, raw_query)
           em = Emulator.new(q.init)
           PP.pp [:query, q]
           $fields.each_with_index {|field, field_index|
-            # PP.pp [:field, field]
+            next if field_index < 5
+            PP.pp [:field, field]
             forward = $t.orderby(index: field)
             backward = $t.orderby(index: r.desc(field))
             [forward, backward].each {|dir|
@@ -168,18 +183,21 @@ $shards.each {|name, nshards|
               # PP.pp [:orig_state, orig_state]
               first = dir[0].default(nil).run_safe
               # PP.pp [:first, first]
+              PP.pp [:deleting, dir.limit(1).run_safe]
               dir.limit(1).delete.run_safe; em.sync(q)
-              # PP.pp [:synced, 1]
+              PP.pp [:inserting, first]
               if first; $t.insert(first).run_safe; em.sync(q); end
-              # PP.pp [:synced, 2]
+              PP.pp [:asserting]
               em.assert_state(orig_state)
+              PP.pp [:asserted]
             }
-            PP.pp [[pop_size_index, query_index, field_index],
-                   [pop_sizes.size, queries.size, $fields.size]]
+            PP.pp [[limit_size_index, pop_size_index, query_index, field_index],
+                   [$limit_sizes.size, pop_sizes.size, queries.size, $fields.size]]
           }
         }
-      ensure
         $t.delete.run_safe
+      ensure
+        # $t.delete.run_safe
       end
     }
   }
