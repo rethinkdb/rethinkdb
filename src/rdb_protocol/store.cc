9e6ea4ea496b46e8b126d43087547e6b6a6f5503
Graham Hughes <graham@rethinkdb.com>
2015-01-20 20:27:03 -0800

    Don't aggregate writes individually, sample them instead.

--- src/rdb_protocol/store.cc
+++ src/rdb_protocol/store.cc
@@ -693,6 +693,7 @@ struct rdb_write_visitor_t : public boost::static_visitor<void> {
                 &replacer,
                 &sindex_cb,
                 ql_env.limits(),
+                sampler,
                 trace);
     }
 
@@ -714,10 +715,12 @@ struct rdb_write_visitor_t : public boost::static_visitor<void> {
                 &replacer,
                 &sindex_cb,
                 bi.limits,
+                sampler,
                 trace);
     }
 
     void operator()(const point_write_t &w) {
+        sampler->new_sample();
         response->response = point_write_response_t();
         point_write_response_t *res =
             boost::get<point_write_response_t>(&response->response);
@@ -731,6 +734,7 @@ struct rdb_write_visitor_t : public boost::static_visitor<void> {
     }
 
     void operator()(const point_delete_t &d) {
+        sampler->new_sample();
         response->response = point_delete_response_t();
         point_delete_response_t *res =
             boost::get<point_delete_response_t>(&response->response);
@@ -744,6 +748,7 @@ struct rdb_write_visitor_t : public boost::static_visitor<void> {
     }
 
     void operator()(const sindex_create_t &c) {
+        sampler->new_sample();
         sindex_create_response_t res;
 
         write_message_t wm;
@@ -773,6 +778,7 @@ struct rdb_write_visitor_t : public boost::static_visitor<void> {
     }
 
     void operator()(const sindex_drop_t &d) {
+        sampler->new_sample();
         sindex_drop_response_t res;
         res.success = store->drop_sindex(sindex_name_t(d.id), &sindex_block);
         response->response = res;
@@ -788,6 +794,7 @@ struct rdb_write_visitor_t : public boost::static_visitor<void> {
 
         bool old_name_found = false;
         for (auto it = sindexes.begin(); it != sindexes.end(); ++it) {
+            sampler->new_sample();
             if (it->first == old_name) {
                 guarantee(!it->first.being_deleted);
                 old_name_found = true;
@@ -812,6 +819,7 @@ struct rdb_write_visitor_t : public boost::static_visitor<void> {
     }
 
     void operator()(const sync_t &) {
+        sampler->new_sample();
         response->response = sync_response_t();
 
         // We know this sync_t operation will force all preceding write transactions
@@ -831,6 +839,7 @@ struct rdb_write_visitor_t : public boost::static_visitor<void> {
                         scoped_ptr_t<real_superblock_t> *_superblock,
                         repli_timestamp_t _timestamp,
                         rdb_context_t *_ctx,
+                        profile::sampler_t *_sampler,
                         profile::trace_t *_trace,
                         write_response_t *_response,
                         signal_t *_interruptor) :
@@ -842,6 +851,7 @@ struct rdb_write_visitor_t : public boost::static_visitor<void> {
         interruptor(_interruptor),
         superblock(_superblock),
         timestamp(_timestamp),
+        sampler(_sampler),
         trace(_trace),
         sindex_block((*superblock)->expose_buf(),
                      (*superblock)->get_sindex_block_id(),
@@ -866,6 +876,7 @@ private:
     signal_t *const interruptor;
     scoped_ptr_t<real_superblock_t> *const superblock;
     const repli_timestamp_t timestamp;
+    profile::sampler_t *const sampler;
     profile::trace_t *const trace;
     buf_lock_t sindex_block;
     profile::event_log_t event_log_out;
@@ -881,13 +892,14 @@ void store_t::protocol_write(const write_t &write,
     scoped_ptr_t<profile::trace_t> trace = ql::maybe_make_profile_trace(write.profile);
 
     {
-        profile::starter_t start_write("Perform write on shard.", trace);
+        profile::sampler_t start_write("Perform write on shard.", trace);
         rdb_write_visitor_t v(btree.get(),
                               this,
                               (*superblock)->expose_buf().txn(),
                               superblock,
                               timestamp.to_repli_timestamp(),
                               ctx,
+                              &start_write,
                               trace.get_or_null(),
                               response,
                               interruptor);
