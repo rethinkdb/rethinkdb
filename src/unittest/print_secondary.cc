92f04bfb5f6a24f986cb6b6c1af83a8e55c677a7
Michael Lucy <mlucy@rethinkdb.com>
2015-01-22 17:00:32 -0800

    Unit test fixes.

--- src/unittest/print_secondary.cc
+++ src/unittest/print_secondary.cc
@@ -11,10 +11,20 @@ void test_mangle(const std::string &pkey, const std::string &skey, boost::option
     auto versions = {
         reql_version_t::v1_13, reql_version_t::v1_14, reql_version_t::v1_16_is_latest};
     for (reql_version_t rv : versions) {
+        ql::skey_version_t skey_version = ql::skey_version_from_reql_version(rv);
         std::string mangled = ql::datum_t::mangle_secondary(
-            ql::skey_version_from_reql_version(rv), skey, pkey, tag_string);
+            skey_version, skey, pkey, tag_string);
         ASSERT_EQ(pkey, ql::datum_t::extract_primary(mangled));
-        ASSERT_EQ(skey, ql::datum_t::extract_secondary(mangled));
+        std::string skey2 = skey;
+        guarantee(!(skey2[0] & 0x80)); // None of our types have the top bit set.
+        switch (skey_version) {
+        case ql::skey_version_t::pre_1_16: break;
+        case ql::skey_version_t::post_1_16:
+            skey2[0] |= 0x80; // Flip the top bit to indicate 1.16+ skey_version.
+            break;
+        default: unreachable();
+        }
+        ASSERT_EQ(skey2, ql::datum_t::extract_secondary(mangled));
         boost::optional<uint64_t> extracted_tag = ql::datum_t::extract_tag(mangled);
         ASSERT_EQ(static_cast<bool>(tag), static_cast<bool>(extracted_tag));
         if (tag) {
--- src/unittest/unittest_utils.cc
+++ src/unittest/unittest_utils.cc
@@ -40,10 +40,7 @@ struct make_sindex_read_t {
                 ql::batchspec_t::default_for(ql::batch_type_t::NORMAL),
                 std::vector<ql::transform_variant_t>(),
                 boost::optional<ql::terminal_variant_t>(),
-                sindex_rangespec_t(
-                    id,
-                    region_t(rng.to_sindex_keyrange(ql::skey_version_t::post_1_16)),
-                    rng),
+                sindex_rangespec_t(id, boost::none, rng),
                 sorting_t::UNORDERED),
             profile_bool_t::PROFILE);
     }
