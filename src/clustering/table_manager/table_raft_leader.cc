81f5302145063234bfe9fd504eacbd75c26c437f
Tim Maxwell <tim@rethinkdb.com>
2015-01-28 17:56:17 -0800

    Improve names and comments

--- src/clustering/table_manager/table_raft_leader.cc
+++ src/clustering/table_manager/table_raft_leader.cc
@@ -1,47 +1,65 @@
 // Copyright 2010-2015 RethinkDB, all rights reserved.
 #include "clustering/table_manager/table_raft_leader.hpp"
 
-table_l2f_msg_t table_raft_leader_t::calculate_l2f(
+namespace table_raft {
+
+/* `calculate_contract()` calculates a new contract for a region. Whenever any of the
+inputs changes, the leader will call `update_contract()` to compute a contract for each
+range of keys. The new contract will often be the same as the old, in which case it
+doesn't get a new contract ID. */
+contract_t calculate_contract(
+        /* The region that we're computing a contract for. This region will never contain
+        any split points. */
         const region_t &region,
-        const table_l2f_msg_t &old_l2f,
+        /* The old contract that contains this region. */
+        const contract_t &old_c,
+        /* The user-specified configuration for the shard containing this region. */
         const table_config_t::shard_t &config,
-        const std::map<server_id_t, table_f2l_msg_t::state_t> &f2l_states,
-        const std::map<server_id_t, version_t> &f2l_versions,
+        /* Contract acks from replicas regarding `old_c`. If a replica hasn't sent us an
+        ack *specifically* for `old_c`, it won't appear in this map. */
+        const std::map<server_id_t, contract_ack_t::state_t> &ack_states,
+        const std::map<server_id_t, version_t> &ack_versions,
+        /* Creates a new branch, starting from the given version, to be hosted on the
+        given server. `update_contract()` should call this at most once. If several calls
+        to `update_contract()` in the same batch all call `branch_maker()` for contiguous
+        regions, it will combine all the calls into the same branch. */
         const std::function<branch_id_t(server_id_t, version_t)> &branch_maker) {
 
-    /* If there are new servers in `config.replicas`, add them to `l2f.replicas` */
+    contract_t new_c = old_c;
+
+    /* If there are new servers in `config.replicas`, add them to `c.replicas` */
     for (const server_id_t &server : config.replicas) {
-        if (new_l2f.replicas.count(server) == 0) {
-            new_l2f.replicas.insert(server);
+        if (new_c.replicas.count(server) == 0) {
+            new_c.replicas.insert(server);
         }
     }
 
-    /* If there is a mismatch between `config.replicas` and `l2f.voters`, then correct
+    /* If there is a mismatch between `config.replicas` and `c.voters`, then correct
     it */
-    if (!static_cast<bool>(old_l2f.temp_voters) && old_l2f.voters != config.replicas) {
-        /* We don't want to initiate the change until a majority of the new replicas are
-        already streaming, or else we'll lose write availability as soon as we set
-        `temp_voters`. */
-        /* RSI(raft): Maybe don't initiate depending on the state of the primary */
+    if (!static_cast<bool>(old_c.temp_voters) && old_c.voters != config.replicas) {
         size_t num_streaming = 0;
         for (const server_id_t &server : config.replicas) {
-            auto it = f2l_states.find(server);
-            if (it != f2l_states.end() &&
-                    (it->second == table_f2l_msg_t::state_t::secondary_streaming ||
-                    (static_cast<bool>(old_l2f.primary) &&
-                        old_l2f.primary->server == server))) {
+            auto it = ack_states.find(server);
+            if (it != ack_states.end() &&
+                    (it->second == contract_ack_t::secondary_streaming ||
+                    (static_cast<bool>(old_c.primary) &&
+                        old_c.primary->server == server))) {
                 ++num_streaming;
             }
         }
+
+        /* We don't want to initiate the change until a majority of the new replicas are
+        already streaming, or else we'll lose write availability as soon as we set
+        `temp_voters`. */
         if (num_streaming > config.replicas.size() / 2) {
             /* OK, we're ready to go */
-            new_l2f.temp_voters = boost::make_optional(config.replicas);
+            new_c.temp_voters = boost::make_optional(config.replicas);
         }
     }
 
     /* If we already initiated a voter change by setting `temp_voters`, it might be time
     to commit that change by setting `voters` to `temp_voters`. */
-    if (static_cast<bool>(old_l2f.temp_voters)) {
+    if (static_cast<bool>(old_c.temp_voters)) {
         /* Before we change `voters`, we have to make sure that we'll preserve the
         invariant that every acked write is on a majority of `voters`. This is mostly the
         job of the primary; it will not report `primary_running` unless it is requiring
@@ -49,33 +67,29 @@ table_l2f_msg_t table_raft_leader_t::calculate_l2f(
         the client, *and* it has ensured that every write that was acked before that
         policy was implemented has been backfilled to a majority of `temp_voters`. So we
         can't switch voters unless the primary reports `primary_running`. */
-        if (static_cast<bool>(old_l2f.primary) &&
-                f2l_states.count(old_l2f.primary->server) == 1 &&
-                f2l_states.at(old_l2f.primary->server) ==
-                    table_f2l_msg_t::state_t::primary_running) {
+        if (static_cast<bool>(old_c.primary) &&
+                ack_states.count(old_c.primary->server) == 1 &&
+                ack_states.at(old_c.primary->server) == contract_ack_t::primary_ready) {
             /* OK, it's safe to commit. */
-            new_l2f.voters = *new_l2f.temp_voters;
-            new_l2f.temp_voters = boost::none;
+            new_c.voters = *new_c.temp_voters;
+            new_c.temp_voters = boost::none;
         }
-
-        /* RSI(raft): Maybe abort and unset `temp_voters` if there's a problem */
     }
 
-    /* If a server was removed from `config.replicas` and `l2f.voters` but it's still in
-    `l2f.replicas`, and it's not primary, then remove it. (If it is primary, it won't be
+    /* If a server was removed from `config.replicas` and `c.voters` but it's still in
+    `c.replicas`, and it's not primary, then remove it. (If it is primary, it won't be
     for long, because we'll detect this case and switch to another primary.) */
     bool should_warm_shutdown_primary = false;
-    for (const server_id_t &server : old_l2f.replicas) {
+    for (const server_id_t &server : old_c.replicas) {
         if (config.replicas.count(server) == 0 &&
-                old_l2f.voters.count(server) == 0 &&
-                (!static_cast<bool>(old_l2f.temp_voters) ||
-                    old_l2f.temp_voters->count(server) == 0)) {
-            if (static_cast<bool>(old_l2f.primary) &&
-                    old_l2f.primary->server == server) {
+                old_c.voters.count(server) == 0 &&
+                (!static_cast<bool>(old_c.temp_voters) ||
+                    old_c.temp_voters->count(server) == 0)) {
+            if (static_cast<bool>(old_c.primary) && old_c.primary->server == server) {
                 /* We'll process this case further down */
                 should_warm_shutdown_primary = true;
             } else {
-                new_l2f.replicas.erase(server);
+                new_c.replicas.erase(server);
             }
         }
     }
@@ -83,10 +97,10 @@ table_l2f_msg_t table_raft_leader_t::calculate_l2f(
     /* If we don't have a primary, choose a primary. Servers are not eligible to be a
     primary unless they are carrying every acked write. In addition, we must choose
     `config.primary_replica` if it is eligible. There will be at least one eligible
-    server if and only if we have reports from a majority of `new_l2f.voters`. */
-    if (!static_cast<bool>(old_l2f.primary)) {
+    server if and only if we have reports from a majority of `new_c.voters`. */
+    if (!static_cast<bool>(old_c.primary)) {
         /* We have an invariant that every acked write must be on the path from the root
-        of the branch history to `old_l2f.branch`. So we project each voter's state onto
+        of the branch history to `old_c.branch`. So we project each voter's state onto
         that path, then sort them by position along the path. Any voter that is at least
         as up to date, according to that metric, as more than half of the voters
         (including itself) is eligible. */
@@ -95,11 +109,11 @@ table_l2f_msg_t table_raft_leader_t::calculate_l2f(
         server ID as a secondary sorting key. This mean we tend to pick the same server
         if we run the algorithm twice; this helps to reduce unnecessary fragmentation. */
         std::vector<std::pair<state_timestamp_t, server_id_t> > replica_states;
-        for (const server_id_t &server : new_l2f.voters) {
-            if (f2l_states.count(server) == 1 && f2l_states.at(server) ==
-                    table_f2l_msg_t::state_t::secondary_need_primary) {
+        for (const server_id_t &server : new_c.voters) {
+            if (ack_states.count(server) == 1 && ack_states.at(server) ==
+                    contract_ack_t::secondary_need_primary) {
                 state_timestamp_t timestamp = version_project_to_branch(
-                    branch_history, f2l_versions.at(server), old_l2f.branch, region);
+                    branch_history, ack_versions.at(server), old_c.branch, region);
                 replica_states.insert(std::make_pair(timestamp, server));
             }
         }
@@ -109,7 +123,7 @@ table_l2f_msg_t table_raft_leader_t::calculate_l2f(
         `new_primary` to `config.primary_replica` if eligible, otherwise the most
         up-to-date other server if there is one, otherwise nothing. */
         boost::optional<server_id_t> new_primary;
-        for (size_t i = new_l2f.voters.size() / 2; i < replica_states.size(); ++i) {
+        for (size_t i = new_c.voters.size() / 2; i < replica_states.size(); ++i) {
             new_primary = replica_states[i].second;
             if (replica_states[i].second == config.primary_replica) {
                 break;
@@ -118,19 +132,20 @@ table_l2f_msg_t table_raft_leader_t::calculate_l2f(
 
         /* RSI(raft): If `config.primary_replica` isn't connected or isn't ready, we
         should elect a different primary. If `config.primary_replica` is connected but
-        just takes a little bit longer to reply to our L2Fs than the other replicas, we
-        should wait for it to reply to our L2Fs and then elect it. Under the current
-        implementation, we don't wait. This could lead to an awkward loop, where we elect
-        the wrong primary, then un-elect it because we realize `config.primary_replica`
-        is ready, and then re-elect the wrong primary instead of electing
-        `config.primary_replica`. */
+        just takes a little bit longer to reply to our contracts than the other replicas,
+        we should wait for it to reply to our contracts and then elect it. Under the
+        current implementation, we don't wait. This could lead to an awkward loop, where
+        we elect the wrong primary, then un-elect it because we realize
+        `config.primary_replica` is ready, and then re-elect the wrong primary instead of
+        electing `config.primary_replica`. */
 
         if (static_cast<bool>(new_primary)) {
-            table_l2f_msg_t::primary_t p;
+            contract_t::primary_t p;
             p.server = *new_primary;
             p.warm_shutdown = false;
-            new_l2f.primary = boost::make_optional(p);
-            new_l2f.branch = branch_maker(*new_primary, f2l_versions.at(*new_primary));
+            p.warm_shutdown_for = nil_uuid();
+            new_c.primary = boost::make_optional(p);
+            new_c.branch = branch_maker(*new_primary, ack_versions.at(*new_primary));
         }
     }
 
@@ -141,58 +156,75 @@ table_l2f_msg_t table_raft_leader_t::calculate_l2f(
         is ready to take over the role
     - `config.primary_replica` isn't ready to take over the role, but the existing
         primary isn't even supposed to be a replica anymore.
-    In the first situation, we'll simply remove `l2f.primary`. In the second and third
-    situations, we'll first set `l2f.primary->warm_shutdown` to `true`, and then only
-    once the primary acknowledges that, we'll remove `l2f.primary`. Either way, once the
-    replicas acknowledge the L2F in which we removed `l2f.primary`, the logic earlier in
-    this function will select a new primary. Note that we can't go straight from the old
-    primary to the new one; we need a majority of replicas to promise to stop receiving
-    updates from the old primary before it's safe to elect a new one. */
-    if (static_cast<bool>(old_l2f.primary)) {
+    In the first situation, we'll simply remove `c.primary`. In the second and third
+    situations, we'll first set `c.primary->warm_shutdown` to `true`, and then only
+    once the primary acknowledges that, we'll remove `c.primary`. Either way, once the
+    replicas acknowledge the contract in which we removed `c.primary`, the logic earlier
+    in this function will select a new primary. Note that we can't go straight from the
+    old primary to the new one; we need a majority of replicas to promise to stop
+    receiving updates from the old primary before it's safe to elect a new one. */
+    if (static_cast<bool>(old_c.primary)) {
         /* Check if we need to do an auto-failover. The precise form of this condition
         isn't important for correctness. If we do an auto-failover when the primary isn't
         actually dead, or don't do an auto-failover when the primary is actually dead,
         the worst that will happen is we'll lose availability. */
         size_t voters_cant_reach_primary = 0;
-        for (const server_id_t &server : new_l2f.voters) {
-            if (f2l_states.count(server) == 1 && f2l_states.at(server) ==
-                    table_f2l_msg_t::state_t::secondary_need_primary) {
+        for (const server_id_t &server : new_c.voters) {
+            if (ack_states.count(server) == 1 && ack_states.at(server) ==
+                    contract_ack::secondary_need_primary) {
                 ++voters_cant_reach_primary;
             }
         }
-        bool should_kill_primary = voters_cant_reach_primary > new_l2f.voters.size() / 2;
+        bool should_kill_primary = voters_cant_reach_primary > new_c.voters.size() / 2;
 
         /* If the current primary isn't supposed to be a replica anymore, we'll already
         have set `should_warm_shutdown_primary` to `true` earlier. Now we also check for
         the case where it's supposed to be a replica but another replica is supposed to
         be primary. */
-        if (old_l2f.primary->server != config.primary_replica &&
-                f2l_states.count(config.primary_replica) == 1 &&
-                f2l_states.at(config.primary_replica) ==
-                    table_f2l_msg_t::state_t::secondary_streaming) {
+        server_id_t warm_shutdown_for = nil_uuid();
+        if (old_c.primary->server != config.primary_replica &&
+                ack_states.count(config.primary_replica) == 1 &&
+                ack_states.at(config.primary_replica) ==
+                    contract_ack_t::secondary_streaming) {
             should_warm_shutdown_primary = true;
+            warm_shutdown_for = config.primary_replica;
         }
 
         if (should_kill_primary) {
-            new_l2f.primary = boost::none;
+            new_c.primary = boost::none;
         } else if (should_warm_shutdown_primary) {
-            /* If the primary already finished the warm shutdown, then kill it.
-            Otherwise, start the warm shutdown. */
-            if (f2l_states.count(old_l2f.primary->server) == 1 &&
-                    f2l_states.at(old_l2f.primary->server) ==
-                        table_f2l_msg_t::state_t::primary_did_warm_shutdown) {
-                new_l2f.primary = boost::none;
+            /* If the primary already finished the warm shutdown, for the desired server,
+            then kill it. Otherwise, start the warm shutdown. */
+            if (old_c.primary->warm_shutdown == true &&
+                    old_c.primary->warm_shutdown_for = warm_shutdown_for &&
+                    ack_states.count(old_c.primary->server) == 1 &&
+                    ack_states.at(old_c.primary->server) ==
+                        contract_ack_t::state_t::primary_ready) {
+                new_c.primary = boost::none;
             } else {
-                new_l2f.primary->warm_shutdown = true;
+                new_c.primary->warm_shutdown = true;
+                new_c.primary->warm_shutdown_for = warm_shutdown_for;
             }
         } else {
             /* Usually this will be a no-op. However, it's possible that we'll start a
             warm shutdown but then the config will change before we finish, and this
             aborts the warm shutdown. */
-            new_l2f.primary->warm_shutdown = false;
+            new_c.primary->warm_shutdown = false;
+            new_c.primary->warm_shutdown_for = nil_uuid();
+
+            /* We might need to issue the primary a new branch ID */
+            if (ack_states.count(old_c.primary->server) == 1 &&
+                    ack_states.at(old_c.primary->server) ==
+                        contract_ack_t::primary_need_new_branch) {
+                new_c.branch = branch_maker(
+                    old_c.primary->server,
+                    ack_versions.at(old_c.primary->server));
+            }
         }
     }
 
-    return new_l2f;
+    return new_c;
 }
+
+} /* namespace table_raft */
 
--- src/clustering/table_manager/table_raft_state.hpp
+++ src/clustering/table_manager/table_raft_state.hpp
@@ -5,7 +5,19 @@
 #include "clustering/administration/tables/table_metadata.hpp"
 #include "clustering/generic/raft_core.hpp"
 
-typedef uuid_u table_msg_id_t;
+namespace table_raft {
+
+/* `table_raft::state_t` is the type of the state that is stored in each table's Raft
+instance. The most important part is a collection of `contract_t`s, which describe the
+current state of the different shards. The table's replicas watch the `contract_t`s and
+perform backfills, accept queries, etc. in response to what the `contract_t`s say. In
+addition, they send `contract_ack_t`s and some other metadata back to the table's Raft
+leader, which updates the `contract_t`s as necessary to perform auto-failover, implement
+the user's configuration changes, and so on. */
+
+class state_t;
+class contract_t;
+enum class contract_ack_t;
 
 /* We provide the following consistency guarantee: Suppose the client sends a write W
 which affects some region R of the table, and the cluster acknowledges that W has been
@@ -14,26 +26,33 @@ happens, unless the administrator issues a manual override. (This assumes that t
 and writes are run with the highest level of consistency guarantees.)
 
 To provide this guarantee, we maintain a bunch of invariants. Let V be the `version_t`
-of the shard after W has been performed. Let `l2f` be the `table_l2f_msg_t` in the Raft
-state for some region R' which overlaps R. Then the following are always true unless the
-user issues a manual override:
-1. A majority of `l2f.voters` have versions on disk which descend from V (or are V) in
-    in the intersection of R and R'.
-2. If `l2f.primary` is present, then `l2f.primary->server` has a version on disk which
+of the shard after W has been performed. Let `c` be the `contract_t` in the Raft state
+for some region R' which overlaps R. Then the following are always true unless the user
+issues a manual override:
+1. A majority of `c.voters` have versions on disk which descend from V (or are V) in the
+    intersection of R and R'.
+2. If `c.primary` is present, then `c.primary->server` has a version on disk which
     descends from V (or is V) in the intersection of R and R'.
-3. The latest version on `l2f.branch` is descended from V (or is V) in the intersection
-    of R and R'.
+3. The latest version on `c.branch` is descended from V (or is V) in the intersection of
+    R and R'.
 */
 
-class table_l2f_msg_t {
+class contract_t {
 public:
     class primary_t {
     public:
         server_id_t server;
         bool warm_shutdown;
+        server_id_t warm_shutdown_for;
     };
     void sanity_check() const {
-        guarantee(!static_cast<bool>(primary) || replicas.count(primary->server) == 1);
+        if (static_cast<bool>(primary)) {
+            guarantee(replicas.count(primary->server) == 1);
+            guarantee(warm_shutdown || warm_shutdown_for.is_nil());
+            if (!warm_shutdown_for.is_nil()) {
+                guarantee(replicas.count(warm_shutdown_for) == 1);
+            }
+        }
         for (const server_id_t &s : voters) {
             guarantee(replicas.count(s) == 1);
         }
@@ -46,23 +65,57 @@ public:
     branch_id_t branch;
 };
 
-class table_f2l_msg_t {
-public:
-    enum class state_t {
-        primary_running,
-        primary_switching_voters,
-        primary_did_warm_shutdown,
-        secondary_need_primary,
-        secondary_backfilling,
-        secondary_streaming,
-        nothing
-    } state;
+/* Each replica looks at what each `contract_t` says about its server ID, and reacts
+according to the following rules:
 
-    /* non-empty for `primary_did_warm_shutdown` and `secondary_need_primary` */
-    boost::optional<region_map_t<version_t> > version;
+If `server_id == contract.primary->server`:
+- Serve backfills to servers that request them
+- If we restarted since `contract.branch` was created, don't handle any queries, and ack
+    `primary_need_new_branch`. This is because another replica might have more up-to-date
+    data on `contract.branch` than we do, so it's not safe to continue adding writes to
+    `contract.branch`.
+- If `contract.primary->warm_shutdown`, then don't handle any queries. If
+    `contract.primary->hand_over_to` is up to date, or it's nil and any member of
+    `voters` is up to date, then ack `primary_ready`. Otherwise, ack
+    `primary_in_progress`.
+- Otherwise, do handle queries. Ack writes to the client when a majority of `voters` and
+    `temp_voters` (if present) have them on disk. If every write that happened before the
+    last time the contract changed has been replicated to a majority of `voters` and
+    `temp_voters` (if present), then ack `primary_ready`. Otherwise, ack
+    `primary_in_progress`.
+
+Otherwise, if `server_id` is in `contract.replicas`:
+- If `contract.primary` is empty or we can't contact `contract.primary->server`, then ack
+    `secondary_need_primary`.
+- Otherwise, backfill data and then stream writes from `contract.primary->server`. While
+    backfilling, ack `secondary_backfilling`; when done backfilling, ack
+    `secondary_streaming`.
+
+If `server_id` is not in `contract.replicas`:
+- Delete all data and ack `nothing`.
+
+If the replica acks `primary_need_new_branch` or `secondary_need_primary`, it sends with
+the ack a `region_map_t<version_t>` describing the data it has on disk, as well as branch
+history for the `version_t`s in the map. */
+
+enum class contract_ack_t {
+    primary_need_new_branch,
+    primary_in_progress,
+    primary_ready,
+    secondary_need_primary,
+    secondary_backfilling,
+    secondary_streaming,
+    nothing
 };
 
-class table_raft_state_t {
+/* Each contract is tagged with a `contract_id_t`. If the contract changes in any way, it
+gets a new ID. All the `contract_ack_t`s are tagged with the contract ID that they are
+responding to. This way, the leader knows exactly which contract the follower is acking.
+*/
+
+typedef uuid_u contract_id_t;
+
+class state_t {
 public:
     class change_t {
     public:
@@ -71,17 +124,17 @@ public:
             table_config_and_shards_t new_config;
         };
 
-        class new_leader_msg_t {
+        class new_contracts_t {
         public:
-            key_range_t range;
-            table_raft_leader_msg_t msg;
+            std::set<contract_id_t> to_remove;
+            std::map<constract_id_t, std::pair<key_range_t, contract_t> > to_add;
         };
 
         change_t() { }
         template<class T>
         change_t(T &&t) : v(t) { }
 
-        boost::variant<set_table_config_t, new_instructions_t> v;
+        boost::variant<set_table_config_t, new_contracts_t> v;
     };
 
     void apply_change(const change_t &c);
@@ -90,13 +143,15 @@ public:
     }
 
     table_config_and_shards_t config;
-    std::map<table_msg_id_t, std::pair<key_range_t, table_l2f_msg_t> > leader_msgs;
+    std::map<contract_id_t, std::pair<key_range_t, contract_t> > contracts;
     std::map<server_id_t, raft_member_id_t> member_ids;
 };
 
 RDB_DECLARE_SERIALIZABLE(table_raft_state_t::change_t::set_table_config_t);
 RDB_DECLARE_SERIALIZABLE(table_raft_state_t::change_t);
 RDB_DECLARE_SERIALIZABLE(table_raft_state_t);
+
+} /* namespace table_raft */
 
 #endif /* CLUSTERING_TABLE_MANAGER_TABLE_RAFT_STATE_HPP_ */
 
