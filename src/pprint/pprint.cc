98c78d2f0a8fac03524d2eff54513c65b17dc37b
Graham Hughes <graham@sigwinch.org>
2015-02-07 05:52:04 -0800

    Make it compile

--- src/pprint/pprint.cc
+++ src/pprint/pprint.cc
@@ -1,16 +1,46 @@
 #include "pprint/pprint.hpp"
 
+#include <vector>
+#include <list>
+#include <functional>
+#include <memory>
+
 namespace pprint {
+
+const doc_handle_t empty = std::make_shared<text_t>("");
+const doc_handle_t br = std::make_shared<cond_t>(" ", "");
+const doc_handle_t dot = std::make_shared<cond_t>(".", ".");
+
+struct text_element_t;
+struct cond_element_t;
+struct nbeg_element_t;
+struct nend_element_t;
+struct gbeg_element_t;
+struct gend_element_t;
+
+class stream_element_visitor_t {
+public:
+    virtual ~stream_element_visitor_t() {}
+
+    virtual void operator()(text_element_t &) = 0;
+    virtual void operator()(cond_element_t &) = 0;
+    virtual void operator()(nbeg_element_t &) = 0;
+    virtual void operator()(nend_element_t &) = 0;
+    virtual void operator()(gbeg_element_t &) = 0;
+    virtual void operator()(gend_element_t &) = 0;
+};
 
 // Streaming version of the document tree, suitable for printing after
 // much processing
 struct stream_element_t {
-    int position() const { return hpos; }
+    int hpos;                   // -1 means not set yet
 
     stream_element_t() : hpos(-1) {}
     stream_element_t(unsigned int n) : hpos(n) {}
+    virtual ~stream_element_t() {}
 
-    int hpos;                   // -1 means not set yet
+    virtual void visit(stream_element_visitor_t &v) = 0;
+    virtual std::unique_ptr<stream_element_t> clone() = 0;
 };
 
 struct text_element_t : public stream_element_t {
@@ -20,8 +50,13 @@ struct text_element_t : public stream_element_t {
         : stream_element_t(hpos), payload(text) {}
     text_element_t(const std::string &text)
         : stream_element_t(), payload(text) {}
+    virtual ~text_element_t() {}
 
-    const std::string &text() const { return payload; }
+    virtual void visit(stream_element_visitor_t &v) { v(*this); }
+    virtual std::unique_ptr<stream_element_t> clone() {
+        return std::unique_ptr<text_element_t>(
+            new text_element_t(payload, hpos));
+    }
 };
 
 struct cond_element_t : public stream_element_t {
@@ -33,175 +68,238 @@ struct cond_element_t : public stream_element_t {
     cond_element_t(const std::string &l, const std::string &t,
                    const std::string &r, unsigned int hpos)
         : stream_element_t(hpos), small(l), tail(t), cont(r) {}
+    virtual ~cond_element_t() {}
 
-    const std::string &text() const { return payload; }
+    virtual void visit(stream_element_visitor_t &v) { v(*this); }
+    virtual std::unique_ptr<stream_element_t> clone() {
+        return std::unique_ptr<cond_element_t>(
+            new cond_element_t(small, tail, cont, hpos));
+    }
 };
 
 struct nbeg_element_t : public stream_element_t {
-
     nbeg_element_t() : stream_element_t() {}
     nbeg_element_t(unsigned int hpos) : stream_element_t(hpos) {}
+    virtual ~nbeg_element_t() {}
+
+    virtual void visit(stream_element_visitor_t &v) { v(*this); }
+    virtual std::unique_ptr<stream_element_t> clone() {
+        return std::unique_ptr<nbeg_element_t>(new nbeg_element_t(hpos));
+    }
 };
 
 struct nend_element_t : public stream_element_t {
-
     nend_element_t() : stream_element_t() {}
     nend_element_t(unsigned int hpos) : stream_element_t(hpos) {}
+    virtual ~nend_element_t() {}
+
+    virtual void visit(stream_element_visitor_t &v) { v(*this); }
+    virtual std::unique_ptr<stream_element_t> clone() {
+        return std::unique_ptr<nend_element_t>(new nend_element_t(hpos));
+    }
 };
 
 struct gbeg_element_t : public stream_element_t {
-
     gbeg_element_t() : stream_element_t() {}
     gbeg_element_t(unsigned int hpos) : stream_element_t(hpos) {}
+    virtual ~gbeg_element_t() {}
+
+    virtual void visit(stream_element_visitor_t &v) { v(*this); }
+    virtual std::unique_ptr<stream_element_t> clone() {
+        return std::unique_ptr<gbeg_element_t>(new gbeg_element_t(hpos));
+    }
 };
 
 struct gend_element_t : public stream_element_t {
-
     gend_element_t() : stream_element_t() {}
     gend_element_t(unsigned int hpos) : stream_element_t(hpos) {}
+    virtual ~gend_element_t() {}
+
+    virtual void visit(stream_element_visitor_t &v) { v(*this); }
+    virtual std::unique_ptr<stream_element_t> clone() {
+        return std::unique_ptr<gend_element_t>(new gend_element_t(hpos));
+    }
 };
 
-typedef boost::variant<text_element_t, cond_element_t, nbeg_element_t,
-                       nend_element_t, gbeg_element_t, gend_element_t> stream_elt_t;
+class fn_wrapper_t {
+    std::shared_ptr<stream_element_visitor_t> v;
 
-class generate_stream_visitor : public boost::static_visitor<> {
-    const std::function<void(stream_elt_t &)> &fn;
 public:
-    generate_stream_visitor(const std::function<void(stream_elt_t &)> &f) : fn(f) {}
+    fn_wrapper_t(std::shared_ptr<stream_element_visitor_t> _v)
+      : v(_v) {}
 
-    void operator(text_t &t) const {
-        fn(text_element_t(t.text));
-    }
-
-    void operator(cond_t &c) const {
-        fn(cond_element_t(c.small, c.tail, c.cont));
-    }
-
-    void operator(concat_t &c) const {
-        std::for_each(c.children.begin(), c.children.end(), boost::apply_visitor(*this));
-    }
-
-    void operator(group_t &g) const {
-        fn(gbeg_element_t());
-        boost::apply_visitor(*this, g.child);
-        fn(gend_element_t());
-    }
-
-    void operator(nest_t &n) const {
-        fn(nbeg_element_t());
-        fn(gbeg_element_t());
-        std::for_each(n.children.begin(), n.children.end(), boost::apply_visitor(*this));
-        fn(gend_element_t());
-        fn(nend_element_t());
+    void operator()(stream_element_t &e) { e.visit(*v); }
+    template <typename T>
+    void operator()(T &&t) { T e(t); e.visit(*v); }
+    template <typename T> void operator()(std::unique_ptr<T> &t) {
+        t->visit(*v);
     }
 };
 
-void generate_stream(const document_t &doc,
-                     const std::function<void(stream_elt_t &)> &fn) {
-    boost::apply_visitor(generate_stream_visitor(fn), doc);
+typedef std::shared_ptr<fn_wrapper_t> thunk_t;
+
+class generate_stream_visitor_t : public document_visitor_t {
+    thunk_t fn;
+public:
+    generate_stream_visitor_t(thunk_t &&f) : fn(f) {}
+    virtual ~generate_stream_visitor_t() {}
+
+    virtual void operator()(const text_t &t) const {
+        (*fn)(text_element_t(t.text));
+    }
+
+    virtual void operator()(const cond_t &c) const {
+        (*fn)(cond_element_t(c.small, c.tail, c.cont));
+    }
+
+    virtual void operator()(const concat_t &c) const {
+        std::for_each(c.children.begin(), c.children.end(),
+                      [this](doc_handle_t d) { d->visit(*this); });
+    }
+
+    virtual void operator()(const group_t &g) const {
+        (*fn)(gbeg_element_t());
+        g.child->visit(*this);
+        (*fn)(gend_element_t());
+    }
+
+    virtual void operator()(const nest_t &n) const {
+        (*fn)(nbeg_element_t());
+        (*fn)(gbeg_element_t());
+        n.child->visit(*this);
+        (*fn)(gend_element_t());
+        (*fn)(nend_element_t());
+    }
+};
+
+void generate_stream(doc_handle_t doc, thunk_t &&fn) {
+    generate_stream_visitor_t v(std::move(fn));
+    doc->visit(v);
 }
 
-class annotate_stream_visitor : public boost::static_visitor<> {
-    const std::function<void(stream_elt_t &)> &fn;
+class annotate_stream_visitor_t : public stream_element_visitor_t {
+    thunk_t fn;
     unsigned int position;
 public:
-    annotate_stream_visitor(const std::function<void(stream_elt_t &)> &f)
-        : fn(f), position(0) {}
+    annotate_stream_visitor_t(thunk_t &&f) : fn(f), position(0) {}
+    virtual ~annotate_stream_visitor_t() {}
 
-    void operator(text_element_t &t) {
+    virtual void operator()(text_element_t &t) {
         position += t.payload.size();
         t.hpos = position;
-        fn(t);
+        (*fn)(t);
     }
 
-    void operator(cond_element_t &c) {
+    virtual void operator()(cond_element_t &c) {
         position += c.small.size();
         c.hpos = position;
-        fn(c);
+        (*fn)(c);
     }
 
-    void operator(gbeg_element_t &e) {
+    virtual void operator()(gbeg_element_t &e) {
         e.hpos = position;
-        fn(e);
+        (*fn)(e);
     }
 
-    void operator(gend_element_t &e) {
+    virtual void operator()(gend_element_t &e) {
         e.hpos = position;
-        fn(e);
+        (*fn)(e);
     }
 
-    void operator(nbeg_element_t &e) {
+    virtual void operator()(nbeg_element_t &e) {
         e.hpos = position;
-        fn(e);
+        (*fn)(e);
     }
 
-    void operator(nend_element_t &e) {
+    virtual void operator()(nend_element_t &e) {
         e.hpos = position;
-        fn(e);
+        (*fn)(e);
     }
 };
 
-std::function<void(stream_elt_t &)> &&
-annotate_stream(const std::function<T(stream_elt_t &)> &fn) {
-    boost::apply_visitor(annotate_stream_visitor(fn));
+thunk_t &&annotate_stream(thunk_t &&fn) {
+    return std::move(std::make_shared<fn_wrapper_t>(
+        std::make_shared<annotate_stream_visitor_t>(std::move(fn))));
 }
 
-class correct_gbeg_visitor : public boost::static_visitor<> {
-    const std::function<void(stream_elt_t &)> &fn;
-    std::vector<std::list<stream_elt_t> > lookahead;
-public:
-    correct_gbeg_visitor(const std::function<void(stream_elt_t &)> &f)
-        : fn(f), lookahead() {}
+class correct_gbeg_visitor_t : public stream_element_visitor_t {
+    thunk_t fn;
+    typedef std::unique_ptr<stream_element_t> elt_t;
+    typedef std::unique_ptr<std::list<elt_t> > buffer_t;
+    std::vector<buffer_t> lookahead;
 
-    template <typename T>
-    void operator(T &t) {
+public:
+    correct_gbeg_visitor_t(thunk_t &&f) : fn(f), lookahead() {}
+    virtual ~correct_gbeg_visitor_t() {}
+
+    void maybe_push(stream_element_t &e) {
         if (lookahead.empty()) {
-            fn(t);
+            (*fn)(e);
         } else {
-            lookahead.back().push_back(t);
+            lookahead.back()->push_back(e.clone());
         }
     }
 
-    void operator(gbeg_element_t &e) {
-        lookahead.push_back(std::list<stream_elt_t>());
+    virtual void operator()(text_element_t &t) {
+        maybe_push(t);
     }
 
-    void operator(gend_element_t &e) {
-        std::list<stream_elt_t> b = lookahead.pop_back();
+    virtual void operator()(cond_element_t &c) {
+        maybe_push(c);
+    }
+
+    virtual void operator()(nbeg_element_t &e) {
+        maybe_push(e);
+    }
+
+    virtual void operator()(nend_element_t &e) {
+        maybe_push(e);
+    }
+
+    virtual void operator()(gbeg_element_t &) {
+        lookahead.push_back(buffer_t(new std::list<elt_t>()));
+    }
+
+    virtual void operator()(gend_element_t &e) {
+        buffer_t b(std::move(lookahead.back()));
+        lookahead.pop_back();
         if (lookahead.empty()) {
             // this is then the topmost group
-            fn(gbeg_element_t(e.hpos));
-            std::for_each(b.begin(), b.end(), fn);
-            fn(e);
+            (*fn)(gbeg_element_t(e.hpos));
+            std::for_each(b->begin(), b->end(), *fn);
+            (*fn)(e);
         } else {
-            std::list<stream_elt_t> &b2 = lookahead.back();
-            b2.push_back(gbeg_element_t(e.hpos));
-            b2.splice(b2.end(), b);
-            b2.push_back(e);
+            buffer_t &b2 = lookahead.back();
+            b2->push_back(
+                std::unique_ptr<gbeg_element_t>(new gbeg_element_t(e.hpos)));
+            b2->splice(b2->end(), *b);
+            b2->push_back(e.clone());
         }
     }
 };
 
-std::function<void(stream_elt_t &)> &&
-correct_gbeg_stream(const std::function<T(stream_elt_t &)> &fn) {
-    boost::apply_visitor(correct_gbeg_visitor(fn));
+thunk_t &&correct_gbeg_stream(thunk_t &&fn) {
+    return std::move(std::make_shared<fn_wrapper_t>(
+        std::make_shared<correct_gbeg_visitor_t>(std::move(fn))));
 }
 
-class output_visitor : public boost::static_visitor<> {
+class output_visitor_t : public stream_element_visitor_t {
     const unsigned int width;
     unsigned int fittingElements, rightEdge, hpos;
     std::vector<unsigned int> indent;
 public:
     std::string result;
-    output_visitor(unsigned int w)
-        : width(w), fittingElements(0), rightEdge(w), hpos(0), result(), indent() {}
+    output_visitor_t(unsigned int w)
+        : width(w), fittingElements(0), rightEdge(w), hpos(0), indent(),
+          result() {}
+    virtual ~output_visitor_t() {}
 
-    void operator(text_element_t &t) {
+    virtual void operator()(text_element_t &t) {
         result += t.payload;
         hpos += t.payload.size();
     }
 
-    void operator(cond_element_t &c) {
+    virtual void operator()(cond_element_t &c) {
         if (fittingElements == 0) {
             unsigned int currentIndent = indent.back();
             result += c.tail;
@@ -217,33 +315,35 @@ public:
         }
     }
 
-    void operator(gbeg_element_t &e) {
-        if (fittingElements != 0 || e.hpos <= rightEdge) {
+    virtual void operator()(gbeg_element_t &e) {
+        if (fittingElements != 0 ||
+            static_cast<unsigned int>(e.hpos) <= rightEdge) {
             ++fittingElements;
         } else {
             fittingElements = 0;
         }
     }
 
-    void operator(gend_element_t &e) {
+    virtual void operator()(gend_element_t &) {
         if (fittingElements != 0)
             --fittingElements;
     }
 
-    void operator(nbeg_element_t &e) {
+    virtual void operator()(nbeg_element_t &) {
         indent.push_back(hpos);
     }
 
-    void operator(nend_element_t &e) {
-        indent.pop();
-    }
+    virtual void operator()(nend_element_t &) { indent.pop_back(); }
 };
 
-std::string &&pretty_print(unsigned int width, const std::shared_ptr<document_t> doc) {
-    output_visitor output(width);
-    generate_stream(doc,
-                    annotate_stream(correct_gbeg_stream(boost::apply_visitor(output))));
-    return std::move(output.result);
+std::string &&pretty_print(unsigned int width, doc_handle_t doc) {
+    std::shared_ptr<output_visitor_t> output =
+        std::make_shared<output_visitor_t>(width);
+    thunk_t corr_gbeg =
+        correct_gbeg_stream(std::make_shared<fn_wrapper_t>(output));
+    thunk_t annotate = annotate_stream(std::move(corr_gbeg));
+    generate_stream(doc, std::move(annotate));
+    return std::move(output->result);
 }
 
 } // namespace pprint
--- src/pprint/pprint.hpp
+++ src/pprint/pprint.hpp
@@ -8,25 +8,45 @@
 
 namespace pprint {
 
-class text_t;
-class cond_t;
-class concat_t;
-class group_t;
-class nest_t;
+struct text_t;
+struct cond_t;
+struct concat_t;
+struct group_t;
+struct nest_t;
 
-typedef boost::variant<text_t, cond_t, concat_t, group_t, nest_t> document_t;
+class document_visitor_t {
+public:
+    virtual ~document_visitor_t() {}
+
+    virtual void operator()(const text_t &) const = 0;
+    virtual void operator()(const cond_t &) const = 0;
+    virtual void operator()(const concat_t &) const = 0;
+    virtual void operator()(const group_t &) const = 0;
+    virtual void operator()(const nest_t &) const = 0;
+};
+
+class document_t {
+public:
+    virtual ~document_t() {}
+    virtual unsigned int width() const = 0;
+    virtual void visit(const document_visitor_t &v) const = 0;
+};
+
+typedef std::shared_ptr<const document_t> doc_handle_t;
 
 // textual element
-struct text_t {
+struct text_t : public document_t {
     std::string text;
 
     text_t(const std::string &str) : text(str) {}
     text_t(std::string &&str) : text(str) {}
+    virtual ~text_t() {}
 
-    unsigned int width() const { return text.length(); }
+    virtual unsigned int width() const { return text.length(); }
+    virtual void visit(const document_visitor_t &v) const { v(*this); }
 };
 
-document_t &&empty() { return text_t(""); }
+extern const doc_handle_t empty;
 
 // primitive class for conditional linebreaks.  `small` is what will be
 // printed if there is no linebreak.  If there is a linebreak, `tail`
@@ -35,145 +55,146 @@ document_t &&empty() { return text_t(""); }
 // of the new line.
 //
 // You probably shouldn't use this; use `br` or `dot`.
-struct cond_t {
-    std::string small, tail, cont;
+struct cond_t : public document_t {
+    std::string small, cont, tail;
 
     cond_t(const std::string &l, const std::string &r)
-        : small(l), cont(r), tail('') {}
+        : small(l), cont(r), tail("") {}
     cond_t(const std::string &l, const std::string &r, const std::string &t)
         : small(l), cont(r), tail(t) {}
-    cond_t(std::string &&l, std::string &&r)
-        : small(l), cont(r), tail('') {}
+    cond_t(std::string &&l, std::string &&r) : small(l), cont(r), tail("") {}
     cond_t(std::string &&l, std::string &&r, std::string &&t)
         : small(l), cont(r), tail(t) {}
+    virtual ~cond_t() {}
 
     // no linebreaks, so only `small` is relevant
     virtual unsigned int width() const { return small.length(); }
+
+    virtual void visit(const document_visitor_t &v) const { v(*this); }
 };
 
-document_t &&br() { return cond_t(" ", ""); }
-document_t &&dot() { return cond_t(".", "."); }
+extern const doc_handle_t br;
+extern const doc_handle_t dot;
 
 // concatenation of multiple documents
-struct concat_t {
-    std::vector<document_t> children;
+struct concat_t : public document_t {
+    std::vector<doc_handle_t> children;
 
-    concat_t(std::vector<document_t> &&args)
+    concat_t(std::vector<doc_handle_t> &&args)
         : children(args) {}
     template <typename It>
     concat_t(It &begin, It &end)
-        : children(args) {}
-    concat_t(std::initializer_list<document_t> init)
+        : children(begin, end) {}
+    concat_t(std::initializer_list<doc_handle_t> init)
         : children(init) {}
+    virtual ~concat_t() {}
 
-    virtual unsigned int width() const {
+    unsigned int width() const {
         unsigned int w = 0;
         for (auto i = children.begin(); i != children.end(); i++) {
-            w += i->width();
+            w += (*i)->width();
         }
         return w;
     }
+
+    virtual void visit(const document_visitor_t &v) const { v(*this); }
 };
 
 // An enclosure where all linebreaks are interpreted consistently.
-struct group_t {
-    document_t child;
+struct group_t : public document_t {
+    doc_handle_t child;
 
-    group_t(const document_t &doc)
-        : child(doc) { }
-    group_t(document_t &&doc)
-        : child(doc) { }
+    group_t(doc_handle_t doc) : child(doc) {}
+    virtual ~group_t() {}
 
-    virtual unsigned int width() const { return child->width(); }
+    unsigned int width() const {
+        return child->width();
+    }
+
+    virtual void visit(const document_visitor_t &v) const { v(*this); }
 };
 
-// concatenation of multiple documents with consistent indentation.
-struct nest_t {
-    std::vector<document_t> children;
+// An enclosure where linebreaks have consistent indentation.
+struct nest_t : public document_t {
+    doc_handle_t child;
 
-    nest_t(std::vector<document_t> &&args)
-        : children(args) {}
-    template <typename It>
-    nest_t(It &begin, It &end)
-        : children(args) {}
-    nest_t(std::initializer_list<document_t> init)
-        : children(init) {}
+    nest_t(doc_handle_t doc) : child(doc) {}
+    virtual ~nest_t() {}
 
-    virtual unsigned int width() const {
-        unsigned int w = 0;
-        for (auto i = children.begin(); i != children.end(); i++) {
-            w += i->width();
-        }
-        return w;
+    unsigned int width() const {
+        return child->width();
     }
+
+    virtual void visit(const document_visitor_t &v) const { v(*this); }
 };
 
 template <typename... Ts>
-document_t &&comma_separated(Ts... docs) {
+doc_handle_t comma_separated(Ts... docs) {
     return comma_separated({docs...});
 }
 
-document_t &&
-comma_separated(std::initializer_list<document_t> init) {
-    if (init.size() == 0) return empty();
-    std::vector<document_t> v;
+doc_handle_t comma_separated(std::initializer_list<doc_handle_t> init) {
+    if (init.size() == 0) return empty;
+    std::vector<doc_handle_t> v;
     auto it = init.begin();
     v.push_back(*it++);
     for (; it != init.end(); it++) {
-        v.push_back(text_t(","));
+        v.push_back(std::make_shared<text_t>(","));
         v.push_back(br);
         v.push_back(*it);
     }
-    return nest_t(v);
+    return std::make_shared<nest_t>(std::make_shared<concat_t>(std::move(v)));
 }
 
 template <typename... Ts>
-document_t &&arglist(Ts... docs) {
+doc_handle_t arglist(Ts... docs) {
     return arglist({docs...});
 }
 
-document_t &&
-arglist(std::initializer_list<document_t> init) {
-    return concat_t(text_t("("), comma_separated(init), text_t(")"));
+doc_handle_t arglist(std::initializer_list<doc_handle_t> init) {
+    static const doc_handle_t lparen = std::make_shared<text_t>("(");
+    static const doc_handle_t rparen = std::make_shared<text_t>(")");
+    std::vector<doc_handle_t> v { lparen, comma_separated(init), rparen };
+    return std::make_shared<concat_t>(std::move(v));
 }
 
 template <typename... Ts>
-document_t &&dotted_list(Ts... docs) {
+doc_handle_t dotted_list(Ts... docs) {
     return dotted_list({docs...});
 }
 
-document_t &&
-dotted_list(std::initializer_list<document_t> init) {
-    static text_t plain_dot('.');
-    if (init.size() == 0) return empty();
-    std::vector<document_t> v;
+doc_handle_t
+dotted_list(std::initializer_list<doc_handle_t> init) {
+    static const doc_handle_t plain_dot = std::make_shared<text_t>(".");
+    if (init.size() == 0) return empty;
+    std::vector<doc_handle_t> v;
     auto it = init.begin();
     v.push_back(*it++);
     bool first = true;
     for (; it != init.end(); it++) {
         if (first) {
-            v.push_back(text_t("."));
+            v.push_back(plain_dot);
             first = false;
         } else {
-            v.push_back(dot());
+            v.push_back(dot);
         }
         v.push_back(*it);
     }
-    return nest_t(v);
+    return std::make_shared<nest_t>(std::make_shared<concat_t>(std::move(v)));
 }
 
 template <typename... Ts>
-document_t &&funcall(const std::string &name, Ts... docs) {
+doc_handle_t funcall(const std::string &name, Ts... docs) {
     return funcall(name, {docs...});
 }
 
-document_t &&
-funcall(const std::string &name,
-        std::initializer_list<document_t> init) {
-    return concat_t(text_t(name), arglist(init));
+doc_handle_t funcall(const std::string &name,
+                     std::initializer_list<doc_handle_t> init) {
+    std::vector<doc_handle_t> v { std::make_shared<text_t>(name), arglist(init) };
+    return std::make_shared<concat_t>(std::move(v));
 }
 
-std::string pretty_print(const document_t &doc);
+std::string pretty_print(doc_handle_t doc);
 
 } // namespace pprint
 
