aea0e0d3e0196484f081cf422bff8c8d6949f25e
Tim Maxwell <tim@rethinkdb.com>
2015-02-09 18:45:19 -0800

    WIP on tim/raft-table-impl

--- src/clustering/table_raft/leader.hpp
+++ src/clustering/table_raft/leader.hpp
@@ -2,10 +2,18 @@
 #ifndef CLUSTERING_TABLE_RAFT_LEADER_HPP_
 #define CLUSTERING_TABLE_RAFT_LEADER_HPP_
 
-class table_raft_leader_t {
+namespace table_raft {
+
+class leader_t {
 public:
-    
+    leader_t(
+        raft_member_t<state_t> *raft,
+        watchable_map_t<std::pair<server_id_t, contract_id_t>, contract_ack_t> *acks);
+
+private:
 };
+
+} /* namespace table_raft */
 
 #endif /* CLUSTERING_TABLE_RAFT_LEADER_HPP_ */
 
