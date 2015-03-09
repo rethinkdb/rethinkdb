d1edb972728560378d287a16cad29b60b7f14a84
Tim Maxwell <tim@rethinkdb.com>
2015-03-09 15:09:58 -0700

    Merge throttled_committer_t and pump_coro_t.

--- src/utils.hpp
+++ src/utils.hpp
@@ -135,12 +135,25 @@ bool blocking_read_file(const char *path, std::string *contents_out);
 template <class T>
 class assignment_sentry_t {
 public:
+    assignment_sentry_t() : var(nullptr) { }
     assignment_sentry_t(T *v, const T &value) :
-        var(v), old_value(*var) {
+            var(v), old_value(*var) {
         *var = value;
     }
     ~assignment_sentry_t() {
-        *var = old_value;
+        reset();
+    }
+    void reset(T *v, const T &value) {
+        reset();
+        var = v;
+        old_value = *var;
+        *var = value;
+    }
+    void reset() {
+        if (var != nullptr) {
+            *var = old_value;
+            var = nullptr;
+        }
     }
 private:
     T *var;
