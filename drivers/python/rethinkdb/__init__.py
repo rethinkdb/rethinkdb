a4d41e4ea0c8ad9852233f26b5da2bd66eed28d1
Yohan Graterol <yohangraterol92@gmail.com>
2015-03-23 22:53:14 -0500

    Import version var from version.py

--- drivers/python/rethinkdb/__init__.py
+++ drivers/python/rethinkdb/__init__.py
@@ -5,10 +5,12 @@ from .query import *
 from .errors import *
 from .ast import *
 from . import docs
-from pkg_resources import get_distribution, DistributionNotFound
+from .version import version
 
- 
-class r(__builtins__['object']): # defends against re-importing obscuring object
+
+# The __builtins__ here defends against re-importing something
+# obscuring `object`.
+class r(__builtins__['object']):
     pass
 
 for module in (net, query, ast, errors):
@@ -19,11 +21,6 @@ rethinkdb = r
 # set the _r attribute to net.Connection
 Connection._r = r
 
-__version__ = None
-
-try:
-    __version__ = get_distribution('rethinkdb').version
-except DistributionNotFound:
-    pass
+__version__ = version
 
 __all__ = ['r', 'rethinkdb'] + errors.__all__
