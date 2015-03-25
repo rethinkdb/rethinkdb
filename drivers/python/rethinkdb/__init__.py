1c7dec9efcef221342bf3a9dfe767324ada04afb
Marc Hesse <marc@rethinkdb.com>
2015-03-25 15:58:59 -0700

    fixing compatibility for pypy - modules cannot be used as a dict

--- drivers/python/rethinkdb/__init__.py
+++ drivers/python/rethinkdb/__init__.py
@@ -1,16 +1,14 @@
 # Copyright 2010-2014 RethinkDB, all rights reserved.
 
+# Define this before importing so nothing can overwrite 'object'
+class r(object):
+    pass
+
 from .net import *
 from .query import *
 from .errors import *
 from .ast import *
 from . import docs
-
-
-# The __builtins__ here defends against re-importing something
-# obscuring `object`.
-class r(__builtins__['object']):
-    pass
 
 for module in (net, query, ast, errors):
     for functionName in module.__all__:
