53d2b675baf1cc0a6662666e5f9eaa0ba67096b1
t3hmrman <t3hmrman@gmail.com>
2015-03-26 15:40:31 -0700

    Change "documents" to "elements"

--- src/rdb_protocol/shards.cc
+++ src/rdb_protocol/shards.cc
@@ -331,7 +331,7 @@ private:
             } else {
                 rcheck_toplevel(
                     size <= env->limits().array_size_limit(), base_exc_t::GENERIC,
-                    strprintf("Array over size limit `%zu`. To change the number of allowed documents, modify the arrayLimit option, or use an index.",
+                    strprintf("Array over size limit `%zu`. To change the number of allowed elements, modify the arrayLimit option, or use an index.",
                               env->limits().array_size_limit()).c_str());
             }
             lst1->reserve(lst1->size() + lst2->size());
@@ -361,7 +361,7 @@ private:
             } else {
                 rcheck_toplevel(
                     size <= env->limits().array_size_limit(), base_exc_t::GENERIC,
-                    strprintf("Array over size limit `%zu`. To change the number of allowed documents, modify the 'arrayLimit' option, or use an index.",
+                    strprintf("Array over size limit `%zu`. To change the number of allowed elements, modify the 'arrayLimit' option, or use an index.",
                               env->limits().array_size_limit()).c_str());
             }
 
