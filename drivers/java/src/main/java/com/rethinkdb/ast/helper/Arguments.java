2acf25783cda641d2d374798a1114446c661abee
Josh Kuhn <josh@rethinkdb.com>
2015-05-05 11:10:21 -0700

    All compiler warnings fixed

--- drivers/java/src/main/java/com/rethinkdb/ast/helper/Arguments.java
+++ drivers/java/src/main/java/com/rethinkdb/ast/helper/Arguments.java
@@ -5,18 +5,18 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Arguments extends ArrayList<Object> {
+
+    public Arguments() {}
     public Arguments(Object arg1) {
         super();
         add(arg1);
     }
 
-    public Arguments(Object... args) {
-        super();
+    public Arguments(Object[] args) {
         addAll(Arrays.asList(args));
     }
 
     public Arguments(List<Object> args) {
-        super();
         addAll(args);
     }
 }
