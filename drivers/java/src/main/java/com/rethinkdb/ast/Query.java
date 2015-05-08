f5fb43093035eb199efa3dad6721eceecc99c57d
Josh Kuhn <josh@rethinkdb.com>
2015-05-08 16:33:28 -0700

    Serialization partially implemented

---
+++ drivers/java/src/main/java/com/rethinkdb/ast/Query.java
@@ -0,0 +1,41 @@
+package com.rethinkdb.ast;
+
+import com.rethinkdb.proto.QueryType;
+import com.rethinkdb.ast.RqlAst;
+import com.rethinkdb.ast.helper.OptArgs;
+
+import java.nio.ByteBuffer;
+import java.util.*;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+
+/* An instance for a query that has been sent to the server. Keeps
+ * track of its token, the args to .run() it was called with, as well as
+*/
+
+public class Query {
+    public final QueryType type;
+    public final RqlAst term;
+    public final int token;
+    public final OptArgs globalOptargs;
+
+    public Query(QueryType type, int token, RqlAst term, OptArgs globalOptargs) {
+        this.type = type;
+        this.token = token;
+        this.term = term;
+        this.globalOptargs = globalOptargs;
+    }
+
+    public ByteBuffer serialize() {
+        JSONArray queryArr = new JSONArray();
+        queryArr.add(type.value);
+        queryArr.add(term.build());
+        queryArr.add(globalOptargs);
+        String queryJson = queryArr.toJSONString();
+        return Util.leByteBuffer(8 + 4 + queryJson.length())
+            .putLong(token)
+            .putInt(queryJson.length())
+            .put(queryJson.getBytes());
+    }
+}
--- drivers/java/templates/gen/Func.java
+++ drivers/java/templates/gen/Func.java
@@ -4,7 +4,7 @@
 import com.rethinkdb.model.RqlFunction2;
 import com.rethinkdb.model.RqlFunction;
 import com.rethinkdb.model.RqlLambda;
-import com.rethinkdb.ast.RqlUtil;
+import com.rethinkdb.ast.Util;
 </%block>
 
 <%block name="constructors">
@@ -15,7 +15,7 @@ import com.rethinkdb.ast.RqlUtil;
                 null,
                 Arguments.make(
                     new MakeArray(new Arguments(1), null),
-                    RqlUtil.toRqlAst(
+                    Util.toRqlAst(
                         ((RqlFunction)function).apply(
                             new Var(new Arguments(1), null)))
                 ),
@@ -27,7 +27,7 @@ import com.rethinkdb.ast.RqlUtil;
                 null,
                 Arguments.make(
                     new MakeArray(Arguments.make(1, 2), null),
-                    RqlUtil.toRqlAst(
+                    Util.toRqlAst(
                         ((RqlFunction2)function).apply(
                             new Var(new Arguments(1), null),
                             new Var(new Arguments(2), null)))
