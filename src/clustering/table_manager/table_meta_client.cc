ad61fc3e04506c948d490bf881d17a34fd4fdbe5
Tim Maxwell <tim@rethinkdb.com>
2015-05-15 14:31:36 -0700

    Fix handling of errors in outdated reads, which solves #4233. Also fix a stall in get_config() if other server disconnects.

--- src/clustering/table_manager/table_meta_client.cc
+++ src/clustering/table_manager/table_meta_client.cc
@@ -130,8 +130,11 @@ void table_meta_client_t::get_config(
             boost::make_optional(table_id), ack_mailbox.get_address());
         wait_any_t done_cond(promise.get_ready_signal(), &dw);
         wait_interruptible(&done_cond, interruptor2);
+        if (!promise.is_pulsed()) {
+            throw_appropriate_exception(table_id);
+        }
         std::map<namespace_id_t, table_config_and_shards_t> maybe_result =
-            promise.wait();
+            promise.assert_get_value();
         if (maybe_result.empty()) {
             throw_appropriate_exception(table_id);
         }
