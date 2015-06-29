406538f7cc9e03e42bfb0b7edc7e2d754405d579
Karl Kuehn <kuehn.karl@gmail.com>
2015-06-29 14:00:46 -0700

    updating copyright date on recently updated file
    over-the-virtual-shoulder by @vexocide

--- test/memcached_workloads/append_stress.py
+++ test/memcached_workloads/append_stress.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-# Copyright 2010-2014 RethinkDB, all rights reserved.
+# Copyright 2010-2015 RethinkDB, all rights reserved.
+
 import sys, socket, random, time, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, 'common')))
 import rdb_workload_common
