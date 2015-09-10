922f62fdbc7ffdc9406bf9eb58955a3d2ef22231
Josh Kuhn <josh@rethinkdb.com>
2015-09-09 20:03:00 -0700

    Track expression types

--- drivers/java/convert_tests.py
+++ drivers/java/convert_tests.py
@@ -19,19 +19,27 @@ def main():
     total_defs = 0
     for testfile in all_yaml_tests(TEST_DIR):
         print("\n### ", testfile)
-        for i, testline in enumerate(TestFile(TEST_DIR, testfile)):
+        reql_vars = ["r"]
+        tf = TestFile(TEST_DIR, testfile)
+        if tf.table_var_name is not None:
+            reql_vars.append(tf.table_var_name)
+        print(" ## ReQL vars:", ", ".join(reql_vars))
+        for i, testline in enumerate(tf):
             # TODO: the body of this loop should output to the test files
             if isinstance(testline, Def):
                 total_defs += 1
-                # print(testline.line)
-                # handle_assignment(testline)
-                # print()
+                print(testline.line)
+                varname = handle_assignment(testline.parsed, reql_vars)
+                if varname is not None:
+                    reql_vars.append(varname)
+                    print(" ## ReQL vars:", ", ".join(reql_vars))
+                print()
             elif isinstance(testline, Query):
                 total_tests += 1
                 print("---- #{}".format(i))
                 print("-", testline.line)
                 try:
-                    result = JavaVisitor().convert(testline.line)
+                    result = ReQLVisitor(reql_vars).convert(testline.parsed)
                     print("+", result)
                 except Exception:
                     total_errors += 1
@@ -68,15 +76,8 @@ class Unhandled(Exception):
 SIGIL = object()
 
 
-Query = namedtuple('Query', 'line expected_value runopts')
-Def = namedtuple('Def', 'line runopts')
-
-
-def handle_assignment(definition):
-    '''Processes an assignment statement'''
-    assignment = ast.parse(definition.line, mode='single').body[0]
-    value = JavaConverter().convert(assignment.value)
-    print("Object", assignment.targets[0].id, "=", value, end=";\n")
+Query = namedtuple('Query', 'parsed line expected_value runopts')
+Def = namedtuple('Def', 'parsed line runopts')
 
 
 class TestFile(object):
@@ -143,7 +144,7 @@ class TestFile(object):
 
         for test in self.raw_test_data:
             runopts = test.get('runopts')
-            expected_value = get_python_value(test)  # ot
+            expected = get_python_value(test)  # ot field
             if 'def' in test:
                 # We want to yield the definition before the test itself
                 define = flexiget(test['def'], ['py', 'cd'], test['def'])
@@ -151,45 +152,73 @@ class TestFile(object):
                 if define and type(define) is not dict:
                     # if define is a dict, it doesn't have a py key
                     # since we already checked
-                    yield Def(py_str(define), runopts)
+                    parsed_define = ast.parse(
+                        py_str(define), mode="single").body[0]
+                    yield Def(parsed_define, py_str(define), runopts)
             pytest = flexiget(test, ['py', 'cd'], None)
             if pytest:
                 if isinstance(pytest, "".__class__):
-                    try:
-                        ast.parse(pytest, mode="eval")
-                    except SyntaxError:
-                        # Alternative syntax for defs is just an assignment
-                        yield Def(py_str(pytest), runopts)
-                    else:
-                        yield Query(pytest, expected_value, runopts)
+                    parsed = ast.parse(pytest, mode="single").body[0]
+                    if type(parsed) == ast.Expr:
+                        yield Query(parsed.value, pytest, expected, runopts)
+                    elif type(parsed) == ast.Assign:
+                        yield Def(parsed, pytest, runopts)
                 elif type(pytest) is dict and 'cd' in pytest:
-                    pytest = py_str(pytest['cd'])
-                    yield Query(pytest, expected_value, runopts)
+                    pytestline = py_str(pytest['cd'])
+                    parsed = ast.parse(pytestline, mode="eval").body
+                    yield Query(parsed, pytestline, expected, runopts)
                 else:
                     # unroll subtests
                     for subtest in pytest:
                         subtestline = py_str(subtest)
-                        yield Query(subtestline, expected_value, runopts)
+                        parsed = ast.parse(subtestline, mode="eval").body
+                        yield Query(parsed, subtestline, expected, runopts)
+
+
+def handle_assignment(assignment, reql_vars):
+    '''Processes an assignment statement'''
+    var = assignment.targets[0].id
+    print(JavaVisitor(reql_vars=reql_vars).convert(assignment))
+    if is_reql(assignment.value, reql_vars):
+        return var
+
+
+def is_reql(node, reql_vars):
+    '''Tries to quickly decide (ignoring many corner cases) if an
+    expression is a ReQL term'''
+    if type(node) == ast.Call:
+        return is_reql(node.func, reql_vars)
+    elif type(node) == ast.Subscript:
+        return is_reql(node.value, reql_vars)
+    elif type(node) == ast.Attribute:
+        return is_reql(node.value, reql_vars)
+    elif type(node) == ast.Name:
+        return node.id in reql_vars
+    elif type(node) == ast.UnaryOp:
+        return is_reql(node.operand, reql_vars)
+    elif type(node) == ast.BinOp:
+        return (is_reql(node.left, reql_vars) or
+                is_reql(node.right, reql_vars))
+    elif type(node) == ast.Compare:
+        return (is_reql(node.left) or
+                any(is_reql(n, reql_vars) for n in node.comparator))
+    else:
+        return False
 
 
 class JavaVisitor(ast.NodeVisitor):
     '''Converts python ast nodes into a java string'''
 
-    def __init__(self, out=None):
-        self.out = out or StringIO()
+    def __init__(self, reql_vars=frozenset("r"), out=None):
+        self.out = StringIO() if out is None else out
+        self.reql_vars = reql_vars
         super(JavaVisitor, self).__init__()
+        self.write = self.out.write
 
-    def convert(self, line):
+    def convert(self, node):
         '''Convert a text line to another text line'''
-        if isinstance(line, "".__class__):
-            node = ast.parse(line, mode='eval').body
-        else:
-            node = line
         self.visit(node)
         return self.out.getvalue()
-
-    def write(self, to_write):
-        self.out.write(to_write)
 
     def join(self, sep, items):
         first = True
@@ -202,7 +231,7 @@ class JavaVisitor(ast.NodeVisitor):
 
     def to_str(self, string):
         self.write('"')
-        self.write(repr(string)[1:-1])
+        self.write(repr(string)[1:-1])  # trim off quotes
         self.write('"')
 
     def to_args(self, args, optargs=[]):
@@ -221,15 +250,36 @@ class JavaVisitor(ast.NodeVisitor):
         print("Got as far as:", ''.join(self.out), file=sys.stderr)
         raise Unhandled("Don't know what this thing is: " + str(type(node)))
 
+    def visit_Assign(self, node):
+        # Check if value is
+        if len(node.targets) != 1:
+            Unhandled("We only support assigning to one variable")
+        if is_reql(node.value, self.reql_vars):
+            self.write("ReqlAst ")
+            self.write(node.targets[0].id)
+            self.write(" = ")
+            ReQLVisitor(self.reql_vars, out=self.out).visit(node.value)
+            self.write(";")
+        else:
+            self.write("Object ")
+            self.write(node.targets[0].id)
+            self.write(" = ")
+            self.visit(node.value)
+            self.write(";")
+
     def visit_Str(self, node):
         self.to_str(node.s)
 
     def visit_Name(self, node):
         self.write(node.id)
 
+    def visit_Attribute(self, node):
+        self.write(".")
+        self.write(node.attr)
+
     def visit_Num(self, node):
         self.write(repr(node.n))
-        if abs(node.n) > 2147483647:  # max int in java
+        if abs(node.n) > 2147483647 and not isinstance(node.n, float):
             self.write(".0")
 
     def visit_Index(self, node):
@@ -240,33 +290,6 @@ class JavaVisitor(ast.NodeVisitor):
         assert not node.starargs
         self.visit(node.func)
         self.to_args(node.args, node.keywords)
-
-    def visit_Attribute(self, node):
-        self.visit(node.value)
-        self.write(".")
-        initial = metajava.dromedary(node.attr)
-        self.write(initial)
-        if initial in metajava.java_term_info.JAVA_KEYWORDS or \
-           initial in metajava.java_term_info.OBJECT_METHODS:
-            self.write('_')
-
-    def visit_Subscript(self, node):
-        self.visit(node.value)
-        if type(node.slice) == ast.Index:
-            # Syntax like a[2] or a["b"]
-            self.write(".bracket(")
-            self.visit(node.slice.value)
-        elif type(node.slice) == ast.Slice:
-            # Syntax like a[1:2] or a[:2]
-            self.write(".slice(")
-            lower = 0 if not node.slice.lower else node.slice.lower.n
-            upper = -1 if not node.slice.upper else node.slice.upper.n
-            self.write(str(lower))
-            self.write(", ")
-            self.write(str(upper))
-        else:
-            raise Unhandled("No translation for ExtSlice")
-        self.write(")")
 
     def visit_Dict(self, node):
         self.write("new MapObject()")
@@ -289,6 +312,76 @@ class JavaVisitor(ast.NodeVisitor):
         self.to_args(node.args.args)
         self.write(" -> ")
         self.visit(node.body)
+
+    def visit_Subscript(self, node):
+        if not node.slice or not node.slice.value or not node.slice.value.n:
+            raise Unhandled("Only integers subscript can be converted")
+        self.write("[")
+        self.visit(node.slice.value)
+        self.write("]")
+
+    def visit_ListComp(self, node):
+        gen = node.generators[0]
+
+        if type(gen.iter) == ast.Call and gen.iter.func.id.endswith('range'):
+            # This is really a special-case hacking of [... for i in
+            # range(i)] comprehensions that are used in the polyglot
+            # tests sometimes. It won't handle translating arbitrary
+            # comprehensions to Java streams.
+            self.write("IntStream.range(")
+            if len(gen.iter.args) == 1:
+                self.write("0, ")
+                self.visit(gen.iter.args[0])
+            elif len(gen.iter.args) == 2:
+                self.visit(gen.iter.args[0])
+                self.write(", ")
+                self.visit(gen.iter.args[1])
+            self.write(")")
+        else:
+            # Somebody came up with a creative new use for
+            # comprehensions in the test suite...
+            raise Unhandled("ListComp hack couldn't handle: ", ast.dump(node))
+        self.write(".map(")
+        self.visit(gen.target)
+        self.write(" -> ")
+        self.visit(node.elt)
+        self.write(").collect(Collectors.toList())")
+
+    def visit_UnaryOp(self, node):
+        opMap = {
+            ast.USub: "-",
+            ast.Not: "!",
+            ast.UAdd: "+",
+            ast.Invert: "~",
+        }
+        self.write(opMap[type(node.op)])
+        self.visit(node.operand)
+
+    def visit_BinOp(self, node):
+        opMap = {
+            ast.Add: " + ",
+            ast.Sub: " - ",
+            ast.Mult: " * ",
+            ast.Div: " / ",
+            ast.Mod: " % ",
+        }
+        t = type(node.op)
+        if t in opMap.keys():
+            self.visit(node.left)
+            self.write(opMap[t])
+            self.visit(node.right)
+        elif t == ast.Pow:
+            self.write("Math.pow(")
+            self.visit(node.left)
+            self.write(", ")
+            self.visit(node.right)
+            self.write(")")
+
+
+class ReQLVisitor(JavaVisitor):
+    '''Mostly the same as the JavaVisitor, but converts some
+    reql-specific stuff. This should only be invoked on an expression
+    if it's already known to return true from is_reql'''
 
     def visit_BinOp(self, node):
         opMap = {
@@ -329,43 +422,33 @@ class JavaVisitor(ast.NodeVisitor):
         self.visit(node.comparators[0])
         self.write(")")
 
-    def visit_ListComp(self, node):
-        gen = node.generators[0]
-
-        if type(gen.iter) == ast.Call and gen.iter.func.id.endswith('range'):
-            # This is really a special-case hacking of [... for i in
-            # range(i)] comprehensions that are used in the polyglot
-            # tests sometimes. It won't handle translating arbitrary
-            # comprehensions to Java streams.
-            self.write("IntStream.range(")
-            if len(gen.iter.args) == 1:
-                self.write("0, ")
-                self.visit(gen.iter.args[0])
-            elif len(gen.iter.args) == 2:
-                self.visit(gen.iter.args[0])
-                self.write(", ")
-                self.visit(gen.iter.args[1])
-            self.write(")")
+    def visit_Subscript(self, node):
+        self.visit(node.value)
+        if type(node.slice) == ast.Index:
+            # Syntax like a[2] or a["b"]
+            self.write(".bracket(")
+            self.visit(node.slice.value)
+        elif type(node.slice) == ast.Slice:
+            # Syntax like a[1:2] or a[:2]
+            self.write(".slice(")
+            lower = 0 if not node.slice.lower else node.slice.lower.n
+            upper = -1 if not node.slice.upper else node.slice.upper.n
+            self.write(str(lower))
+            self.write(", ")
+            self.write(str(upper))
         else:
-            # Somebody came up with a creative new use for
-            # comprehensions in the test suite...
-            raise Unhandled("ListComp hack couldn't handle: ", ast.dump(node))
-        self.write(".map(")
-        self.visit(gen.target)
-        self.write(" -> ")
-        self.visit(node.elt)
-        self.write(").collect(Collectors.toList())")
+            raise Unhandled("No translation for ExtSlice")
+        self.write(")")
 
-    def visit_UnaryOp(self, node):
-        if type(node.op) == ast.USub:
-            self.write("-")
-            self.visit(node.operand)
-        elif type(node.op) in (ast.Invert, ast.Not):
-            self.write("r.not(")
-            self.visit(node.operand)
-            self.write(")")
-        else:
-            raise Unhandled("Not sure what to do with:", ast.dump(node))
+    def visit_Attribute(self, node):
+        self.visit(node.value)
+        self.write(".")
+        initial = metajava.dromedary(node.attr)
+        self.write(initial)
+        if initial in metajava.java_term_info.JAVA_KEYWORDS or \
+           initial in metajava.java_term_info.OBJECT_METHODS:
+            self.write('_')
+
 
 if __name__ == '__main__':
     main()
