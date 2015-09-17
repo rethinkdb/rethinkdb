6a4d333c7f2055ff6dc868d65144b77afeecdab0
Michael Lucy <mlucy@rethinkdb.com>
2015-09-17 12:37:45 -0700

    Found a big bug.

--- test/rql_test/get_all.rb
+++ test/rql_test/get_all.rb
@@ -2,14 +2,19 @@ require 'pp'
 require 'eventmachine'
 # require_relative './importRethinkDB.rb'
 
-
 $port ||= (ARGV[0] || ENV['RDB_DRIVER_PORT'] || raise('driver port not supplied')).to_i
 ARGV.clear
 $c = r.connect(port: $port).repl
 
+def to_truncated(i)
+  'a'*300 + sprintf("%05d", i)
+end
+
 r.table_create('test').run rescue nil
-indexes = ['a', 'truncated', 'collision', 'collision_truncated']
-r.table('test').index_create(*indexes).run rescue nil
+indexes = ['a', 'truncated', 'collision', 'truncated_collision']
+indexes.each {|index|
+  r.table('test').index_create(index).run rescue nil
+}
 
 [{max_batch_rows: 10}, {}].each {|runopts|
   puts "----- Testing with runopts #{runopts} -----";
@@ -45,15 +50,16 @@ r.table('test').index_create(*indexes).run rescue nil
   raise "Descending sindex retrieve failed." if res.count != 300
 
   puts "Testing get_all..."
-  res = r.table('test').get_all(10, 20, -1, 3, 4).run(runopts)
-  raise "Get all failed." if res.to_a.count != 4
-  res = r.table('test').get_all(10, 20, -1, 3, 4, index: 'a').run(runopts)
-  raise "Get all failed." if res.to_a.count != 4
-  keys = (0...100).map{|i| (i - 10)*2}
+  # RSI: this is broken because of the way we're doing region calculations.
+  res = r.table('test').get_all(10, 20, -1, 3, 3, 4, 3).run(runopts)
+  raise "Short get_all failed." if res.to_a.count != 6
+  res = r.table('test').get_all(10, 20, -1, 3, 3, 4, 3, index: 'a').run(runopts)
+  raise "Short indexed get_all failed." if res.to_a.count != 6
+  keys = (0...100).map{|i| (i - 10)*2}*2
   res = r.table('test').get_all(*keys).run(runopts)
-  raise "Get all failed." if res.to_a.count != 90
+  raise "Long get_all failed." if res.to_a.count != 180
   res = r.table('test').get_all(*keys, index: 'a').run(runopts)
-  raise "Get all failed." if res.to_a.count != 90
+  raise "Long indexed get_all failed." if res.to_a.count != 180
 
   ####
 
@@ -62,10 +68,10 @@ r.table('test').index_create(*indexes).run rescue nil
   # RSI: multi
   input = (0...1000).map {|i|
     {
-      id: i,
-      truncated: 'a'*300 + sprintf("%05d", i),
+      id: -i,
+      truncated: to_truncated(i),
       collision: i % 10,
-      truncated_collision: 'a'*300 + sprintf("%05d", i % 10)
+      truncated_collision: to_truncated(i % 10)
     }
   }
   r.table('test').insert(input).run(runopts)
@@ -73,9 +79,40 @@ r.table('test').index_create(*indexes).run rescue nil
   puts "Testing ordering..."
   res = r.table('test').run(runopts).to_a
   raise "Retrieve failed." if res.count != 1000
-  res = r.table('test').order_by(index: 'id').run(runopts)
-  raise "Ordered retrieve failed." if res.count != 1000
-  res = r.table('test').order_by(index: r.desc('id')).run(runopts)
-  raise "Descending retrieve failed." if res.count != 1000
+  ['id', 'truncated', 'collision', 'truncated_collision'].each {|field|
+    puts "Testing #{field}..."
+    res = r.table('test').order_by(index: field).run(runopts)
+    raise "Ordered retrieve failed (#{field})." if res.count != 1000
+    res = r.table('test').order_by(index: r.desc(field)).run(runopts)
+    raise "Descending retrieve failed (#{field})." if res.count != 1000
+  }
+
+  puts "Testing between..."
+  puts "Testing truncated..."
+  res = r.table('test').order_by(index: 'truncated').
+          between(to_truncated(200), to_truncated(500)).run(runopts)
+  raise "Ordered retrieve failed (truncated)." if res.count != 300
+  res = r.table('test').order_by(index: r.desc('truncated')).
+          between(to_truncated(200), to_truncated(500)).run(runopts)
+  raise "Descending retrieve failed (truncated)." if res.count != 300
+  puts "Testing collision..."
+  res = r.table('test').order_by(index: 'collision').between(2, 5).run(runopts)
+  raise "Ordered retrieve failed (collision)." if res.count != 300
+  res = r.table('test').order_by(index: r.desc('collision')).between(2, 5).run(runopts)
+  raise "Descending retrieve failed (collision)." if res.count != 300
+  puts "Testing truncated_collision..."
+  res = r.table('test').order_by(index: 'truncated_collision').
+          between(to_truncated(2), to_truncated(5)).run(runopts)
+  raise "Ordered retrieve failed (truncated_collision)." if res.count != 300
+  res = r.table('test').order_by(index: r.desc('truncated_collision')).
+          between(to_truncated(2), to_truncated(5)).run(runopts)
+  raise "Descending retrieve failed (truncated_collision)." if res.count != 300
+
+  puts "Testing get_all..."
+  short_keys = [10, 20, -1, 3, 3, 4, 3]
+  res = r.table('test').get_all(short_keys.map{|x| to_truncated(x)},
+                                index: 'truncated').run(runopts)
+  raise "Get all failed." if res.to_a.count != 6
+
 }
 puts "Done!"
