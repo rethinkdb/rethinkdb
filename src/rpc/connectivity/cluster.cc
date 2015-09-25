df53c7ca99c5603fdc88514d61d209a9a70a8771
Michael Lucy <mlucy@rethinkdb.com>
2015-09-24 17:24:58 -0700

    Updated CLUSTER_VERSION_STRING.

--- src/rpc/connectivity/cluster.cc
+++ src/rpc/connectivity/cluster.cc
@@ -33,7 +33,7 @@
 static_assert(cluster_version_t::CLUSTER == cluster_version_t::v2_1_is_latest,
               "We need to update CLUSTER_VERSION_STRING when we add a new cluster "
               "version.");
-#define CLUSTER_VERSION_STRING "2.1.2"
+#define CLUSTER_VERSION_STRING "2.1.5"
 
 const std::string connectivity_cluster_t::cluster_proto_header("RethinkDB cluster\n");
 const std::string connectivity_cluster_t::cluster_version_string(CLUSTER_VERSION_STRING);
