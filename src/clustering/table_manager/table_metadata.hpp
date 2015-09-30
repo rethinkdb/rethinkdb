3d3690f1794d2f135de733f68ce04435028cb605
Etienne Laurin <atnnn@rethinkdb.com>
2015-09-30 13:59:23 -0700

    fix confusing overload resolution error

--- src/clustering/table_manager/table_metadata.hpp
+++ src/clustering/table_manager/table_metadata.hpp
@@ -50,7 +50,14 @@ public:
         microtime_t timestamp;
         uuid_u id;
 
-        RDB_DECLARE_ME_SERIALIZABLE(epoch_t);
+        // TODO ATN: msc things that epoch_t == int
+        // RDB_DECLARE_ME_SERIALIZABLE(epoch_t);
+
+        template <cluster_version_t W>
+        friend void serialize(write_message_t *, const class epoch_t &);
+
+        template <cluster_version_t W>
+        friend archive_result_t deserialize(read_stream_t *s, epoch_t *thing);
     };
 
     static multi_table_manager_timestamp_t min() {
