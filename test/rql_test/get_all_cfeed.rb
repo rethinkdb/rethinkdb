382cdd24c8131e711c91f6420e932f71b36fba42
Michael Lucy <mlucy@rethinkdb.com>
2015-09-30 17:22:30 -0700

    fix get_all_cfeed.rb

--- test/rql_test/get_all_cfeed.rb
+++ test/rql_test/get_all_cfeed.rb
@@ -1,6 +1,6 @@
 require 'pp'
 require 'eventmachine'
-# require_relative './importRethinkDB.rb'
+require_relative './importRethinkDB.rb'
 
 $port ||= (ARGV[0] || ENV['RDB_DRIVER_PORT'] || raise('driver port not supplied')).to_i
 ARGV.clear
