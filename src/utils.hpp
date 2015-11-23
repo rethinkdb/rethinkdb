dc3193d2a9bcc0b3d317ba4f89b08c9e23c73cbf
Etienne Laurin <atnnn@rethinkdb.com>
2015-11-23 08:23:28 -0800

    Compiler-specific attributes and macros (proper)

--- src/utils.hpp
+++ src/utils.hpp
@@ -28,9 +28,7 @@ struct const_charslice {
     const_charslice() : beg(NULL), end(NULL) { }
 };
 
-void *raw_malloc_aligned(size_t size, size_t alignment);
-void *raw_malloc_page_aligned(size_t size);
-void raw_free_aligned(void *ptr);
+void *malloc_aligned(size_t size, size_t alignment);
 
 /* Calls `malloc()` and checks its return value to crash if the allocation fails. */
 void *rmalloc(size_t size);
