2da69ec04e80a1da26a70f9e039203accbb3e3dd
Michael Lucy <mlucy@rethinkdb.com>
2015-12-15 03:27:49 -0800

    Made cursor.rb pass.

--- test/rql_test/connections/cursor.rb
+++ test/rql_test/connections/cursor.rb
@@ -105,7 +105,7 @@ class Cursor_Test < Test::Unit::TestCase
                          "end = new Date(new Date().getTime() + 2);" +
                          "while (new Date() < end) { }" +
                          "return row;" +
-                         "})")).run(@conn, :max_batch_rows => 2) }
+                         "})")).run(@conn, :max_batch_rows => 500) }
     cursor_counts = Array.new(num_cursors) { 0 }
     cursor_timeouts = Array.new(num_cursors) { 0 }
   
