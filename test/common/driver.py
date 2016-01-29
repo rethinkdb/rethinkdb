80fd319a6f656814984ff0e4f66edc4d84f4ed62
Karl Kuehn <karl_kuehn@apple.com>
2016-01-29 12:48:40 -0800

    fixing old whitespace merge errors

--- test/common/driver.py
+++ test/common/driver.py
@@ -800,13 +800,13 @@ class Process(object):
                 utils.kill_process_group(self, timeout=20)
                 self.returncode = self.process.poll()
                 if self.returncode is None:
-                        try:
-                            self.returncode = self.process.wait()
-                        except OSError:
-                            sys.stderr.write('The subprocess module lost the connection to the %s %s, assuming it closed cleanly (2)\n' % (self.server_type, self.name))
-                            sys.stderr.flush()
-                            self.returncode = 0
-                            assert self.returncode is not None, '%s %s failed to exit!' % (self.server_type.capitalize(), self.name)
+                    try:
+                        self.returncode = self.process.wait()
+                    except OSError:
+                        sys.stderr.write('The subprocess module lost the connection to the %s %s, assuming it closed cleanly (2)\n' % (self.server_type, self.name))
+                        sys.stderr.flush()
+                        self.returncode = 0
+                assert self.returncode is not None, '%s %s failed to exit!' % (self.server_type.capitalize(), self.name)
         if self.process:
             try:
                 self.returncode = self.process.wait()
