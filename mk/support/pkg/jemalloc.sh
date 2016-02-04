506663f433f2ecc007cea050516c7d0d72b58cae
mbains <manjinder.bains@gmail.com>
2016-02-04 15:25:03 -0800

    fix jemalloc for systems with /lib64 library paths

--- mk/support/pkg/jemalloc.sh
+++ mk/support/pkg/jemalloc.sh
@@ -4,8 +4,9 @@ version=3.6.0
 src_url=http://www.canonware.com/download/jemalloc/jemalloc-$version.tar.bz2
 
 pkg_install () {
+    configure_flags="--libdir=${install_dir}/lib"
     if [[ "$CROSS_COMPILING" = 1 ]]; then
-        configure_flags="--host=$($CXX -dumpmachine)"
+        configure_flags+="--host=$($CXX -dumpmachine)"
     fi
 
     pkg_copy_src_to_build
