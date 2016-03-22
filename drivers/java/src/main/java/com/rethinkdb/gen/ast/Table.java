049e52d224d08f98ea693771ba7e6c98a2d54922
Jeroen Habraken <jeroen@rethinkdb.com>
2016-03-21 20:05:56 -0700

    Fixes the Java driver

--- drivers/java/src/main/java/com/rethinkdb/gen/ast/Table.java
+++ drivers/java/src/main/java/com/rethinkdb/gen/ast/Table.java
@@ -69,6 +69,12 @@ public Table optArg(String optname, Object value) {
         Arguments arguments = new Arguments(this);
         return new Sync(arguments);
     }
+    public Grant grant(Object expr, Object exprA) {
+        Arguments arguments = new Arguments(this);
+        arguments.coerceAndAdd(expr);
+        arguments.coerceAndAdd(exprA);
+        return new Grant(arguments);
+    }
     public IndexCreate indexCreate(Object expr) {
         Arguments arguments = new Arguments(this);
         arguments.coerceAndAdd(expr);
