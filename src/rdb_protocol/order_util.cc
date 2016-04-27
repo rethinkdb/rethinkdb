1351b63d83aca2695fd9976331fef0f17691baf0
Nighelles <nighelles@rethinkdb.com>
2016-04-27 15:34:28 -0700

    Disallowing non-deterministic functions in orderBy for #5548
    OTS by @mlucy

--- src/rdb_protocol/order_util.cc
+++ src/rdb_protocol/order_util.cc
@@ -31,12 +31,16 @@ build_comparison(const term_t* target,
 
     check_r_args(target, item);
 
+    counted_t<const func_t> comparison_function = arg->as_func(GET_FIELD_SHORTCUT);
+    rcheck_target(target,
+                  comparison_function->is_deterministic() != deterministic_t::no,
+                  base_exc_t::LOGIC,
+                  "Sorting by a non-deterministic function is not supported.");
+
     if (item.type() == Term::DESC) {
-        return std::make_pair(DESC,
-                              arg->as_func(GET_FIELD_SHORTCUT));
+        return std::make_pair(DESC, comparison_function);
     } else {
-        return std::make_pair(ASC,
-                              arg->as_func(GET_FIELD_SHORTCUT));
+        return std::make_pair(ASC, comparison_function);
     }
 }
 
