dca800ca34db3c50b6221d057112b4347bae39d4
Raman Gupta <rocketraman@gmail.com>
2016-06-09 20:48:14 -0400

    Java Cursor implements Closeable (#5835)
    Fixes #5468

--- drivers/java/src/main/java/com/rethinkdb/net/Cursor.java
+++ drivers/java/src/main/java/com/rethinkdb/net/Cursor.java
@@ -5,6 +5,7 @@ import com.rethinkdb.gen.exc.ReqlDriverError;
 import com.rethinkdb.gen.exc.ReqlRuntimeError;
 import com.rethinkdb.gen.proto.ResponseType;
 
+import java.io.Closeable;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
@@ -17,7 +18,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 
-public abstract class Cursor<T> implements Iterator<T>, Iterable<T> {
+public abstract class Cursor<T> implements Iterator<T>, Iterable<T>, Closeable {
 
     // public immutable members
     public final long token;
