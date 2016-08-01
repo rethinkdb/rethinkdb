3d55990a227dea1f3f01744cf978e11297b05b07
mbains <manjinder.bains@gmail.com>
2016-08-01 15:55:51 -0700

    Build fixes for systems with /lib64 library paths (#5360)
    * fix jemalloc for systems with /lib64 library paths
    * fix protobuf lib64 issue

--- mk/support/pkg/jemalloc.sh
+++ mk/support/pkg/jemalloc.sh
@@ -5,8 +5,9 @@ src_url=http://www.canonware.com/download/jemalloc/jemalloc-$version.tar.bz2
 src_url_sha1=450a2f0331cd4544140bb3be5d6e11cc82383cdf
 
 pkg_install () {
+    configure_flags="--libdir=${install_dir}/lib"
     if [[ "$CROSS_COMPILING" = 1 ]]; then
-        configure_flags="--host=$($CXX -dumpmachine)"
+        configure_flags+="--host=$($CXX -dumpmachine)"
     fi
 
     pkg_copy_src_to_build
