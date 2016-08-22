d1322adc2a0e7384eeedbfcb801811d5352c03bc
Daniel Mewes <daniel@rethinkdb.com>
2016-08-22 11:30:12 -0700

    Implement conflict detection for resize operations.
    In the old code, resize operations would occasionally shrink a file while there were still outstanding reads for that section of the file.

--- src/arch/io/disk.hpp
+++ src/arch/io/disk.hpp
@@ -70,12 +70,6 @@ public:
     };
 
     int64_t get_file_size();
-    /* WARNING: resize operations do not wait for other operations to finish (with
-    the exception of other resize operations).
-    They do block other operations, but are not blocked themselves.
-    Only issue a `set_file_size()` with a size smaller than the current one if you can
-    make absolutely sure that no ongoing I/O operation wants to access the space
-    that gets truncated anymore. */
     void set_file_size(int64_t size);
     void set_file_size_at_least(int64_t size);
 
