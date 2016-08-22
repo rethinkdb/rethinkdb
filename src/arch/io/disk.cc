78c71d808dd952428696d1ebd636036ad800296d
Daniel Mewes <daniel@rethinkdb.com>
2016-08-22 11:30:12 -0700

    Implement conflict detection for resize operations.
    In the old code, resize operations would occasionally shrink a file while there were still outstanding reads for that section of the file.

--- src/arch/io/disk.cc
+++ src/arch/io/disk.cc
@@ -112,13 +112,13 @@ public:
                                a));
     }
 
-    void submit_resize(fd_t fd, int64_t new_size,
+    void submit_resize(fd_t fd, int64_t old_size, int64_t new_size,
                       void *account, linux_iocallback_t *cb,
                       bool wrap_in_datasyncs) {
         threadnum_t calling_thread = get_thread_id();
 
         action_t *a = new action_t(calling_thread, cb);
-        a->make_resize(fd, new_size, wrap_in_datasyncs);
+        a->make_resize(fd, old_size, new_size, wrap_in_datasyncs);
         a->account = static_cast<accounting_diskmgr_t::account_t *>(account);
 
         do_on_thread(home_thread(),
@@ -230,9 +230,7 @@ int64_t linux_file_t::get_file_size() {
     return file_size;
 }
 
-/* If you want to use this for downsizing a file, please check the WARNING about
-`set_file_size()` in disk.hpp. */
-void linux_file_t::set_file_size(int64_t size) {
+void linux_file_t::set_file_size(int64_t new_size) {
     assert_thread();
     rassert(diskmgr, "No diskmgr has been constructed (are we running without an event queue?)");
 
@@ -250,10 +248,11 @@ void linux_file_t::set_file_size(int64_t size) {
     };
     rs_callback_t *rs_callback = new rs_callback_t();
     rs_callback->lock = file_size_ops_drainer.lock();
-    diskmgr->submit_resize(fd.get(), size, default_account->get_account(),
+    diskmgr->submit_resize(fd.get(), file_size, new_size,
+                           default_account->get_account(),
                            rs_callback, true);
 
-    file_size = size;
+    file_size = new_size;
 }
 
 // For growing in large chunks at a time.
