a1a8885b632b84e48c3ed79e8df3af43359e95f9
Daniel Parnell <me@danielparnell.com>
2016-10-06 20:52:49 +1100

    Fixed gtest download link

--- mk/support/pkg/gtest.sh
+++ mk/support/pkg/gtest.sh
@@ -1,6 +1,6 @@
 version=1.7.0
-src_url=https://googletest.googlecode.com/files/gtest-$version.zip
-src_url_sha1=f85f6d2481e2c6c4a18539e391aa4ea8ab0394af
+src_url=https://github.com/google/googletest/archive/release-$version.zip
+src_url_sha1=f89bc9f55477df2fde082481e2d709bfafdb057b
 
 pkg_install () {
     pkg_copy_src_to_build
