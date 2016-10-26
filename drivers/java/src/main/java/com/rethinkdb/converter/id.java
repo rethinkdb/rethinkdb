44daba7fd06c0ceadefe742928519112e12b8f4b
Jp78 <jp78@protonmail.ch>
2016-10-26 10:28:41 -0700

    Tested and Fixed @id annotation
    Tested and then fixed the @id annotation

--- drivers/java/src/main/java/com/rethinkdb/converter/id.java
+++ drivers/java/src/main/java/com/rethinkdb/converter/id.java
@@ -1,5 +1,6 @@
 package com.rethinkdb.converter;
 
+import com.fasterxml.jackson.annotation.JacksonAnnotationsInside;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import java.lang.annotation.ElementType;
@@ -10,9 +11,10 @@ import java.lang.annotation.Target;
 /**
  * Created by thejp on 10/25/2016.
  */
-@JsonInclude(JsonInclude.Include.NON_NULL)
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
+@JacksonAnnotationsInside
+@JsonInclude(JsonInclude.Include.NON_NULL)
 
 /**
  * Use this with the primary key/id of your pojo
