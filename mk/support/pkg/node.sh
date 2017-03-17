16484d56771e6edb277991095abb2f0e10910b09
Etienne Laurin <etienne@atnnn.com>
2017-03-17 03:24:00 +0000

    static node version specification

--- mk/support/pkg/node.sh
+++ mk/support/pkg/node.sh
@@ -1,6 +1,5 @@
-major=6
-minor=10.0
-version=$major.$minor
+version=6.10.0
+major=${version%%.*}
 
 src_url=http://nodejs.org/dist/latest-v$major.x/node-v$version.tar.gz
 src_url_sha1=3bb2629ed623f38b8c3011cf422333862d3653a3
