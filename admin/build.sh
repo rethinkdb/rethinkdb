13e699bd2c3dab28212b59b92b0c5e2324b137d2
Etienne Laurin <etienne@atnnn.com>
2017-04-30 05:27:01 +0000

    fix webui build script

--- admin/build.sh
+++ admin/build.sh
@@ -7,24 +7,24 @@ build_all () {
     build_static
 }
 
-build_js () {
+build_js () (
     ${BROWSERIFY:-browserify} \
 	-t ${WEBUI_NODE_MODULES:-}coffeeify \
 	-t ${WEBUI_NODE_MODULES}browserify-handlebars \
 	${WEBUI_BUNDLE:+ -x rethinkdb} \
 	src/coffee/app.coffee \
 	-o ${WEBUI_DIST_DIR:-dist}/cluster.js
-}
+)
 
-build_css () {
+build_css () (
     cd src/less
     ${LESSC:-lessc} styles-new.less ${WEBUI_DIST_DIR:-../../dist}/cluster.css
-}
+)
 
-build_static () {
+build_static () (
     cd assets
     cp -a * ${WEBUI_DIST_DIR:-../dist}/
-}
+)
 
 build_${1:-all}
 
