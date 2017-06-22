b717efa63135a7781518ba5fcbbcba62bd5b4b53
Sam Hughes <sam@samuelhughes.com>
2017-06-22 14:02:35 -0700

    Make unittest code do_writes_raft use get_kiloticks

--- src/unittest/clustering_utils_raft.cc
+++ src/unittest/clustering_utils_raft.cc
@@ -409,7 +409,7 @@ void dummy_raft_traffic_generator_t::do_changes(auto_drainer_t::lock_t keepalive
 void do_writes_raft(dummy_raft_cluster_t *cluster, int expect, int ms) {
 #ifdef ENABLE_RAFT_DEBUG
     RAFT_DEBUG("begin do_writes(%d, %d)\n", expect, ms);
-    microtime_t start = current_microtime();
+    kiloticks_t start = get_kiloticks();
 #endif /* ENABLE_RAFT_DEBUG */
 
     std::set<uuid_u> committed_changes;
@@ -438,7 +438,7 @@ void do_writes_raft(dummy_raft_cluster_t *cluster, int expect, int ms) {
         ADD_FAILURE() << "completed only " << committed_changes.size() << "/" << expect
             << " changes in " << ms << "ms";
     }
-    RAFT_DEBUG("end do_writes() in %" PRIu64 "ms\n", (current_microtime() - start) / 1000);
+    RAFT_DEBUG("end do_writes() in %" PRIu64 "ms\n", (get_kiloticks().micros - start.micros) / 1000);
 }
 
 }   /* namespace unittest */
