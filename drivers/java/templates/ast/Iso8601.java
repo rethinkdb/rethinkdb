142e5c3427b501f37d330bf17d3c1ca2bdde8984
Gabor Boros <gabor.brs@gmail.com>
2020-05-17 07:43:34 +0200

    Remove extracted drivers from the repo

--- drivers/java/templates/ast/Iso8601.java
+++
@@ -1,7 +0,0 @@
-<%inherit file="../AstSubclass.java" />
-
-<%block name="special_methods">
-    public static ${classname} fromString(String iso) {
-        return new ${classname}(new Arguments(iso), null);
-    }
-</%block>
--- drivers/javascript/Makefile
+++
@@ -1,5 +0,0 @@
-OVERRIDE_GOALS := clean=js-clean publish=js-publish dist=js-dist default-goal=js-dist
-
-TOP := ../..
-include $(TOP)/Makefile
-
