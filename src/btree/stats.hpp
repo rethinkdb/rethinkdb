be03fb80a6df3a8481f460231a7525c2c54bc20b
Sam Hughes <sam@samuelhughes.com>
2022-02-20 14:03:30 -0800

    Make perfmon_rate_monitor_t not use MAX_THREADS

--- src/btree/stats.hpp
+++ src/btree/stats.hpp
@@ -9,8 +9,8 @@ public:
                            const std::string &identifier,
                            int n_threads)
         : btree_collection(),
-          pm_keys_read(secs_to_ticks(1)),
-          pm_keys_set(secs_to_ticks(1)),
+          pm_keys_read(secs_to_ticks(1), n_threads),
+          pm_keys_set(secs_to_ticks(1), n_threads),
           pm_total_keys_read(n_threads),
           pm_total_keys_set(n_threads),
           pm_keys_membership(&btree_collection,
