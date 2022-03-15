d5c78228e7641db5e734d9aea9b303110c5b84e6
Sam Hughes <sam@samuelhughes.com>
2022-03-15 15:35:18 -0700

    Point to QuickJS without function pointer warning

--- src/build.mk
+++ src/build.mk
@@ -425,7 +425,7 @@ build-clean:
 check-syntax:
 	$(RT_CXX) $(RT_CXXFLAGS) -c -o /dev/null $(patsubst %,$(CWD)/%,$(CHK_SOURCES))
 
-VENDORED_COMMIT := d5cb3f41338f683c1ef42ac3586b4eed9f72e753
+VENDORED_COMMIT := f74d69398fd8bc19eb83a29e624c66911a2d4be7
 VENDORED_REMOTE_REPO := https://github.com/rethinkdb/rethinkdb-vendored.git
 
 # Right now, rethinkdb-vendored's history is light, so we don't bother
