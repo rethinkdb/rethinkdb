2cbeef4faeee536360a09936592007fe724c85d8
unx <unxcepted9@gmail.com>
2022-06-01 16:15:16 +0200

    deps: bump gtest to v1.8.1
    Required for compilation on windows.
    This commit also removes old gtest source files from this repo, as no-fetch approach is no longer valid.

--- mk/support/pkg/gtest.sh
+++ mk/support/pkg/gtest.sh
@@ -1,26 +1,34 @@
-version=1.7.0
+version=1.8.1
 src_url=https://github.com/google/googletest/archive/release-$version.tar.gz
-src_url_sha1=d7aa4b8536f3a007b480cf016be8a4333dbf4768
+src_url_sha1=152b849610d91a9dfa1401293f43230c2e0c33f8
+
+pkg_install-include () {
+    test -e "$install_dir/include" && rm -rf "$install_dir/include"
+    if [[ -e "$src_dir/googletest/include" ]]; then
+        mkdir -p "$install_dir/include"
+        cp -vRL "$src_dir/googletest/include/." "$install_dir/include"
+    fi
+}
 
 pkg_install () {
     pkg_copy_src_to_build
-    make -C "$build_dir/make" gtest.a
+    make -C "$build_dir/googletest/make" gtest.a
     mkdir -p "$install_dir/lib"
-    cp "$build_dir/make/gtest.a" "$install_dir/lib/libgtest.a"
+    cp "$build_dir/googletest/make/gtest.a" "$install_dir/lib/libgtest.a"
 }
 
 pkg_install-windows () {
     pkg_copy_src_to_build
 
-    local generator
-    case "$PLATFORM" in
-        x64) generator="Visual Studio 14 Win64" ;;
-        Win32) generator="Visual Studio 14 2015" ;;
-    esac
+    local out
+    out=gtest.lib
+    if [[ "$DEBUG" = 1 ]]; then
+        out=gtestd.lib
+    fi
 
-    rm -rf "$build_dir"/{CMakeCache.txt,CMakeFiles}
-    in_dir "$build_dir" "$CMAKE" -G"$generator"
-    in_dir "$build_dir" "$MSBUILD" /nologo /maxcpucount /p:Configuration=$CONFIGURATION /p:Platform=$PLATFORM gtest.vcxproj
+    rm -rf "$build_dir/googletest"/{CMakeCache.txt,CMakeFiles}
+    in_dir "$build_dir/googletest" cmake -G "Visual Studio 17 2022" -T "v141" -A "$PLATFORM"
+    in_dir "$build_dir/googletest" cmake --build . --config "$CONFIGURATION"
 
-    cp "$build_dir/$CONFIGURATION/gtest.lib" "$windows_deps_libs/"
+    cp "$build_dir/googletest/$CONFIGURATION/$out" "$windows_deps_libs/gtest.lib"
 }
