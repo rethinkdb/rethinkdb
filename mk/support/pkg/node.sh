90b7b250a4ff125f125fc3a43cb7405cbaaee15b
Sam Hughes <sam@samuelhughes.com>
2022-10-28 13:38:03 -0700

    Make nodejs dependency use an https link
    I got a bad hash when downloading from a cafe.

--- mk/support/pkg/node.sh
+++ mk/support/pkg/node.sh
@@ -1,4 +1,4 @@
 version=6.17.1
 
-src_url=http://nodejs.org/dist/v$version/node-v$version.tar.gz
+src_url=https://nodejs.org/dist/v$version/node-v$version.tar.gz
 src_url_sha1=7497b72ad8e216b95e3dea864adeb6e5e4100509
